6bd353a616dbefad4f0ba3b78e9f5a313ddd3aed
Juhana Laurinharju <fingerzam@gmail.com>
2012-05-21 22:24:28 +0300

    Initial commit

---
+++ src/p_p_p_pokerface/core.clj
@@ -0,0 +1,34 @@
+(ns p-p-p-pokerface.core)
+
+(defn rank [card]
+  nil)
+
+(defn suit [card]
+  nil)
+
+(defn pair? [hand]
+  nil)
+
+(defn two-pairs? [hand]
+  nil)
+
+(defn three-of-a-kind? [hand]
+  nil)
+
+(defn four-of-a-kind? [hand]
+  nil)
+
+(defn straight? [hand]
+  nil)
+
+(defn flush? [hand]
+  nil)
+
+(defn full-house? [hand]
+  nil)
+
+(defn straight-flush? [hand]
+  nil)
+
+(defn value [hand]
+  nil)
