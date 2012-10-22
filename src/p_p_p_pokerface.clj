36ff417bde7d40748459c079a542829a1d36c017
jiri <jiri.hamberg@gmail.com>
2012-10-22 19:37:05 +0300

    Pokeritehtävät

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,57 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+  (let [[r] card values {\T 10, \J 11, \Q 12, \K 13, \A 14}  ]
+	(if (Character/isDigit r)
+      (Integer/valueOf (str r))
+      (get values r)
+     )))
 
 (defn suit [card]
-  nil)
+  (let [[_ s] card]
+	(str s)
+    ))
 
 (defn pair? [hand]
-  nil)
+  (>= (apply max (vals (frequencies (map rank hand)))) 2))
 
 (defn two-pairs? [hand]
-  nil)
+  (or
+   (= 2 (get (frequencies (vals (frequencies (map rank hand)))) 2))
+   (= 1 (get (frequencies (vals (frequencies (map rank hand)))) 4))
+   ))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (>= (apply max (vals (frequencies (map rank hand)))) 3))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (>= (apply max (vals (frequencies (map rank hand)))) 4))
 
 (defn straight? [hand]
-  nil)
+  (let [sorted (sort (map rank hand)) smallest (first sorted)
+        alt (sort (replace {14 1} sorted)) alt-smallest (first alt)]
+    (or
+     (= sorted (range smallest (+ smallest 5)))
+     (= alt (range alt-smallest (+ alt-smallest 5)))
+     )))
 
 (defn flush? [hand]
-  nil)
+  (= (apply max (vals (frequencies (map suit hand)))) 5))
 
 (defn full-house? [hand]
-  nil)
+  (let [freqs (set (vals (frequencies (map rank hand))))]
+	(and (contains? freqs 3) (contains? freqs 2))))
 
 (defn straight-flush? [hand]
-  nil)
+  (and
+   (straight? hand)
+   (flush? hand)
+   ))
 
 (defn value [hand]
-  nil)
+  (let [high-card? (fn [hand] true)
+        checkers [high-card? pair? two-pairs? three-of-a-kind? straight?
+                flush? full-house? four-of-a-kind? straight-flush?]]
+    (apply max (filter (fn [i] ((get checkers i) hand)) (range 0 9)))
+
+  ))
