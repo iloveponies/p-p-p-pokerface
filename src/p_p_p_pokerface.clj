82a46b73a56c2523f3dbbde93428df234876142d
Daria Antonova <daria.antonova@helsinki.fi>
2012-10-23 18:46:49 +0300

    poker dne

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,49 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+  (let[rank-map {\T 10, \J 11, \Q 12, \K 13, \A 14}
+       [card-rank _] card]
+    (if(Character/isDigit card-rank) (Integer/valueOf (str card-rank)) (get rank-map card-rank))))
 
 (defn suit [card]
-  nil)
+  (let [[_ snd] card] (str snd)))
 
 (defn pair? [hand]
-  nil)
+   (contains? (set (vals (frequencies (map rank hand)))) 2))
 
 (defn two-pairs? [hand]
-  nil)
+  (let [struc (sort (vals (frequencies (map rank hand))))]
+    (or (= struc [1 2 2]) (= struc [1 4]))))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (contains? (set (vals (frequencies (map rank hand)))) 3))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (contains? (set (vals (frequencies (map rank hand)))) 4))
 
 (defn straight? [hand]
-  nil)
+ (let [ranks (sort (map rank hand))
+       min-rank (first ranks)
+       straight (range min-rank (+ min-rank 5))
+       replace-ace (sort (replace {14 1} ranks))
+       min-rank2 (first replace-ace)
+       straight2 (range min-rank2 (+ min-rank2 5))]
+ (or (= ranks straight)
+     (= replace-ace straight2))))
 
 (defn flush? [hand]
-  nil)
+  (apply = (map suit hand)))
 
 (defn full-house? [hand]
-  nil)
+  (= (sort (vals (frequencies (map rank hand)))) [2 3]))
 
 (defn straight-flush? [hand]
-  nil)
+  (and (straight? hand) (flush? hand)))
 
 (defn value [hand]
-  nil)
+  (let [high-card? (fn [hand] true)
+        checkers [high-card? pair? two-pairs? three-of-a-kind? straight?
+                flush? full-house? four-of-a-kind? straight-flush?]
+        has-value? (fn [hand val] ((get checkers val) hand))
+        try-value (fn [x] (has-value? hand x))]
+     (apply max (filter try-value (range 0 9)))))
