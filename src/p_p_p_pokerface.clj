cffd4cf989e8363e91b61b6c9db19232d559a477
Juho Gävert <jgavert@cs.helsinki.fi>
2012-10-24 02:41:15 +0300

    done

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,64 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+  (let [[rank _] card
+        mapah {\T 10, \J 11, \Q 12, \K 13, \A 14}
+        toInt (fn [x] (Integer/valueOf (str x)))]
+    (if (not(Character/isDigit rank)) (mapah rank) (toInt rank))))
 
 (defn suit [card]
-  nil)
+  (let [[_ suit] card]
+    (str suit)))
+
+(defn- handRankFreq [hand]
+  (frequencies (map rank hand)))
+
+(defn- handSuitFreq [hand]
+  (frequencies (map suit hand)))
+
+(defn- maxSameValue [value hand]
+  (let [func (fn [x] (== x value))]
+  	(== 1 (count (filter func (vals (handRankFreq hand)))))))
 
 (defn pair? [hand]
-  nil)
+  (maxSameValue 2 hand))
+
+(defn- real-two-pairs? [hand]
+  (let [func (fn [x] (== 2 x))]
+    (== 2 (count (filter func (vals (handRankFreq hand)))))))
 
 (defn two-pairs? [hand]
-  nil)
+  (or (real-two-pairs? hand) (maxSameValue 4 hand)))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (maxSameValue 3 hand))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (maxSameValue 4 hand))
 
 (defn straight? [hand]
-  nil)
+  (let [shvalues (sort (map rank hand))]
+    (let [smvalues (sort (replace {14 1} shvalues))]
+      (let [[minv _ _ _ highv] shvalues]
+        (let [minVals (range (- minv 1) (+ minv 4))
+              maxVals (range (- highv 4) (+ highv 1))]
+		  (or (= maxVals shvalues) (= minVals smvalues)))))))
 
 (defn flush? [hand]
-  nil)
+  (== 1 (count (handSuitFreq hand))))
 
 (defn full-house? [hand]
-  nil)
+  (and (pair? hand) (three-of-a-kind? hand)))
 
 (defn straight-flush? [hand]
-  nil)
+  (and (straight? hand) (flush? hand)))
+
+(defn hand-has-value? [hand value]
+  (let [checkers [(fn [_] true) pair? two-pairs? three-of-a-kind? straight?
+                flush? full-house? four-of-a-kind? straight-flush?]]
+    ((checkers value) hand)))
 
 (defn value [hand]
-  nil)
+  (let [values [0 1 2 3 4 5 6 7 8]
+        func (fn [x] (if (hand-has-value? hand x) x -1))]
+    (apply max (map func values))))
