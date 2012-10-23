0df64f0e45c21db18a328156812936c83f7a9146
kuxtprkfexn <M8R-dja7v4@sogetthis.com>
2012-10-23 22:41:14 +0300

    All claimed facts (43) have been confirmed.

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,72 @@
 (ns p-p-p-pokerface)
 
+(defn in? [lst elem]
+  (not (empty? (filter (fn [x] (= x elem)) lst))))
+
 (defn rank [card]
-  nil)
+  (let [replacements {\T 10 \J 11 \Q 12 \K 13 \A 14}
+       [rank _] card]
+    (if (Character/isDigit rank)
+      (Integer/valueOf (str rank))
+      (get replacements rank))))
 
 (defn suit [card]
-  nil)
+  (let [[_ suit] card]
+    (str suit)))
+
+(defn ranks [hand]
+  (map rank hand))
+
+(defn suits [hand]
+  (map suit hand))
+
+(defn freqs [hand]
+  (vals (frequencies (ranks hand))))
 
 (defn pair? [hand]
-  nil)
-
-(defn two-pairs? [hand]
-  nil)
+  (in? (freqs hand) 2))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (in? (freqs hand) 3))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (in? (freqs hand) 4))
+
+(defn two-pairs? [hand]
+    (or
+        (= (get (frequencies (freqs hand)) 2) 2)
+        (four-of-a-kind? hand)))
 
 (defn straight? [hand]
-  nil)
+  (let [check (fn [rnks]
+                (and
+                     (== (count (frequencies rnks)) 5)
+                     (== (- (apply max rnks) (apply min rnks)) 4)))
+        rnks (ranks hand)]
+    (or (check rnks) (check (replace {14 1} rnks)))))
 
 (defn flush? [hand]
-  nil)
+  (let [sts (suits hand)]
+    (every? (fn [s] (= s (first sts))) (rest sts))))
 
 (defn full-house? [hand]
-  nil)
+  (and
+       (in? (freqs hand) 2)
+       (in? (freqs hand) 3)))
 
 (defn straight-flush? [hand]
-  nil)
+  (and
+       (straight? hand)
+       (flush? hand)))
 
 (defn value [hand]
-  nil)
+  (cond
+    (straight-flush?  hand) 8
+    (four-of-a-kind?  hand) 7
+    (full-house?      hand) 6
+    (flush?           hand) 5
+    (straight?        hand) 4
+    (three-of-a-kind? hand) 3
+    (two-pairs?       hand) 2
+    (pair?            hand) 1
+    :else 0))
