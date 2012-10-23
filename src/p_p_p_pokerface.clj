a238ddc05b1e7c6e8f33772f2448ec39e0aeeb38
Kalle Lammenoja <kalle.lammenoja@gmail.com>
2012-10-23 23:07:38 +0300

    helvetti sentään

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -11,30 +11,38 @@
       (Integer/valueOf (str s)))))
 
 (defn pair? [hand]
-    (if( < 1 (apply max (vals(frequencies(map rank hand)))))
-      true
-      false))
+  (< 1 (apply max (vals(frequencies(map rank hand))))))
 
 (defn two-pairs? [hand]
-  nil)
+  (let [[x y] (reverse(sort(vals(frequencies(map rank hand)))))]
+    (or (and (<= 2 x) (<= 2 y)) (<= 4 x))))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (< 2 (apply max (vals(frequencies(map rank hand))))))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (< 3 (apply max (vals(frequencies(map rank hand))))))
 
 (defn straight? [hand]
-  nil)
+  (let [x (sort(map rank hand))
+        y (range (first x)(+ 1 (first(reverse x))))]
+	(reverse x)
+    (if(== 0 (compare (apply str x) (apply str y)))
+      true
+      (if(contains? x "14")
+        ((sort(replace {"14" "1"} x))
+         (== 0 (compare (sort x) (range (first x)(+ 1 (first(reverse x)))))))
+        false))))
 
 (defn flush? [hand]
-  nil)
+  (= 5 (apply max(vals(frequencies(map suit hand))))))
 
 (defn full-house? [hand]
-  nil)
+  (let [[x y] (sort(vals(frequencies(map rank hand))))]
+    (and (== 2 x) (== 3 y ))))
 
 (defn straight-flush? [hand]
-  nil)
+  (and (flush? hand) (straight? hand)))
 
 (defn value [hand]
   nil)
