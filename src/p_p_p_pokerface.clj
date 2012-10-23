0b7d3274dc0d79990543f812f59b441b1f6584c4
Mikko Nikkilä <mikknikk@wow-24.cs.helsinki.fi>
2012-10-23 14:41:10 +0300

    solutions

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,59 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+  (let [[rank _] card]
+    (def replacements {\T 10, \J 11, \Q 12, \K 13, \A 14})
+    (if (Character/isDigit rank)
+      (Integer/valueOf (str rank))
+      (replacements rank))))
 
 (defn suit [card]
-  nil)
+  (let [[_ suit] card]
+    (str suit)))
 
 (defn pair? [hand]
-  nil)
+  (let [ranks (mapv (fn [card] (rank card)) hand)]
+    (>= (apply max (vals (frequencies ranks))) 2)))
 
 (defn two-pairs? [hand]
-  nil)
+  (let [ranks (mapv (fn [card] (rank card)) hand)]
+    (or (= (sort (vals (frequencies ranks))) (seq [1 2 2]))
+        (= (sort (vals (frequencies ranks))) (seq [2 3]))
+        (= (sort (vals (frequencies ranks))) (seq [1 4])))))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (let [ranks (mapv (fn [card] (rank card)) hand)]
+    (>= (apply max (vals (frequencies ranks))) 3)))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (let [ranks (mapv (fn [card] (rank card)) hand)]
+    (>= (apply max (vals (frequencies ranks))) 4)))
 
 (defn straight? [hand]
-  nil)
+  (let [ranks (sort (mapv (fn [card] (rank card)) hand))]
+    (or (and (== (- (apply max ranks) (apply min ranks)) 4)
+             (not (pair? hand)))
+        (= ranks (seq [2 3 4 5 14])))))
 
 (defn flush? [hand]
-  nil)
+  (let [suits (mapv (fn [card] (suit card)) hand)]
+    (== (apply max (vals (frequencies suits))) 5)))
 
 (defn full-house? [hand]
-  nil)
+  (let [ranks (mapv (fn [card] (rank card)) hand)]
+    (= (sort (vals (frequencies ranks))) (seq [2 3]))))
 
 (defn straight-flush? [hand]
-  nil)
+  (and (straight? hand)
+       (flush? hand)))
 
 (defn value [hand]
-  nil)
+  (cond (straight-flush? hand)  8
+        (four-of-a-kind? hand)  7
+        (full-house? hand)      6
+        (flush? hand)           5
+        (straight? hand)        4
+        (three-of-a-kind? hand) 3
+        (two-pairs? hand)       2
+        (pair? hand)            1
+        :else                   0))
