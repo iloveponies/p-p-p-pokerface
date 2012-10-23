4baa79ddb6dea2ab4d6c9e0afe48aaba67c18cb9
Antti O P Karjalainen <aopkarja@dhp-20.cs.helsinki.fi>
2012-10-23 18:26:12 +0300

    It's rudimetary ~work~ dear Watson

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,30 +1,51 @@
 (ns p-p-p-pokerface)
 
-(defn rank [card]
-  nil)
+(defn card-value [rank]
+  (let [final (get {\T 10 \J 11 \Q 12 \K 13 \A 14} rank)]
+    (if (= final nil) rank final)))
 
-(defn suit [card]
-  nil)
+(defn rank [swell]
+  (let [[wanna-be --] swell]
+    (Integer/parseInt(str (card-value wanna-be)))))
+
+(defn suit [swell]
+  (let [[-- wanna-be] swell]
+  (str wanna-be)))
+
+(defn amount-of-certain-amount-of-same-ranks [hand amount]
+  (count
+   (filter (fn [asdf] (= asdf amount))
+           (vals (frequencies (map rank hand))))))
 
 (defn pair? [hand]
-  nil)
-
-(defn two-pairs? [hand]
-  nil)
-
-(defn three-of-a-kind? [hand]
-  nil)
+  (>= (amount-of-certain-amount-of-same-ranks hand 2) 1))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (= (amount-of-certain-amount-of-same-ranks hand 4) 1))
+
+(defn two-pairs? [hand]
+  (or (= (amount-of-certain-amount-of-same-ranks hand 2) 2)
+      (four-of-a-kind? hand)))
+
+(defn three-of-a-kind? [hand]
+  (= (amount-of-certain-amount-of-same-ranks hand 3) 1))
 
 (defn straight? [hand]
-  nil)
+  ;TODO
+  )
+
+(defn straight? [hand]
+  )
 
 (defn flush? [hand]
-  nil)
+  (contains? (set (vals (frequencies (map suit hand)))) 5))
 
 (defn full-house? [hand]
+  (and (= (amount-of-certain-amount-of-same-ranks hand 2) 1)
+           (three-of-a-kind? hand)
+           (= (amount-of-certain-amount-of-same-ranks hand 1) 0)))
+
+(defn straight-flush? [hand]
   nil)
 
 (defn straight-flush? [hand]
