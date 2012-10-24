a6480ccde6fc16c04657afd0a1b63ebd6dcd1581
otto <otto@comp.(none)>
2012-10-24 19:22:19 +0300

    from pair? to two-pairs?

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,29 +1,45 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+  (let [replacements {\T 10, \J 11, \Q 12, \K 13, \A 14}
+        ch (first card)]
+    (if (Character/isDigit ch)
+      (Integer/valueOf (str ch))
+      (replacements ch))))
 
 (defn suit [card]
-  nil)
+  (str (second card)))
+
+(defn n-of-a-kind? [n hand]
+  (let [ranks (map rank hand)]
+  (>= (apply max (vals (frequencies ranks))) n)))
 
 (defn pair? [hand]
-  nil)
+  (n-of-a-kind? 2 hand))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (n-of-a-kind? 3 hand))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (n-of-a-kind? 4 hand))
 
 (defn flush? [hand]
-  nil)
+  (let [suits (map suit hand)]
+    (== 1 (count (set suits)))))
 
 (defn full-house? [hand]
-  nil)
+  (let [ranks (map rank hand)]
+    (and (== 2 (apply min (vals (frequencies ranks))))
+         (== 3 (apply max (vals (frequencies ranks)))))))
 
 (defn two-pairs? [hand]
-  nil)
-
+  (let [ranks (map rank hand)]
+    (cond
+     (four-of-a-kind? hand) true
+     (full-house? hand) true
+     (and (not (three-of-a-kind? hand)) (== 3 (count (frequencies ranks)))) true
+     :else false)))
+ 
 (defn straight? [hand]
   nil)
 
