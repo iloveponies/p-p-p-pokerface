f380bc4003af7a9b8333de6ec9548cf45c083002
Laura Pesola <laura.pesola@helsinki.fi>
2012-10-24 15:43:12 +0300

    ex 1 - 8

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,28 +1,49 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+  (let [[r _] card]
+  (if (Character/isDigit r)
+  (Integer/valueOf (str r)) 
+  (cond
+    (= r \T) 10
+    (= r \J) 11
+    (= r \Q) 12
+    (= r \K) 13
+    (= r \A) 14
+  ))))
 
 (defn suit [card]
-  nil)
+  (let [[_ s] card]
+  (str s)  
+ ))
 
 (defn pair? [hand]
-  nil)
+  (let [freq (frequencies (map rank hand))]
+  (< 1 (first (vals freq)) 3 )))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (let [freq (frequencies (map rank hand))]
+  (< 2 (first (vals freq)) 4)))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (let [freq (frequencies (map rank hand))]
+  (< 3 (first (vals freq)) 5 )))
 
 (defn flush? [hand]
-  nil)
+  (let [freq (frequencies (map suit hand))]
+  (== 5 (first (vals freq)))
+))
 
 (defn full-house? [hand]
-  nil)
+  (let [freq (sort (vals (frequencies (map rank hand))))]
+  (= freq [2 3])
+  ))
 
 (defn two-pairs? [hand]
-  nil)
+  (let [freq (sort (vals (frequencies (map rank hand))))]
+  (or (four-of-a-kind? hand) 
+      (= freq [2 2])))  
+)
 
 (defn straight? [hand]
   nil)
