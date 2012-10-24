0f74fbc434aa41dec54efa73c415b33e3adf6ae0
Jian Liu <jqliu@dhp-7.cs.helsinki.fi>
2012-10-24 11:36:42 +0300

    p-p-p-pokerface by Jian Liu

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,66 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+   (let [[fst snd] card]
+     (if (Character/isDigit fst)
+        (Integer/valueOf (str fst))
+        (get {\T 10, \J 11, \Q 12, \K 13, \A 14} fst)
+     )
+   )
+)
 
 (defn suit [card]
-  nil)
+   (let [[fst snd] card]
+     (str snd)
+   )
+)
 
 (defn pair? [hand]
-  nil)
+   (contains? (set (vals (frequencies (map rank hand)))) 2)  
+)
 
 (defn two-pairs? [hand]
-  nil)
+  (or (= (sort (vals (frequencies (map rank hand)))) [1 2 2])
+      (= (sort (vals (frequencies (map rank hand)))) [1 4]))
+)
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (= (apply max (vals (frequencies (map rank hand)))) 3)
+ )
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (= (apply max (vals (frequencies (map rank hand)))) 4)
+ )
 
 (defn straight? [hand]
-  nil)
+  (let [handrank (sort (map rank hand))]
+  (or (= handrank (range (apply min handrank) (+ (apply max handrank) 1)))
+      (= handrank [2 3 4 5 14]))
+   )
+)
 
 (defn flush? [hand]
-  nil)
+  (= (apply max (vals (frequencies (map suit hand)))) 5)
+)
 
 (defn full-house? [hand]
-  nil)
+  (= (sort (vals (frequencies (map rank hand)))) (range 2 4))
+)
 
 (defn straight-flush? [hand]
-  nil)
+   (and (straight? hand) (flush? hand))
+)
 
 (defn value [hand]
-  nil)
+   (cond
+     (straight-flush? hand) 8
+     (four-of-a-kind? hand) 7
+     (full-house? hand) 6
+     (flush? hand) 5
+     (straight? hand) 4
+     (three-of-a-kind? hand) 3
+     (two-pairs? hand) 2
+     (pair? hand) 1
+     :else 0
+   )
+)
