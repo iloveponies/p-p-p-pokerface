5c0fbb39a21f474d037cfc3c6dbb2a73a7918aea
Junlong Xiang <jxiang@dhp-6.cs.helsinki.fi>
2012-10-24 13:33:25 +0300

    message here

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,75 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+   (let [[rank _] card]
+    (if(Character/isDigit rank)
+     (Integer/valueOf (str rank))
+       (get {\T 10, \J 11, \Q 12, \K 13, \A 14} rank))))
 
 (defn suit [card]
-  nil)
+  (let [[_ suit] card]
+    (str suit)))
 
 (defn pair? [hand]
-  nil)
+  (if ( == (apply max(vals(frequencies(map rank hand)))) 2 )
+    true
+    false))
 
 (defn three-of-a-kind? [hand]
-  nil)
+   (if ( == (apply max(vals(frequencies(map rank hand)))) 3 )
+    true
+    false))
 
 (defn four-of-a-kind? [hand]
-  nil)
+    (if ( == (apply max(vals(frequencies(map rank hand)))) 4 )
+    true
+    false))
 
 (defn flush? [hand]
-  nil)
+    (if (apply = (map suit hand))
+    true
+    false))
 
 (defn full-house? [hand]
-  nil)
+  (let [v (vals (frequencies(map rank hand)))]
+    (if (= (sort v) (seq [2 3]))
+      true
+      false
+      )))
 
 (defn two-pairs? [hand]
-  nil)
+   (let [v (vals (frequencies(map rank hand)))]
+    (if (or (= (sort v) (seq [1 4])) (= (sort v) (seq [1 2 2])))
+      true
+      false
+      )))
 
 (defn straight? [hand]
-  nil)
+  (let [s1 (sort (map rank hand))
+        sm1 (first s1)
+        s2 (sort (replace {14 1} s1))
+        sm2 (first s2)
+        ]
+    (if (or (= s1 (range sm1 (+ sm1 5))) (= s2 (range sm2 (+ sm2 5))))
+      true
+      false
+      )))
 
 (defn straight-flush? [hand]
-  nil)
+  (if (and (flush? hand) (straight? hand))
+    true
+    false
+    ))
 
 (defn value [hand]
-  nil)
+  (cond
+   (straight-flush? hand) 8
+   (four-of-a-kind? hand) 7
+   (full-house? hand) 6
+   (flush? hand) 5
+   (straight? hand) 4
+   (three-of-a-kind? hand) 3
+   (two-pairs? hand) 2
+   (pair? hand) 1
+   :else 0
+   ))
