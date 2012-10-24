e78ec19b247d74d36d4b2f4f94de60dc1d4f3cf6
Esko Niiranen <eniirane@kaihoapina.local>
2012-10-24 16:46:11 +0300

    poker exercises, ugly as ever.

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,89 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+  (let [[fst _ ] card]
+    (cond
+      (= fst \T) 10
+      (= fst \J) 11
+      (= fst \Q) 12
+      (= fst \K) 13
+      (= fst \A) 14
+      :else (Integer/valueOf (str fst)))))
 
 (defn suit [card]
-  nil)
+  (let [[_ snd ] card]
+    (str snd)))
 
 (defn pair? [hand]
-  nil)
+  (cond 
+    (< 1 (apply max (vals (frequencies (map rank hand))))) true
+    :else false))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (cond 
+    (< 2 (apply max (vals (frequencies (map rank hand))))) true
+    :else false))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (cond 
+    (< 3 (apply max (vals (frequencies (map rank hand))))) true
+    :else false))
 
 (defn flush? [hand]
-  nil)
+  (cond 
+    (< 4 (apply max (vals (frequencies (map suit hand))))) true
+    :else false))
 
 (defn full-house? [hand]
-  nil)
+  (let [handfreq (vals (frequencies (map rank hand)))]
+    (cond
+      (and (= 3 (first handfreq))
+           (= 2 (count handfreq))) 
+        true
+      :else false)))
 
 (defn two-pairs? [hand]
-  nil)
+  (let [handfreq (vals (frequencies (map rank hand)))]
+    (cond
+      (= (first handfreq) 
+         (second handfreq)
+         2) 
+        true 
+      (four-of-a-kind? hand) true
+      :else false)))
 
 (defn straight? [hand]
-  nil)
+  (let [ranklist (map 
+                   (fn [x] (Integer/valueOf (str x))) 
+                   (sort (map rank hand)))
+        ; case of ace?  i considered this to be the most readable solution.
+        rankaces (sort (replace {14 1} ranklist))
+        rankfreq (vals (frequencies ranklist))]
+    (cond
+      (and
+        (= (count rankfreq) 5)
+        (or
+          (= 4 (- (last ranklist) (first ranklist)))
+          (= 4 (- (last rankaces) (first rankaces))))) true
+      :else false)))
 
 (defn straight-flush? [hand]
-  nil)
+  (cond
+    (and (flush? hand)
+         (straight? hand)) true
+    :else false))
 
 (defn value [hand]
-  nil)
+  ; why not like this?  boring and static?
+  (cond
+    (straight-flush? hand) 8
+    (four-of-a-kind? hand) 7
+    (full-house? hand) 6
+    (flush? hand) 5
+    (straight? hand) 4
+    (three-of-a-kind? hand) 3
+    (two-pairs? hand) 2
+    (pair? hand) 1
+    :else 0))
+
+
