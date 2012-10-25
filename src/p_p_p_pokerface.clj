4edc07b3360e7f364ff92dc07c9d54ea1e9ea7b0
Tero Nurmiluoto <tero.nurmiluoto@helsinki.fi>
2012-10-25 21:01:17 +0300

    oh, how we laughed and laughed

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,70 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+  (let [v (first card)]
+    (if (Character/isDigit v)
+      (Integer/valueOf (str v))
+      (cond (= \T v) 10
+            (= \J v) 11
+            (= \Q v) 12
+            (= \K v) 13
+            (= \A v) 14
+            true (throw (Exception. (str "the goat enters")))))))
+                                                          ; ^^^^ f-yeah.
 
 (defn suit [card]
-  nil)
+  (str (last card)))
 
 (defn pair? [hand]
-  nil)
+  (not (= nil (some (fn [x] (= x 2)) (vals (frequencies (map rank hand)))))))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (not (= nil (some (fn [x] (= x 3)) (vals (frequencies (map rank hand)))))))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (not (= nil (some (fn [x] (= x 4)) (vals (frequencies (map rank hand)))))))
 
 (defn flush? [hand]
-  nil)
+  (= 1 (count (set (map suit hand)))))
 
 (defn full-house? [hand]
-  nil)
+  (and (three-of-a-kind? hand) (pair? hand)))
 
 (defn two-pairs? [hand]
-  nil)
+  ; lulz
+  (or (= 2 (get (frequencies (vals (frequencies (map rank hand)))) 2))
+      (four-of-a-kind? hand)))
 
 (defn straight? [hand]
-  nil)
+  (let [testfn (fn [foot] 
+                (let [fst (first foot)
+                      lst (last foot)]
+                  (= (range fst (+ 1 lst)) foot)))
+        sortedHand (sort (map rank hand))]
+    (if (testfn sortedHand)
+      true
+      (if (= 14 (last sortedHand))
+        (testfn (sort (assoc (vec sortedHand) 4 1)))
+        false))))
+
 
 (defn straight-flush? [hand]
-  nil)
+  (and (straight? hand) (flush? hand)))
+
+(defn high-card? [hand]
+  true)
 
 (defn value [hand]
-  nil)
+  (let [checkers [high-card? pair? two-pairs? three-of-a-kind? straight?
+                  flush? full-house? four-of-a-kind? straight-flush?]
+        sincere (map #(%1 hand) checkers)
+        haha (reduce (fn [res item] (let [index (first res)
+                                          resv (second res)]
+                                      (if (= true item)
+                                        (list (+ 1 index) index)
+                                        (list (+ 1 index) resv))))
+                     (list 0 0) sincere)]
+    ; because we can
+    (second haha)))
+
+;    (map #(%1 hand) checkers)))
