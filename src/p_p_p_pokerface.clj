ae7d57bd65db9580708e096233542e88491be087
Juuso Lehtinen <jupelehtinen@gmail.com>
2012-10-26 01:30:39 +0300

    done

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,64 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+  (let [[fst _] card
+        replacements {\A 14 \K 13 \Q 12 \J 11 \T 10}]
+    (if (Character/isDigit fst) 
+      (Integer/valueOf (str fst)) 
+      (replacements fst))))
 
 (defn suit [card]
-  nil)
+  (let [[_ snd] card]
+    (str snd)))
 
 (defn pair? [hand]
-  nil)
+  (= 2 (apply max (vals (frequencies (map rank hand))))))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (= 3 (apply max (vals (frequencies (map rank hand))))))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (= 4 (apply max (vals (frequencies (map rank hand))))))
 
 (defn flush? [hand]
-  nil)
+  (= 5 (apply max (vals (frequencies (map suit hand))))))
 
 (defn full-house? [hand]
-  nil)
+  (and (= [3 2] (seq (vals (frequencies (map rank hand)))))))
 
 (defn two-pairs? [hand]
-  nil)
+  (let [values (vals (frequencies (map rank hand)))
+        freq (vals (frequencies (seq values)))]
+    (or (= [2 1] (seq freq)) (= [1 1] (seq freq)))))
 
 (defn straight? [hand]
-  nil)
+  (let [hand-values (sort (map rank hand))
+        test-straight (range (
+                              first hand-values) 
+                             (+ 1 (last hand-values)))
+        hand-values2 (sort (replace {14 1} hand-values))
+        test-straight2 (range (
+                              first hand-values2) 
+                             (+ 1 (last hand-values2)))]
+    (or 
+     (= (seq hand-values) (seq test-straight))
+     (= (seq test-straight2)
+        (seq hand-values2)))))
 
 (defn straight-flush? [hand]
-  nil)
+  (and (straight? hand) (flush? hand)))
 
 (defn value [hand]
-  nil)
+  (cond 
+   (straight-flush? hand) 8
+   (four-of-a-kind? hand) 7
+   (full-house? hand) 6
+   (flush? hand) 5
+   (straight? hand) 4
+   (three-of-a-kind? hand) 3
+   (two-pairs? hand) 2
+   (pair? hand) 1
+   :else 0))
+
+(defn high-card? [hand]
+  (last (sort(map rank hand))))
