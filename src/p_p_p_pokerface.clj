82dbaab25c3ab2b0851a982ffb77a6bec8dcc73f
tta <fc6fa0e9@nomail.com>
2012-10-26 04:55:38 +0300

    Harjoitukset tehty.

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,48 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+  (let [[rankchr _] card]
+    (if (Character/isDigit rankchr)
+      (Integer/valueOf (str rankchr))
+      (get {\T 10 \J 11 \Q 12 \K 13 \A 14} rankchr))))
 
 (defn suit [card]
-  nil)
+  (str (second card)))
 
 (defn pair? [hand]
-  nil)
+  (>= (apply max (vals (frequencies (map rank hand)))) 2))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (>= (apply max (vals (frequencies (map rank hand)))) 3))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (>= (apply max (vals (frequencies (map rank hand)))) 4))
 
 (defn flush? [hand]
-  nil)
+  (>= (apply max (vals (frequencies (map suit hand)))) 5))
 
 (defn full-house? [hand]
-  nil)
+  (= (sort (vals (frequencies (map rank hand)))) [2 3]))
 
 (defn two-pairs? [hand]
-  nil)
+  (let [nranks (sort (vals (frequencies (map rank hand))))]
+    (or (= nranks [1 2 2]) (= nranks [2 3]) (= nranks [1 4]))))
 
 (defn straight? [hand]
-  nil)
+  (let [hlpr (fn [r] (= r (range (first r) (+ (first r) 5))))
+        ranks (sort (keys (frequencies (map rank hand))))]
+    (boolean (or (hlpr ranks) (hlpr (sort (replace {14 1} ranks)))))))
 
 (defn straight-flush? [hand]
-  nil)
+  (and (straight? hand) (flush? hand)))
 
 (defn value [hand]
-  nil)
+  (cond (straight-flush? hand) 8
+        (four-of-a-kind? hand) 7
+        (full-house? hand) 6
+        (flush? hand) 5
+        (straight? hand) 4
+        (three-of-a-kind? hand) 3
+        (two-pairs? hand) 2
+        (pair? hand) 1
+        :else 0))
