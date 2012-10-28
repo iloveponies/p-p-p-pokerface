459996abad70c1beeb425bdae41ab319dd76d97c
Eetu <esaaksvu@ubuntu.(none)>
2012-10-28 23:56:59 +0200

    vastaukset

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,13 +1,19 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+	(let [[fst _] card]
+	 (if (Character/isDigit fst) 
+	    (Integer/valueOf (str fst))
+	    (get {\A 14, \K 13, \Q 12, \J 11, \T 10} fst)))
+)
 
 (defn suit [card]
-  nil)
+	(let [[_ snd] card]
+ 	 (str snd))
+)
 
 (defn pair? [hand]
-  nil)
+ nil)
 
 (defn three-of-a-kind? [hand]
   nil)
