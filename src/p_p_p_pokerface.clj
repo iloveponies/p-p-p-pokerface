e2c35ed2cf9d6c4ddd8815ff36a716c13aefb240
Ville <ville@vih-99.(none)>
2012-10-28 16:31:14 +0200

    pari vielä puuttuu

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,43 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+  (let [[ran _] card]
+    (if (Character/isDigit ran) 
+      (Integer/valueOf (str ran))
+      (get {\T 10 \J 11 \Q 12 \K 13 \A 14} ran))))
 
 (defn suit [card]
-  nil)
+  (let [[_ sui] card]
+   (str sui)))
 
-(defn pair? [hand]
-  nil)
+(defn pair? [hand] 
+  (== 2 (apply max (vals (frequencies (map rank hand))))))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (== 3 (apply max (vals (frequencies (map rank hand))))))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (== 4 (apply max (vals (frequencies (map rank hand))))))
 
 (defn flush? [hand]
-  nil)
+  (== 5 (apply max (vals (frequencies (map suit hand))))))
 
 (defn full-house? [hand]
-  nil)
+  (and (== 2 (apply min (vals (frequencies (map rank hand))))) 
+       (three-of-a-kind? hand)))
 
 (defn two-pairs? [hand]
-  nil)
+  (or (= [1 2 2] (sort (vals (frequencies (map rank hand)))))
+      (= [1 4] (sort (vals (frequencies (map rank hand)))))))
 
 (defn straight? [hand]
-  nil)
+  ())
 
 (defn straight-flush? [hand]
-  nil)
+  (and (flush? hand) (straight? hand)))
+
+(defn high-card? [hand]
+  true) ; All hands have a high card.
 
 (defn value [hand]
-  nil)
+  ())
