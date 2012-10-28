c2a8a30d3642c35818d9e26537e458e826791cb7
kaanders <kaanders@asdf>
2012-10-28 23:40:01 +0200

    kaikki

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,67 @@
 (ns p-p-p-pokerface)
 
-(defn rank [card]
-  nil)
+(def r-conv {\T 10, \J 11, \Q 12, \K 13, \A 14})
 
-(defn suit [card]
-  nil)
+(defn rank [[r _]]
+  (if (Character/isDigit r)
+    (Integer/valueOf (str r))
+    (r-conv r)))
+
+(defn suit [[_ s]]
+  (str s))
+
+(defn freq-hand [hand]
+  (vals (frequencies (map rank hand))))
 
 (defn pair? [hand]
-  nil)
+  (if (= (count (filter #(= 2 %) (freq-hand hand))) 1)
+    true
+    false))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (if (= (count (filter #(= 3 %) (freq-hand hand))) 1)
+    true
+    false))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (if (= (count (filter #(= 4 %) (freq-hand hand))) 1)
+    true
+    false))
 
 (defn flush? [hand]
-  nil)
+  (if (apply = (map suit hand))
+    true
+    false))
 
 (defn full-house? [hand]
-  nil)
+  (and (three-of-a-kind? hand) (pair? hand)))
 
 (defn two-pairs? [hand]
-  nil)
+  (if (or (= (count (filter #(= 2 %) (freq-hand hand))) 2)
+          (four-of-a-kind? hand))
+    true
+    false))
 
 (defn straight? [hand]
-  nil)
+  (let [ranks (vec (map rank hand))]
+  (if (and (= (count ranks) (count (distinct ranks)))
+           (or (= (- (apply max ranks) (apply min ranks)) 4)
+               (= (sort ranks) [2, 3, 4, 5, 14]))) ; low ace case
+    true
+    false)))
 
 (defn straight-flush? [hand]
-  nil)
+  (if (and (straight? hand) (flush? hand))
+    true
+    false))
 
 (defn value [hand]
-  nil)
+  (cond (straight-flush? hand) 8
+        (four-of-a-kind? hand) 7
+        (full-house? hand) 6
+        (flush? hand) 5
+        (straight? hand) 4
+        (three-of-a-kind? hand) 3
+        (two-pairs? hand) 2
+        (pair? hand) 1
+        :else 0))
