fdadda6badd09113df71715873f41cdddc50fba9
Jani Rahkola <jani.rahkola@gmail.com>
2013-03-09 13:35:56 +0200

    updated project

--- project.clj
+++ project.clj
@@ -1,9 +1,5 @@
 (defproject p-p-p-pokerface "1.0.0-SNAPSHOT"
-  :description "P-p-p-pokerface"
-  :dependencies [[org.clojure/clojure "1.4.0"]]
-  :repositories {"stuart" "http://stuartsierra.com/maven2"}
-  :plugins [[lein-midje "2.0.0-SNAPSHOT"]]
-  :profiles
-    {:dev
-     {:dependencies [[midje "1.4.0"]
-                     [com.stuartsierra/lazytest "1.2.3"]]}})
+  :dependencies [[org.clojure/clojure "1.5.0"]]
+  :profiles {:dev
+             {:dependencies [[midje "1.5-RC1"]]
+              :plugins [[lein-midje "3.0-RC1"]]}})
