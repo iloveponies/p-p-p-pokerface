83564a1def5a90aa2db6d269c7c7c1a55c37c820
juho H <juhoth@ovi.com>
2013-10-14 21:26:25 +0300

    a few flushes away

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,66 @@
 (ns p-p-p-pokerface)
 
+(def replacements {\T 10, \J 11, \Q 12, \K 13, \A 14})
+
 (defn rank [card]
-  nil)
+  (let [[rank suit] card]
+    (cond
+      (Character/isDigit rank) (Integer/valueOf (str rank))
+      :else (get replacements rank))))
 
 (defn suit [card]
-  nil)
+  (let [[rank suit] card]
+    (str suit)))
+
+(defn n-same-cards-by [typ cards n]
+  (let [values (map typ cards)
+        freqs (frequencies values)
+        same? (fn [key] (== n (get freqs key)))]
+    (filter same? (keys freqs))))
+
+(defn n-same-ranks [cards n]
+  (n-same-cards-by rank cards n))
+  ;; (let [ranks (map rank cards)
+  ;;       freqs (frequencies ranks)
+  ;;       same? (fn [key] (== n (get freqs key)))]
+  ;;   (filter same? (keys freqs))))
+(defn n-same-suits [cards n]
+  (n-same-cards-by suit cards n))
 
 (defn pair? [hand]
-  nil)
+  (== 1 (count (n-same-ranks hand 2))))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (== 1 (count (n-same-ranks hand 3))))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (== 1 (count (n-same-ranks hand 4))))
 
 (defn flush? [hand]
-  nil)
+  (== 1 (count (n-same-suits hand 5))))
 
 (defn full-house? [hand]
-  nil)
+  (and
+    (pair? hand)
+    (three-of-a-kind? hand)))
 
 (defn two-pairs? [hand]
-  nil)
+  (== 2 (count (n-same-ranks hand 2))))
 
 (defn straight? [hand]
-  nil)
+  (let [ranks (sort (map rank hand))]
+    (or
+      (apply < ranks)
+      (and 
+        (== 14 (last ranks))
+        (== 2 (first ranks))
+        (apply < (sort (assoc ranks 4 1)))))))
+
 
 (defn straight-flush? [hand]
-  nil)
+  (and
+    (flush? hand)
+    (straight? hand)))
 
 (defn value [hand]
   nil)
