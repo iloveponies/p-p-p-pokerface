e83fb8d9afb113ba1808e553207ee9a00cfbdbcf
niko.desktop <niko.ryytty@gmail.com>
2013-10-18 08:07:11 +0300

    mein pony

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,31 +1,45 @@
 (ns p-p-p-pokerface)
 
-(defn rank [card]
-  nil)
+(defn rank [[r s]]
+  (def rep {\T 10, \J 11, \Q 12, \K 13, \A 14})
+  (if (Character/isDigit r) (Integer/valueOf (str r)) (rep r)))
 
-(defn suit [card]
-  nil)
+(defn suit [[r s]]
+  (str s))
+
+(defn suit-occurs-more-often-than? [hand, count]
+  (> (apply max (vals (frequencies (map suit hand)))) count))
+
+(defn rank-occurs-more-often-than? [hand, count]
+  (> (apply max (vals (frequencies (map rank hand)))) count))
 
 (defn pair? [hand]
-  nil)
+  (rank-occurs-more-often-than? hand 1))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (rank-occurs-more-often-than? hand 2))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (rank-occurs-more-often-than? hand 3))
 
 (defn flush? [hand]
-  nil)
+  (suit-occurs-more-often-than? hand 4))
+
+(defn are-ranks-composed-of? [hand, rank-composition]
+  (= rank-composition (vals (frequencies (map rank hand)))))
+
+(defn are-suits-composed-of? [hand, suit-composition]
+  (= suit-composition (vals (frequencies (map suit hand)))))
 
 (defn full-house? [hand]
-  nil)
+  (are-ranks-composed-of? hand [3 2]))
 
 (defn two-pairs? [hand]
-  nil)
+  (are-ranks-composed-of? hand [2 2 1]))
 
 (defn straight? [hand]
-  nil)
+  (def suits (sort (map rank hand)))
+  (= suits (seq [(min suits) (+ (min suits) 5)])))
 
 (defn straight-flush? [hand]
   nil)
