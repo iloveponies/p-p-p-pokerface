5b94c08484f61db44d8dd45b5de7d6d6c187cb05
Matias Juntunen <matias.juntunen@gmail.com>
2013-10-20 02:13:04 +0300

    almost done

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,22 +1,42 @@
 (ns p-p-p-pokerface)
 
+(def replacements
+  {\T 10
+   \J 11
+   \Q 12
+   \K 13
+   \A 14})
+
 (defn rank [card]
-  nil)
+  (let [[rank _] card]
+    (if (Character/isDigit rank)
+      (Integer/valueOf (str rank))
+      (get replacements rank))))
 
 (defn suit [card]
-  nil)
+  (let [[_ suit] card]
+    (str suit)))
 
 (defn pair? [hand]
-  nil)
+  (let [ranks (vals (frequencies (map rank hand)))]
+    (and
+      (contains? (set ranks) 2)
+      (not (== (count (set ranks)) (count ranks))))))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (let [ranks (vals (frequencies (map rank hand)))]
+    (and
+      (contains? (set ranks) 3)
+      (not (== (count (set ranks)) (count ranks))))))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (let [ranks (vals (frequencies (map rank hand)))]
+    (contains? (set ranks) 4)))
 
 (defn flush? [hand]
-  nil)
+  (let [ranks (map rank hand)]
+    (or (apply < ranks)
+        (apply > ranks))))
 
 (defn full-house? [hand]
   nil)
