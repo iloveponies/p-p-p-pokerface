54e0356723a394fa6e4317310b4f660493e8f956
Samppalol <sami.salonen@yahoo.com>
2013-10-22 02:01:14 +0300

    all done here

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -15,6 +15,7 @@
 (defn pair? [hand]
   (== 2 (apply max(vals (frequencies (map (fn [card] (rank card)) hand))))))
 
+
 ;(def high-seven ["2H" "3S" "4C" "5C" "7D"])
 ;(def pair-hand ["2H" "2S" "4C" "5C" "7D"])
 
@@ -34,19 +35,59 @@
   (== 5 (apply max(vals (frequencies (map (fn [card] (suit card)) hand))))))
 
 (defn full-house? [hand]
-  (keys(map (fn [card] (rank card)) hand)))
+  (let [value-counts (max(vals (frequencies (map (fn [card] (rank card)) hand))))
+        [a b] value-counts]
+    (and(count value-counts) (and (== a 3) (== b 2)))))
 
-(full-house? ["2H" "5D" "2D" "2C" "5S"])
+
+;(full-house? ["2H" "5D" "2D" "2C" "5S"])
 
 (defn two-pairs? [hand]
-  nil)
+  (let [value-counts (max(vals (frequencies (map (fn [card] (rank card)) hand))))
+        [a b] value-counts]
+    (and(count value-counts) (and (== a 2) (== b 2)))))
+
+
+;(two-pairs? ["2H" "2S" "4C" "4D" "7D"])
 
 (defn straight? [hand]
-  nil)
+  (let [ranks (map (fn [card] (rank card)) hand)
+        ranks-sorted (sort ranks)
+        thingie (range (first ranks-sorted) (+ 5 (first ranks-sorted)))
+        alter-thingie (drop-last thingie)
+        alter-ranks-sorted (drop-last ranks-sorted)]
+
+    (or (and (= alter-thingie alter-ranks-sorted) (= (last ranks-sorted) 14))
+        (= thingie ranks-sorted))))
+
+
+;the solution found above is both rickety and highly experimental
+
+;(straight? ["2H" "3S" "6C" "5D" "4D"])
+;(straight? ["2D" "3D" "4D" "5D" "AD"])
+;(straight? ["2D" "3D" "4D" "5D" "KD"])
 
 (defn straight-flush? [hand]
-  nil)
+  (and (straight? hand) (flush? hand)))
+
+(defn high-card? [hand]
+  (not (or (flush? hand) (straight? hand) (pair? hand) (three-of-a-kind? hand))))
+
+
+;(high-card? ["2H" "3S" "4C" "5C" "7D"])
 
 (defn value [hand]
-  nil)
+  (cond
+   (straight-flush? hand) 8
+   (four-of-a-kind? hand) 7
+   (full-house? hand) 6
+   (flush? hand) 5
+   (straight? hand) 4
+   (three-of-a-kind? hand) 3
+   (two-pairs? hand) 2
+   (pair? hand) 1
+   :else 0))
+
+
+;(value ["2H" "3S" "4C" "5C" "7D"])
 
