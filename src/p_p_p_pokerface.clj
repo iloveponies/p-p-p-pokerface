f1ef09e934363da7dadc7114e7789cae244578c7
Samppalol <sami.salonen@yahoo.com>
2013-10-22 00:20:35 +0300

    first 6 exercises done

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,25 +1,42 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+  (let [[fst snd] card
+       ranknums {\A 14, \K 13, \Q 12, \J 11, \T 10}]
+  (if (Character/isDigit fst)
+    (Integer/valueOf (str fst)) ;truthy, digit to integer
+    (get ranknums fst) ;falsey, resolve integer value
+    )))
 
 (defn suit [card]
-  nil)
+  (let [[fst snd] card]
+    (str snd)))
 
 (defn pair? [hand]
-  nil)
+  (== 2 (apply max(vals (frequencies (map (fn [card] (rank card)) hand))))))
+
+;(def high-seven ["2H" "3S" "4C" "5C" "7D"])
+;(def pair-hand ["2H" "2S" "4C" "5C" "7D"])
+
+;(pair? pair-hand)
+;(pair? high-seven)
+
+;(pair? ["2H" "2S" "4C" "5C" "7D"])
+;(pair? ["2S" "4S" "4C" "9D" "KS"])
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (== 3 (apply max(vals (frequencies (map (fn [card] (rank card)) hand))))))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (== 4 (apply max(vals (frequencies (map (fn [card] (rank card)) hand))))))
 
 (defn flush? [hand]
-  nil)
+  (== 5 (apply max(vals (frequencies (map (fn [card] (suit card)) hand))))))
 
 (defn full-house? [hand]
-  nil)
+  (keys(map (fn [card] (rank card)) hand)))
+
+(full-house? ["2H" "5D" "2D" "2C" "5S"])
 
 (defn two-pairs? [hand]
   nil)
@@ -32,3 +49,4 @@
 
 (defn value [hand]
   nil)
+
