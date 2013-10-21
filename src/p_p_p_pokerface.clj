416fa5b102f332bb36a138be6f26f5992d18b651
Elias Nygren <elias.nygren@outlook.com>
2013-10-21 21:14:28 +0300

    Can you hold your pokerface when you see my solutions for the last three functions?

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,98 @@
 (ns p-p-p-pokerface)
 
+
+;Functions dealing with hands and cards:
 (defn rank [card]
-  nil)
+  "Helper function that returns the rank of a single card"
+  (let [[char] card
+        values {\T 10, \J 11, \Q 12, \K 13, \A 14}]
+    (if (Character/isDigit char)
+      (Integer/valueOf (str char))
+      (get values char)
+      )))
+
+
 
 (defn suit [card]
-  nil)
+  (let [[_ b] card]
+    (str b)))
 
+(defn hand [a-hand]
+  a-hand)
+
+(defn card [a-card]
+  a-card)
+
+;Helper function for some of the assignments:
+(defn hand->val-frqs [hand]
+  "Helper function that returns the ranks of the whole hand"
+  (let[ints-hand (map rank hand)]
+    (vals(frequencies ints-hand))))
+
+
+;Assignments evaluating hands:
 (defn pair? [hand]
-  nil)
+  (>= (apply max (hand->val-frqs hand)) 2))
+
+
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (>= (apply max (hand->val-frqs hand)) 3))
+
+
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (>= (apply max (hand->val-frqs hand)) 4))
+
 
 (defn flush? [hand]
-  nil)
+  (let [suits (map suit hand)]
+    (apply = suits)))
 
 (defn full-house? [hand]
-  nil)
+  (let [frqs (set(hand->val-frqs hand))]
+    (and
+    (contains? frqs 2)
+    (contains? frqs 3))))
 
 (defn two-pairs? [hand]
-  nil)
+  (let [frqs-of-frqs (frequencies(hand->val-frqs hand))]
+    (and
+    (= (get frqs-of-frqs 1) 1)
+    (= (get frqs-of-frqs 2) 2))))
 
+;trolling them assignmentz:
 (defn straight? [hand]
-  nil)
+  (if (pair? hand)
+    false
+    (let[ranks-hand (map rank hand)
+         sorted-hand (sort ranks-hand)
+         [a] sorted-hand
+         min a
+         [_ _ _ b] sorted-hand
+         second-max b
+         [_ _ _ _ c] sorted-hand
+         max c]
+        (cond
+         (= (- max min) 4) true
+         (= max 14) (= (- second-max 1) 4)
+         :else false))))
+
+
 
 (defn straight-flush? [hand]
-  nil)
+  (and (flush? hand) (straight? hand)))
 
+;Values "simple edition":
 (defn value [hand]
-  nil)
+  (cond
+    (straight-flush? hand) 8
+    (four-of-a-kind? hand) 7
+    (full-house? hand)6
+    (flush? hand) 5
+    (straight? hand) 4
+    (three-of-a-kind? hand) 3
+    (two-pairs? hand) 2
+    (pair? hand) 1
+    :else 0))
+
