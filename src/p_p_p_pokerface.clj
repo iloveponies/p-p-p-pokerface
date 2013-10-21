7554ec47a07c35124ca59c2e49dc21381ed090b0
Mikael Svenn <mikael.svenn@cs.helsinki.fi>
2013-10-21 22:15:35 +0300

    Done.

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -5,41 +5,59 @@
         mappings {\T 10, \J 11, \Q 12, \K 13, \A 14}]
      (if(Character/isDigit r)
        (Integer/valueOf (str r))
-       (Integer/valueOf (str (mappings r)))))
-)
+       (Integer/valueOf (str (mappings r))))))
 
 (defn suit [card]
   (let [[_ s] card]
-    (str s)
-  )
-)
+    (str s)))
+
+(defn rank-freq-values [hand]
+  (vals (frequencies (map rank hand))))
 
 (defn pair? [hand]
-  (let [rank-values (frequencies (map rank hand))]
-    (not (= 5 (count (vals rank-values)))))
-)
+  (= 4 (count (rank-freq-values hand))))
+
+(defn has-amount-of-same? [values n]
+  (not (empty? (filter (fn [value] (= value n)) values))))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (has-amount-of-same? (rank-freq-values hand) 3))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (has-amount-of-same? (rank-freq-values hand) 4))
 
 (defn flush? [hand]
-  nil)
+  (let [suit-values (vals (frequencies (map suit hand)))]
+    (= 1 (count suit-values))))
 
 (defn full-house? [hand]
-  nil)
+  (= [2 3] (sort (rank-freq-values hand))))
 
 (defn two-pairs? [hand]
-  nil)
+  (or (= [1 2 2] (sort (rank-freq-values hand))) (four-of-a-kind? hand)))
 
 (defn straight? [hand]
-  nil)
+  (let [ranks (map rank hand)
+        sorted-ranks (sort (map rank hand))
+        sorted-ranks-ace-first (sort (replace {14 1} ranks))]
+    (or (= sorted-ranks (range (apply min sorted-ranks) (+ 1 (apply max sorted-ranks))))
+        (= sorted-ranks-ace-first (range 1 6)))))
 
 (defn straight-flush? [hand]
-  nil)
+  (and (flush? hand) (straight? hand)))
+
+(defn why-on-earth-wouldnt-i-use-cond-to-determine-the-value-of-this [hand]
+  (cond
+   (straight-flush? hand) 8
+   (four-of-a-kind? hand) 7
+   (full-house? hand) 6
+   (flush? hand) 5
+   (straight? hand) 4
+   (three-of-a-kind? hand) 3
+   (two-pairs? hand) 2
+   (pair? hand) 1
+   :else 0)
+  )
 
 (defn value [hand]
-  nil)
-
+  (why-on-earth-wouldnt-i-use-cond-to-determine-the-value-of-this hand))
