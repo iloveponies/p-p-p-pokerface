52ff0f45b70413f59ca84dd0646b194635cd99cf
Leo Muona <leo.muona@gmail.com>
2013-10-22 12:49:45 +0300

    pokerface with nasty cards in my sleeve

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,62 @@
 (ns p-p-p-pokerface)
 
-(defn rank [card]
-  nil)
+(defn rank [[fst snd]]
+  (let [pic-cards {\T 10 \J 11 \Q 12 \K 13 \A 14}]
+    (if (Character/isDigit fst)
+      (Integer/valueOf (str fst))
+      (pic-cards fst))))
 
-(defn suit [card]
-  nil)
+(defn suit [[fst snd]]
+  (str snd))
 
-(defn pair? [hand]
-  nil)
+(defn same-rank-counts [hand]
+  (frequencies (vals (frequencies (map rank hand)))))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (contains? (same-rank-counts hand) 3))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (contains? (same-rank-counts hand) 4))
+
+(defn pair? [hand]
+  (boolean (or (contains? (same-rank-counts hand) 2)
+               (three-of-a-kind? hand)
+               (four-of-a-kind? hand))))
 
 (defn flush? [hand]
-  nil)
+  (contains? (frequencies (vals (frequencies (map suit hand)))) 5))
 
 (defn full-house? [hand]
-  nil)
+  (and (three-of-a-kind? hand)
+       (contains? (same-rank-counts hand) 2)))
 
 (defn two-pairs? [hand]
-  nil)
+  (or (four-of-a-kind? hand)
+      (full-house? hand)
+      (and (contains? (same-rank-counts hand) 2)
+           (== ((same-rank-counts hand) 2) 2))))
 
 (defn straight? [hand]
-  nil)
+  (let [hand-vals (map rank hand)
+        ace-hand-vals (replace {14 1} hand-vals)
+        min-max-diff (fn [sorted-hand] (- (last sorted-hand) (first sorted-hand)))]
+    (and (not (pair? hand))
+         (or (== (min-max-diff (sort hand-vals)) 4)
+             (== (min-max-diff (sort ace-hand-vals)) 4)))))
 
 (defn straight-flush? [hand]
-  nil)
+  (and (straight? hand) (flush? hand)))
+
+(defn high-card? [hand]
+  true) ; How high are you? Yes.
+
+(defn hand-has-value? [hand value]
+  (let [checkers [high-card? pair? two-pairs? three-of-a-kind? straight? flush?
+                  full-house? four-of-a-kind? straight-flush?]]
+    ((get checkers value) hand)))
+
+(defn closest-value [hand value]
+  (if (hand-has-value? hand value) value (closest-value hand (- value 1))))
 
 (defn value [hand]
-  nil)
+  (closest-value hand 8)) ; recursion helper functions ftw.
