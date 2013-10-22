d76ebd1f8d4878e9f9a6e92eb11b96593d75db85
Essi Salmenkivi <essi.salmenkivi@helsinki.fi>
2013-10-22 13:14:03 +0300

    moi

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,86 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+  (let [[r _] card]
+    (if (Character/isDigit r)
+      (Integer/valueOf (str r))
+      (get {\T 10, \J 11, \Q 12, \K 13, \A 14} r))))
 
 (defn suit [card]
-  nil)
+  (let [[_ s] card]
+    (str s)))
 
 (defn pair? [hand]
-  nil)
+  (let [ranks (map rank hand)
+        freqs (frequencies ranks)]
+    (if (<= 2 (apply max (vals freqs)))
+     true
+     false)))
+
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (let [ranks (map rank hand)
+        freqs (frequencies ranks)]
+    (if (<= 3 (apply max (vals freqs)))
+     true
+     false)))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (let [ranks (map rank hand)
+        freqs (frequencies ranks)]
+    (if (<= 4 (apply max (vals freqs)))
+     true
+     false)))
 
 (defn flush? [hand]
-  nil)
+  (let [suits (map suit hand)
+        freqs (frequencies suits)]
+    (if (= 5 (apply max (vals freqs)))
+      true
+      false)))
 
 (defn full-house? [hand]
-  nil)
+  (let [ranks (map rank hand)
+        freqs (frequencies ranks)]
+    (if (= (sort (vals freqs)) [2 3])
+      true
+      false)))
 
 (defn two-pairs? [hand]
-  nil)
+   (let [ranks (map rank hand)
+        freqs (frequencies ranks)]
+    (if (= (sort (vals freqs)) [1 2 2])
+      true
+      false)))
 
 (defn straight? [hand]
-  nil)
+  (let [values (map rank hand)
+        high (apply max values)]
+    (if (= (sort values) (range (- high 4) (+ high 1)))
+      true
+      (if (= (sort (replace {14 1} values)) (range 1 6))
+       true
+       false))))
 
 (defn straight-flush? [hand]
-  nil)
+  (and (straight? hand) (flush? hand)))
 
 (defn value [hand]
-  nil)
+  (if (straight-flush? hand)
+    8
+    (if (four-of-a-kind? hand)
+      7
+      (if (full-house? hand)
+        6
+        (if (flush? hand)
+          5
+          (if (straight? hand)
+            4
+            (if (three-of-a-kind? hand)
+              3
+              (if (two-pairs? hand)
+                2
+                (if (pair? hand)
+                  1
+                  0)))))))))
+
