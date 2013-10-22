40da8044142ef2a4a4326e72db1f8e761544992e
Aleksi Paavola <ahpaavola@gmail.com>
2013-10-22 13:09:26 +0300

    Made some progress with p-p-p-pokerface

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,25 +1,42 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+  (let [[fst _] card]
+  (if (Character/isDigit fst)
+    (Integer/valueOf (str fst))
+    (get {\A 14, \K 13, \T 10
+          \Q 12, \J 11} fst))))
 
 (defn suit [card]
-  nil)
+  (let [[_ snd] card]
+   (str snd)))
 
 (defn pair? [hand]
-  nil)
+  (if (empty?
+       (filter #(= 2 %)
+       (vals (frequencies (map #(rank %) hand)))))
+    false true))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (if (empty?
+       (filter #(= 3 %)
+       (vals (frequencies (map #(rank %) hand)))))
+    false true))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (if (empty?
+       (filter #(= 4 %)
+       (vals (frequencies (map #(rank %) hand)))))
+    false true))
 
 (defn flush? [hand]
-  nil)
+  (if (empty?
+       (filter #(= 5 %)
+       (vals (frequencies (map #(suit %) hand)))))
+    false true))
 
 (defn full-house? [hand]
-  nil)
+  )
 
 (defn two-pairs? [hand]
   nil)
