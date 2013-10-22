b00f8f055489845d83fd91c08fb2d23ace4cb41a
Jari <juri@juri-HP-620.(none)>
2013-10-22 22:32:47 +0300

    hyvät oddsit et tehty

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,56 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+  (let [letter-ranks {\T 10, \J 11, \Q 12
+                      \K 13, \A 14}
+        rank (get card 0)]
+    (if (Character/isDigit rank)
+      (Integer/valueOf (str rank))
+      (get letter-ranks rank))))
 
 (defn suit [card]
-  nil)
+  (str (get card 1)))
 
 (defn pair? [hand]
-  nil)
+  (contains?(set (vals (frequencies (map rank hand)))) 2))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (contains?(set (vals (frequencies (map rank hand)))) 3))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (contains?(set (vals (frequencies (map rank hand)))) 4))
 
 (defn flush? [hand]
-  nil)
+  (contains?(set (vals (frequencies (map suit hand)))) 5))
 
 (defn full-house? [hand]
-  nil)
+  (and (pair? hand) (three-of-a-kind? hand)))
 
 (defn two-pairs? [hand]
-  nil)
+  (let [ranks (map rank hand)]
+  	(or (= (sort (vals (frequencies ranks))) (seq [1 2 2]))
+  	    (four-of-a-kind? hand))))
 
 (defn straight? [hand]
-  nil)
+  (let [sorted-ranks (vec (sort (map rank hand)))
+         contains-ace (contains?(set sorted-ranks) 14)
+         sorted-lowace (vec (sort (replace {14 1} sorted-ranks)))]
+     (or
+      (= (range (get sorted-ranks 0) (+ (get sorted-ranks 4) 1)) sorted-ranks)
+      (= (range (get sorted-lowace 0) (+ (get sorted-lowace 4) 1)) sorted-lowace))))
+;tän ois voinu tehä niin paljon helpomminkin...
 
 (defn straight-flush? [hand]
-  nil)
+  (and (straight? hand) (flush? hand)))
 
 (defn value [hand]
-  nil)
+  (cond
+   (straight-flush? hand) 8
+   (four-of-a-kind? hand) 7
+   (full-house? hand) 6
+   (flush? hand) 5
+   (straight? hand) 4
+   (three-of-a-kind? hand) 3
+   (two-pairs? hand) 2
+   (pair? hand) 1
+   :else 0))
