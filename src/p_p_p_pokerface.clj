6776a843c8339eabfea358777b2632201063ea13
Lauri Kaitala <lawkaita@gmail.com>
2013-10-23 23:40:21 +0300

    Enää values -tehtävä jäljellä

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,74 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+  (let [[fst _] card]
+    (if (Character/isDigit fst)
+      (Integer/valueOf (str fst))
+      ({\T 10 \J 11 \Q 12
+       \K 13 \A 14} fst))))
 
 (defn suit [card]
-  nil)
+  (let [[_ snd] card]
+    (str snd)))
 
 (defn pair? [hand]
-  nil)
+  (let [rank-seq (map rank hand)
+        freq-rank (frequencies rank-seq)]
+    (contains? (set (vals freq-rank)) 2)))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (let [rank-seq (map rank hand)
+        freq-rank (frequencies rank-seq)]
+    (contains? (set (vals freq-rank)) 3)))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (let [rank-seq (map rank hand)
+        freq-rank (frequencies rank-seq)]
+    (contains? (set (vals freq-rank)) 4)))
 
 (defn flush? [hand]
-  nil)
+  (let [suits-in-hand (map suit hand)
+        num-of-suits (count (set suits-in-hand))]
+    (if (= num-of-suits 1)
+      true
+      false)))
 
 (defn full-house? [hand]
-  nil)
+  (and (pair? hand)
+       (three-of-a-kind? hand)))
 
 (defn two-pairs? [hand]
-  nil)
+  (let [rank-seq (map rank hand)
+      freq-rank (frequencies rank-seq)
+      vals-freq (vals freq-rank)
+      freq-vals (frequencies vals-freq)
+      vals-fv (vals freq-vals)]
+  (if (or (and (contains? (set vals-fv) 2)
+               (contains? (set vals-freq) 2))
+          (contains? (set vals-freq) 4))
+    true
+    false)))
 
 (defn straight? [hand]
-  nil)
+  (let [sorted-hand (sort (map rank hand))
+        hand-min-rank (apply min sorted-hand)
+        would-be-flush-hand-seq (range hand-min-rank
+                                       (+ 5 hand-min-rank))]
+    (if (= sorted-hand would-be-flush-hand-seq)
+      true
+      (let [new-hand (replace {14 1} (map rank hand))
+           sorted-new-hand (sort new-hand)
+           new-hand-min-rank (apply min sorted-new-hand)
+           new-would-be-flush-hand-seq (range new-hand-min-rank
+                                       (+ 5 new-hand-min-rank))]
+        (if (= sorted-new-hand new-would-be-flush-hand-seq)
+          true
+          false)))))
 
 (defn straight-flush? [hand]
-  nil)
+  (and (straight? hand)
+       (flush? hand)))
 
 (defn value [hand]
   nil)
+
