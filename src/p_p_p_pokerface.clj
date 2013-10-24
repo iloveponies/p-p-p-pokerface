c5ee6dd4244222907db728e029314ed13cb6b606
karrirasinmaki <karri.rasinmaki@gmail.com>
2013-10-24 20:35:03 +0300

    Yep

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,64 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+  (let [[r _] card
+        big {\T 10
+             \J 11
+             \Q 12
+             \K 13
+             \A 14}]
+    (if (Character/isDigit r)
+      (Integer/valueOf (str r))
+      (get big r))))
 
 (defn suit [card]
-  nil)
+  (let [[_ s] card]
+    (str s)))
+
+(defn high-card? [hand]
+  (max (hand)))
 
 (defn pair? [hand]
-  nil)
+  (>= (apply max (vals (frequencies (map rank hand)))) 2))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (>= (apply max (vals (frequencies (map rank hand)))) 3))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (>= (apply max (vals (frequencies (map rank hand)))) 4))
 
 (defn flush? [hand]
-  nil)
+  (apply = (keys (frequencies (map suit hand)))))
 
 (defn full-house? [hand]
-  nil)
+  (let [k (sort (vals (frequencies (map rank hand))))]
+    (= [2 3] k)))
 
 (defn two-pairs? [hand]
-  nil)
+  (let [k (sort (vals (frequencies (map rank hand))))]
+    (= [1 2 2] k)))
 
 (defn straight? [hand]
-  nil)
+  (let [h (map rank hand)
+        h1 (map (fn [a] (if (= a 14) 1 a)) h)
+        te (fn [hnd] (apply = (map - (sort hnd) [0 1 2 3 4])))]
+    (or (te h) (te h1))))
 
 (defn straight-flush? [hand]
-  nil)
+  (and (straight? hand) (flush? hand)))
 
 (defn value [hand]
-  nil)
+  (cond
+   (straight-flush? hand) 8
+   (four-of-a-kind? hand) 7
+   (full-house? hand) 6
+   (flush? hand) 5
+   (straight? hand) 4
+   (three-of-a-kind? hand) 3
+   (two-pairs? hand) 2
+   (pair? hand) 1
+   :else 0))
+
+
+
+
