fe64c2e696d6ede485c38e5607953b7d97f4233e
Pii <pii@piis.(none)>
2013-10-24 19:42:08 +0300

    ensimmäinen osa

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,31 +1,48 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+  (let [[fst _] card
+        ranks {\T 10, \J 11, \Q 12, \K 13, \A 14}]
+    (if (Character/isDigit fst)
+      (Integer/valueOf (str fst))
+      (get ranks fst))))
 
 (defn suit [card]
-  nil)
+  (let [[_ snd] card]
+    (str snd)))
 
 (defn pair? [hand]
-  nil)
+  (let [occurance (set (vals(frequencies (map rank hand))))]
+     (contains? occurance 2)))
 
 (defn three-of-a-kind? [hand]
-  nil)
+   (let [occurance (set (vals(frequencies (map rank hand))))]
+    (contains? occurance 3)))
 
 (defn four-of-a-kind? [hand]
-  nil)
+   (let [occurance (set (vals(frequencies (map rank hand))))]
+    (contains? occurance 4)))
 
 (defn flush? [hand]
-  nil)
+  (apply = (map suit hand)))
 
 (defn full-house? [hand]
-  nil)
+  (and (three-of-a-kind? hand)
+       (pair? hand)))
 
 (defn two-pairs? [hand]
-  nil)
+  (let [occurance (vals (frequencies (map rank hand)))]
+     (and (== 3 (count occurance))
+          (pair? hand))))
 
 (defn straight? [hand]
-  nil)
+  (let [sorted-ranks (sort (map rank hand))
+        no-friequencies? (== 5 (count (set sorted-ranks)))]
+    (cond
+      (and (no-friequencies?) (== 4 (- (max sorted-ranks) (min sorted-ranks)))) true
+      (and (no-friequencies?) (and (== 14 (max sorted-ranks)) (== 2 (min sorted-ranks)))
+           (== 4 (- (max (sort (replace {14 1} sorted-ranks))) (min (sort (replace {14 1} sorted-ranks)))))) true
+     :else false)))
 
 (defn straight-flush? [hand]
   nil)
