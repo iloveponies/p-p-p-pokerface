a5a26b74fc0f949c7bd673291cc11d330a931c0a
Jesse Immonen <jesseimm@cs.helsinki.fi>
2013-10-25 22:23:03 +0300

    All.

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -11,29 +11,58 @@
   (let [[_ snd] card]
     (str snd)))
 
+(defn hand-contains-x-of-a-kind? [hand x]
+  (<= x (apply max (vals (frequencies (map rank hand))))))
+
+(defn all-cards-have-atleast-x-of-the-same-kind? [hand x]
+  (<= x (apply min (vals (frequencies (map rank hand))))))
+
 (defn pair? [hand]
-  nil)
+  (hand-contains-x-of-a-kind? hand 2))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (hand-contains-x-of-a-kind? hand 3))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (hand-contains-x-of-a-kind? hand 4))
 
 (defn flush? [hand]
-  nil)
+  (= 5 (apply max (vals (frequencies (map suit hand))))))
 
 (defn full-house? [hand]
-  nil)
+  (and (three-of-a-kind? hand))
+       (all-cards-have-atleast-x-of-the-same-kind? hand 2))
 
 (defn two-pairs? [hand]
-  nil)
+  (or (= [1 2 2] (sort (vals (frequencies (map rank hand)))))
+       (four-of-a-kind? hand)
+       (full-house? hand)))
 
 (defn straight? [hand]
-  nil)
+  (let [sorted-hand-map (sort (map rank hand))
+        sorted-hand-map-14-as-1 (sort (replace {14 1} sorted-hand-map))
+        max-card (apply max sorted-hand-map)
+        min-card (apply min sorted-hand-map)]
+
+    (if (and (= max-card 14) (= min-card 2))
+        (= sorted-hand-map-14-as-1 (range 1 (+ 1 (apply max sorted-hand-map-14-as-1))))
+        (= (range min-card (+ 1 max-card)) sorted-hand-map)
+      )
+    )
+  )
 
 (defn straight-flush? [hand]
-  nil)
+  (and (flush? hand)
+       (straight? hand)))
 
 (defn value [hand]
-  nil)
+  (cond
+   (straight-flush? hand) 8
+   (four-of-a-kind? hand) 7
+   (full-house? hand) 6
+   (flush? hand) 5
+   (straight? hand) 4
+   (three-of-a-kind? hand) 3
+   (two-pairs? hand) 2
+   (pair? hand) 1
+   :else 0))
