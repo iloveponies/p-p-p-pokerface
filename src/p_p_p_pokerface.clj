c0c46084b0e37a1778f82303b99f0ffe6be873ea
Mikael <->
2013-10-25 14:10:13 +0300

    -

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,55 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+  (def replacements {\A 14, \K 13, \Q 12, \J 11, \T 10})
+  (let [[fst _] card]
+    (if (Character/isDigit fst)
+      (Integer/valueOf (str fst))
+      (replacements fst)
+      )))
 
 (defn suit [card]
-  nil)
+  (let [[_ snd] card]
+    (str snd)))
 
 (defn pair? [hand]
-  nil)
+  (>= (apply max(vals (frequencies (map rank hand)))) 2))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (>= (apply max(vals (frequencies (map rank hand)))) 3))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (= (apply max(vals (frequencies (map rank hand)))) 4))
 
 (defn flush? [hand]
-  nil)
+  (= (apply max(vals (frequencies (map suit hand)))) 5))
 
 (defn full-house? [hand]
-  nil)
+  (and
+   (= (apply max(vals (frequencies (map rank hand)))) 3)
+   (= (apply min(vals (frequencies (map rank hand)))) 2)))
 
 (defn two-pairs? [hand]
   nil)
 
 (defn straight? [hand]
-  nil)
+  (def minrank (apply min(map rank hand)))
+  (if (or
+       (=
+        (sort (map rank hand))
+        (range minrank (+ minrank 5)))
+       (=
+        (sort (map rank hand))
+        (sort [2 3 4 5 14])))
+    true
+    false))
 
 (defn straight-flush? [hand]
-  nil)
+  (if (and
+       (straight? hand)
+       (flush? hand))
+  true
+  false))
 
 (defn value [hand]
   nil)
