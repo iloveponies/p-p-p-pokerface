96b3f1d404ab242a4abc16582115090b3f1a6125
Atte <atte@Atte-MacBook-Air.local>
2013-10-26 17:25:36 +0300

    juuh

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,70 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+  (let [[x _] card]
+  (if (Character/isDigit x)
+    (Integer/valueOf (str x))
+    (get {\T 10, \J 11, \Q 12, \K 13, \A 14} x))))
 
 (defn suit [card]
-  nil)
+  (let [[_ st] card]
+  (str st)))
 
 (defn pair? [hand]
-  nil)
+  (let [hand (map rank hand)
+        freq (vals (frequencies hand))]
+  (contains? (set freq) 2)))
 
 (defn three-of-a-kind? [hand]
-  nil)
+    (let [hand (map rank hand)
+        freq (vals (frequencies hand))]
+  (contains? (set freq) 3)))
 
 (defn four-of-a-kind? [hand]
-  nil)
+    (let [hand (map rank hand)
+        freq (vals (frequencies hand))]
+  (contains? (set freq) 4)))
 
 (defn flush? [hand]
-  nil)
+    (let [hand (map suit hand)
+        freq (vals (frequencies hand))]
+  (contains? (set freq) 5)))
 
 (defn full-house? [hand]
-  nil)
+  (and (pair? hand) (three-of-a-kind? hand)))
 
 (defn two-pairs? [hand]
-  nil)
+  (let [hand (map rank hand)
+        freq (vals (frequencies hand))]
+    (== (count (filter (fn [x] (== 2 x)) freq)) 2)))
+
+(defn straight [first]
+  (range first (+ first 5)))
 
 (defn straight? [hand]
-  nil)
+  (let [sortedhand (sort (map rank hand))
+        firstinhand (first sortedhand)
+        lastinhand (last sortedhand)
+        straighthand (straight firstinhand)]
+
+    (or (= sortedhand straighthand)
+        (= (straight 1) (sort (replace {14 1} sortedhand))))))
 
 (defn straight-flush? [hand]
-  nil)
+  (and (straight? hand) (flush? hand)))
+
+;(defn high-card? [hand]
+;  true)
 
 (defn value [hand]
-  nil)
+  (cond
+    (straight-flush? hand) 8
+    (four-of-a-kind? hand) 7
+    (full-house? hand) 6
+    (flush? hand) 5
+    (straight? hand) 4
+    (three-of-a-kind? hand) 3
+    (two-pairs? hand) 2
+    (pair? hand) 1
+    :else 0))
+
