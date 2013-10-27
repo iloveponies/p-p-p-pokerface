9302fb2d6e74d3ce9c82bcbcd04ba98a0440dbf9
Atte Tamminen <attammin@cs.helsinki.fi>
2013-10-27 14:25:26 +0200

    jee

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,31 +1,88 @@
 (ns p-p-p-pokerface)
 
-(defn rank [card]
-  nil)
+(def replacements { \T 10
+                    \J 11 
+                    \Q 12 
+                    \K 13 
+                    \A 14 })
 
+(defn rank [card]
+  (let [[rnk s] card]
+    (cond (Character/isDigit rnk) (Integer/valueOf (str rnk))
+    :else                         (replacements rnk)))
+)
 (defn suit [card]
-  nil)
+  (let [[rnk s] card]
+    (str s))
+)
 
 (defn pair? [hand]
-  nil)
+  (let [handranks (vec (map rank hand))]
+    (let [valfqs (vec (vals (frequencies handranks)))]
+      (cond (= (apply max valfqs ) 2) true
+            :else false))) 
+)
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (let [handranks (vec (map rank hand))]
+    (let [valfqs (vec (vals (frequencies handranks)))]
+      (cond (= (apply max valfqs ) 3) true
+            :else false))) 
+)
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (let [handranks (vec (map rank hand))]
+    (let [valfqs (vec (vals (frequencies handranks)))]
+      (cond (= (apply max valfqs ) 4) true
+            :else false)))
+)
 
 (defn flush? [hand]
-  nil)
+  (let [handsuits (vec (map suit hand))]
+    (let [keycount (vec (keys (frequencies handsuits)))]
+      (cond (= (count keycount ) 1) true
+            :else false)))
+  )
 
 (defn full-house? [hand]
-  nil)
+  (let [handranks (vec (map rank hand))]
+    (let [valfqs (sort (vec (vals (frequencies handranks))))]
+      (if (= valfqs [2 3])
+        true
+        false)))
+)
 
 (defn two-pairs? [hand]
-  nil)
+  (let [handranks (vec (map rank hand))]
+    (let [valfqs (sort (vec (vals (frequencies handranks))))]
+      (if (= valfqs [1 2 2])
+        true
+        false)))
+)
+(defn handranks [hand] 
+  (sort (vec (map rank hand))))
+
+(defn minmaxdif [hand] 
+  (let [ranks (handranks hand)]
+    (- (apply max ranks) (apply min ranks))))
+
+(defn valfqs [hand]
+  (let [ranks (handranks hand)]
+    (sort (vec (vals (frequencies ranks))))))
+
+(defn acereplaced [hand]
+  (sort (replace {14 1} (handranks hand))))
 
 (defn straight? [hand]
-  nil)
+  (let [rephand (acereplaced hand)]
+    (cond (and (= 4 (minmaxdif hand)) 
+               (= 5 (count (vals (frequencies hand))))
+               (< (handranks hand))) true
+          (and (= 4 (minmaxdif rephand))
+               (= 5 (count (valfqs rephand))) 
+               (< (handranks rephand))) true
+          :else   false)))
+
 
 (defn straight-flush? [hand]
   nil)
