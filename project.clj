32f0c3292cff54a47fca34d46069741f448c82df
Jani Rahkola <jani.rahkola@iki.fi>
2013-11-11 11:19:00 +0200

    Tests as a separate project

--- project.clj
+++ project.clj
@@ -1,6 +1,4 @@
 (defproject p-p-p-pokerface "1.0.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [midje-grader "0.1.0-SNAPSHOT"]]
-  :profiles {:dev
-             {:dependencies [[midje "1.5.1"]]
-              :plugins [[lein-midje "3.1.1"]]}})
+                 [iloveponies.tests/p-p-p-pokerface "0.1.0-SNAPSHOT"]]
+  :profiles {:dev {:plugins [[lein-midje "3.1.1"]]}})
