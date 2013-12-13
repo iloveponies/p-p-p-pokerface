9c9ff153051e87228b65b8e6aadd4b56a722757a
Ali Asad Lotia <ali.asad.lotia@gmail.com>
2013-12-13 12:37:22 +0000

    Partially solved.

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,28 +1,42 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+  (let [replacements {\T 10 \J 11 \Q 12  \K 13 \A 14}
+        [rank _] card]
+    (cond
+     (Character/isDigit rank) (Integer/valueOf (str rank))
+     :else (get replacements rank))))
 
 (defn suit [card]
-  nil)
+  (str (get card 1)))
+
+(defn rank-count [hand]
+  (vals (frequencies (map rank hand))))
+
+(defn has-num-same-cards? [numcards hand]
+  (== (apply max (rank-count hand)) numcards))
 
 (defn pair? [hand]
-  nil)
+  (has-num-same-cards? 2 hand))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (has-num-same-cards? 3 hand))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (has-num-same-cards? 4 hand))
 
 (defn flush? [hand]
-  nil)
+  (apply = (map suit hand)))
 
 (defn full-house? [hand]
-  nil)
+  (let [rank-list (sort (rank-count hand))]
+    (= (range 2 4) rank-list)))
 
 (defn two-pairs? [hand]
-  nil)
+  (let [rank-list (sort (rank-count hand))]
+    (or
+     (four-of-a-kind? hand)
+     (= [1 2 2] (sort (rank-count hand))))))
 
 (defn straight? [hand]
   nil)
