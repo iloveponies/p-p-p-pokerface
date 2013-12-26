2909fbe59b40c3c81078fdc511d01123759615dc
Miko Mynttinen <miko.mynttinen@helsinki.fi>
2013-12-26 17:08:08 +0200

    update

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -66,6 +66,18 @@
   (and (flush? hand)
        (straight? hand)))
 
+(defn high-card? [hand]
+  true)
+
 (defn value [hand]
-  nil)
+  (cond
+   (straight-flush? hand) 8
+   (four-of-a-kind? hand) 7
+   (full-house? hand) 6
+   (flush? hand) 5
+   (straight? hand) 4
+   (three-of-a-kind? hand) 3
+   (two-pairs? hand) 2
+   (pair? hand) 1
+   :else 0))
 
