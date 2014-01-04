2a0236de38b56767e88e10063d6bc8f91c101ea8
Kari Rantanen <kari.rantanen@helsinki.fi>
2014-01-04 22:44:47 +0000

    asdf

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,71 @@
 (ns p-p-p-pokerface)
 
-(defn rank [card]
-  nil)
-
 (defn suit [card]
-  nil)
+  (let [[_ b] card]
+    (str b)))
+
+(def rank-names {
+  \T 10, \J 11, \Q 12,
+  \K 13, \A 14 })
+
+(defn rank [card]
+  (let [[a _] card]
+    (if (Character/isDigit a)
+      (Integer/valueOf (str a))
+      (get rank-names a))))
+
+(defn rank-frequencies [hand]
+  (vals (frequencies (map rank hand))))
+
+(defn has-height? [hand height]
+  (> (.indexOf (rank-frequencies hand) height) -1))
 
 (defn pair? [hand]
-  nil)
+  (has-height? hand 2))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (has-height? hand 3))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (has-height? hand 4))
 
 (defn flush? [hand]
-  nil)
+  (== (apply max (vals (frequencies
+    (map suit hand)))) 5))
 
 (defn full-house? [hand]
-  nil)
+  (and
+    (pair? hand)
+    (three-of-a-kind? hand)))
 
 (defn two-pairs? [hand]
-  nil)
+  (== (count (filter #{2}
+    (rank-frequencies hand))) 2))
 
 (defn straight? [hand]
-  nil)
+  (let [sorted (sort (map rank hand))]
+    (or (= [2 3 4 5 14] sorted)
+      (and
+        (apply < sorted)
+        (== (last sorted) (+ 4 (first sorted)))))))
 
 (defn straight-flush? [hand]
-  nil)
+  (and (straight? hand) (flush? hand)))
+
+(defn high-card? [hand]
+  true)
+
+(def hand-values #{
+  [high-card? 0]
+  [pair? 1]
+  [two-pairs? 2]
+  [three-of-a-kind? 3]
+  [straight? 4]
+  [flush? 5]
+  [full-house? 6]
+  [four-of-a-kind? 7]
+  [straight-flush? 8]})
 
 (defn value [hand]
-  nil)
+  (let [kinds (filter #((first %) hand) hand-values)]
+    (apply max (map second kinds))))
