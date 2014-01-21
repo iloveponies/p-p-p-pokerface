e308216fef13d4cd855f50848dbb88ed807f0294
Gregg Williams <gregg13@innerpaths.net>
2014-01-20 23:41:41 -0800

    moved files from iloveponies to gw666; src/p_p_p_pokerface.cljpasses all tests

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,80 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+  (let [[ch _]  card,
+        rnum    (- (int ch) (int \0))]
+    (cond
+      (< rnum 10)   rnum
+      (= (str ch) "T") 10
+      (= (str ch) "J") 11
+      (= (str ch) "Q") 12
+      (= (str ch) "K") 13
+      (= (str ch) "A") 14)))
 
 (defn suit [card]
-  nil)
+  (let [[_ ch] card]
+    (str ch)))
+
+(defn contains-N? [hand N]
+  (->
+   (vals (frequencies (map rank hand)))
+   (set)
+   (contains? N)))
 
 (defn pair? [hand]
-  nil)
+  (contains-N? hand 2))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (contains-N? hand 3))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (contains-N? hand 4))
 
 (defn flush? [hand]
-  nil)
+  (->
+   (map suit hand)
+   (set)
+   (count)
+   (= 1)))
 
 (defn full-house? [hand]
-  nil)
+  (->
+   (vals (frequencies (map rank hand)))
+   (set)
+   (= #{2 3})))
 
 (defn two-pairs? [hand]
-  nil)
+  (->
+   (vals (frequencies (map rank hand)))
+   (sort)
+   (= [1 2 2])))
 
 (defn straight? [hand]
-  nil)
+  (let [values (sort (map rank hand))
+        min-card (first values)
+
+        values2
+        (sort (replace {14 1} values))
+        min-card2 (first values2)]
+    (or
+     (=
+      values
+      (range min-card (+ min-card 5)))
+     (=
+      values2
+      (range min-card2 (+ min-card2 5))))))
 
 (defn straight-flush? [hand]
-  nil)
+  (and (flush? hand) (straight? hand)))
 
 (defn value [hand]
-  nil)
+  (cond
+   (two-pairs? hand)          2
+   (full-house? hand)         6
+   (pair? hand)               1
+   (three-of-a-kind? hand)    3
+   (straight-flush? hand)     8
+   (straight? hand)           4
+   (flush? hand)              5
+   (four-of-a-kind? hand)     7
+   :else                      0))
