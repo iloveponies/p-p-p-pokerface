7a3c292224d1f2b6583fc56adecd28488edd0f89
David Pathakjee <dpathakjee@gmail.com>
2014-02-23 23:05:15 -0800

    all tests pass

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,59 @@
 (ns p-p-p-pokerface)
 
-(defn rank [card]
-  nil)
+(defn rank [[rank suit]]
+  (let [ranks {\T 10 \J 11 \Q 12 \K 13 \A 14}]
+    (if (Character/isDigit rank)
+      (Character/digit rank 10)
+      (ranks rank))))
 
-(defn suit [card]
-  nil)
+(defn suit [[rank suit]]
+  (str suit))
+
+(defn has-n-of-a-kind [n hand]
+  (->> hand (map rank) frequencies vals (some #{n}) nil? not))
 
 (defn pair? [hand]
-  nil)
+   (has-n-of-a-kind 2 hand))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (has-n-of-a-kind 3 hand))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (has-n-of-a-kind 4 hand))
 
 (defn flush? [hand]
-  nil)
+  (apply = (map suit hand)))
 
 (defn full-house? [hand]
-  nil)
+  (and (pair? hand) (three-of-a-kind? hand)))
 
 (defn two-pairs? [hand]
-  nil)
+  (->> hand (map rank) frequencies vals (filter #{2}) count #{2} nil? not))
 
 (defn straight? [hand]
-  nil)
+  (let [ranks (->> hand (map rank))
+        sorted-ranks (sort ranks)
+        lowest (first sorted-ranks)
+        alternative (sort (replace {14 1 1 14} ranks))
+        alternative-lowest (first alternative)]
+    (or (= sorted-ranks (range lowest (+ 5 lowest)))
+        (= alternative (range alternative-lowest (+ 5 alternative-lowest))))))
 
 (defn straight-flush? [hand]
-  nil)
+  (and (straight? hand)
+       (flush? hand)))
 
 (defn value [hand]
-  nil)
+  (let [values #{[(fn [hand] true) 0]
+                 [pair? 1]
+                 [two-pairs? 2]
+                 [three-of-a-kind? 3]
+                 [straight? 4]
+                 [flush? 5]
+                 [full-house? 6]
+                 [four-of-a-kind? 7]
+                 [straight-flush? 8]}]
+    (apply max
+           (map second
+                (filter #((first %) hand)
+                        values)))))
