37f782dd8b2fda7aacfb9efb90793f5b67fd9a73
Alexey Naydenov <alexey.naydenovREMOVETHIS@gmail.com>
2014-02-25 16:36:02 +0400

    Test passed.

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,57 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+  (let [letter-ranks {\T 10, \J 11, \Q 12, \K 13, \A 14}
+        [r _] card]
+    (if (Character/isDigit r)
+      (Integer/valueOf (str r))
+      (letter-ranks r))))
 
 (defn suit [card]
-  nil)
+  (let [[_ s] card]
+      (str s)))
+
+(defn hand-count [extract hand]
+  (-> extract (map hand) frequencies vals))
+
+(defn max-hand-count [extract hand]
+  (apply max (hand-count extract hand)))
 
 (defn pair? [hand]
-  nil)
+    (> (max-hand-count rank hand) 1))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (> (max-hand-count rank hand) 2))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (> (max-hand-count rank hand) 3))
 
 (defn flush? [hand]
-  nil)
+  (== (max-hand-count suit hand) 5))
 
 (defn full-house? [hand]
-  nil)
+  (= (sort (hand-count rank hand)) [2 3]))
 
 (defn two-pairs? [hand]
-  nil)
+  (= (sort (hand-count rank hand)) [1 2 2]))
 
 (defn straight? [hand]
-  nil)
+  (let [ranks1 (sort (map rank hand))
+        ranks2 (sort (replace {14 1} ranks1))
+        sequential? (fn [xs] (= xs (range (apply min xs) (+ (apply max xs) 1))))]
+    (or (sequential? ranks1) (sequential? ranks2))))
 
 (defn straight-flush? [hand]
-  nil)
+  (and (flush? hand) (straight? hand)))
 
 (defn value [hand]
-  nil)
+  (cond
+   (straight-flush? hand) 8
+   (four-of-a-kind? hand) 7
+   (full-house? hand) 6
+   (flush? hand) 5
+   (straight? hand) 4
+   (three-of-a-kind? hand) 3
+   (two-pairs? hand) 2
+   (pair? hand) 1
+   :else 0))
