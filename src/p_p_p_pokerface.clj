800c88a321c95a6cd1833922ed9dab6f8071182b
Alex Rodioukov <alex.rodioukov@outpace.com>
2014-03-07 16:11:55 -0700

    Poker, n. A game said to be played with cards for some purpose to this lexicographer unknown

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,65 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+  (let [[rank-char _] card
+        rank-map {\T 10 \J 11 \Q 12 \K 13 \A 14}]
+    (if (Character/isDigit rank-char)
+      (Integer/valueOf (str rank-char))
+      (rank-map rank-char))))
 
 (defn suit [card]
-  nil)
+  (let [[_ suit] card]
+    (str suit)))
+
+(defn- hand-frequency [f hand]
+  (vals (frequencies (map f hand))))
+
+(defn- max-frequency [f hand]
+  (apply max (hand-frequency f hand)))
 
 (defn pair? [hand]
-  nil)
+  (= 2 (max-frequency rank hand)))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (= 3 (max-frequency rank hand)))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (= 4 (max-frequency rank hand)))
 
 (defn flush? [hand]
-  nil)
+  (= 5 (max-frequency suit hand)))
 
 (defn full-house? [hand]
-  nil)
+  (= [2 3] (sort (hand-frequency rank hand))))
 
 (defn two-pairs? [hand]
-  nil)
+  (= [1 2 2] (sort (hand-frequency rank hand))))
+
+(defn- straight-range [ranks]
+  (range (first ranks) (inc (last ranks))))
 
 (defn straight? [hand]
-  nil)
+  (let [ranks (sort (map rank hand))
+        ranks-with-ace-as-1 (sort (replace {14 1} ranks))]
+    (or (= ranks (straight-range ranks))
+        (= ranks-with-ace-as-1 (straight-range ranks-with-ace-as-1)))))
 
 (defn straight-flush? [hand]
-  nil)
+  (and (straight? hand) (flush? hand)))
+
+(defn high-card? [hand]
+  true)
 
 (defn value [hand]
-  nil)
+  (let [checkers #{[high-card? 0]
+                   [pair? 1]
+                   [two-pairs? 2]
+                   [three-of-a-kind? 3]
+                   [straight? 4]
+                   [flush? 5]
+                   [full-house? 6]
+                   [four-of-a-kind? 7]
+                   [straight-flush? 8]}
+        matches (filter (fn [checker] ((first checker) hand)) checkers)
+        values (map second matches)]
+    (apply max values)))
