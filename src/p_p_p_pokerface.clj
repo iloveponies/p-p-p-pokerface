08fc3d5fdcd8d60e6b12dabfa921b4b88c5c87d7
Rick DeNatale <rick@outpace.com>
2014-03-15 16:55:29 -0400

    all passing

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,63 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+  (let [[fst _] card]
+    ({\1 1 \2 2 \3 3 \4 4 \5 5 \6 6 \7 7
+      \8 8 \9 9 \T 10 \J 11 \Q 12 \K 13 \A 14}  fst)))
 
 (defn suit [card]
-  nil)
+  (let [[_ snd] card]
+    (str snd)))
+
+(defn ranks [hand]
+  (map rank hand))
+
+(defn suits [hand]
+  (map suit hand))
+
+(defn rank-frequencies [hand]
+  (frequencies (ranks hand)))
+
+(defn has-tuple [count hand]
+  (not (= nil (some #{count} (vals (rank-frequencies hand))))))
 
 (defn pair? [hand]
-  nil)
+  (has-tuple 2 hand))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (has-tuple 3 hand))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (has-tuple 4 hand))
 
 (defn flush? [hand]
-  nil)
+  (= 1 (count (set (suits hand)))))
 
 (defn full-house? [hand]
-  nil)
+  (= [2 3] (sort (vals (rank-frequencies hand)))))
 
 (defn two-pairs? [hand]
-  nil)
+  (= [1 2 2] (sort (vals (rank-frequencies hand)))))
 
 (defn straight? [hand]
-  nil)
+  (let [sorted-ranks (sort (ranks hand))
+        first-rank (first sorted-ranks)
+        shifted-ranks (map (fn [r] (- r first-rank)) sorted-ranks)]
+    (or (= [0 1 2 3 4] shifted-ranks)
+        (= [0 1 2 3 12] shifted-ranks))))
 
 (defn straight-flush? [hand]
-  nil)
+  (and (straight? hand)
+       (flush? hand)))
 
 (defn value [hand]
-  nil)
+  (cond
+   (straight-flush? hand) 8
+   (four-of-a-kind? hand) 7
+   (full-house? hand) 6
+   (flush? hand) 5
+   (straight? hand) 4
+   (three-of-a-kind? hand) 3
+   (two-pairs? hand) 2
+   (pair? hand) 1
+   :else 0))
