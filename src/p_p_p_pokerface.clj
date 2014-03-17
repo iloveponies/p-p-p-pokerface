9dcbc81b5d30069431719ca7f55c36d3f034e519
Timo Olsthoorn <timoolsthoorn@gmail.com>
2014-03-17 19:57:40 +0100

    Reimplementation of some of the functions to make it more readable and more efficient

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,70 +1,88 @@
 (ns p-p-p-pokerface)
 
-(defn high-card? [hand]
-  true)
+;; Domain functions
+(defn card [a-card]
+  a-card)
+
+(defn hand [a-hand]
+  a-hand)
 
 (defn rank [card]
-  (let [[rankchar _] card
-        values {\T 10 \J 11 \Q 12 \K 13 \A 14}
-        rank (if (Character/isDigit rankchar)
-               (Integer/valueOf (str rankchar))
-               (get values rankchar))]
-    rank))
+  (let [[rank _] card
+        rank-values {\1 1  \2 2  \3 3  \4 4
+                     \5 5  \6 6  \7 7  \8 8
+                     \9 9 \T 10 \J 11 \Q 12
+                     \K 13 \A 14}]
+    (rank-values rank)))
 
 (defn suit [card]
   (let [[_ suit] card]
     (str suit)))
 
-(defn pair?
-  [hand]
-  (= (count (frequencies (map rank hand)))
+;; Helper functions
+(defn frequencies-of-rank [hand]
+  (frequencies (map rank hand)))
+
+(defn frequencies-of-suit [hand]
+  (frequencies (map suit hand)))
+
+;; Hand functions
+(defn pair? [hand]
+  ;; If there are only 4 elements left after grouping
+  ;; the ranks there must be a pair
+  (= (count (frequencies-of-rank hand))
      4))
 
 (defn three-of-a-kind? [hand]
-  (= (apply max(vals (frequencies (map rank hand))))
+  ;; If there are only 3 elements left after grouping
+  ;; the ranks there must be a set (three-of-a-kind)
+  (= (apply max(vals (frequencies-of-rank hand)))
      3))
 
 (defn four-of-a-kind? [hand]
-  (= (apply max(vals (frequencies (map rank hand))))
+  ;; If there is a tuple with a value of 4 there was a
+  ;; four-of-a-kind
+  (= (apply max(vals (frequencies-of-rank hand)))
      4))
 
 (defn flush? [hand]
-  (= (count (frequencies (map suit hand)))
+  ;; If all cards in the hand are of one suit there was
+  ;; a flush
+  (= (count (frequencies-of-suit hand))
      1))
 
 (defn full-house? [hand]
-  (= (sort (vals (frequencies (map rank hand))))
+  ;; If the resulting tuple values of a frequency count
+  ;; are equal to [2 3] there was a fullhouse
+  (= (sort (vals (frequencies-of-rank hand)))
      (seq [2 3])))
 
 (defn two-pairs? [hand]
-  (let [hand-values (vals (frequencies (map rank hand)))]
     (or
-      (= (count (filter (fn [x] (= x 2)) hand-values))
-         2)
-      (= (count (filter (fn [x] (= x 4)) hand-values))
-         1))))
+      (= (sort (vals (frequencies-of-rank hand)))
+         (seq [1 2 2]))
+      (four-of-a-kind? hand)))
 
 (defn straight? [hand]
-  (let [hand-values (sort (map rank hand))
-        transformed-hand-values (if (= (apply min hand-values) 2)
-                                  (sort (replace {14 1} hand-values))
-                                  (sort hand-values))
-        lowest-hand-value (apply min transformed-hand-values)
-        straight-range (range lowest-hand-value (+ lowest-hand-value 5))]
-    (= transformed-hand-values straight-range)))
+  (let[sorted-rank (sort (keys (frequencies-of-rank hand)))
+       first-rank (first sorted-rank)
+       shifted-rank (map (fn [x] (- x first-rank)) sorted-rank)]
+    (or (= shifted-rank [0 1 2 3 4])
+        (= shifted-rank [0 1 2 3 12]))))
 
 (defn straight-flush? [hand]
   (and (flush? hand)
        (straight? hand)))
 
+;; Value function
 (defn value [hand]
-  (let [checkers #{[high-card? 0]  [pair? 1]
-                   [two-pairs? 2]  [three-of-a-kind? 3]
-                   [straight? 4]   [flush? 5]
-                   [full-house? 6] [four-of-a-kind? 7]
-                   [straight-flush? 8]}
-        eval-hand-fn (fn [x] (vector (second x) ((first x) hand)))
-        eval-hand (map eval-hand-fn checkers)
-        filtered-eval-hand (filter (fn [x] (not (= (second x) false))) eval-hand)
-        hand-values (sort (map (fn [x] (first x)) filtered-eval-hand))]
-    (apply max hand-values)))
+  (cond
+    (straight-flush? hand) 8
+    (four-of-a-kind? hand) 7
+    (full-house? hand) 6
+    (flush? hand) 5
+    (straight? hand) 4
+    (three-of-a-kind? hand) 3
+    (two-pairs? hand) 2
+    (pair? hand) 1
+    :else 0))
