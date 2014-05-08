fe06a8abab7e593510a01b726af1f1754998c097
James Chochlinski <jchochlinski@xpzen.com>
2014-05-08 07:46:15 -0400

    to exercise 10

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -12,9 +12,12 @@
       (Integer/valueOf (str rank))
       (replacements rank))))
 
+(defn rank-hand [hand]
+  (map rank hand))
+
 ; private
 (defn rank-count [hand]
-  (frequencies (map rank hand)))
+  (frequencies (rank-hand hand)))
 
 ; private
 (defn rank-counts [hand]
@@ -41,16 +44,27 @@
   (apply = (map suit hand)))
 
 (defn full-house? [hand]
-  nil)
+  (= [3 2] (rank-counts hand)))
 
 (defn two-pairs? [hand]
-  nil)
+  (= [2 2 1] (rank-counts hand)))
 
-(defn straight? [hand]
-  nil)
+(defn straight-ranked-hand? [ranked-hand]
+  (let [sorted-hand (sort ranked-hand)
+        min-card (first sorted-hand)]
+    (=
+     (range min-card (+ min-card 5))
+     sorted-hand)))
+
+(defn straight? [hand] 
+  (let [ranked-hand (rank-hand hand)]
+    (or (straight-ranked-hand? ranked-hand)
+        (straight-ranked-hand? (replace {14 1} ranked-hand)))))
 
 (defn straight-flush? [hand]
-  nil)
+  (and
+   (straight? hand)
+   (flush? hand)))
 
 (defn value [hand]
   nil)
