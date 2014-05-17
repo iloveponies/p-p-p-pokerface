fa43168f8540b75bb63f5ffd63bb8fe5e3ee6421
kaz-yos <kazky@mac.com>
2014-05-17 11:03:53 -0400

    deleted test codes, all midje tests passed

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -28,13 +28,13 @@
       (Integer/valueOf (str c-rank))
       (get replace-map c-rank))))
 ;;
-(rank "2H") ;=> 2
-(rank "4S") ;=> 4
-(rank "TS") ;=> 10
-(rank "JS") ;=> 11
-(rank "QS") ;=> 12
-(rank "KS") ;=> 13
-(rank "AS") ;=> 14
+;; (rank "2H") ;=> 2
+;; (rank "4S") ;=> 4
+;; (rank "TS") ;=> 10
+;; (rank "JS") ;=> 11
+;; (rank "QS") ;=> 12
+;; (rank "KS") ;=> 13
+;; (rank "AS") ;=> 14
 
 
 ;; Exercise 1
@@ -44,26 +44,26 @@
 ;; Get the second letter
 (defn suit [card]
   (str (second card)))
-(suit "2H") ;=> "H"
-(suit "2D") ;=> "D"
-(suit "2C") ;=> "C"
-(suit "3S") ;=> "S"
+;; (suit "2H") ;=> "H"
+;; (suit "2D") ;=> "D"
+;; (suit "2C") ;=> "C"
+;; (suit "3S") ;=> "S"
 
 
 ;; hands
-(def high-seven                   ["2H" "3S" "4C" "5C" "7D"])
-(def pair-hand                    ["2H" "2S" "4C" "5C" "7D"])
-(def two-pairs-hand               ["2H" "2S" "4C" "4D" "7D"])
-(def three-of-a-kind-hand         ["2H" "2S" "2C" "4D" "7D"])
-(def four-of-a-kind-hand          ["2H" "2S" "2C" "2D" "7D"])
-(def straight-hand                ["2H" "3S" "6C" "5D" "4D"])
-(def low-ace-straight-hand        ["2H" "3S" "4C" "5D" "AD"])
-(def high-ace-straight-hand       ["TH" "AS" "QC" "KD" "JD"])
-(def flush-hand                   ["2H" "4H" "5H" "9H" "7H"])
-(def full-house-hand              ["2H" "5D" "2D" "2C" "5S"])
-(def straight-flush-hand          ["2H" "3H" "6H" "5H" "4H"])
-(def low-ace-straight-flush-hand  ["2D" "3D" "4D" "5D" "AD"])
-(def high-ace-straight-flush-hand ["TS" "AS" "QS" "KS" "JS"])
+;; (def high-seven                   ["2H" "3S" "4C" "5C" "7D"])
+;; (def pair-hand                    ["2H" "2S" "4C" "5C" "7D"])
+;; (def two-pairs-hand               ["2H" "2S" "4C" "4D" "7D"])
+;; (def three-of-a-kind-hand         ["2H" "2S" "2C" "4D" "7D"])
+;; (def four-of-a-kind-hand          ["2H" "2S" "2C" "2D" "7D"])
+;; (def straight-hand                ["2H" "3S" "6C" "5D" "4D"])
+;; (def low-ace-straight-hand        ["2H" "3S" "4C" "5D" "AD"])
+;; (def high-ace-straight-hand       ["TH" "AS" "QC" "KD" "JD"])
+;; (def flush-hand                   ["2H" "4H" "5H" "9H" "7H"])
+;; (def full-house-hand              ["2H" "5D" "2D" "2C" "5S"])
+;; (def straight-flush-hand          ["2H" "3H" "6H" "5H" "4H"])
+;; (def low-ace-straight-flush-hand  ["2D" "3D" "4D" "5D" "AD"])
+;; (def high-ace-straight-flush-hand ["TS" "AS" "QS" "KS" "JS"])
 
 
 ;; Exercise 3
@@ -77,8 +77,8 @@
     ;; Check max, if 2+, then true, otherwise false.
     (>= (reduce max (vals freq-map)) 2)))
 ;;
-(pair? pair-hand)  ;=> true
-(pair? high-seven) ;=> false
+;; (pair? pair-hand)  ;=> true
+;; (pair? high-seven) ;=> false
 
 
 ;; Exercise 4
@@ -91,8 +91,8 @@
   (let [freq-map (frequencies (map rank hand))]
     ;; Check max, if 3+, then true, otherwise false.
     (>= (reduce max (vals freq-map)) 3)))
-(three-of-a-kind? two-pairs-hand)       ;=> false
-(three-of-a-kind? three-of-a-kind-hand) ;=> true
+;; (three-of-a-kind? two-pairs-hand)       ;=> false
+;; (three-of-a-kind? three-of-a-kind-hand) ;=> true
 
 
 ;; Exercise 5
@@ -104,8 +104,8 @@
     ;; Check max, if 4+, then true, otherwise false.
     (>= (reduce max (vals freq-map)) 4)))
 ;;
-(four-of-a-kind? two-pairs-hand)      ;=> false
-(four-of-a-kind? four-of-a-kind-hand) ;=> true
+;; (four-of-a-kind? two-pairs-hand)      ;=> false
+;; (four-of-a-kind? four-of-a-kind-hand) ;=> true
 
 
 ;; Exercise 6
@@ -117,8 +117,8 @@
   ;; if all suits are equal that is flush
   (apply = (map suit hand)))
 ;;
-(flush? pair-hand)  ;=> false
-(flush? flush-hand) ;=> true)
+;; (flush? pair-hand)  ;=> false
+;; (flush? flush-hand) ;=> true)
 
 
 ;; Exercise 7
@@ -131,8 +131,8 @@
     ;; If a pair and 3 of kind, then true, otherwise false
     (= #{2 3} (set (vals freq-map)))))
 ;;
-(full-house? three-of-a-kind-hand) ;=> false
-(full-house? full-house-hand)      ;=> true
+;; (full-house? three-of-a-kind-hand) ;=> false
+;; (full-house? full-house-hand)      ;=> true
 
 
 ;; Exercise 8
@@ -150,9 +150,9 @@
      ;; four-of-a-kind is two parirs
      (four-of-a-kind? hand))))
 ;;
-(two-pairs? two-pairs-hand)      ;=> true
-(two-pairs? pair-hand)           ;=> false
-(two-pairs? four-of-a-kind-hand) ;=> true
+;; (two-pairs? two-pairs-hand)      ;=> true
+;; (two-pairs? pair-hand)           ;=> false
+;; (two-pairs? four-of-a-kind-hand) ;=> true
 
 
 ;; Exercise 9
@@ -175,11 +175,11 @@
      (= sorted-hand-a-as-1  correspond-seq-1)
      (= sorted-hand-a-as-14 correspond-seq-14))))
 ;;
-(straight? two-pairs-hand)             ;=> false
-(straight? straight-hand)              ;=> true
-(straight? low-ace-straight-hand)      ;=> true
-(straight? ["2H" "2D" "3H" "4H" "5H"]) ;=> false
-(straight? high-ace-straight-hand)     ;=> true
+;; (straight? two-pairs-hand)             ;=> false
+;; (straight? straight-hand)              ;=> true
+;; (straight? low-ace-straight-hand)      ;=> true
+;; (straight? ["2H" "2D" "3H" "4H" "5H"]) ;=> false
+;; (straight? high-ace-straight-hand)     ;=> true
 
 
 ;; Exercise 10
@@ -192,11 +192,11 @@
    (flush?    hand)
    (straight? hand)))
 ;;
-(straight-flush? straight-hand)                ;=> false
-(straight-flush? flush-hand)                   ;=> false
-(straight-flush? straight-flush-hand)          ;=> true
-(straight-flush? low-ace-straight-flush-hand)  ;=> true
-(straight-flush? high-ace-straight-flush-hand) ;=> true
+;; (straight-flush? straight-hand)                ;=> false
+;; (straight-flush? flush-hand)                   ;=> false
+;; (straight-flush? straight-flush-hand)          ;=> true
+;; (straight-flush? low-ace-straight-flush-hand)  ;=> true
+;; (straight-flush? high-ace-straight-flush-hand) ;=> true
 
 
 
@@ -218,21 +218,21 @@
    (pair?            hand) 1
    :else                   0))
 ;;
-(value high-seven)           ;=> 0
-(value pair-hand)            ;=> 1
-(value two-pairs-hand)       ;=> 2
-(value three-of-a-kind-hand) ;=> 3
-(value straight-hand)        ;=> 4
-(value flush-hand)           ;=> 5
-(value full-house-hand)      ;=> 6
-(value four-of-a-kind-hand)  ;=> 7
-(value straight-flush-hand)  ;=> 8
+;; (value high-seven)           ;=> 0
+;; (value pair-hand)            ;=> 1
+;; (value two-pairs-hand)       ;=> 2
+;; (value three-of-a-kind-hand) ;=> 3
+;; (value straight-hand)        ;=> 4
+;; (value flush-hand)           ;=> 5
+;; (value full-house-hand)      ;=> 6
+;; (value four-of-a-kind-hand)  ;=> 7
+;; (value straight-flush-hand)  ;=> 8
 ;;
 ;; The example below checks for all patters, cond will short-circuit, and is more efficient.
 ;;
 ;; It might be helpful to add a checker (high-card? hand):
-(defn high-card? [hand]
-  true) ; All hands have a high card.
+;; (defn high-card? [hand]
+;;   true) ; All hands have a high card.
 ;; You can create a sequence of [matcher value] pairs like so:
 ;; (let [checkers #{[high-card? 0]  [pair? 1]
 ;;                  [two-pairs? 2]  [three-of-a-kind? 3]
@@ -241,5 +241,5 @@
 ;;                  [straight-flush? 8]}]
 ;;   ...)
 ;; You can now use filter, map and apply max to get the highest value that a hand has. The function second can be useful. Remember to use let to give the intermediate results readable names.
-(second [:i :am :a :sequence]) ;=> :am
-(second [two-pairs? 2])        ;=> 2
+;; (second [:i :am :a :sequence]) ;=> :am
+;; (second [two-pairs? 2])        ;=> 2
