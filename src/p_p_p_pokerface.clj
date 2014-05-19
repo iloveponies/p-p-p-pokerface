56e5714dca6010f84370aa095a24f19b0c855a88
Andrey Bakuta <anb@anahoret.com>
2014-05-19 21:21:27 +0300

    WIP

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,10 +1,24 @@
 (ns p-p-p-pokerface)
 
-(defn rank [card]
-  nil)
+(def ranks {\2 2
+            \3 3
+            \4 4
+            \5 5
+            \6 6
+            \7 7
+            \8 8
+            \9 9
+            \T 10
+            \J 11
+            \Q 12
+            \K 13
+            \A 14})
 
-(defn suit [card]
-  nil)
+(defn rank [[r _]]
+  (ranks r))
+
+(defn suit [[_ s]]
+  (str s))
 
 (defn pair? [hand]
   nil)
