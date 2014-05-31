c1e0fe8fede36f0756dc2ea5e64e85ea08472be6
Jeremy Sellars <jeremy.sellars@softekinc.com>
2014-05-31 12:11:57 -0500

    Solved!

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,110 @@
 (ns p-p-p-pokerface)
 
+(def rank-of-char
+ {\2 2
+  \3 3
+  \4 4
+  \5 5
+  \6 6
+  \7 7
+  \8 8
+  \9 9
+  \T 10
+  \J 11
+  \Q 12
+  \K 13
+  \A 14})
+
+(def rank-of-char-low
+ {\A 1
+  \2 2
+  \3 3
+  \4 4
+  \5 5
+  \6 6
+  \7 7
+  \8 8
+  \9 9
+  \T 10
+  \J 11
+  \Q 12
+  \K 13})
+
 (defn rank [card]
-  nil)
+  (let [[r s] card]
+    (if (Character/isDigit r) (Integer/valueOf (str r)))
+    (rank-of-char r)))
+
+(defn rank-low [card]
+  (let [[r s] card]
+    (if (Character/isDigit r) (Integer/valueOf (str r)))
+    (rank-of-char-low r)))
 
 (defn suit [card]
-  nil)
+  (str (second card)))
+
+(defn- of-a-kind-frequencies [hand]
+  (let [ranks (map rank hand)]
+    (-> ranks
+        frequencies
+        vals
+        frequencies)))
+
+(defn- of-a-kind [hand n]
+  (-> hand
+      of-a-kind-frequencies
+      (contains? n)))
 
 (defn pair? [hand]
-  nil)
+  (of-a-kind hand 2))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (of-a-kind hand 3))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (of-a-kind hand 4))
 
 (defn flush? [hand]
-  nil)
+  (->> hand
+       (map suit)
+       frequencies
+       count
+       (== 1)))
 
 (defn full-house? [hand]
-  nil)
+  (let [freqs (of-a-kind-frequencies hand)]
+    (and (contains? freqs 3)
+         (contains? freqs 2))))
 
 (defn two-pairs? [hand]
-  nil)
+  (let [freqs (of-a-kind-frequencies hand)
+        pair-count (get freqs 2)]
+    (== 2 (or pair-count 0))))
 
-(defn straight? [hand]
-  nil)
+(defn straight?
+  ([hand rank]
+   (let [ranks (map rank hand)]
+     (-> ranks
+         sort
+         (#(= % (range (first %) (+ (first %) 5)))))))
+
+  ([hand]
+    (or (straight? hand rank)
+        (straight? hand rank-low))))
 
 (defn straight-flush? [hand]
-  nil)
+  (boolean
+    (and (straight? hand)
+         (flush? hand))))
 
 (defn value [hand]
-  nil)
+  (condp #(%1 %2) hand
+    straight-flush?  8
+    four-of-a-kind?  7
+    full-house?      6
+    flush?           5
+    straight?        4
+    three-of-a-kind? 3
+    two-pairs?       2
+    pair?            1
+                     0))
