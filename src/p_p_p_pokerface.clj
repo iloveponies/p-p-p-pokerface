ea200f862dcd8d3196b7036811eb3f6e3ca9b55f
Kyle <kyle@tyrfingr.is>
2014-05-30 21:33:58 -0700

    Mum mum mum mah

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,98 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+  (let [[face _] card
+        royalty {"T" 10 "J" 11 "Q" 12 "K" 13 "A" 14}]
+    (cond
+     (Character/isDigit face) (Integer/valueOf (str face))
+     :else (get royalty (str face)))))
 
 (defn suit [card]
-  nil)
+  (let [[_ suit] card]
+    (str suit)))
 
 (defn pair? [hand]
-  nil)
+  (not
+   (empty?
+    (filter (fn [count]
+              (> (second count) 1))
+            (frequencies
+             (map rank hand))))))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (not
+   (empty?
+    (filter (fn [count]
+              (= (second count) 3))
+            (frequencies
+             (map rank hand))))))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (not
+   (empty?
+    (filter (fn [count]
+              (= (second count) 4))
+            (frequencies
+             (map rank hand))))))
 
 (defn flush? [hand]
-  nil)
+  (= 1
+     (count
+      (frequencies
+       (map suit hand)))))
 
 (defn full-house? [hand]
-  nil)
+  (let [frequency (mapv second
+                        (frequencies (map rank hand)))]
+    (= [2 3] (sort frequency))))
 
 (defn two-pairs? [hand]
-  nil)
+  (or
+   (four-of-a-kind? hand)
+   (= 2
+      (count
+       (filter
+        (fn [count]
+          (= 2 (second count)))
+        (frequencies
+         (map rank hand)))))))
 
 (defn straight? [hand]
-  nil)
+  (let [sorted (sort (map rank hand))
+        start  (apply min sorted)
+        has-ace? (= (apply max sorted) 14)]
+    (if
+     (= (range start
+               (+ 5 start))
+        sorted) true
+        (if has-ace?
+          (let [alt-sorted (sort (replace {14 1} sorted))]
+            (= (range 1 6)
+               alt-sorted))
+          false))))
 
 (defn straight-flush? [hand]
-  nil)
+  (and (straight? hand)
+       (flush?    hand)))
+
+(defn high-card? [hand] true)
 
 (defn value [hand]
-  nil)
+  (let [checkers #{[high-card? 0]  [pair? 1]
+                   [two-pairs? 2]  [three-of-a-kind? 3]
+                   [straight? 4]   [flush? 5]
+                   [full-house? 6] [four-of-a-kind? 7]
+                   [straight-flush? 8]}]
+    (apply max
+     (sort
+      (map (fn [checker]
+             (if ((first checker) hand)
+               (second checker)
+               0))
+           checkers)))))
+
+(defn hand [a-hand]
+  a-hand)
+
+(defn card [a-card]
+  a-card)
