5e7c64ddcb28f4f67f49de76b78fe560b23160c1
J Manuel Valladares Pernas <jmvalladares@buguroo.com>
2014-06-26 00:51:29 +0200

    Working pokerface

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,66 @@
 (ns p-p-p-pokerface)
 
+(def replacements { \T 10, \J 11, \Q 12, \K 13, \A 14 })
+
 (defn rank [card]
-  nil)
+  (let [[fst _] card]
+    (if (Character/isDigit fst)
+      (Integer/valueOf (str fst))
+      (get replacements fst))))
 
 (defn suit [card]
-  nil)
+  (let [[_ snd] card]
+    (str snd)))
+
+(defn- ranks [hand]
+  (map rank hand))
+
+(defn- frequencies-of-rank [hand]
+  (vals (frequencies (ranks hand))))
+
+(defn- max-rank [hand]
+  (apply max (frequencies-of-rank hand)))
 
 (defn pair? [hand]
-  nil)
+  (= (max-rank hand) 2))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (= (max-rank hand) 3))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (= (max-rank hand) 4))
 
 (defn flush? [hand]
-  nil)
+  (apply = (map suit hand)))
 
 (defn full-house? [hand]
-  nil)
+  (= '(2 3) (sort (frequencies-of-rank hand))))
 
 (defn two-pairs? [hand]
-  nil)
+  (= '(1 2 2) (sort (frequencies-of-rank hand))))
 
 (defn straight? [hand]
-  nil)
+  (let [hand-ranks (ranks hand)]
+    (letfn [(aux-straight? [hr]
+      (let [min-rank (apply min hr)
+            max-rank (apply max hr)]
+        (= (range min-rank (inc max-rank)) (sort hr))))]
+      (or (aux-straight? hand-ranks) (aux-straight? (replace {14 1} hand-ranks))))))
 
 (defn straight-flush? [hand]
-  nil)
+  (and (flush? hand) (straight? hand)))
+
+(defn high-card? [hand]
+  true)
 
 (defn value [hand]
-  nil)
+  (let [checkers #{[high-card? 0]
+                   [pair? 1]
+                   [two-pairs? 2]
+                   [three-of-a-kind? 3]
+                   [straight? 4]
+                   [flush? 5]
+                   [full-house? 6]
+                   [four-of-a-kind? 7]
+                   [straight-flush? 8]}]
+    (apply max (filter identity (map (fn [[f? v]] (if (f? hand) v nil)) checkers)))))
