dcdba031dee6bc2b71cc08ad271f5ab9d89721a5
Raimon Grau <raimonster@gmail.com>
2014-06-29 03:55:22 +0200

    popopopokafeis

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,28 +1,54 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+  (let [[rnk _] card]
+    (let [maps {"1" 1 "2" 2 "3" 3
+                "4" 4 "5" 5 "6" 6
+                "7" 7 "8" 8 "9" 9
+                "T" 10 "J" 11 "Q" 12
+                "K" 13 "A" 14}]
+      (get maps (str rnk)))))
 
 (defn suit [card]
-  nil)
+  (let [[_ suitt] card] (str suitt)))
+
+(defn frequency [hand]
+  (if (empty? hand)
+    {}
+    (let [card (first hand)
+          partial-freq (frequency (rest hand))
+          inc-or-one (fnil inc 0)
+          ]
+      (assoc partial-freq (rank card)
+             (inc-or-one (get partial-freq (rank card)))))))
+
+(defn pairs [hand]
+  (filter (fn [x] ( =  (last x) 2))
+        (frequency hand)))
 
 (defn pair? [hand]
-  nil)
+  (pos? (count (pairs hand))))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (boolean
+   (some (fn [x] ( =  (last x) 3))
+         (frequency hand))))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (boolean
+   (some (fn [x] ( =  (last x) 4))
+         (frequency hand))))
 
 (defn flush? [hand]
-  nil)
+  (let [kind (suit (first hand))]
+   (every? (fn [x] ( = (suit x) kind)) hand)))
 
 (defn full-house? [hand]
-  nil)
+  (and (pair? hand)
+       (three-of-a-kind? hand)))
 
 (defn two-pairs? [hand]
-  nil)
+  ( = 2 (count (pairs hand))))
 
 (defn straight? [hand]
   nil)
