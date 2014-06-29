5511d5255f9e9163fb67e989dbc7ed20500daaa2
Adam Washington <rprospero@gmail.com>
2014-06-29 14:16:48 +0100

    First attempt at solution

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,72 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+  (let [[r s] card]
+    (if (Character/isDigit r)
+      (Integer/valueOf (str r))
+      (cond
+       (= r \T) 10
+       (= r \J) 11
+       (= r \Q) 12
+       (= r \K) 13
+       (= r \A) 14
+       true nil))))
 
 (defn suit [card]
-  nil)
+  (let [[r s] card]
+    (str s)))
 
 (defn pair? [hand]
-  nil)
+  (== 2 (apply max (vals (frequencies (map rank hand))))))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (== 3 (apply max (vals (frequencies (map rank hand))))))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (== 4 (apply max (vals (frequencies (map rank hand))))))
 
 (defn flush? [hand]
-  nil)
+  (let [[count] (vals (frequencies (map suit hand)))]
+    (== 5 count)))
+    
 
 (defn full-house? [hand]
-  nil)
+  (let [[a b] (vals (frequencies (map rank hand)))]
+    (or (and (== a 3) (== b 2))
+        (and (== a 2) (== b 3)))))
 
 (defn two-pairs? [hand]
-  nil)
+  (let [freqs (vals (frequencies (map rank hand)))]
+    (or
+     (and (== 3 (count freqs))
+          (== 2 (apply max freqs)))
+     (== 4 (apply max freqs)))))
 
 (defn straight? [hand]
-  nil)
+  (let [freq (frequencies (map rank hand))
+        low-freq (frequencies (replace {14 1} (map rank hand)))
+        test-straight (fn [freq] 
+                        (and
+                         (== (count freq) 5)
+                         (== 4 (- (apply max (keys freq))
+                                  (apply min (keys freq))))))]
+    (or
+     (test-straight freq)
+     (test-straight low-freq))))
+
 
 (defn straight-flush? [hand]
-  nil)
+  (and (straight? hand)
+       (flush? hand)))
 
 (defn value [hand]
-  nil)
+  (cond
+   (straight-flush? hand) 8
+   (four-of-a-kind? hand) 7
+   (full-house? hand) 6
+   (flush? hand) 5
+   (straight? hand) 4
+   (three-of-a-kind? hand) 3
+   (two-pairs? hand) 2
+   (pair? hand) 1
+   true 0))
