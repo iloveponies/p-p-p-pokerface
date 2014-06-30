e54ee86637129efe24dc376cf81b818b357ac9f8
Marc <marc.novel@gmail.com>
2014-06-30 13:40:49 +0200

    flush is not correct yet

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,95 @@
 (ns p-p-p-pokerface)
 
+
+(def replacements {\T 10, \J 11, \Q 12, \K 13, \A 14})
+
 (defn rank [card]
-  nil)
+  (let [[fst snd] card]
+    (cond
+     (Character/isDigit fst) (Integer/valueOf (str fst))
+     :else (Integer/valueOf (str (replacements fst))))))
 
 (defn suit [card]
-  nil)
+  (let [[_ snd] card]
+  (str snd)))
 
 (defn pair? [hand]
-  nil)
+  (cond
+  (= (apply max
+            (vals (frequencies
+                   (map rank hand))))2) true
+   :else false))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (cond
+  (= (apply max
+            (vals (frequencies
+                   (map rank hand))))3) true
+   :else false))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (cond
+  (= (apply max
+            (vals (frequencies
+                   (map rank hand))))4) true
+   :else false))
 
 (defn flush? [hand]
-  nil)
+  (cond
+  (= (apply max
+            (vals (frequencies
+                   (map suit hand))))5) true
+  :else false))
 
 (defn full-house? [hand]
-  nil)
+  (cond
+  (= (sort
+            (vals (frequencies
+                   (map rank hand))))
+           (range 2 (+ 2 2))) true
+   :else false))
 
 (defn two-pairs? [hand]
-  nil)
+  (cond
+  (= (sort
+            (vals (frequencies
+                   (map rank hand))))
+            [1 2 2] ) true
+   (= (sort
+            (vals (frequencies
+                   (map rank hand))))
+            [1 4] ) true
+   :else false))
 
 (defn straight? [hand]
-  nil)
+   (cond
+   (= (sort
+            (map rank hand))
+      (range 2 (+ 2 5))) true
+    (= (sort
+        (vals (frequencies
+            (map rank hand))))
+      (take 5(cycle [1]))) true
+   :else false))
 
 (defn straight-flush? [hand]
-  nil)
+  (cond
+   (and (straight? hand) (flush? hand)) true
+   :else false))
+
+(defn high-card? [hand]
+  true)
+
 
 (defn value [hand]
-  nil)
+  (let [checkers #{[high-card? 0]  [pair? 1]
+                 [two-pairs? 2]  [three-of-a-kind? 3]
+                 [straight? 4]   [flush? 5]
+                 [full-house? 6] [four-of-a-kind? 7]
+                 [straight-flush? 8]}
+        filtered (filter (fn [check] ((first check ) hand ))checkers)]
+   ;(filter (fn[x] map checkers x)hand)
+    (apply max (map second filtered))
+  ))
+
+;http://java.dzone.com/articles/clojure-handling-state
