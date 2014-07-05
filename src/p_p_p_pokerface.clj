ca297334384b947b5012e6fa95cba85d9928cd50
David Hovemeyer <david.hovemeyer@gmail.com>
2014-07-05 15:31:06 -0400

    implemented three-of-a-kind?, four-of-a-kind?, and full-house?
    Factored pair? into a generic has-combo-of-size? function.

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -11,31 +11,29 @@
 (defn suit [[card-rank card-suit]]
   (str card-suit))
 
-;(defn has-combo-of-size? [hand size]
-;  )
-
-(defn pair? [hand]
+(defn has-combo-of-size? [hand size]
   (let [ranks (map rank hand)
         rank-freqs-map (frequencies ranks)    ; ranks->frequency count
         rank-freqs (vals rank-freqs-map)      ; just frequencies of ranks
         combos-freqs (frequencies rank-freqs) ; frequencies of rank combinations
         ]
-    ; A pair is a combination of exactly 2 cards with the same rank
-    (contains? (set (keys combos-freqs)) 2)))
-        
-    
+    ; See if there is a combo of exactly size
+    (contains? (set (keys combos-freqs)) size)))
+
+(defn pair? [hand]
+  (has-combo-of-size? hand 2))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (has-combo-of-size? hand 3))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (has-combo-of-size? hand 4))
 
 (defn flush? [hand]
   nil)
 
 (defn full-house? [hand]
-  nil)
+  (and (pair? hand) (three-of-a-kind? hand)))
 
 (defn two-pairs? [hand]
   nil)
