a78c059637e5c17aa4a0b2e8372924da43c69b4d
Stas Turlo <stanislav.turlo@rightscale.com>
2014-07-05 23:15:53 +0300

    Complete exercises

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,65 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+  (let [[r s] card
+        replacement {\T 10 \J 11 \Q 12 \K 13 \A 14}]
+    (if (Character/isDigit r)
+      (Integer/valueOf (str r))
+      (replacement r))))
 
 (defn suit [card]
-  nil)
+  (let [[r s] card]
+    (str s)))
 
 (defn pair? [hand]
-  nil)
+  (contains? (set (vals (frequencies (map rank hand)))) 2))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (contains? (set (vals (frequencies (map rank hand)))) 3))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (contains? (set (vals (frequencies (map rank hand)))) 4))
 
 (defn flush? [hand]
-  nil)
+  (contains? (set (vals (frequencies (map suit hand)))) 5))
 
 (defn full-house? [hand]
-  nil)
+  (and (pair? hand)
+       (three-of-a-kind? hand)))
 
 (defn two-pairs? [hand]
-  nil)
+  (let [ranks (vals (frequencies (map rank hand)))
+        pairs (filter (fn [x] (= 2 x)) ranks)]
+    (= 2 (count pairs))))
+
+(defn seqiential? [col]
+  (let [col (sort col)
+        f (first col)]
+    (= col (range f (+ f 5)))))
+
+(defn seqiential-hand? [hand]
+  (let [ranks (map rank hand)]
+    (or (seqiential? ranks)
+        (seqiential? (replace {14 1} ranks)))))
 
 (defn straight? [hand]
-  nil)
+  (let [suits (set (map suit hand))]
+    (and (>= (count suits) 2)
+         (seqiential-hand? hand))))
 
 (defn straight-flush? [hand]
-  nil)
+  (let [suits (set (map suit hand))]
+    (and (= (count suits) 1)
+         (seqiential-hand? hand))))
 
 (defn value [hand]
-  nil)
+  (cond
+    (straight-flush? hand)  8
+    (four-of-a-kind? hand)  7
+    (full-house? hand)      6
+    (flush? hand)           5
+    (straight? hand)        4
+    (three-of-a-kind? hand) 3
+    (two-pairs? hand)       2
+    (pair? hand)            1
+    :else                   0))
