7cc1725bd49e238845a2c2e3391e8fec4e826686
NeQuissimus <steinbach.tim@googlemail.com>
2014-07-06 10:18:55 -0400

    Ex 1-5

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,19 +1,23 @@
 (ns p-p-p-pokerface)
 
-(defn rank [card]
-  nil)
+(defn rank [[rank _]]
+  (let [ranks {\T 10, \J 11, \Q 12, \K 13, \A 14}]
+    (cond
+      (Character/isDigit rank) (Integer/valueOf (str rank))
+      :else (get ranks rank)
+  )))
 
-(defn suit [card]
-  nil)
+(defn suit [[_ suit]]
+  (str suit))
 
 (defn pair? [hand]
-  nil)
+  (>= (apply max (vals (frequencies (map rank hand)))) 2))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (>= (apply max (vals (frequencies (map rank hand)))) 3))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (>= (apply max (vals (frequencies (map rank hand)))) 4))
 
 (defn flush? [hand]
   nil)
