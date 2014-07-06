e9d8f37ea57e546638cf921aef0cd43954e16e53
Jordan Biserkov <jordan@biserkov.com>
2014-07-06 15:51:04 +0300

    You can't read my <repository name>

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,60 @@
 (ns p-p-p-pokerface)
 
-(defn rank [card]
-  nil)
+(defn rank [[rg st]]
+  (if (Character/isDigit rg)
+    (Integer/valueOf (str rg))
+    (get {\T 10, \J 11, \Q 12, \K 13, \A 14} rg)))
 
 (defn suit [card]
-  nil)
+  (-> card second str))
+
+(defn n-of-a-kind? [hand n]
+  (contains? (set (vals (frequencies (map rank hand)))) n))
 
 (defn pair? [hand]
-  nil)
+  (n-of-a-kind? hand 2))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (n-of-a-kind? hand 3))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (n-of-a-kind? hand 4))
 
 (defn flush? [hand]
-  nil)
+   (apply = (map suit hand)))
 
 (defn full-house? [hand]
-  nil)
+  (and (pair? hand)
+       (three-of-a-kind? hand)))
 
 (defn two-pairs? [hand]
-  nil)
+  (= (get (frequencies (vals (frequencies (map rank hand)))) 2) 2))
 
 (defn straight? [hand]
-  nil)
+  (let [ranks (sort (map rank hand))
+        low (first ranks)
+        high (last ranks)
+        ]
+    (and ;(> (count (frequencies (map suit hand)))
+         ;   1)
+         (or (= (range low (inc high)) ranks)
+             (= (range 1 6) (sort (replace {14 1} ranks)))))))
 
 (defn straight-flush? [hand]
-  nil)
+  (and (straight? hand)
+       (flush? hand)))
+
+(defn high-card? [hand]
+  true) ; All hands have a high card.
 
 (defn value [hand]
-  nil)
+  (let [checkers #{[high-card? 0]
+                   [pair? 1]
+                   [two-pairs? 2]
+                   [three-of-a-kind? 3]
+                   [straight? 4]
+                   [flush? 5]
+                   [full-house? 6]
+                   [four-of-a-kind? 7]
+                   [straight-flush? 8]}]
+    (apply max (map second (filter (fn [check] ((first check) hand)) checkers)))))
