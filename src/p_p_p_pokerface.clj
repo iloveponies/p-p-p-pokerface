635af6395f64c75ab4cca5115ad9c79ff7c45c7b
Grzegorz Jacykowski <nickt@o2.pl>
2014-07-13 21:50:48 +0200

    Work in progress

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -11,25 +11,47 @@
   (str color)))
 
 (defn pair? [hand]
-  nil)
+  (let [wart (map rank hand)]
+  (if (= 2 (apply max (vals (frequencies wart))))
+  true
+  false)))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (let [wart (map rank hand)]
+  (if (= 3 (apply max (vals (frequencies wart))))
+  true
+  false)))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (let [wart (map rank hand)]
+  (if (= 4 (apply max (vals (frequencies wart))))
+  true
+  false))) 
 
 (defn flush? [hand]
-  nil)
+  (let [wart (map suit hand)]
+  (if (= 5 (apply max (vals (frequencies wart))))
+  true
+  false)))
 
 (defn full-house? [hand]
-  nil)
+  (let [wart (map rank hand)]
+  (if (and (= 3 (apply max (vals (frequencies wart)))) (= 2 (apply min (vals (frequencies wart)))))
+  true
+  false))) 
 
 (defn two-pairs? [hand]
-  nil)
+  (let [wart (map rank hand)]
+  (if (and (= 2 (apply max (vals (frequencies wart)))) (= 2 (apply max (rest (vals (frequencies wart))))))
+  true
+  false))) 
 
 (defn straight? [hand]
-  nil)
+ (let [wart (map rank hand)
+      zakres (range (apply min wart) (+ 1 (apply max wart)))]
+ (if (and (= zakres (range (apply min wart) (+ 5 (apply min wart)))) (not (pair? hand)))
+  true
+  false)))
 
 (defn straight-flush? [hand]
   nil)
