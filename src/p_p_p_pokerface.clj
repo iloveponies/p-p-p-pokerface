58c4cab625639be6b6ef863247db58cf1c0987cd
Chris Howe-Jones <chris.howejones@gmail.com>
2014-07-14 13:29:55 +0100

    Implemented up to and including full-house?

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,25 +1,51 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+  (let [[rank-char _] card
+        rank-map {\T 10 \J 11 \Q 12 \K 13 \A 14}]
+    (if (Character/isDigit rank-char)
+      (Integer/valueOf (str rank-char))
+      (get rank-map rank-char))))
 
 (defn suit [card]
-  nil)
+  (let [[_ suit] card]
+    (str suit)))
+
+(defn- max-same-rank [hand]
+  (apply max (vals (frequencies (map rank hand)))))
 
 (defn pair? [hand]
-  nil)
+  (let [max-same-rank (max-same-rank hand)]
+    (if (= 2 max-same-rank)
+      true
+      false)))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (let [max-same-rank (max-same-rank hand)]
+    (if (= 3 max-same-rank)
+      true
+      false)))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (let [max-same-rank (max-same-rank hand)]
+    (if (= 4 max-same-rank)
+      true
+      false)))
+
+(defn max-same-suit [hand]
+  (apply max (vals (frequencies (map suit hand)))))
 
 (defn flush? [hand]
-  nil)
+  (let [max-same-suit (max-same-suit hand)]
+    (if (= (count hand) max-same-suit)
+      true
+      false)))
 
 (defn full-house? [hand]
-  nil)
+  (let [rank-frequencies (vals (frequencies (map rank hand)))]
+    (if (= (seq [2 3]) (sort rank-frequencies))
+      true
+      false)))
 
 (defn two-pairs? [hand]
   nil)
