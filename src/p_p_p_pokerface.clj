e65259569192038ac25a27bbfc84848a185e8632
Massimo Battestini <MassimoBattestini@powa.com>
2014-07-14 19:35:40 +0100

    p-p-p-pokerface

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,56 @@
 (ns p-p-p-pokerface)
 
-(defn rank [card]
-  nil)
+(def replacement { \T 10 \J 11 \Q 12 \K 13 \A 14 })
 
-(defn suit [card]
-  nil)
+(defn rank [[rank _]]
+  (if (Character/isDigit rank)
+    (Character/getNumericValue rank)
+    (replacement rank)))
+
+(defn suit [[_ suit]]
+  (str suit))
+
+(defn repetitions [hand]
+  (let [ranks (map rank hand)]
+    (vals (frequencies ranks))))
 
 (defn pair? [hand]
-  nil)
+  (boolean (some #{2} (repetitions hand))))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (boolean (some #{3} (repetitions hand))))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (boolean (some #{4} (repetitions hand))))
 
 (defn flush? [hand]
-  nil)
+  (apply = (map suit hand)))
 
 (defn full-house? [hand]
-  nil)
+  (= (sort (repetitions hand)) [2 3]))
 
 (defn two-pairs? [hand]
-  nil)
+  (= (sort (repetitions hand)) [1 2 2]))
+
+(defn adjacent? [list]
+  (let [sorted (sort list)
+        minimum (first sorted)]
+    (= sorted (range minimum (+ minimum 5)))))
 
 (defn straight? [hand]
-  nil)
+  (let [ranks (map rank hand)]
+    (or (adjacent? ranks)
+        (adjacent? (replace {14 1} ranks)))))
 
 (defn straight-flush? [hand]
-  nil)
+  (and (straight? hand) (flush? hand)))
+
+(defn high-card? [hand] true)
 
 (defn value [hand]
-  nil)
+  (let [checkers #{[high-card? 0]  [pair? 1]
+                   [two-pairs? 2]  [three-of-a-kind? 3]
+                   [straight? 4]   [flush? 5]
+                   [full-house? 6] [four-of-a-kind? 7]
+                   [straight-flush? 8]}]
+    (apply max (keep #(if ((first %) hand) (second %)) checkers))))
