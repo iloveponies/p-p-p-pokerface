24dcc08d15e80e83cb69e90d22891b3eee84e95c
Matteo Traina <matteo.traina.mail@gmail.com>
2014-07-15 15:41:26 +0100

    third

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -12,22 +12,6 @@
   (let [[_ snd] card]
     (str snd)))
 
-(comment
-(def high-seven                   ["2H" "3S" "4C" "5C" "7D"])
-(def pair-hand                    ["2H" "2S" "4C" "5C" "7D"])
-(def two-pairs-hand               ["2H" "2S" "4C" "4D" "7D"])
-(def three-of-a-kind-hand         ["2H" "2S" "2C" "4D" "7D"])
-(def four-of-a-kind-hand          ["2H" "2S" "2C" "2D" "7D"])
-(def straight-hand                ["2H" "3S" "6C" "5D" "4D"])
-(def low-ace-straight-hand        ["2H" "3S" "4C" "5D" "AD"])
-(def high-ace-straight-hand       ["TH" "AS" "QC" "KD" "JD"])
-(def flush-hand                   ["2H" "4H" "5H" "9H" "7H"])
-(def full-house-hand              ["2H" "5D" "2D" "2C" "5S"])
-(def straight-flush-hand          ["2H" "3H" "6H" "5H" "4H"])
-(def low-ace-straight-flush-hand  ["2D" "3D" "4D" "5D" "AD"])
-(def high-ace-straight-flush-hand ["TS" "AS" "QS" "KS" "JS"])
-  )
-
 (defn n-kind [n hand]
   (if (some #(= n %) (vals (frequencies (map #(rank %) hand)))) true false))
 
@@ -49,8 +33,10 @@
 
 (defn two-pairs? [hand]
   (let [freq (vals (frequencies (map #(rank %) hand)))]
-    (if (some #(= 4 %) freq) true
-      (if (some #(= 0 (mod % 2)) (vals (frequencies freq))) true false))))
+    (cond
+     (some #(= 4 %) freq) true
+     (some #(= 0 (mod % 2)) (vals (frequencies (filter #(= 2 %) freq)))) true
+      :else false)))
 
 (defn sorted-ranks [hand]
   (sort (map #(rank %) hand)))
@@ -66,7 +52,17 @@
     (= sort-ranks straight)))
 
 (defn straight-flush? [hand]
-  nil)
+  (and (straight? hand) (flush? hand)))
+
+(defn high-card? [hand]
+  true)
 
 (defn value [hand]
-  nil)
+  (let [checkers #{[high-card? 0]  [pair? 1]
+                 [two-pairs? 2]  [three-of-a-kind? 3]
+                 [straight? 4]   [flush? 5]
+                 [full-house? 6] [four-of-a-kind? 7]
+                [straight-flush? 8]}]
+    (apply max (map #(if ((first %) hand) (second %) 0) checkers))))
+
+
