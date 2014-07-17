4fa57a5455ea664a91a81227b08c95f4b67e1b80
Dmitry Mylnev <Dmitry.Mylnev@traderstraffic.com>
2014-07-17 17:03:22 +0400

    Finished Exercise 2

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,7 +1,14 @@
 (ns p-p-p-pokerface)
 
+
+;; takes a single card and returns the rank as a number between 2 and 14.
+
 (defn rank [card]
-  nil)
+  (let [[n] card
+        replacements {\T 10, \J 11, \Q 12, \K 13, \A 14}]
+    (if (Character/isDigit n)
+      (Integer/valueOf (str n))
+      (replacements n))))
 
 
 ;; takes a singe card and returns the suit of the card as a one character string.
