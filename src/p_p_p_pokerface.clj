be0ed017528f3c5b4a9f5b8cb01d094a731ade59
zeroDivisible <zerodivisible@gmail.com>
2014-07-17 23:40:24 +0100

    implemented straight?

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -8,30 +8,54 @@
       (Integer/valueOf (str rank))
       (get replacements rank))))
 
+(defn ranks [hand]
+  (map rank hand))
+
 (defn suit [card]
   (let [[_ suit] card]
     (str suit)))
 
+(defn suits [hand]
+  (map suit hand))
+
+(defn rank-values [hand]
+  (vals (frequencies (ranks hand))))
+
+(defn suit-values [hand]
+  (vals (frequencies (suits hand))))
+
+(defn contains-x-same-ranks [x hand]
+  (contains? (set (rank-values hand)) x))
+
 (defn pair? [hand]
-  (contains? (set (vals (frequencies (map rank hand)))) 2))
+  (contains-x-same-ranks 2 hand))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (contains-x-same-ranks 3 hand))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (contains-x-same-ranks 4 hand))
 
 (defn flush? [hand]
-  nil)
+  (contains? (set (suit-values hand)) 5))
 
 (defn full-house? [hand]
-  nil)
+  (= #{2 3} (set (rank-values hand))))
 
 (defn two-pairs? [hand]
-  nil)
+  (let [freqs (filter (fn [x] (not (= 1 x))) (rank-values hand))]
+    (not (= (count freqs) (count (set freqs))))))
 
 (defn straight? [hand]
-  nil)
+  (let [r (sort (ranks hand))
+        min-r (reduce min r)
+        r-reduced (map (fn [x] (- x min-r)) r)
+        replaced (sort (replace {14 1} r))
+        min-replaced (reduce min replaced)
+        replaced-reduced (map (fn [x] (- x min-replaced)) replaced)
+        straight (range 0 5)]
+    (if (or (= r-reduced straight) (= replaced-reduced straight)) true false)))
+    
 
 (defn straight-flush? [hand]
   nil)
