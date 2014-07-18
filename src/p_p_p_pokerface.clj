8ec85dae654a566bc3f8751191e972fa20b2b994
Gianluca Romanin <gianluca.romanin@vimar.com>
2014-07-18 18:25:50 +0200

    salvo

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,13 +1,36 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+  (let [rank_rep {\T 10 \J 11 \Q 12 \K 13 \A 14}
+        [r s] card]
+    (if (Character/isDigit r)
+         (Integer/valueOf (str r))
+         (rank_rep r))))
 
 (defn suit [card]
-  nil)
+  (let [[_ snd] card]
+    (str snd)))
+
+;; (def high-seven                   ["2H" "3S" "4C" "5C" "7D"])
+;; (def pair-hand                    ["2H" "2S" "4C" "5C" "7D"])
+;; (def two-pairs-hand               ["2H" "2S" "4C" "4D" "7D"])
+;; (def three-of-a-kind-hand         ["2H" "2S" "2C" "4D" "7D"])
+;; (def four-of-a-kind-hand          ["2H" "2S" "2C" "2D" "7D"])
+;; (def straight-hand                ["2H" "3S" "6C" "5D" "4D"])
+;; (def low-ace-straight-hand        ["2H" "3S" "4C" "5D" "AD"])
+;; (def high-ace-straight-hand       ["TH" "AS" "QC" "KD" "JD"])
+;; (def flush-hand                   ["2H" "4H" "5H" "9H" "7H"])
+;; (def full-house-hand              ["2H" "5D" "2D" "2C" "5S"])
+;; (def straight-flush-hand          ["2H" "3H" "6H" "5H" "4H"])
+;; (def low-ace-straight-flush-hand  ["2D" "3D" "4D" "5D" "AD"])
+;; (def high-ace-straight-flush-hand ["TS" "AS" "QS" "KS" "JS"])
 
 (defn pair? [hand]
-  nil)
+  (let [ranks (map rank hand)
+        freqs (frequencies ranks)
+        v (vals freqs)
+        mx (apply max v)]
+    (= 2 mx)))
 
 (defn three-of-a-kind? [hand]
   nil)
