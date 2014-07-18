bdf52130c045023220245617a902285705ca4a99
Patrick Martin <prof.pmartin10@gmail.com>
2014-07-18 13:27:36 -0400

    Completed all exercises - maps and functions rule.

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -13,45 +13,93 @@
   (let [[_ suit-char] card]
     (str suit-char)))
 
+(defn rank-freq-map [hand]
+  (frequencies (map rank hand)))
+
+(defn suit-freq-map [hand]
+  (frequencies (map suit hand)))
+
 (defn pair? [hand]
-  (let [hand-ranks (map rank hand)
-        rank-counts (frequencies hand-ranks)]
+  (let [rank-counts (frequencies (map rank hand))]
     (if (> (apply max (vals rank-counts)) 1)
       true
       false)))
 
 (defn three-of-a-kind? [hand]
   (let [rank-counts (frequencies (map rank hand))]
-    (if (> (apply max (vals rank-counts)) 2)
+    (if (= (apply max (vals rank-counts)) 3)
       true
       false)))
 
 (defn four-of-a-kind? [hand]
-  (let [rank-counts (frequencies (map rank hand))]
-    (if (> (apply max (vals rank-counts)) 3)
+  (let [rank-counts (rank-freq-map hand)]
+    (if (= (apply max (vals rank-counts)) 4)
       true
       false)))
 
 (defn flush? [hand]
-  (let [hand-suits (frequencies (map suit hand))]
-    (if (> (apply max (vals hand-suits)) 4)
+  (let [hand-suits (suit-freq-map hand)]
+    (if (= (apply max (vals hand-suits)) 5)
       true
       false)))
 
 (defn full-house? [hand]
-  nil)
+  (if (three-of-a-kind? hand)
+    ; check to see if the hand has an additional pair
+    (let [rank-freqs-map (frequencies (map rank hand))
+          freq-set (set (vals rank-freqs-map))]
+      (contains? freq-set 2))
+    false))
 
 (defn two-pairs? [hand]
-  nil)
+  (if (four-of-a-kind? hand)
+    true
+    (let [curr-pairs (filter
+                      (fn [f-entry] (= (val f-entry) 2))
+                      (rank-freq-map hand))]
+      (= (count curr-pairs) 2))))
 
 (defn straight? [hand]
-  nil)
+  (let [sorted-hand-ranks (sort (map rank hand))
+        low-card (first sorted-hand-ranks)]
+    (if (= sorted-hand-ranks (range low-card (+ low-card 5)))
+      true
+      ; Check for an ace!
+      (let [first-four (take 4 sorted-hand-ranks)]
+        (if (contains? (set sorted-hand-ranks) 14)
+        ; put a low ace on front and check again!
+        (if (= (cons 1 first-four) (range 1 (+ 1 5)))
+          true
+          false
+          )
+        false)))))
 
 (defn straight-flush? [hand]
-  nil)
+  (if (and (straight? hand) (flush? hand))
+    true
+    false))
+
+(defn high-card? [hand]
+  true)
 
 (defn value [hand]
-  nil)
+  ; checkers is a set of functions and values
+  (let [checkers #{[high-card? 0] [pair? 1]
+                   [two-pairs? 2] [three-of-a-kind? 3]
+                   [straight? 4] [flush? 5]
+                   [full-house? 6] [four-of-a-kind? 7]
+                   [straight-flush? 8]
+                   }
+        ; Create a function that applies a checker to a hand.
+        hand-value (fn [checker]
+                     (let [[matcher value] checker]
+                       (if (matcher hand)
+                         value
+                         ; otherwise, set it to a negative value!
+                         -1)))]
+    ; Map this function accross all checkers and grabe the max value.
+    (apply max (map hand-value checkers))
+    ))
 
 ; TESTING DEFS!
 ;(def high-seven ["2H" "3S" "4C" "5C" "7D"])
