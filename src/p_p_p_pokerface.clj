349d2111791d57e5d172afa1240ac664730f8c4f
madHamish42 <pe_di@gmx.de>
2014-07-20 17:43:17 +0200

    finished

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -70,26 +70,49 @@
     (= sorted ref-range)))
 
 (defn straight-flush? [hand]
-  nil)
+  (and (flush? hand)
+       (straight? hand)))
+
+(defn high-card? [hand]
+  true)
+
+(defn value2 [hand]
+  (cond
+   (straight-flush? hand) 8
+   (four-of-a-kind? hand) 7
+   (full-house? hand) 6
+   (flush? hand) 5
+   (straight? hand) 4
+   (three-of-a-kind? hand) 3
+   (two-pairs? hand) 2
+   (pair? hand) 1
+   :else 0))
 
 (defn value [hand]
-  nil)
+  (let [checkers #{[high-card? 0]  [pair? 1]
+                 [two-pairs? 2]  [three-of-a-kind? 3]
+                 [straight? 4]   [flush? 5]
+                 [full-house? 6] [four-of-a-kind? 7]
+                 [straight-flush? 8]}
+        checked (filter (fn [[fun _]] (fun hand)) checkers)
+        vals (map second checked)]
+    (apply max vals)))
 
+(comment
+  (def high-seven                   ["2H" "3S" "4C" "5C" "7D"])
+  (def pair-hand                    ["2H" "2S" "4C" "5C" "7D"])
+  (def two-pairs-hand               ["2H" "2S" "4C" "4D" "7D"])
+  (def three-of-a-kind-hand         ["2H" "2S" "2C" "4D" "7D"])
+  (def four-of-a-kind-hand          ["2H" "2S" "2C" "2D" "7D"])
+  (def straight-hand                ["2H" "3S" "6C" "5D" "4D"])
+  (def low-ace-straight-hand        ["2H" "3S" "4C" "5D" "AD"])
+  (def high-ace-straight-hand       ["TH" "AS" "QC" "KD" "JD"])
+  (def flush-hand                   ["2H" "4H" "5H" "9H" "7H"])
+  (def full-house-hand              ["2H" "5D" "2D" "2C" "5S"])
+  (def straight-flush-hand          ["2H" "3H" "6H" "5H" "4H"])
+  (def low-ace-straight-flush-hand  ["2D" "3D" "4D" "5D" "AD"])
+  (def high-ace-straight-flush-hand ["TS" "AS" "QS" "KS" "JS"])
 
-(def high-seven                   ["2H" "3S" "4C" "5C" "7D"])
-(def pair-hand                    ["2H" "2S" "4C" "5C" "7D"])
-(def two-pairs-hand               ["2H" "2S" "4C" "4D" "7D"])
-(def three-of-a-kind-hand         ["2H" "2S" "2C" "4D" "7D"])
-(def four-of-a-kind-hand          ["2H" "2S" "2C" "2D" "7D"])
-(def straight-hand                ["2H" "3S" "6C" "5D" "4D"])
-(def low-ace-straight-hand        ["2H" "3S" "4C" "5D" "AD"])
-(def high-ace-straight-hand       ["TH" "AS" "QC" "KD" "JD"])
-(def flush-hand                   ["2H" "4H" "5H" "9H" "7H"])
-(def full-house-hand              ["2H" "5D" "2D" "2C" "5S"])
-(def straight-flush-hand          ["2H" "3H" "6H" "5H" "4H"])
-(def low-ace-straight-flush-hand  ["2D" "3D" "4D" "5D" "AD"])
-(def high-ace-straight-flush-hand ["TS" "AS" "QS" "KS" "JS"])
+  (value2 straight-flush-hand) 
 
-(straight? high-ace-straight-hand) 
-
-
+  (filter even? [1 2 3]))
