ddb30e60b4d1093cc655a43d93c695b2f3828016
Matteo Barbieri <barbieri.matteo@gmail.com>
2014-07-31 00:05:29 +0200

    Some exercises

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,25 +1,30 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+  (let [[ r _ ] card
+        rank-values { \T 10 \J 11 \Q 12 \K 13 \A 14}]
+    (if (Character/isDigit r)
+          (Integer/valueOf (str r))
+          (rank-values r))))
 
 (defn suit [card]
-  nil)
+  (let [[ _ s ] card]
+    (str s)))
 
 (defn pair? [hand]
-  nil)
+  (>= (apply max (vals (frequencies (map rank hand)))) 2))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (>= (apply max (vals (frequencies (map rank hand)))) 3))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (>= (apply max (vals (frequencies (map rank hand)))) 4))
 
 (defn flush? [hand]
-  nil)
+  (>= (apply max (vals (frequencies (map suit hand)))) 5))
 
 (defn full-house? [hand]
-  nil)
+  (= (sort (vals (frequencies (map rank hand)))) [2 3]))
 
 (defn two-pairs? [hand]
   nil)
