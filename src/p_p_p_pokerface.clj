218b4a85626879387ebacb82732478ba4dbf397a
Kristian Nygaard Jensen <freeduck@member.fsf.org>
2014-07-30 11:47:40 +0200

    Rank

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,7 +1,11 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+  (let [[rank_char _] card
+        nobility {\T 10 \J 11 \Q 12 \K 13 \A 14}]
+    (if (Character/isDigit rank_char)
+      (Integer/valueOf (str rank_char))
+      (get nobility  rank_char))))
 
 (defn suit [card]
   nil)
