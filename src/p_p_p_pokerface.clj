405bcff391e94bd20ae1c004631967784ab5eb81
karri <kvrkredd@ymail.com>
2014-07-31 14:13:16 +0530

    set 5

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,71 @@
 (ns p-p-p-pokerface)
-
-(defn rank [card]
-  nil)
+(require 'clojure.tools.trace)
 
 (defn suit [card]
-  nil)
+  (let [ [_ s] card
+         ]
+    (str s))
+  )
+
+(defn rank [card]
+  (let [rank-map {\T 10 \J 11 \Q 12 \K 13 \A 14}
+        [r _] card ]
+    ( if (Character/isDigit r)
+      (Integer/valueOf (str r))
+      (rank-map r))
+    )
+  )
 
 (defn pair? [hand]
-  nil)
+  (> (apply max (vals (frequencies (map rank hand) ) ) ) 1)
+  )
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (> (apply max (vals (frequencies (map rank hand)))) 2)
+  )
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (> (apply max (vals (frequencies (map rank hand)))) 3)
+  )
 
 (defn flush? [hand]
-  nil)
+  (== (apply max (vals (frequencies (map suit hand)))) 5))
 
 (defn full-house? [hand]
-  nil)
+  (= (range 2 4)
+     (sort (vals (frequencies (map rank hand))))))
 
 (defn two-pairs? [hand]
-  nil)
+  (>= (apply + (filter (fn [x] (>= x 2)) (vals (frequencies (map rank hand))))) 4)
+  )
 
-(defn straight? [hand]
-  nil)
+(defn ^:dynamic straight? [hand]
+  (let [ace-low {14 1}
+        sorted-hand-ranks (sort (map rank hand))
+        hand-min (first sorted-hand-ranks)
+        sorted-ace-low-hand-ranks (sort (replace ace-low (map rank hand)))
+        ace-low-hand-min (first sorted-ace-low-hand-ranks )
+        hand-size 5]
+    (cond
+     (= (range hand-min (+ hand-min hand-size)) sorted-hand-ranks) true
+     (= (range ace-low-hand-min (+ ace-low-hand-min hand-size)) sorted-ace-low-hand-ranks) true
+     :else false
+     )
+  )
+  )
 
 (defn straight-flush? [hand]
-  nil)
+  (and (straight? hand) (flush? hand)))
 
 (defn value [hand]
-  nil)
+  (cond
+   (straight-flush? hand) 8
+   (four-of-a-kind? hand) 7
+   (full-house? hand) 6
+   (flush? hand) 5
+   (straight? hand) 4
+   (three-of-a-kind? hand) 3
+   (two-pairs? hand) 2
+   (pair? hand) 1
+   :else 0)
+ )
