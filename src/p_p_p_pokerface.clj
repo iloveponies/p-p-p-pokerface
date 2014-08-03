b718f59a35fdc43bd6e8d214d9cdfd52415c675b
Matteo Barbieri <barbieri.matteo@gmail.com>
2014-08-03 22:54:42 +0200

    Exercises completed

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -27,13 +27,27 @@
   (= (sort (vals (frequencies (map rank hand)))) [2 3]))
 
 (defn two-pairs? [hand]
-  nil)
+  (>= (count (filter #(>= % 2) (vals (frequencies (map rank hand))))) 2))
 
 (defn straight? [hand]
-  nil)
+  (let [ranks-high-ace (sort (map rank hand))
+        ranks-low-ace (sort (replace {14 1} ranks-high-ace))
+        check-straight (fn [ranks]
+                         (= (range (first ranks) (+ (first ranks) 5)) ranks))]
+    (or (check-straight ranks-high-ace)
+        (check-straight ranks-low-ace))))
 
 (defn straight-flush? [hand]
-  nil)
+  (and (straight? hand) (flush? hand)))
+
+(defn high-card? [hand]
+  true)
 
 (defn value [hand]
-  nil)
+  (let [checkers #{[high-card? 0] [pair? 1]
+                   [two-pairs? 2] [three-of-a-kind? 3]
+                   [straight? 4] [flush? 5]
+                   [full-house? 6] [four-of-a-kind? 7]
+                   [straight-flush? 8]}
+        check #((first %) hand)]
+    (apply max (map second (filter check checkers)))))
