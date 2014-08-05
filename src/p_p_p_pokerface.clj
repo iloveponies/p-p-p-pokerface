e1b7393be318c89698d3e3c8b46666cd737abebe
Salvador Perez <sisekeom@gmail.com>
2014-08-05 17:40:49 +0200

    Solve the poker exercises

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,61 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+  (let [[rank _] card
+        values {\A 14, \K 13, \Q 12, \J 11, \T 10}]
+    (if (Character/isDigit rank)
+      (Integer/valueOf (str rank))
+      (get values rank))))
 
 (defn suit [card]
-  nil)
+  (let [[_ suit] card]
+    (str suit)))
+
+(defn groups? [hand n]
+  (let [freq (vals (frequencies (map rank hand)))
+        fun (fn [x] (== x n))]
+    (> (count (filter fun freq)) 0)))
 
 (defn pair? [hand]
-  nil)
+  (groups? hand 2))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (groups? hand 3))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (groups? hand 4))
 
 (defn flush? [hand]
-  nil)
+  (let [ suit-seq (map suit hand)]
+    (apply = suit-seq)))
 
 (defn full-house? [hand]
-  nil)
+  (let [three (three-of-a-kind? hand)
+        pair (pair? hand)]
+    (and three pair)))
 
 (defn two-pairs? [hand]
-  nil)
+  (let [freq (vals (frequencies (map rank hand)))
+        fun (fn [x] (== x 2))]
+    (== (count (filter fun freq)) 2)))
 
 (defn straight? [hand]
-  nil)
+  (let [hand-vals (map rank hand)
+        alt-vals (replace {14 1} hand-vals)
+        fun (fn [x] (= x (range (apply min x) (+ (apply min x) 5))))]
+    (or (fun (sort hand-vals)) (fun (sort alt-vals)))))
 
 (defn straight-flush? [hand]
-  nil)
+  (and (straight? hand) (flush? hand)))
 
 (defn value [hand]
-  nil)
+  (cond
+    (straight-flush? hand) 8
+    (four-of-a-kind? hand) 7
+    (full-house? hand) 6
+    (flush? hand) 5
+    (straight? hand) 4
+    (three-of-a-kind? hand) 3
+    (two-pairs? hand) 2
+    (pair? hand) 1
+    :else 0 ))
