26c132b1b345426234837aae2ca5f17244f13df5
JayZar21 <romaninz@gmail.com>
2014-08-14 23:26:24 +0200

    finished

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -11,47 +11,65 @@
   (let [[_ snd] card]
     (str snd)))
 
-;; (def high-seven                   ["2H" "3S" "4C" "5C" "7D"])
-;; (def pair-hand                    ["2H" "2S" "4C" "5C" "7D"])
-;; (def two-pairs-hand               ["2H" "2S" "4C" "4D" "7D"])
-;; (def three-of-a-kind-hand         ["2H" "2S" "2C" "4D" "7D"])
-;; (def four-of-a-kind-hand          ["2H" "2S" "2C" "2D" "7D"])
-;; (def straight-hand                ["2H" "3S" "6C" "5D" "4D"])
-;; (def low-ace-straight-hand        ["2H" "3S" "4C" "5D" "AD"])
-;; (def high-ace-straight-hand       ["TH" "AS" "QC" "KD" "JD"])
-;; (def flush-hand                   ["2H" "4H" "5H" "9H" "7H"])
-;; (def full-house-hand              ["2H" "5D" "2D" "2C" "5S"])
-;; (def straight-flush-hand          ["2H" "3H" "6H" "5H" "4H"])
-;; (def low-ace-straight-flush-hand  ["2D" "3D" "4D" "5D" "AD"])
-;; (def high-ace-straight-flush-hand ["TS" "AS" "QS" "KS" "JS"])
-
-(defn pair? [hand]
+(defn max-number-of-ranked-cards [hand m]
   (let [ranks (map rank hand)
         freqs (frequencies ranks)
         v (vals freqs)
         mx (apply max v)]
-    (= 2 mx)))
+    (= m mx)))
+
+(defn pair? [hand]
+  (max-number-of-ranked-cards hand 2))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (max-number-of-ranked-cards hand 3))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (max-number-of-ranked-cards hand 4))
 
 (defn flush? [hand]
-  nil)
+  (let [s (map suit hand)
+        freqs (frequencies s)
+        k (keys freqs)]
+    (= 1 (count k))))
 
 (defn full-house? [hand]
-  nil)
+  (let [r (map rank hand)
+        freqs (frequencies r)
+        v (sort (vals freqs))]
+    (= v [2 3])))
+
 
 (defn two-pairs? [hand]
-  nil)
+  (let [r (map rank hand)
+        freqs (frequencies r)
+        v (sort (vals freqs))]
+    (= v [1 2 2])))
+
+(defn _straight [r]
+  (let [_max (apply max r)
+        _min (apply min r)
+        _upper (+ 1 _max)
+        _rangedh (take 5 (range _min _upper))
+        _sortedh (sort r)]
+    (= _rangedh _sortedh)))
 
 (defn straight? [hand]
-  nil)
+  (let [r (map rank hand)]
+    (or (_straight (replace {14 1} r))
+        (_straight r))))
 
 (defn straight-flush? [hand]
-  nil)
+  (and (straight? hand)
+       (flush? hand)))
+
+(defn high-card? [hand]
+  true)
 
 (defn value [hand]
-  nil)
+  (let [checkers #{[high-card? 0]  [pair? 1]
+                 [two-pairs? 2]  [three-of-a-kind? 3]
+                 [straight? 4]   [flush? 5]
+                 [full-house? 6] [four-of-a-kind? 7]
+                 [straight-flush? 8]}]
+    (apply max (map #(second %) (filter #((first %) hand) checkers)))))
