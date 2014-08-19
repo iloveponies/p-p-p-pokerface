dc6cd777857ca2a6b66c53b7ee87d26b01b367f4
razi <ssj.raziel@gmail.com>
2014-08-19 21:14:45 +0200

    remove test hands

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -60,18 +60,3 @@
                  [full-house? 6] [four-of-a-kind? 7]
                  [straight-flush? 8]}]
     (apply max (map (fn [[f p]] (if (f hand) p 0)) checkers))))
-
-(comment
-(def high-seven                   ["2H" "3S" "4C" "5C" "7D"])
-(def pair-hand                    ["2H" "2S" "4C" "5C" "7D"])
-(def two-pairs-hand               ["2H" "2S" "4C" "4D" "7D"])
-(def three-of-a-kind-hand         ["2H" "2S" "2C" "4D" "7D"])
-(def four-of-a-kind-hand          ["2H" "2S" "2C" "2D" "7D"])
-(def straight-hand                ["2H" "3S" "6C" "5D" "4D"])
-(def low-ace-straight-hand        ["2H" "3S" "4C" "5D" "AD"])
-(def high-ace-straight-hand       ["TH" "AS" "QC" "KD" "JD"])
-(def flush-hand                   ["2H" "4H" "5H" "9H" "7H"])
-(def full-house-hand              ["2H" "5D" "2D" "2C" "5S"])
-(def straight-flush-hand          ["2H" "3H" "6H" "5H" "4H"])
-(def low-ace-straight-flush-hand  ["2D" "3D" "4D" "5D" "AD"])
-(def high-ace-straight-flush-hand ["TS" "AS" "QS" "KS" "JS"]))
