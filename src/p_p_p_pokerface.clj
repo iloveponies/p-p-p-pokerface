1ec611700052e88ee9a78bf2a0e8e27b64c1fe18
Niko Sikkilä <niko.sikkila@gmail.com>
2014-09-07 10:03:49 +0300

    rank

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,7 +1,9 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+  (let [relpacements {\T 10, \J 11, \Q 12, \K 13, \A 14}
+        [fst _] card]
+    (if (Character/isDigit fst) (Integer/valueOf (str fst)) (get relpacements fst))))
 
 (defn suit [card]
   (let [[_ snd] card]
