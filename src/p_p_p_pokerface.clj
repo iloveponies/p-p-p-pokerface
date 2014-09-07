9c42b9f631ee987e0a7eb35f0b9e9f1fc2ece5fe
Niko Sikkilä <niko.sikkila@gmail.com>
2014-09-07 11:21:48 +0300

    straight?

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,9 +1,9 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  (let [relpacements {\T 10, \J 11, \Q 12, \K 13, \A 14}
+  (let [replacements {\T 10, \J 11, \Q 12, \K 13, \A 14}
         [fst _] card]
-    (if (Character/isDigit fst) (Integer/valueOf (str fst)) (get relpacements fst))))
+    (if (Character/isDigit fst) (Integer/valueOf (str fst)) (get replacements fst))))
 
 (defn suit [card]
   (let [[_ snd] card]
@@ -30,7 +30,12 @@
         (= [1 4] ranks))))
 
 (defn straight? [hand]
-  nil)
+  (let [ranks (sort (map rank hand))
+        ranks2 (sort (replace {14 1} ranks))
+      consec (fn [a b c d e] (== (+ a 4) (+ b 3) (+ c 2) (+ d 1) e))]
+    (or (apply consec ranks)
+      (apply consec ranks2))))
+
 
 (defn straight-flush? [hand]
   nil)
