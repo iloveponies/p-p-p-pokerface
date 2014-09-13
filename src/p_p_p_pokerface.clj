2f2bcae2d2cd5bae16159cd84b5cc09c23ab066d
unknown <slohand1@gmail.com>
2014-09-13 13:07:18 +0300

    poker exercises

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,63 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+  (let [[s _]
+        card]
+    (cond
+     (Character/isDigit s) (Integer/valueOf (str s))
+     :else ({\T 10, \J 11, \Q 12, \K 13, \A 14} s))))
 
 (defn suit [card]
-  nil)
+  (let [[_ r]
+        card]
+    (str r)))
+
+(defn hand-rank [hand]
+  (vals(frequencies (map rank hand))))
 
 (defn pair? [hand]
-  nil)
+  (cond
+   (contains? (set(hand-rank hand)) 4) true
+   (contains? (set(hand-rank hand)) 3) true
+   (contains? (set(hand-rank hand)) 2) true
+   :else false))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (cond
+   (contains? (set(hand-rank hand)) 3) true
+   (contains? (set(hand-rank hand)) 4) true
+   :else false))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (contains? (set(hand-rank hand)) 4))
 
 (defn flush? [hand]
-  nil)
+  (contains? (set(vals(frequencies (map suit hand)))) 5))
 
 (defn full-house? [hand]
-  nil)
+  (= [2 3] (sort (hand-rank hand))))
 
 (defn two-pairs? [hand]
-  nil)
+  (= [1 2 2] (sort (hand-rank hand))))
 
 (defn straight? [hand]
-  nil)
+  (let [a-to-one (fn [x] (sort(replace {14 1} (mapv rank hand))))]
+    (cond
+     (== (apply min(mapv rank hand)) 2) (= (a-to-one hand) (range (apply min (a-to-one hand)) (+ (apply min (a-to-one hand)) 5)))
+     :else (= (sort(mapv rank hand)) (range (apply min (mapv rank hand)) (+ (apply min (mapv rank hand)) 5))))))
 
 (defn straight-flush? [hand]
-  nil)
+  (and (flush? hand) (straight? hand)))
+
 
 (defn value [hand]
-  nil)
+  (cond
+     (straight-flush? hand) 8
+     (four-of-a-kind? hand) 7
+     (full-house? hand) 6
+     (flush? hand) 5
+     (straight? hand) 4
+     (three-of-a-kind? hand) 3
+     (two-pairs? hand) 2
+     (pair? hand) 1
+     :else 0))
