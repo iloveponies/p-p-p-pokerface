fe83b46ceed46831a0e1f2d4a7ca575fd64d8546
gabesotto <gabesotto@gmail.com>
2014-10-14 11:31:08 -0600

    such poker, much clojure, wow.

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,64 @@
 (ns p-p-p-pokerface)
 
-(defn rank [card]
-  nil)
+(defn in? [seq elem]
+  (boolean (some #(= % elem) seq)))
 
-(defn suit [card]
-  nil)
+(defn high-card? [hand]
+  true)
+
+(defn rank [ [r _] ]
+  (def values {\T 10, \J 11, \Q 12, \K 13, \A 14})
+  (if (Character/isDigit r )
+    (Integer/valueOf (str r))
+    (values r)))
+
+(defn suit [ [_ s] ]
+  (str s))
 
 (defn pair? [hand]
-  nil)
+  (let [ranks (map rank hand)]
+    (>= (apply max (vals (frequencies ranks))) 2)))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (let [ranks (map rank hand)]
+    (>= (apply max (vals (frequencies ranks))) 3)))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (let [ranks (map rank hand)]
+    (>= (apply max (vals (frequencies ranks))) 4)))
 
 (defn flush? [hand]
-  nil)
+  (let [suits (map suit hand)]
+    (== (apply max (vals (frequencies suits))) 5)))
 
 (defn full-house? [hand]
-  nil)
+  (let [ranks (map rank hand)]
+    (= (range 2 4) (sort(vals (frequencies ranks))))))
 
 (defn two-pairs? [hand]
-  nil)
+  (let [ranks (map rank hand)]
+    (= (seq [1 2 2]) (sort(vals (frequencies ranks))))))
 
 (defn straight? [hand]
-  nil)
+  (let [ranks   (sort(map rank hand))
+        low-ace (sort(replace {14 1} ranks))
+        card-range (fn [x] (range (apply min x) (+ (apply max x) 1)))]
+    (if (in? ranks 2)
+      (= low-ace (card-range low-ace))
+      (= ranks (card-range ranks)))))
 
 (defn straight-flush? [hand]
-  nil)
+  (and (straight? hand) (flush? hand)))
 
 (defn value [hand]
-  nil)
+  (let [checkers #{[high-card? 0]  [pair? 1]
+                   [two-pairs? 2]   [three-of-a-kind? 3]
+                   [straight? 4]   [flush? 5]
+                   [full-house? 6] [four-of-a-kind? 7]
+                   [straight-flush? 8]}
+        hands    (filter #((first %) hand) checkers)]
+    (apply max (map second hands))))
+
+
+
+
