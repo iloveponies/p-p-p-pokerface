e3036a02b81261e5303dbd8e430330d4b4793bb0
Martin <martin.b.radev@gmail.com>
2014-10-15 03:32:04 +0300

    all done

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,174 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+
+  (let
+
+    [r (get card 0)
+     mp {\T 10, \J 11, \Q 12, \K 13, \A 14}
+     ]
+
+    (if (Character/isDigit r)
+      (Integer/valueOf (str r))
+      (get mp r)
+      )
+
+    )
+
+  )
+
 
 (defn suit [card]
-  nil)
+
+  (str (get card 1))
+
+  )
+
+
 
 (defn pair? [hand]
-  nil)
+
+  (let
+    [freq (frequencies (map rank hand))
+
+     occurs (vals freq)
+     maximum (apply max occurs)
+
+     ]
+    (== 2 maximum)
+    )
+
+
+  )
+
 
 (defn three-of-a-kind? [hand]
-  nil)
+
+  (let
+
+    [freq (frequencies (map rank hand))
+
+     occurs (vals freq)
+     maximum (apply max occurs)
+
+     ]
+
+    (and (== 3 maximum) (== 3 (count occurs)))
+    )
+
+  )
+
+
+
 
 (defn four-of-a-kind? [hand]
-  nil)
 
-(defn flush? [hand]
-  nil)
+  (let
 
-(defn full-house? [hand]
-  nil)
+    [freq (frequencies (map rank hand))
 
-(defn two-pairs? [hand]
-  nil)
+     occurs (vals freq)
+     maximum (apply max occurs)
+     ]
+     (== 4 maximum)
+
+    )
+
+  )
 
 (defn straight? [hand]
-  nil)
+
+  (let
+    [sorted (sort (map rank hand))
+    repl (sort (replace {14 1} sorted))
+     ordered (range (first sorted) (+ 5 (first sorted) ))
+     ordereds (range (first repl) (+ 5 (first repl) ))
+
+     ]
+
+    (or (= sorted ordered) (= repl ordereds))
+    )
+
+  )
+
+(defn flush? [hand]
+
+  (let
+
+    [freq (frequencies (map suit hand))
+     occurs (vals freq)
+     cnt (count occurs)
+
+     ]
+
+    (and (== cnt 1) (not (straight? hand)))
+
+  )
+)
+
+
+(defn full-house? [hand]
+  (let
+    [
+     freq (frequencies (map rank hand))
+     occurs (vals freq)
+     cnt (count occurs)
+     maximum (apply max occurs)
+     ]
+    (and (== 2 cnt) (== 3 maximum))
+    )
+  )
+
+
+(defn two-pairs? [hand]
+
+  (let
+    [
+     freq (frequencies (map rank hand))
+     occurs (vals freq)
+     cnt (count occurs)
+     maximum (apply max occurs)
+     ]
+    (or (== 4 maximum) (and (== 2 maximum) (== 3 cnt)) )
+    )
+
+  )
+
+
+
+
+
 
 (defn straight-flush? [hand]
-  nil)
+
+  (let
+
+    [freq (frequencies (map suit hand))
+     occurs (vals freq)
+     cnt (count occurs)
+     ]
+
+
+  (and (== 1 cnt) (straight? hand))
+  )
+ )
 
 (defn value [hand]
-  nil)
+
+
+  (cond
+   (and (straight? hand) (not (straight-flush? hand))) 4
+   (straight-flush? hand) 8
+   (four-of-a-kind? hand) 7
+   (full-house? hand) 6
+   (flush? hand) 5
+
+   (three-of-a-kind? hand) 3
+   (two-pairs? hand) 2
+   (pair? hand) 1
+   :else
+   0
+   )
+
+  )
+
