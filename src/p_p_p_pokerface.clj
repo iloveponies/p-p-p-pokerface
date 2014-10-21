19ab2487923638a2f14c03429e284db017b197bf
s0rbus <soorbus@gmail.com>
2014-10-21 20:18:39 +0100

    all done except two-pairs? and value

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,36 +1,42 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  (let [[rnk _] card]
-  (let rnknum (if (Character/isDigit rnk) (Integer/valueOf (str rnk)))
+  (let [[rnk _] card
+       faces {\T 10, \J 11, \Q 12, \K 13, \A 14}]
+  (if (Character/isDigit rnk) (Integer/valueOf (str rnk)) (faces rnk))))
 
 (defn suit [card]
   (let [[_ suit] card]
   (str suit)))
 
 (defn pair? [hand]
-  nil)
-
+  (= (count (filter #(= 2 %) (vals (frequencies (map rank hand))))) 1))
+  
 (defn three-of-a-kind? [hand]
-  nil)
-
+  (= (count (filter #(= 3 %) (vals (frequencies (map rank hand))))) 1))
+  
 (defn four-of-a-kind? [hand]
-  nil)
+  (= (count (filter #(= 4 %) (vals (frequencies (map rank hand))))) 1))
 
 (defn flush? [hand]
-  nil)
-
+  (= (apply max (vals (frequencies (map suit hand)))) 5))
+  
 (defn full-house? [hand]
-  nil)
+  (= (vals (frequencies (map rank hand))) [3 2]))
 
 (defn two-pairs? [hand]
   nil)
 
 (defn straight? [hand]
-  nil)
+  (let [sorted (sort (map rank hand))
+       minsorted (apply min sorted)
+       repsorted (sort (replace {14 1} (map rank hand)))
+       minrepsorted (apply min repsorted)]
+  (or (= sorted (range minsorted (+ minsorted 5)))
+    (= repsorted (range minrepsorted (+ minrepsorted 5))))))
 
 (defn straight-flush? [hand]
-  nil)
+  (and (straight? hand) (flush? hand)))
 
 (defn value [hand]
   nil)
