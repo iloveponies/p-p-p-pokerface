e509713929cae98073f0f5b6d19c6f2c7e5b226a
fergulis <ferdi@anagramix.com>
2014-10-29 22:01:45 +0100

    Poker game

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,58 @@
 (ns p-p-p-pokerface)
 
-(defn rank [card]
-  nil)
+(defn rank [[first _]]
+  (let [maph {\T 10 \J 11 \Q 12 \K 13 \A 14}]
+    (if (Character/isDigit first) (Integer/valueOf (str first)) (maph first))))
 
-(defn suit [card]
-  nil)
+(defn suit [[_ second]]
+  (str second))
+
+(defn rank-distrib [hand]
+  (vals (frequencies (map rank hand))))
 
 (defn pair? [hand]
-  nil)
+  (>= (apply max (rank-distrib hand)) 2))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (>= (apply max (rank-distrib hand)) 3))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (>= (apply max (rank-distrib hand)) 4))
 
 (defn flush? [hand]
-  nil)
+  (apply = (map suit hand)))
 
 (defn full-house? [hand]
-  nil)
+  (= (sort (rank-distrib hand)) [2 3]))
 
 (defn two-pairs? [hand]
-  nil)
+  (let [sort-seq (sort (rank-distrib hand))
+        eq-sort (fn [seq] (= seq sort-seq))]
+    (cond
+      (eq-sort [1 2 2]) true
+      (eq-sort [2 3]) true
+      (eq-sort [1 4]) true
+      :else false)))
 
 (defn straight? [hand]
-  nil)
+  (let [sort-hand (sort (map rank hand))
+        min-sort-hand (first sort-hand)
+        sort-hand-r (sort (replace {14 1} (map rank hand)))]
+    (cond
+      (= sort-hand (range min-sort-hand (+ 5 min-sort-hand) )) true
+      (= sort-hand-r (range 1 6)) true
+      :else false)))
 
 (defn straight-flush? [hand]
-  nil)
+  (and (straight? hand) (flush? hand)))
+
+(defn high-card? [hand]
+  true)
 
 (defn value [hand]
-  nil)
+  (let [checkers #{[high-card? 0]  [pair? 1]
+                 [two-pairs? 2]  [three-of-a-kind? 3]
+                 [straight? 4]   [flush? 5]
+                 [full-house? 6] [four-of-a-kind? 7]
+                 [straight-flush? 8]}]
+  (apply max (map (fn [[ff xx]] (if (ff hand) xx 0)) checkers))))
