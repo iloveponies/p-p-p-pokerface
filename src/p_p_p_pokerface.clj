ab5b0cb11ce107cb7a9004672d845c3e5fe8750e
unknown <creamcheese2z@gmail.com>
2014-11-07 04:17:10 +0200

    pokeri

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,56 @@
 (ns p-p-p-pokerface)
 
+(def replacements {\T 10, \J 11, \Q 12, \K 13, \A 14})
+
 (defn rank [card]
-  nil)
+  (let [[rank _] card]
+    (if (Character/isDigit rank)
+    (Integer/valueOf (str rank))
+    (replacements rank))))
 
 (defn suit [card]
-  nil)
+  (let [[_ suit] card]
+    (str suit)))
 
 (defn pair? [hand]
-  nil)
+  (<= 2 (apply max (vals (frequencies (map rank hand))))))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (<= 3 (apply max (vals (frequencies (map rank hand))))))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (<= 4 (apply max (vals (frequencies (map rank hand))))))
 
 (defn flush? [hand]
-  nil)
+  (apply = (map suit hand)))
 
 (defn full-house? [hand]
-  nil)
+  (let [[f s] (sort (vals (frequencies (map rank hand))))]
+    (and (== 2 f) (== 3 s))))
 
 (defn two-pairs? [hand]
-  nil)
+  (let [[f s t] (sort (vals (frequencies (map rank hand))))]
+    (or (four-of-a-kind? hand) (== 2 s t))))
 
 (defn straight? [hand]
-  nil)
+  (let [v (sort (map rank hand))
+        f (first v)
+        l (nth v 4)]
+    (if (== l 14)
+      (or (= v (range 10 15)) (= v [2 3 4 5 14]))
+      (= v (range f (+ l 1))))))
 
 (defn straight-flush? [hand]
-  nil)
+  (and (flush? hand) (straight? hand)))
 
 (defn value [hand]
-  nil)
+  (cond
+   (straight-flush? hand) 8
+   (four-of-a-kind? hand) 7
+   (full-house? hand) 6
+   (flush? hand) 5
+   (straight? hand) 4
+   (three-of-a-kind? hand) 3
+   (two-pairs? hand) 2
+   (pair? hand) 1
+   :else 0))
