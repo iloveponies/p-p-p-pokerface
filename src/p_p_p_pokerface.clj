4fd99a51cc765dca4592139cc536216aa3bccbad
mssimo <matkarki@gmail.com>
2014-11-15 21:49:52 +0200

    poker completed

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,54 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+  (let [[rank _] card]
+    (if (Character/isDigit rank)
+      (Integer/valueOf (str rank))
+      (get {\T 10, \J 11, \Q 12, \K 13, \A 14} rank))))
 
 (defn suit [card]
-  nil)
+  (let [[_ suit] card]
+    (str suit)))
 
 (defn pair? [hand]
-  nil)
+  (= (apply max (vals(frequencies(map rank hand)))) 2))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (= (apply max (vals(frequencies(map rank hand)))) 3))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (= (apply max (vals(frequencies(map rank hand)))) 4))
 
 (defn flush? [hand]
-  nil)
+  (= (apply max (vals(frequencies(map suit hand)))) 5))
 
 (defn full-house? [hand]
-  nil)
+  (let [full-house (sort(vals(frequencies(map rank hand))))]
+    (= [2 3] full-house)))
 
 (defn two-pairs? [hand]
-  nil)
+  (let [two-pairs (sort(vals(frequencies(map rank hand))))]
+    (or (= [1 2 2] two-pairs) (= [1 4] two-pairs))))
 
 (defn straight? [hand]
-  nil)
+  (let [straight (sort(map rank hand))]
+    (if (= straight (range (first straight) (+ (first straight) 5)))
+      true
+      (if (= (last straight) 14)
+        (= straight [2 3 4 5 14])
+        false))))
 
 (defn straight-flush? [hand]
-  nil)
+  (and (flush? hand) (straight? hand)))
 
 (defn value [hand]
-  nil)
+  (cond
+   (straight-flush? hand) 8
+   (four-of-a-kind? hand) 7
+   (full-house? hand) 6
+   (flush? hand) 5
+   (straight? hand) 4
+   (three-of-a-kind? hand) 3
+   (two-pairs? hand) 2
+   (pair? hand) 1
+   :else 0))
