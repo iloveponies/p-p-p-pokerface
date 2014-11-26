ef2e7df82622c8ad240d8e892de0bced9cf3b6c4
Toby Nance <toby.nance@gmail.com>
2014-11-25 21:38:04 -0600

    done!

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,74 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+  (let [r (first card)]
+    (case r
+      \A 14
+      \K 13
+      \Q 12
+      \J 11
+      \T 10
+      (Integer/valueOf (str r)))))
 
 (defn suit [card]
-  nil)
+  (str (second card)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defn in?
+  "true if seq contains elem"
+  [seq elem]
+  (= true (some #(= elem %) seq)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defn card-frequencies [hand]
+  (vals (frequencies (map rank hand))))
 
 (defn pair? [hand]
-  nil)
+  (in? (card-frequencies hand) 2))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (in? (card-frequencies hand) 3))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (in? (card-frequencies hand) 4))
 
 (defn flush? [hand]
-  nil)
+  (= 1 (count (set (map suit hand)))))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defn full-house? [hand]
-  nil)
+  (and
+    (in? (card-frequencies hand) 3)
+    (in? (card-frequencies hand) 2)))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defn two-pairs? [hand]
-  nil)
+  (= 2 ((frequencies (card-frequencies hand)) 2)))
 
 (defn straight? [hand]
-  nil)
+  (let [ranks (sort (map rank hand))
+        low-ranks (sort (replace {14 1} ranks))]
+    (or
+      (let [lowest (apply min ranks)]
+        (= ranks (range lowest (+ lowest 5))))
+      (let [lowest (apply min low-ranks)]
+        (= low-ranks (range lowest (+ lowest 5)))))))
+
+
 
 (defn straight-flush? [hand]
-  nil)
+  (and
+    (straight? hand)
+    (flush? hand)))
 
 (defn value [hand]
-  nil)
+  (cond
+    (straight-flush? hand) 8
+    (four-of-a-kind? hand) 7
+    (full-house? hand) 6
+    (flush? hand) 5
+    (straight? hand) 4
+    (three-of-a-kind? hand) 3
+    (two-pairs? hand) 2
+    (pair? hand) 1
+    :else 0))
