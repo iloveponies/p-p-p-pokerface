1e57c8a05b1e0b960147ecd5ceb4a12ecd980453
Olli Björkqvist <olof@crunchbang.(none)>
2014-12-05 18:49:25 +0200

    melkein ok

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,61 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+  (let [[fst _] card] 
+  (get {\2 2, \3 3, \4 4, \5 5, \6 6, \7 7, \8 8, \9 9, \T 10, \J 11, \Q 12, \K 13, \A 14} fst))
+)
 
 (defn suit [card]
-  nil)
+  (let [[_ snd] card] (str snd))
+)
+
+(defn freq [hand]
+(vals (frequencies (map (fn [x] (rank x)) hand)))
+)
 
 (defn pair? [hand]
-  nil)
+(<= 2 (apply max (freq hand)))
+)
 
 (defn three-of-a-kind? [hand]
-  nil)
+(<= 3 (apply max (freq hand)))
+)
 
 (defn four-of-a-kind? [hand]
-  nil)
+(<= 4 (apply max (freq hand)))
+)
 
 (defn flush? [hand]
-  nil)
+(= 5 (apply max(vals (frequencies (map (fn [x] (suit x)) hand)))))
+)
 
 (defn full-house? [hand]
-  nil)
+(and (= (first(freq hand)) 3) (= (last (freq hand)) 2))
+)
 
 (defn two-pairs? [hand]
-  nil)
+(and (<= (first(freq hand)) 2) (= (first (rest (freq hand))) 2))
+)
 
 (defn straight? [hand]
-  nil)
+(or (= (sort (map (fn [x] (rank x)) hand)) [2,3,4,5,14]) (= (sort (map (fn [x] (rank x)) hand)) (range (apply min (map (fn [x] (rank x)) hand)) (+ (apply min (map (fn [x] (rank x)) hand)) 5))))
+)
 
 (defn straight-flush? [hand]
-  nil)
+(and (flush? hand) (straight? hand))
+)
+
+(defn high-card? [hand] true)
 
 (defn value [hand]
-  nil)
+(let [handvalue (cond 
+(straight-flush? hand) 8 
+(four-of-a-kind? hand) 7
+(full-house? hand) 6
+(flush? hand) 5
+(straight? hand) 4
+(three-of-a-kind? hand) 3
+(two-pairs? hand) 2
+(pair? hand) 1
+:else 0)] (Integer/valueOf handvalue))
+)
