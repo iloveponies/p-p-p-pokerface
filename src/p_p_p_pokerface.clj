29fbce7a654f958b588b6ffabedb9e3719d42a21
cminekime <thor@asgard.local>
2014-12-11 16:14:32 -0500

    First stab at the solution

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,55 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+  (let [[r s]      card
+        face-cards {\T 10 \J 11 \Q 12 \K 13 \A 14}]
+    (if (Character/isDigit r)
+      (Integer/valueOf (str r))
+      (get face-cards r))))
 
 (defn suit [card]
-  nil)
+  (let [[r s] card]
+    (str s)))
 
 (defn pair? [hand]
-  nil)
+  (let [x (frequencies (map rank hand))
+        n (count (filter #{2} (vals x)))]
+    (= n 1)))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (let [x (frequencies (map rank hand))
+        n (count (filter #{3} (vals x)))]
+    (= n 1)))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (let [x (frequencies (map rank hand))
+        n (count (filter #{4} (vals x)))]
+    (= n 1)))
 
 (defn flush? [hand]
-  nil)
+  (let [x (frequencies (map suit hand))]
+    (contains? (set (vals x)) 5)))
 
 (defn full-house? [hand]
-  nil)
+  (and (pair? hand) (three-of-a-kind? hand)))
 
 (defn two-pairs? [hand]
-  nil)
+  (let [x (frequencies (map rank hand))
+        n (count (filter #{2} (vals x)))]
+    (= n 2)))
 
 (defn straight? [hand]
-  nil)
+  (let [ranked-hand (map rank hand)
+        sorted-hand (sort (cond
+                           (contains? (set ranked-hand) 13) ranked-hand
+                           (contains? (set ranked-hand) 2) (replace {14 1} ranked-hand)
+                           :else ranked-hand))
+        min-card (apply min sorted-hand)
+        base-hand (range min-card (+ min-card 5))]
+    (= base-hand sorted-hand)))
 
 (defn straight-flush? [hand]
-  nil)
+  (and (straight? hand) (flush? hand)))
 
 (defn value [hand]
   nil)
