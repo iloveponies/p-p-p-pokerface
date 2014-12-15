46a30c4ad6f54cb44a93667c402fcb82b6f863ac
Ilari Oras <happosade@users.noreply.github.com>
2014-12-15 04:16:17 +0200

    Doned
    #fyollo

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,60 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+  (let [[v _] card
+        values {\T 10, \J 11, \Q 12, \K 13, \A 14}]
+    (if (Character/isDigit v)
+      (Integer/valueOf (str v))
+      (values v))))
+
+(defn such-ranks [hand]
+  (vals (frequencies (map rank hand))))
 
 (defn suit [card]
-  nil)
+  (let [[_ s] card]
+    (str s)))
+
+(defn such-suit [hand]
+  (vals (frequencies (map suit hand))))
 
 (defn pair? [hand]
-  nil)
+  (contains? (set (such-ranks hand)) 2))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (contains? (set (such-ranks hand)) 3))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (contains? (set (such-ranks hand)) 4))
 
 (defn flush? [hand]
-  nil)
+  (contains? (set (such-suit hand)) 5))
 
 (defn full-house? [hand]
-  nil)
+  (= [2 3] (sort (set (such-ranks hand)))))
 
 (defn two-pairs? [hand]
-  nil)
+  (= [1 2 2] (sort (such-ranks hand))))
 
 (defn straight? [hand]
-  nil)
+  (let [ranks (sort (map rank hand))
+        aranks (sort (replace {14 1} ranks))
+        shouldbe (range (apply min ranks) (+ (apply min ranks) 5))
+        ashouldbe (range (apply min aranks) (+ (apply min aranks) 5))]
+    (or (= ranks shouldbe)
+        (= aranks ashouldbe))))
 
 (defn straight-flush? [hand]
-  nil)
+  (and (flush? hand)
+       (straight? hand)))
 
 (defn value [hand]
-  nil)
+  (cond
+    (straight-flush? hand) 8
+    (four-of-a-kind? hand) 7
+    (full-house? hand) 6
+    (flush? hand) 5
+    (straight? hand) 4
+    (three-of-a-kind? hand) 3
+    (two-pairs? hand) 2
+    (pair? hand) 1
+    :else 0))
