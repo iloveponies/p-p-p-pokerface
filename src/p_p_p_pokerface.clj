e3bc237a8503c98eb62c29bee4511417d380c58a
jjelfs <jjelfs@travelrepublic.co.uk>
2014-12-19 11:44:20 +0000

    passing all tests

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,12 +1,31 @@
 (ns p-p-p-pokerface)
 
+;;some test hands
+;;(def high-seven                   ["2H" "3S" "4C" "5C" "7D"])
+;;(def pair-hand                    ["2H" "2S" "4C" "5C" "7D"])
+;;(def two-pairs-hand               ["2H" "2S" "4C" "4D" "7D"])
+;;(def three-of-a-kind-hand         ["2H" "2S" "2C" "4D" "7D"])
+;;(def four-of-a-kind-hand          ["2H" "2S" "2C" "2D" "7D"])
+;;(def straight-hand                ["2H" "3S" "6C" "5D" "4D"])
+;;(def low-ace-straight-hand        ["2H" "3S" "4C" "5D" "AD"])
+;;(def high-ace-straight-hand       ["TH" "AS" "QC" "KD" "JD"])
+;;(def flush-hand                   ["2H" "4H" "5H" "9H" "7H"])
+;;(def full-house-hand              ["2H" "5D" "2D" "2C" "5S"])
+;;(def straight-flush-hand          ["2H" "3H" "6H" "5H" "4H"])
+;;(def low-ace-straight-flush-hand  ["2D" "3D" "4D" "5D" "AD"])
+;;(def high-ace-straight-flush-hand ["TS" "AS" "QS" "KS" "JS"])
+
 (def pic-cards { \T 10, \J 11, \Q 12, \K 13, \A 14 })
 
-(defn rank [card]
-  (let [[fst _] card]
-	(if (Character/isDigit fst) 
-		(Integer/valueOf (str fst))
-		(get pic-cards fst))))
+(defn high-card? [hand] true)
+
+(defn rank 
+	([card] (rank card pic-cards))
+	([card pic-map]
+	  (let [[fst _] card]
+		(if (Character/isDigit fst) 
+			(Integer/valueOf (str fst))
+			(get pic-map fst)))))
 
 (defn suit [card]
   (let [[_ snd] card] (str snd)))
@@ -25,7 +44,7 @@
 	(n-of-a-kind? hand 3))
 
 (defn four-of-a-kind? [hand]
-	(n-of-a-kind? hand 3))
+	(n-of-a-kind? hand 4))
 
 (defn flush? [hand]
   (= (apply max (freq-vals suit hand)) 5))
@@ -38,18 +57,40 @@
   	(let [fv (freq-vals rank hand)]
   		(= [1 2 2] (sort fv))))
 
-;;sort the ranks
-;;create a range that starts at the same first val
-;;compare
-;;if not equal replace 14 with 1 and try again
-(defn straight? [hand]
-  (let [r (sort (map rank hand))
-		fs (first r)
+(defn aces-low-rank [card] (rank card (assoc pic-cards \A 1)))
+
+(defn sorted-ranks [ranker hand]
+	(sort (map ranker hand)))
+
+(defn straight-range? [ranks]
+	(let [fs (first ranks)
 		rng (range fs (+ fs 5))]
-		(= rng r))) 
+		(= ranks rng)))
+
+(defn straight? [hand]
+  (let [r (sorted-ranks rank hand)
+  		l (sorted-ranks aces-low-rank hand)]
+  		(or (straight-range? r) (straight-range? l))))
 
 (defn straight-flush? [hand]
-  nil)
+  (and (straight? hand) (flush? hand)))
+
+(def checkers #{
+	[high-card? 0]
+	[pair? 1]
+	[two-pairs? 2]
+	[three-of-a-kind? 3]
+	[straight? 4]
+	[flush? 5]
+	[full-house? 6]
+	[four-of-a-kind? 7]
+	[straight-flush? 8]})
+
+(defn filter-checkers [hand checker]
+	(let [[checker-fn? _] checker]
+		(checker-fn? hand)))
 
 (defn value [hand]
-  nil)
+  (let [matching-checkers (filter (partial filter-checkers hand) checkers)
+  		matching-scores (map #(second %) matching-checkers)]
+  		(apply max matching-scores)))
