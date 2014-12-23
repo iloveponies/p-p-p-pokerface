2cec5e839e81b2f42e74d614f37071cd5b3608d0
Dexter Lowe <dexter.lowe@imagini.net>
2014-12-23 15:21:59 +0000

    EX-11 Implemented hand valuation function, also added a high-card checker function as a helper

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -66,5 +66,15 @@
     (flush? hand)
     (straight? hand)))
 
+(defn high-card? [hand]
+  true)
+
 (defn value [hand]
-  nil)
+  (let [checkers #{[high-card? 0]  [pair? 1]
+                 [two-pairs? 2]  [three-of-a-kind? 3]
+                 [straight? 4]   [flush? 5]
+                 [full-house? 6] [four-of-a-kind? 7]
+                 [straight-flush? 8]}
+        scorer (fn [[checker score]] (if (checker hand) score -1))
+        scores (map scorer checkers)]
+    (apply max scores)))
