fe1ee03b01a7722ebec995aa285c28a7c6c75142
Anton Paronen <revverbi@gmail.com>
2014-12-28 21:32:55 +0200

    8/11 exercises done; added helper functions for the exercises

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,28 +1,60 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+  (let [ranks {\T 10, \J 11, \Q 12, \K 13, \A 14}
+        [card-rank _] card]
+    (if (Character/isDigit card-rank)
+      (Integer/valueOf (str card-rank))
+      (ranks card-rank))))
 
 (defn suit [card]
-  nil)
+  (let [[_ card-suit] card]
+    (str card-suit)))
+
+
+; Helper functions
+
+(defn rank-frequencies
+  "Returns a map from ranks in a hand to the number of times they appear."
+  [hand]
+  (let [hand-ranks (map rank hand)]
+    (frequencies hand-ranks)))
+
+(defn max-of-a-kind
+  "Returns the greatest quantity of cards with mutual ranks in a hand."
+  [hand]
+  (let [rank-freqs (rank-frequencies hand)]
+    (apply max (vals rank-freqs))))
+
+(defn sorted-counts
+  "Returns the sorted counts of cards with mutual ranks."
+  [hand]
+  (let [rank-freqs (rank-frequencies hand)]
+    (sort (vals rank-freqs))))
+
 
 (defn pair? [hand]
-  nil)
+  (>= (max-of-a-kind hand) 2))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (>= (max-of-a-kind hand) 3))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (== (max-of-a-kind hand) 4))
 
 (defn flush? [hand]
-  nil)
+  (let [hand-suits (map suit hand)]
+    (== (count (set hand-suits)) 1)))
 
 (defn full-house? [hand]
-  nil)
+  (let [sorted-cs (sorted-counts hand)]
+    (= sorted-cs [2 3])))
 
 (defn two-pairs? [hand]
-  nil)
+  (let [sorted-cs (sorted-counts hand)]
+    (or
+      (= sorted-cs [1 2 2])
+      (= sorted-cs [1 4]))))
 
 (defn straight? [hand]
   nil)
