ac8d7bc7e57acf3013a43fe1114703799730a1b3
ejaasaari <elias.jaasaari@gmail.com>
2014-12-29 17:21:45 +0200

    done

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,54 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+  (let [[rank _] card]
+    (if (Character/isDigit rank)
+      (Integer/valueOf (str rank))
+      (get {\T 10, \J 11, \Q 12, \K 13, \A 14} rank))))
 
 (defn suit [card]
-  nil)
+  (let [[_ suit] card]
+    (str suit)))
+
+(defn n-of-a-kind? [n hand]
+  (contains? (set (vals (frequencies (map rank hand)))) n))
 
 (defn pair? [hand]
-  nil)
+  (n-of-a-kind? 2 hand))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (n-of-a-kind? 3 hand))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (n-of-a-kind? 4 hand))
 
 (defn flush? [hand]
-  nil)
+  (= (count (set (map suit hand))) 1))
 
 (defn full-house? [hand]
-  nil)
+  (and (n-of-a-kind? 2 hand)
+       (n-of-a-kind? 3 hand)))
 
 (defn two-pairs? [hand]
-  nil)
+  (and (<= 2 (count (set (map rank hand))) 3)
+       (not (three-of-a-kind? hand))))
 
 (defn straight? [hand]
-  nil)
+  (let [f (fn [r] (and (apply < (sort r))
+                       (== (- (apply max r) (apply min r)) 4)))
+        ranks (map rank hand)]
+    (or (f ranks) (f (replace {14 1} ranks)))))
 
 (defn straight-flush? [hand]
-  nil)
+  (and (straight? hand) (flush? hand)))
 
 (defn value [hand]
-  nil)
+  (cond (straight-flush?  hand) 8
+        (four-of-a-kind?  hand) 7
+        (full-house?      hand) 6
+        (flush?           hand) 5
+        (straight?        hand) 4
+        (three-of-a-kind? hand) 3
+        (two-pairs?       hand) 2
+        (pair?            hand) 1
+        :else                   0))
