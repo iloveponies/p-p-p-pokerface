d926781c521b4bf30da65757bf4a0b6c084a412c
Samuli Thomasson <simsaladin@paivola.fi>
2015-01-05 19:59:12 +0200

    done

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,49 @@
 (ns p-p-p-pokerface)
 
-(defn rank [card]
-  nil)
+(defn rank [[r _]]
+  (if (Character/isDigit r)
+    (Integer/valueOf (str r))
+    (get {\T 10, \J 11, \Q 12, \K 13, \A 14} r)))
 
-(defn suit [card]
-  nil)
+(defn suit [[_ s]]
+  (str s))
 
 (defn pair? [hand]
-  nil)
+  (boolean (first (filter (fn [x] (< 1 x)) (vals (frequencies (map rank hand)))))))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (boolean (first (filter (fn [x] (< 2 x)) (vals (frequencies (map rank hand)))))))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (boolean (first (filter (fn [x] (< 3 x)) (vals (frequencies (map rank hand)))))))
 
 (defn flush? [hand]
-  nil)
+  (apply = (map suit hand)))
 
 (defn full-house? [hand]
-  nil)
+  (= [2 3] (sort (vals (frequencies (map rank hand))))))
 
 (defn two-pairs? [hand]
-  nil)
+  (= [1 2 2] (sort (vals (frequencies (map rank hand))))))
 
 (defn straight? [hand]
-  nil)
+  (let [sorted (sort (map rank hand))]
+    (or
+      (= (range (first sorted) (+ 5 (first sorted))) sorted)
+      (= [2 3 4 5 14] sorted))))
 
 (defn straight-flush? [hand]
-  nil)
+  (and (straight? hand) (flush? hand)))
 
 (defn value [hand]
-  nil)
+  (let [xs [[pair? 1]
+            [two-pairs? 2]
+            [three-of-a-kind? 3]
+            [straight? 4]
+            [flush? 5]
+            [full-house? 6]
+            [four-of-a-kind? 7]
+            [straight-flush? 8]
+            [(fn [_] true) 0]]
+        match (fn [[f _]] (f hand))]
+    (apply max (map second (filter match xs)))))
