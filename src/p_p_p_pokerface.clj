a096e43c993059ac81eb7c0ac2e3a104a10b9617
Prakhar Srivastav <prakhar1989@gmail.com>
2015-01-06 20:35:49 +0530

    Completed exercise on Pokerface

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,62 @@
 (ns p-p-p-pokerface)
 
-(defn rank [card]
-  nil)
-
 (defn suit [card]
-  nil)
+  (let [[_ s] card] (str s)))
+
+(defn rank [card] 
+  (let [[r _] card
+        replacements {\T 10 \J 11 \Q 12 \K 13 \A 14}]
+    (if (Character/isDigit r) (Integer/valueOf (str r)) 
+     (get replacements r))))
 
 (defn pair? [hand]
-  nil)
+  (let [counts (vals (frequencies (map rank hand)))]
+    (if (nil? (some #(= % 2) counts)) false true)))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (let [counts (vals (frequencies (map rank hand)))]
+    (if (nil? (some #(= % 3) counts)) false true)))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (let [counts (vals (frequencies (map rank hand)))]
+    (if (nil? (some #(= % 4) counts)) false true)))
 
 (defn flush? [hand]
-  nil)
+  (let [counts (first (vals (frequencies (map suit hand))))]
+    (= counts (count hand))))
 
 (defn full-house? [hand]
-  nil)
+  (let [ranks (map rank hand)
+        counts (vals (frequencies ranks))]
+   (= [2 3] (sort counts))))
 
 (defn two-pairs? [hand]
-  nil)
+  (let [counts (vals (frequencies (map rank hand)))]
+    (if (contains? (into #{} counts) 4) true ; handle case of 4
+      (= 2 (get (frequencies counts) 2)))))
 
+;; not working
 (defn straight? [hand]
-  nil)
+  (let [ranks (map rank hand)
+        sorted-ranks (sort ranks)
+        low-ace-sorted-ranks (sort (replace {14 1} ranks)) 
+        min-rank (first ranks)
+        min-sorted-rank (first low-ace-sorted-ranks)]
+    (or (= sorted-ranks (range min-rank (+ 5 min-rank)))
+        (= low-ace-sorted-ranks 
+           (range min-sorted-rank (+ 5 min-sorted-rank))))))
 
 (defn straight-flush? [hand]
-  nil)
+  (and (straight? hand) (flush? hand)))
 
 (defn value [hand]
-  nil)
+  (cond
+    (straight-flush? hand) 8
+    (four-of-a-kind? hand) 7
+    (full-house? hand) 6
+    (flush? hand) 5
+    (straight? hand) 4
+    (three-of-a-kind? hand) 3
+    (two-pairs? hand) 2
+    (pair? hand) 1
+    :else 0))
