95191b85aed54e36fb9cfaf452d5b0e559ce9581
Josh Kaderlan <jkaderlan@yahoo.com>
2015-01-10 18:07:41 -0800

    exercise 6

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,22 +1,27 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+  (let [[rank _] card
+        replacements {\T 10, \J 11, \Q 12, \K 13, \A 14}]
+    (if (not (Character/isDigit rank))
+      (replacements rank)
+      (Integer/valueOf (str rank)))))
 
 (defn suit [card]
-  nil)
+  (let [[_ suit] card]
+    (str suit)))
 
 (defn pair? [hand]
-  nil)
+  (< 1 (apply max (vals (frequencies (map rank hand))))))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (< 2 (apply max (vals (frequencies (map rank hand))))))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (< 3 (apply max (vals (frequencies (map rank hand))))))
 
 (defn flush? [hand]
-  nil)
+  (== 5 (apply max (vals (frequencies (map suit hand))))))
 
 (defn full-house? [hand]
   nil)
