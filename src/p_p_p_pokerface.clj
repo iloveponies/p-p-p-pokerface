057671689b5157583cdcdf3b94eced04100ede12
Kaltsoon <Kalle.ilves@helsinki.fi>
2015-01-18 17:59:39 +0200

    5 exercices done

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,25 +1,40 @@
 (ns p-p-p-pokerface)
 
-(defn rank [card]
-  nil)
+(def replacements {\T 10, \J 11, \Q 12, \K 13, \A 14})
 
-(defn suit [card]
-  nil)
+(defn rank [card] (let [[rank _] card]
+  (if (Character/isDigit rank)
+  (Integer/valueOf (str rank))
+  (get replacements rank))))
 
-(defn pair? [hand]
-  nil)
+(defn suit [card] (let [[_ suit] card] (str suit)))
 
-(defn three-of-a-kind? [hand]
-  nil)
+(defn ranks_of_hand [hand] (map rank hand))
 
-(defn four-of-a-kind? [hand]
-  nil)
+(defn suits_of_hand [hand] (map suit hand))
 
-(defn flush? [hand]
-  nil)
+(defn frequency_of_ranks [hand] (vals (frequencies (ranks_of_hand hand))))
 
-(defn full-house? [hand]
-  nil)
+(defn frequency_of_suits [hand] (keys (frequencies (suits_of_hand hand))))
+
+(defn filter_value_from_rank_frequency [hand value] (filter (fn [v] (== value v)) (frequency_of_ranks hand)))
+
+(defn pair? [hand] (< 0 (count (filter_value_from_rank_frequency hand 2))))
+nil)
+
+(defn three-of-a-kind? [hand] (and
+  (== 1 (count (filter_value_from_rank_frequency hand 3)))
+  (== 2 (count (filter_value_from_rank_frequency hand 1)))))
+
+(defn four-of-a-kind? [hand] (and
+  (== 1 (count (filter_value_from_rank_frequency hand 4)))
+  (== 1 (count (filter_value_from_rank_frequency hand 1)))))
+
+(defn flush? [hand] (== 1 (count (frequency_of_suits hand))))
+
+(defn full-house? [hand] (and
+  (== 1 (count (filter_value_from_rank_frequency hand 3)))
+  (== 1 (count (filter_value_from_rank_frequency hand 2)))))
 
 (defn two-pairs? [hand]
   nil)
