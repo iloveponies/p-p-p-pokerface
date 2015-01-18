f5f688794dbfe61759e812fbdad85f2600ee79ba
Tyrone Palmer <hateration@gmail.com>
2015-01-18 23:46:26 +0000

    Poker hand evaluator implementations complete! Tests passing :D

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -24,16 +24,36 @@
    (== 5 (apply max (vals (frequencies (map suit hand))))))
 
 (defn full-house? [hand]
-  nil)
+  (= [2 3] (sort (vals (frequencies (map rank hand))))))
 
 (defn two-pairs? [hand]
-  nil)
+  (or
+   (four-of-a-kind? hand)
+   (= [1 2 2] (sort (vals (frequencies (map rank hand)))))))
 
 (defn straight? [hand]
-  nil)
+  (let [hand-rank (map rank hand)
+        sorted-rank-high (sort hand-rank)
+        sorted-rank-low (sort (replace {14 1} hand-rank))
+        low-card-rank-high (first sorted-rank-high)
+        low-card-rank-low (first sorted-rank-low)]
+    (or
+     (= (range low-card-rank-high (+ low-card-rank-high 5)) sorted-rank-high)
+     (= (range low-card-rank-low (+ low-card-rank-low 5)) sorted-rank-low))))
 
 (defn straight-flush? [hand]
-  nil)
+  (and
+   (straight? hand)
+   (flush? hand)))
+
+(defn high-card? [hand]
+  true)
 
 (defn value [hand]
-  nil)
+  (let [match? (fn [[func _]] (func hand))
+        checkers #{[high-card? 0]  [pair? 1]
+                 [two-pairs? 2]  [three-of-a-kind? 3]
+                 [straight? 4]   [flush? 5]
+                 [full-house? 6] [four-of-a-kind? 7]
+                 [straight-flush? 8]}]
+    (apply max (map second (filter match? checkers)))))
