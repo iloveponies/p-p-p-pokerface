825da0acb277545e2a6f73269cf56b5389c84453
Kaltsoon <Kalle.ilves@helsinki.fi>
2015-01-18 18:59:29 +0200

    all tests passed

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -3,9 +3,9 @@
 (def replacements {\T 10, \J 11, \Q 12, \K 13, \A 14})
 
 (defn rank [card] (let [[rank _] card]
-  (if (Character/isDigit rank)
-  (Integer/valueOf (str rank))
-  (get replacements rank))))
+                     (if (Character/isDigit rank)
+                        (Integer/valueOf (str rank))
+                        (get replacements rank))))
 
 (defn suit [card] (let [[_ suit] card] (str suit)))
 
@@ -19,31 +19,51 @@
 
 (defn filter_value_from_rank_frequency [hand value] (filter (fn [v] (== value v)) (frequency_of_ranks hand)))
 
+
+
+
 (defn pair? [hand] (< 0 (count (filter_value_from_rank_frequency hand 2))))
-nil)
 
 (defn three-of-a-kind? [hand] (and
-  (== 1 (count (filter_value_from_rank_frequency hand 3)))
-  (== 2 (count (filter_value_from_rank_frequency hand 1)))))
+                                (== 1 (count (filter_value_from_rank_frequency hand 3)))
+                                (== 2 (count (filter_value_from_rank_frequency hand 1)))))
 
 (defn four-of-a-kind? [hand] (and
-  (== 1 (count (filter_value_from_rank_frequency hand 4)))
-  (== 1 (count (filter_value_from_rank_frequency hand 1)))))
+                               (== 1 (count (filter_value_from_rank_frequency hand 4)))
+                               (== 1 (count (filter_value_from_rank_frequency hand 1)))))
 
 (defn flush? [hand] (== 1 (count (frequency_of_suits hand))))
 
 (defn full-house? [hand] (and
-  (== 1 (count (filter_value_from_rank_frequency hand 3)))
-  (== 1 (count (filter_value_from_rank_frequency hand 2)))))
+                           (== 1 (count (filter_value_from_rank_frequency hand 3)))
+                           (== 1 (count (filter_value_from_rank_frequency hand 2)))))
 
-(defn two-pairs? [hand]
-  nil)
+(defn two-pairs? [hand] (and
+                          (== 2 (count (filter_value_from_rank_frequency hand 2)))
+                          (== 1 (count (filter_value_from_rank_frequency hand 1)))))
 
-(defn straight? [hand]
-  nil)
+(defn straight? [hand] (let [min_of_ranks_ace_14 (apply min (ranks_of_hand hand))
+                             min_of_ranks_ace_one (apply min (replace {14 1} (ranks_of_hand hand)))]
+                         (or
+                          (=
+                           (sort (ranks_of_hand hand))
+                           (sort (range min_of_ranks_ace_14 (+ min_of_ranks_ace_14 5))))
+                          (=
+                           (sort (replace {14 1} (ranks_of_hand hand)))
+                           (sort (range min_of_ranks_ace_one (+ min_of_ranks_ace_one 5)))))))
 
-(defn straight-flush? [hand]
-  nil)
+(defn straight-flush? [hand] (and (straight? hand) (flush? hand)))
 
-(defn value [hand]
-  nil)
+(defn high-card? [hand] true)
+
+(defn value [hand] (let [checkers #{[high-card? 0]  [pair? 1]
+                 [two-pairs? 2]  [three-of-a-kind? 3]
+                 [straight? 4]   [flush? 5]
+                 [full-house? 6] [four-of-a-kind? 7]
+                 [straight-flush? 8]}]
+                     (apply max
+                       (map
+                        second
+                         (filter
+                          (fn [poker_hand_value_pair] ((first poker_hand_value_pair) hand))
+                           checkers)))))
