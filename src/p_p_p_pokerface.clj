f7d0b234af611f6cf97f3583f9876e4c50459616
Tero Mäkinen <tero@tero.(none)>
2015-01-30 03:49:36 +0100

    all checks succeeded, first commit

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,53 @@
 (ns p-p-p-pokerface)
 
+(def replacements {\T 10, \J 11, \Q 12, \K 13, \A 14})
+
+(defn rank->int [r]
+  (Integer/valueOf (str r)))
+
 (defn rank [card]
-  nil)
+  (let [r (get card 0)]
+    (if (Character/isDigit r) (rank->int r) (rank->int (replacements r)))))
 
 (defn suit [card]
-  nil)
+  (str (get card 1)))
+
+(defn elem [values value]
+  (boolean (some #{value} values)))
 
 (defn pair? [hand]
-  nil)
+  (elem (vals (frequencies (map rank hand))) 2))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (elem (vals (frequencies (map rank hand))) 3))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (elem (vals (frequencies (map rank hand))) 4))
 
 (defn flush? [hand]
-  nil)
+  (= (count (set (map suit hand))) 1))
 
 (defn full-house? [hand]
-  nil)
+  (and (pair? hand) (three-of-a-kind? hand)))
 
 (defn two-pairs? [hand]
-  nil)
+  (= (count (filter #{2} (vals (frequencies (map rank hand))))) 2))
 
 (defn straight? [hand]
-  nil)
+  (let [r (sort (map rank hand))
+	s (apply min r)]
+    (or (= r (range s (+ s 5)))
+        (= r (seq [2 3 4 5 14])))))
 
 (defn straight-flush? [hand]
-  nil)
+  (and (flush? hand) (straight? hand)))
 
 (defn value [hand]
-  nil)
+  (if (straight-flush? hand) 8
+    (if (four-of-a-kind? hand) 7
+      (if (full-house? hand) 6
+        (if (flush? hand) 5
+          (if (straight? hand) 4
+            (if (three-of-a-kind? hand) 3
+              (if (two-pairs? hand) 2
+                (if (pair? hand) 1 0)))))))))
