b3dc0036c57a219ee7d0eeba2c2158069f662bb1
jackrl <jackrl@users.noreply.github.com>
2015-02-02 20:57:40 +0200

    p-p-p-pokerface done

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,59 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+  (let [[rank _] card
+        replacements {\T 10
+                      \J 11
+                      \Q 12
+                      \K 13
+                      \A 14}]
+    (if (Character/isDigit rank)
+      (Integer/valueOf (str rank))
+      (replacements rank))))
 
 (defn suit [card]
-  nil)
+  (let [[_ suit] card]
+    (str suit)))
+
+(defn high-card? [hand]
+  true)
 
 (defn pair? [hand]
-  nil)
+  (contains? (set (vals (frequencies (map rank hand)))) 2))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (contains? (set (vals (frequencies (map rank hand)))) 3))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (contains? (set (vals (frequencies (map rank hand)))) 4))
 
 (defn flush? [hand]
-  nil)
+  (contains? (set (vals (frequencies (map suit hand)))) 5))
 
 (defn full-house? [hand]
-  nil)
+  (and (pair? hand) (three-of-a-kind? hand)))
 
 (defn two-pairs? [hand]
-  nil)
+  (if (pair? hand)
+    (== (count (vals (frequencies (map rank hand)))) 3)
+    false))
 
 (defn straight? [hand]
-  nil)
+  (let [ranked-hand              (sort (map rank hand))
+        ranked-hand-min          (first ranked-hand)
+        ace-replaced-hand        (sort (replace {14 1} ranked-hand))
+        ace-replaced-hand-min    (first ace-replaced-hand)]
+    (or
+      (= ranked-hand (range ranked-hand-min (+ ranked-hand-min 5)))
+      (= ace-replaced-hand (range ace-replaced-hand-min (+ ace-replaced-hand-min 5))))))
 
 (defn straight-flush? [hand]
-  nil)
+  (and (straight? hand) (flush? hand)))
 
 (defn value [hand]
-  nil)
+  (let [checkers      #{[high-card? 0]  [pair? 1]
+                        [two-pairs? 2]  [three-of-a-kind? 3]
+                        [straight? 4]   [flush? 5]
+                        [full-house? 6] [four-of-a-kind? 7]
+                        [straight-flush? 8]}]
+    (apply max (map second (filter (fn [x] ((first x) hand)) checkers)))))
