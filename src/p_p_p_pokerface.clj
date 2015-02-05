4e5f0313117b2a0b0cb6c0f05e69ef7aece3cb34
saleer <eero.t.salminen>
2015-02-05 14:52:25 +0200

    Completeness of readyness
    ...

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,63 @@
 (ns p-p-p-pokerface)
 
-(defn rank [card]
-  nil)
+; helper
+(def rankmap {\T 10, \J 11, \Q 12, \K 13, \A 14})
 
 (defn suit [card]
-  nil)
+  (let [ [fst snd] card ]
+    (str snd)))
+
+(defn rank [card]
+  (let [ [fst snd] card ]
+    (if (Character/isDigit fst)
+        (Integer/valueOf (str fst))
+        (get rankmap fst))))
 
 (defn pair? [hand]
-  nil)
+  (= 2 (apply max (vals (frequencies (map rank hand))))))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (= 3 (apply max (vals (frequencies (map rank hand))))))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (= 4 (apply max (vals (frequencies (map rank hand))))))
 
 (defn flush? [hand]
-  nil)
+  (= 5 (apply max (vals (frequencies (map suit hand))))))
 
 (defn full-house? [hand]
-  nil)
+  (and (not (four-of-a-kind? hand))
+       (= 2 (count (vals (frequencies (map rank hand)))))))
 
 (defn two-pairs? [hand]
-  nil)
+  (or
+     (and (not (three-of-a-kind? hand) )
+          (= 3 (count (vals (frequencies (map rank hand))))))
+     (four-of-a-kind? hand)))
+
 
 (defn straight? [hand]
-  nil)
+  (let [hvals (map rank hand)
+        hvalsb (replace {1 14, 14 1} hvals)
+        ]
+    (or
+       (= (sort hvals) (range (apply min hvals) (+ (apply min hvals) 5)))
+       (= (sort hvalsb) (range (apply min hvalsb) (+ (apply min hvalsb) 5))))))
+
 
 (defn straight-flush? [hand]
-  nil)
+  (and
+    (flush? hand)
+    (straight? hand)))
 
 (defn value [hand]
-  nil)
+  (cond
+        (straight-flush? hand) 8
+        (four-of-a-kind? hand) 7
+        (full-house? hand) 6
+        (flush? hand) 5
+        (straight? hand) 4
+        (three-of-a-kind? hand) 3
+        (two-pairs? hand) 2
+        (pair? hand) 1
+        :else 0))
