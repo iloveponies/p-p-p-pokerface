fb2f37a94fecebabc73a1c8f077cbfd94b20df02
Ken Are Astrup <ken.astrup@gmail.com>
2015-02-15 18:45:42 +0100

    Ferdig med alle

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -52,5 +52,17 @@
   (and (straight? hand)
        (flush? hand)))
 
+(defn high-card? [hand]
+  true)
+
 (defn value [hand]
-  nil)
+  (let [checkers #{[high-card? 0]
+                   [pair? 1]
+                   [two-pairs? 2]
+                   [three-of-a-kind? 3]
+                   [straight? 4]
+                   [flush? 5]
+                   [full-house? 6]
+                   [four-of-a-kind? 7]
+                   [straight-flush? 8]}]
+    (apply max (map second (filter (fn [x] ((first x) hand)) checkers)))))
