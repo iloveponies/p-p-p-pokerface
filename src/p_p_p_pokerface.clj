f305fcd500161d09fa6859da2645bcb41f2580ce
Ken Are Astrup <ken.astrup@gmail.com>
2015-02-15 01:10:57 +0100

    mange er nå løst

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,31 +1,52 @@
 (ns p-p-p-pokerface)
 
+(def toppkortene {\T 10
+                  \J 11
+                  \Q 12
+                  \K 13
+                  \A 14})
+
 (defn rank [card]
-  nil)
+  (let [[first _] card]
+    (if (Character/isDigit first)
+      (Integer/valueOf (str first))
+      (get toppkortene first))))
 
 (defn suit [card]
-  nil)
+  (let [[_ second] card]
+    (str second)))
+
+(defn max-frekvens [hand]
+  (apply max (vals (frequencies (map rank hand)))))
 
 (defn pair? [hand]
-  nil)
+  (= 2 (max-frekvens hand)))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (= 3 (max-frekvens hand)))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (= 4 (max-frekvens hand)))
 
 (defn flush? [hand]
-  nil)
+  (= 5 (apply max (vals (frequencies (map suit hand))))))
 
 (defn full-house? [hand]
-  nil)
+  (= (range 2 4) (sort (vals (frequencies (map rank hand))))))
 
 (defn two-pairs? [hand]
-  nil)
+  (or
+    (= 2 (get (frequencies (vals (frequencies (map rank hand)))) 2))
+    (four-of-a-kind? hand)))
 
 (defn straight? [hand]
-  nil)
+  (let [handverdier (map rank hand)
+        handverdier-b (replace {14 1} handverdier)]
+    (and
+      (= 1 (max-frekvens hand))
+      (or
+        (= 4 (- (min handverdier) (max handverdier)))
+        (= 4 (- (min handverdier-b) (max handverdier-b)))))))
 
 (defn straight-flush? [hand]
   nil)
