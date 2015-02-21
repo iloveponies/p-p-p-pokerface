6145406935db59fb63d8a68739c0ca9287c1b7af
Mika Hämäläinen <mika@mikakalevi.com>
2015-02-21 16:38:45 +0200

    toimii

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,63 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+  (let [[rank _] card]
+    (if (Character/isDigit rank)
+      (Integer/valueOf (str rank))
+      (get {\T 10, \J 11, \Q 12, \K 13, \A 14} rank))))
 
 (defn suit [card]
-  nil)
+  (let [[_ suit] card]
+    (str suit)))
 
 (defn pair? [hand]
-  nil)
+  (< (count(set (map rank hand))) (count hand)))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (> (count (filter (fn [x] (>= x 3)) (vals
+   (frequencies (map rank hand))))) 0))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (> (count (filter (fn [x] (>= x 4)) (vals
+   (frequencies (map rank hand))))) 0))
 
 (defn flush? [hand]
-  nil)
+  (== (count (set (map suit hand))) 1))
 
 (defn full-house? [hand]
-  nil)
+  (let [shand (sort hand)
+        fcard (rank (first shand))]
+    (and
+     (three-of-a-kind? (filter (fn [x] (== (rank x) fcard)) shand))
+     (pair? (filter (fn [x] (not (== (rank x) fcard))) shand)))))
+
 
 (defn two-pairs? [hand]
-  nil)
+  (let [shand (sort hand)
+        fcard (rank (first shand))]
+    (and
+     (pair? (filter (fn [x] (== (rank x) fcard)) shand))
+     (pair? (filter (fn [x] (not (== (rank x) fcard))) shand)))))
 
 (defn straight? [hand]
-  nil)
+  (let [shand (sort (map rank hand))
+        comp-cards (fn [h] (= h (range (first h) (+ (first h) 5))))]
+    (or (comp-cards shand) (comp-cards (sort ( replace {14 1} shand))))))
 
 (defn straight-flush? [hand]
-  nil)
+  (and (straight? hand) (flush? hand)))
+
+(defn high-card? [hand]
+true)
 
 (defn value [hand]
-  nil)
+ (let [checkers #{[high-card? 0]  [pair? 1]
+                 [two-pairs? 2]  [three-of-a-kind? 3]
+                 [straight? 4]   [flush? 5]
+                 [full-house? 6] [four-of-a-kind? 7]
+                 [straight-flush? 8]}]
+  (apply max
+         (map second
+              (filter
+               (fn [checker]
+                 ( (first checker) hand)) checkers)))))
