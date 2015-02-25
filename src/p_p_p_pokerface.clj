2a78dfc14a9fb7c22c57058a49da4dbb3a2ca544
Evgeny Minkevich <eminkevich@scentregroup.com>
2015-02-25 18:24:10 +1100

    Completed

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -63,5 +63,16 @@
 (defn straight-flush? [hand]
   (and (straight? hand) (flush? hand)))
 
+(defn high-card? [hand]
+  true)
+
 (defn value [hand]
-  nil)
+  (let [checkers #{[high-card? 0] [pair? 1]
+                   [two-pairs? 2] [three-of-a-kind? 3]
+                   [straight? 4] [flush? 5]
+                   [full-house? 6] [four-of-a-kind? 7]
+                   [straight-flush? 8]}
+        _valid (filter #((first %) hand) checkers)
+        _values (map second _valid)]
+    (apply max _values)))
+
