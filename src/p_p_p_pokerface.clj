1865927be675c5dfdec697ca6a8e0f40334d9978
satukark <Satu@82-181-230-236.bb.dnainternet.fi>
2015-03-12 22:57:28 +0200

    7-11
    Poker hands

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -11,6 +11,9 @@
 (defn card-rank [x]
   (let [[fst _] x] fst))
 
+(defn high-card? [hand]
+  true)
+
 (defn pair? [hand]
   (= 2 (apply max (vals (frequencies (map rank hand))))))
 
@@ -24,16 +27,27 @@
   (= 5 (apply max (vals(frequencies (map suit hand))))))
 
 (defn full-house? [hand]
-  nil)
+  (= [2 3] (sort(vals(frequencies (map rank hand))))))
 
 (defn two-pairs? [hand]
-  nil)
+  (let [freq (sort(vals(frequencies (map rank hand))))]
+    (or (= [1 2 2] freq ) (= [1 4] freq ))))
 
 (defn straight? [hand]
-  nil)
+  (let [sorted-a-f (sort (replace {14 1} (map rank hand))) sorted-a-l (sort (map rank hand))
+        freq (= [1 1 1 1 1] (vals(frequencies (map rank hand))))]
+  (or (and (= 4 (- (last sorted-a-f) (first sorted-a-f))) freq)
+      (and (= 4 (- (last sorted-a-l) (first sorted-a-l))) freq))))
 
 (defn straight-flush? [hand]
-  nil)
+  (and (straight? hand) (flush? hand)))
 
 (defn value [hand]
-  nil)
+  (let [checkers #{[high-card? 0]  [pair? 1]
+                 [two-pairs? 2]  [three-of-a-kind? 3]
+                 [straight? 4]   [flush? 5]
+                 [full-house? 6] [four-of-a-kind? 7]
+                 [straight-flush? 8]}
+         if-pair (fn [[pair _]]
+                 (pair hand))]
+    (apply max (map last (filter if-pair checkers)))))
