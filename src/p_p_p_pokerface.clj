ae4a6a8aa672d84a2539b74571560b68d1b1c856
Senni Kaipio <senni.kaipio@eficode.com>
2015-03-14 13:13:12 +0200

    All done

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,70 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+  (let [ranks {\A 14, \T 10, \J 11, \Q 12, \K 13}
+        [fst _] card]
+    (if (Character/isDigit fst) (Integer/valueOf (str fst)) (get ranks fst))))
+
+(defn ranks [hand]
+  (map rank hand))
+
+(defn is-two? [r] (= 2 r))
+
+(defn is-three? [r] (= 3 r))
+
+(defn rank-freqs [hand]
+  (vals (frequencies (ranks hand))))
+
+(defn rank-count [p freqs]
+  (count (filter p freqs)))
 
 (defn suit [card]
-  nil)
+  (let [[_ snd] card]
+    (str snd)))
+
+(defn suits [card]
+  (map suit card))
 
 (defn pair? [hand]
-  nil)
+  (= 1 (rank-count is-two? (rank-freqs hand))))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (= 1 (rank-count is-three? (rank-freqs hand))))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (= 1 (rank-count (fn [r] (= 4 r)) (rank-freqs hand))))
+
+(defn ranks-sorted [hand]
+  (sort (ranks hand)))
 
 (defn flush? [hand]
-  nil)
+    (apply = (suits hand)))
 
 (defn full-house? [hand]
-  nil)
+  (and (= 1 (rank-count is-two? (rank-freqs hand)))
+       (= 1 (rank-count is-three? (rank-freqs hand)))))
 
 (defn two-pairs? [hand]
-  nil)
+   (= 2 (rank-count is-two? (rank-freqs hand))))
 
 (defn straight? [hand]
-  nil)
+  (let [is-straight? (fn [sorted]
+                       (let [fst (first sorted)]
+                         (= sorted (range fst (+ fst 5)))))]
+    (or (is-straight? (ranks-sorted hand))
+        (is-straight? (sort (replace {14 1} (ranks hand)))))))
 
 (defn straight-flush? [hand]
-  nil)
+  (and (flush? hand) (straight? hand)))
+
+(defn high-card? [hand] true)
 
 (defn value [hand]
-  nil)
+  (let [checkers #{[high-card? 0]  [pair? 1]
+                 [two-pairs? 2]  [three-of-a-kind? 3]
+                 [straight? 4]   [flush? 5]
+                 [full-house? 6] [four-of-a-kind? 7]
+                 [straight-flush? 8]}
+        p (fn [[k v]] (k hand))
+        points (map (fn [[k v]] v) (filter p checkers))]
+    (apply max points)))
