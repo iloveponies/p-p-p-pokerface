35d830103152dfa146cb57bf752e24b97cb84c1b
acaramia <user@gmail.com>
2015-03-22 18:10:55 +0100

    test

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,85 @@
 (ns p-p-p-pokerface)
 
+(defn rank2 [card]
+  (let [[r s] card]
+    (if (Character/isDigit r) (Integer/valueOf (str r))
+      (cond
+        (= r \T) 10
+        (= r \J) 11
+        (= r \Q) 12
+        (= r \K) 13
+        (= r \A) 14))))
+
+(def ranks {\T 10, \J 11, \Q 12, \K 13, \A 14})
+
 (defn rank [card]
-  nil)
+  (let [[r s] card]
+    (if (Character/isDigit r)
+      (Integer/valueOf (str r))
+      (ranks r))))
 
 (defn suit [card]
-  nil)
+  (let [[_ s] card]
+    (str s)))
 
 (defn pair? [hand]
-  nil)
+  (>= (apply max (vals (frequencies (map rank hand))))
+      2))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (>= (apply max (vals (frequencies (map rank hand))))
+      3))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (= 4
+     (apply max (vals (frequencies (map rank hand))))))
 
 (defn flush? [hand]
-  nil)
+  ;(println (vals (frequencies (map suit hand))))
+  (= 5
+     (apply max (vals (frequencies (map suit hand))))))
 
 (defn full-house? [hand]
-  nil)
+  (= (range 2 4)
+     (sort (vals (frequencies (map rank hand))))))
 
 (defn two-pairs? [hand]
-  nil)
+  (let [signature (sort (vals (frequencies (map rank hand))))]
+    ;(println "sign" signature)
+    (cond
+     (= [1 2 2] signature) true ; doppia coppia
+     (= [1 4] signature) true ; poker
+     :else
+       false)))
 
 (defn straight? [hand]
-  nil)
+ (let [signature (sort (map rank hand))
+       unique (apply max (vals (frequencies signature))) ; massimo tra i valori forse meglio apply max (map rank hand)
+       rng (- (apply max signature) (apply min signature))] ; differenza tra massimo e minimo
+
+   ;(println "h:" hand "s:" signature "u:" unique "r:" rng)
+   (if (and (= unique 1) (or (= rng 4) (= rng 12))  ) ; se max-min = 4 o 12 (14-2) e i valori sono unici
+     true
+     false)))
 
 (defn straight-flush? [hand]
-  nil)
+  ;(println "sf")
+  (and (flush? hand) (straight? hand)))
+
+(defn high-card? [hand]
+  true) ; All hands have a high card.
 
 (defn value [hand]
-  nil)
+  (let [checkers #{[high-card? 0]  [pair? 1]
+                 [two-pairs? 2]  [three-of-a-kind? 3]
+                 [straight? 4]   [flush? 5]
+                 [full-house? 6] [four-of-a-kind? 7]
+                 [straight-flush? 8]}
+        fl (filter (fn [x] ((first x) hand)) checkers)
+        va (map second fl)
+        mx (apply max va)] ; applico ciascuna fx a hand e prendo solo le true
+
+
+    (println "h" hand "va" va "mx" mx )
+    mx))
+
