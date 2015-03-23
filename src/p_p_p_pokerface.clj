852ce17bb8b73bce9b97946132b5d7f47cc70c9c
jarcane <jarcane@users.noreply.github.com>
2015-03-23 22:17:11 +0200

    Stuck on straight?

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,31 +1,50 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+  (let [subs {\T 10
+              \J 11
+              \Q 12
+              \K 13
+              \A 14}
+        [rank _] card]
+    (if (Character/isDigit rank)
+      (Integer/valueOf (str rank))
+      (subs rank))))
 
 (defn suit [card]
-  nil)
+  (let [[_ suit] card]
+    (str suit)))
+
+(defn more-n? [n h]
+  (>= (apply max (vals (frequencies (map rank h))))
+      n))
 
 (defn pair? [hand]
-  nil)
+  (more-n? 2 hand))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (more-n? 3 hand))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (more-n? 4 hand))
 
 (defn flush? [hand]
-  nil)
+  (apply = (map suit hand)))
 
 (defn full-house? [hand]
-  nil)
+  (= '(2 3)
+     (sort (vals (frequencies (map rank hand))))))
 
 (defn two-pairs? [hand]
-  nil)
+  (= '(1 2 2)
+     (sort (vals (frequencies (map rank hand))))))
 
 (defn straight? [hand]
-  nil)
+  (let [ace-high (sort (map rank hand))
+        ace-low  (sort (replace {14 1} (map rank hand)))
+        ordered? #(= % (range (first %) (last %)))]
+    (or (ordered? ace-high)
+        (ordered? ace-low))))
 
 (defn straight-flush? [hand]
   nil)
