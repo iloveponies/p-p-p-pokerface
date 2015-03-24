c46a42bef3a46e4450095c824b932572478f3928
Nea <nea.pirttinen@cs.helsinki.fi>
2015-03-24 19:04:37 +0200

    Ms. Gaga would be proud

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,58 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+  (def replacements {\T 10, \J 11, \Q 12, \K 13, \A 14})
+    (let [[first _] card]
+      (if (Character/isDigit first)
+        (Integer/valueOf (str first))
+        (replacements first))))
 
 (defn suit [card]
-  nil)
+  (let [[_ second] card]
+    (str second)))
 
 (defn pair? [hand]
-  nil)
+  (== (apply max (vals (frequencies (map rank hand)))) 2))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (== (apply max (vals (frequencies (map rank hand)))) 3))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (== (apply max (vals (frequencies (map rank hand)))) 4))
 
 (defn flush? [hand]
-  nil)
+  (== (apply min (vals (frequencies (map suit hand)))) 5))
 
 (defn full-house? [hand]
-  nil)
+  (and (not (four-of-a-kind? hand))
+       (== (count (vals (frequencies (map rank hand)))) 2)))
 
 (defn two-pairs? [hand]
-  nil)
+  (or (and (not (three-of-a-kind? hand))
+           (== (count (vals (frequencies (map rank hand)))) 3))
+      (four-of-a-kind? hand)))
 
 (defn straight? [hand]
-  nil)
+  (let [handvals (map rank hand)
+        handvalsb (replace {14 1} handvals)]
+    (or (= (sort handvals)
+           (range (apply min handvals)
+                  (+ (apply min handvals) 5)))
+        (= (sort handvalsb)
+           (range (apply min handvalsb)
+                  (+ (apply min handvalsb) 5))))))
 
 (defn straight-flush? [hand]
-  nil)
+  (and (straight? hand) (flush? hand)))
 
 (defn value [hand]
-  nil)
+  (cond
+   (straight-flush? hand) 8
+   (four-of-a-kind? hand) 7
+   (full-house? hand) 6
+   (flush? hand) 5
+   (straight? hand) 4
+   (three-of-a-kind? hand) 3
+   (two-pairs? hand) 2
+   (pair? hand) 1
+   :else 0))
