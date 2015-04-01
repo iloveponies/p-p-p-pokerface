48cd41535d028c0348956844566f6eebb3f30ac1
Eugene Tan <jmingtan@gmail.com>
2015-04-01 22:00:58 +0800

    Up to ex 3

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,13 +1,24 @@
 (ns p-p-p-pokerface)
 
+(def replacements
+  {\T 10
+   \J 11
+   \Q 12
+   \K 13
+   \A 14})
+
 (defn rank [card]
-  nil)
+  (let [[fst _] card]
+    (if (Character/isDigit fst)
+      (Integer/valueOf (str fst))
+      (replacements fst))))
 
 (defn suit [card]
-  nil)
+  (let [[_ snd] card]
+    (str snd)))
 
 (defn pair? [hand]
-  nil)
+  (>= (apply max (vals (frequencies (map rank hand)))) 2))
 
 (defn three-of-a-kind? [hand]
   nil)
