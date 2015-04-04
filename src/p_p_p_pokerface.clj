c15d020f0a91394110c9301f48294505c97ff8f2
Rochet2 <tqbattlenet@gmail.com>
2015-04-04 18:44:03 +0300

    assignmentss

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,46 @@
 (ns p-p-p-pokerface)
 
-(defn rank [card]
-  nil)
+(def ranks {\T 10, \J 11, \Q 12, \K 13, \A 14})
 
-(defn suit [card]
-  nil)
+(defn rank [[rank suit]]
+  (if (Character/isDigit rank)
+    (Integer/valueOf (str rank))
+    (get ranks rank)))
+
+(defn suit [[rank suit]]
+  (str suit))
 
 (defn pair? [hand]
-  nil)
+  (>= (apply max (vals (frequencies (map rank hand)))) 2))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (>= (apply max (vals (frequencies (map rank hand)))) 3))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (>= (apply max (vals (frequencies (map rank hand)))) 4))
 
 (defn flush? [hand]
-  nil)
+  (apply = (map suit hand)))
 
 (defn full-house? [hand]
-  nil)
+  (= (sort (vals (frequencies (map rank hand)))) [2 3]))
 
 (defn two-pairs? [hand]
-  nil)
+  (= (sort (vals (frequencies (map rank hand)))) [1 2 2]))
 
 (defn straight? [hand]
-  nil)
+  (let [vs (sort (map rank hand))]
+    (let [vsmin (apply min vs)]
+      (let [vscomp (map (fn [x] (- x vsmin)) vs)]
+        (or (= vscomp [0 1 2 3 4]) (= vscomp [0 1 2 3 12]))))))
 
 (defn straight-flush? [hand]
-  nil)
+  (and (straight? hand) (flush? hand)))
 
 (defn value [hand]
-  nil)
+  (let [checkers #{[pair? 1]
+                 [two-pairs? 2]  [three-of-a-kind? 3]
+                 [straight? 4]   [flush? 5]
+                 [full-house? 6] [four-of-a-kind? 7]
+                 [straight-flush? 8]}]
+    (apply max (map (fn [[check val]] (if (check hand) val 0)) checkers))))
