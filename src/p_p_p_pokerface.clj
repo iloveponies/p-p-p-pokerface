7ccbef2d62abba72b4c2596d9bf23ef076cb68cc
foghr <antti.kotiranta@cs.helsinki.fi>
2015-04-11 18:07:26 +0300

    tehtäviä

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,25 +1,45 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+  (let [[rnk _] card]
+  (cond
+   (Character/isDigit rnk) (Integer/valueOf (str rnk))
+   :else ({\T 10, \J 11, \Q 12, \K 13, \A 14} rnk))))
+
 
 (defn suit [card]
-  nil)
+  (let [[_ snd] card]
+  (str snd)))
 
 (defn pair? [hand]
-  nil)
+  (if (=
+       (apply max (vals ( frequencies (map rank hand))))
+          2) true false))
 
 (defn three-of-a-kind? [hand]
-  nil)
+   (if (=
+       (apply max (vals ( frequencies (map rank hand))))
+          3) true false))
 
 (defn four-of-a-kind? [hand]
-  nil)
+   (if (=
+       (apply max (vals ( frequencies (map rank hand))))
+          4) true false))
 
 (defn flush? [hand]
-  nil)
+    (if (=
+       (apply max (vals ( frequencies (map suit hand))))
+          5) true false))
 
 (defn full-house? [hand]
-  nil)
+  (let [[c1 c2 c3 c4 c5] hand]
+      (sort hand)
+         (cond
+          ((pair?[c1 c2])(three-of-a-kind?[c3 c4 c5])) true
+          ((pair?[c1 c2 c3])(three-of-a-kind?[c3 c4])) true
+          :else (false))))
+
+
 
 (defn two-pairs? [hand]
   nil)
@@ -32,3 +52,5 @@
 
 (defn value [hand]
   nil)
+
+
