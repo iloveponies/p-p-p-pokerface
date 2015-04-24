354a0f6d1b1e3d17f94aa388f3d570ab6a6d8e20
Paolo Banfi <paolo.banfi@rumbo.com>
2015-04-24 17:26:40 +0200

    manca solo il value

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -26,15 +26,29 @@
   (= (sort (vals (frequencies (map rank hand)))) [2 3]))
 
 (defn two-pairs? [hand]
-  (or (= (sort (vals (frequencies (map rank hand)))) [1 2 2])
-      (= (sort (vals (frequencies (map rank hand)))) [1 4])))
+  (let [sorted-freq (fn [hand] (sort (vals (frequencies (map rank hand)))))]
+  (or (= (sorted-freq hand) [1 2 2])
+      (= (sorted-freq hand) [1 4]))))
 
 (defn straight? [hand]
-  (= (sort (keys (frequencies (map rank hand)))) (range (apply min (map rank hand)) (+ 1 (apply max (map rank hand)))))
+ (or (= (sort (keys (frequencies (map rank hand))))
+      (range (apply min (map rank hand)) (+ 5 (apply min (map rank hand)))))
+     (= (sort (keys (frequencies (replace {14 1} (map rank hand) ))))
+      (range 1 6))))
 
 
 (defn straight-flush? [hand]
-  nil)
+  (and (flush? hand) (straight? hand)))
+
+(defn high-card? [hand]
+  true)
 
 (defn value [hand]
-  nil)
+  (let [checkers #{[high-card? 0]  [pair? 1]
+                 [two-pairs? 2]  [three-of-a-kind? 3]
+                 [straight? 4]   [flush? 5]
+                 [full-house? 6] [four-of-a-kind? 7]
+                 [straight-flush? 8]}]
+
+
+    ))
