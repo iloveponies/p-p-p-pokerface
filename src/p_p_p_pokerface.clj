2df841631c181ededc6749ea2ccd342a2003db0b
Thorsten Gawantka <t.gawantka@gmail.com>
2015-05-05 16:51:51 +0200

    Solved exercise 7.

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -32,7 +32,7 @@
     (not (empty? (filter five? (vals (frequencies (map suit hand))))))))
 
 (defn full-house? [hand]
-  nil)
+  (and (pair? hand) (three-of-a-kind? hand)))
 
 (defn two-pairs? [hand]
   nil)
