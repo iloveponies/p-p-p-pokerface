c43a8300e5944f41149c06e67617952fc96e5ac9
Tomi <tpheiska@saunalahti.fi>
2015-05-17 14:25:40 +0300

    tehty

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,54 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+  (let [[rank _] card]
+   (if (Character/isDigit rank) (Integer/valueOf (str rank)) 
+       (get {\T 10, \J 11, \Q 12, \K 13, \A 14} rank))))
 
 (defn suit [card]
-  nil)
+  (let [[_ suit] card]
+   (str suit)))
 
 (defn pair? [hand]
-  nil)
+  (let [vals (vals (frequencies (map rank hand)))] do 
+  (if (and (== (apply max vals) 2) (== (count vals) 4)) true false)))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (let [vals (vals (frequencies (map rank hand)))] do
+  (if (and (== (apply max vals) 3) (== (count vals) 3)) true false)))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (let [vals (vals (frequencies (map rank hand)))] do
+  (if (and (== (apply max vals) 4) (== (count vals) 2)) true false)))
 
 (defn flush? [hand]
-  nil)
+  (let [vals (vals (frequencies (map suit hand)))] do
+  (if (== (apply max vals) 5) true false)))
 
 (defn full-house? [hand]
-  nil)
+  (let [vals (vals (frequencies (map rank hand)))] do
+  (if (and (== (apply max vals) 3) (== (count vals) 2)) true false)))
 
 (defn two-pairs? [hand]
-  nil)
+  (let [vals (vals (frequencies (map rank hand)))] do
+  (if (and (== (apply max vals) 2) (== (count vals) 3)) true false)))
 
 (defn straight? [hand]
-  nil)
+  (let [cards (sort (map rank hand))
+	min (apply min cards)] do 
+  (if (or (= cards (range min (+ min 5))) (= (sort (replace {14 1} cards)) 
+          (range 1 6))) true false)))
 
 (defn straight-flush? [hand]
-  nil)
+  (if (and (straight? hand) (flush? hand)) true false))
+
+(defn high-card? [hand]
+  true)
 
 (defn value [hand]
-  nil)
+  (let [checkers #{[(high-card? hand) 0] [(pair? hand) 1] [(two-pairs? hand) 2] 
+       [(three-of-a-kind? hand) 3] [(straight? hand) 4] [(flush? hand) 5] 
+       [(full-house? hand) 6] [(four-of-a-kind? hand) 7] 
+       [(straight-flush? hand) 8]}] do 
+  (apply max (map second (filter (fn [match] (first match)) checkers)))))
+
