8b16199b4cfa3525dc3352cd900fe516dfc55c3b
Matti Palomäki <matti.palomaki@helsinki.fi>
2015-05-18 09:20:53 +0000

    v-v-v-valmiita

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,81 @@
 (ns p-p-p-pokerface)
 
+(def isot {"2" 2, "3" 3, "4" 4, "5" 5, "6" 6, "7" 7, "8" 8, "9" 9, "T" 10, "J" 11, "Q" 12, "K" 13, "A" 14})
+
+(def pienet {"2" 2, "3" 3, "4" 4, "5" 5, "6" 6, "7" 7, "8" 8, "9" 9, "T" 10, "J" 11, "Q" 12, "K" 13, "A" 1})
+
 (defn rank [card]
-  nil)
+  (let [[r _] card]
+    (get isot (str r))
+  ))
+
+(defn rank2 [card]
+  (let [[r _] card]
+    (get pienet (str r))
+  ))
 
 (defn suit [card]
-  nil)
+  (let [[_ s] card]
+    (str s)))
 
 (defn pair? [hand]
-  nil)
+  (contains? (set (vals (frequencies (map rank hand)))) 2))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (contains? (set (vals (frequencies (map rank hand)))) 3))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (contains? (set (vals (frequencies (map rank hand)))) 4))
 
 (defn flush? [hand]
-  nil)
+  (contains? (set (vals (frequencies (map suit hand)))) 5))
 
 (defn full-house? [hand]
-  nil)
+  (and (pair? hand) (three-of-a-kind? hand)))
 
 (defn two-pairs? [hand]
-  nil)
+  ;  (if (contains? (set (keys (frequencies (vals (frequencies (map rank hand)))))) 2)
+    ;    (> (get 
+        ;        (set (keys (frequencies (vals (frequencies 
+                  ;                  (map rank hand)))))) 
+        ;        2) 1)
+    ;    (contains? (set (vals (frequencies (map rank hand)))) 4))
+  (and (or (four-of-a-kind? hand) (not (three-of-a-kind? hand))) (> (count hand) (+ 1 (count (set (map rank hand))))))
+)
+
+(defn iso-suora? [hand]
+  (let [kortit (map rank hand)]
+    (and 
+      (== (+ (apply min kortit) 4) 
+        (apply max kortit)) 
+      (== (apply max (vals (frequencies kortit))) 
+        1)
+    )))
+
+(defn pikku-suora? [hand]
+  (let [kortit (map rank2 hand)]
+    (and 
+      (== (+ (apply min kortit) 4) 
+        (apply max kortit)) 
+      (== (apply max (vals (frequencies kortit))) 
+        1)      
+    )))
 
 (defn straight? [hand]
-  nil)
+  (or (iso-suora? hand) (pikku-suora? hand)))
 
 (defn straight-flush? [hand]
-  nil)
+  (and (straight? hand) (flush? hand)))
 
 (defn value [hand]
-  nil)
+  (cond
+    (straight-flush? hand) 8
+    (four-of-a-kind? hand) 7
+    (full-house? hand) 6
+    (flush? hand) 5
+    (straight? hand) 4
+    (three-of-a-kind? hand) 3
+    (two-pairs? hand) 2
+    (pair? hand) 1
+    :else 0
+  ))
