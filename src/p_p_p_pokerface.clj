2b12f9e489e3002b0b6d1f9e936a49d18cc0f26c
ybykov <aS5bPr6n>
2015-05-20 22:31:39 +0200

    intermediate commit

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,28 +1,44 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+  (let [[rnk _] card
+        mp {\T 10
+            \J 11
+            \Q 12
+            \K 13
+            \A 14}]
+    (if (Character/isDigit rnk)
+      (Integer/valueOf (str rnk))
+      (mp rnk))))
 
 (defn suit [card]
-  nil)
+  (let [[_ st] card]
+    (str st)))
 
 (defn pair? [hand]
-  nil)
+  (let [ranks (map rank hand)]
+    (= 2 (apply max (vals (frequencies ranks))))))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (let [ranks (map rank hand)]
+    (= 3 (apply max (vals (frequencies ranks))))))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (let [ranks (map rank hand)]
+    (= 4 (apply max (vals (frequencies ranks))))))
 
 (defn flush? [hand]
-  nil)
+  (let [suits (map suit hand)]
+    (= 5 (apply max (vals (frequencies suits))))))
 
 (defn full-house? [hand]
-  nil)
+  (let [ranks (map rank hand)]
+    (= '(2 3) (sort (vals (frequencies ranks))))))
 
 (defn two-pairs? [hand]
-  nil)
+  (let [ranks (map rank hand)
+        sorted-ranks (sort (vals (frequencies ranks)))]
+    (or (= '(1 2 2) sorted-ranks) (= '(1 4) sorted-ranks))))
 
 (defn straight? [hand]
   nil)
