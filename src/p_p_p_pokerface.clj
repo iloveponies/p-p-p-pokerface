82bc963bf8b1a69aa979743b2aea6babffa0d805
zjmil <zjmiller@purdue.edu>
2015-05-26 12:47:21 -0400

    completed solutions

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,55 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+  (let [high-cards {\T 10, \J 11, \Q 12, \K 13, \A 14}
+        [rank- suit] card]
+    (if (Character/isDigit rank-)
+        (Integer/valueOf (str rank-))
+        (get high-cards rank-))))
 
 (defn suit [card]
-  nil)
+  (let [[rank suit] card]
+    (str suit)))
+
+(defn high-card? [hand]
+    true) ; All hands have a high card.
 
 (defn pair? [hand]
-  nil)
+  (not (empty? (filter (fn [x] (> x 1)) (vals (frequencies (map rank hand)))))))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (not (empty? (filter (fn [x] (> x 2)) (vals (frequencies (map rank hand)))))))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (not (empty? (filter (fn [x] (> x 3)) (vals (frequencies (map rank hand)))))))
 
 (defn flush? [hand]
-  nil)
+  (apply = (map suit hand)))
 
 (defn full-house? [hand]
-  nil)
+  (let [rank-freqs (set (vals (frequencies (map rank hand))))]
+    (= (set [2 3]) rank-freqs)))
 
 (defn two-pairs? [hand]
-  nil)
+  (>= (apply + (filter (fn [x] (> x 1)) (vals (frequencies (map rank hand))))) 4))
 
 (defn straight? [hand]
-  nil)
+  (let [sorted-hand (sort (map rank hand))
+        min-rank (first sorted-hand)
+        straight-length 5]
+    (if (and (= (last sorted-hand) 14) (= min-rank 2))
+        (= (sort (replace {14 1} sorted-hand)) (range 1 (+ 1 straight-length)))
+        (= sorted-hand (range min-rank (+ min-rank straight-length))))))
 
 (defn straight-flush? [hand]
-  nil)
+  (and (straight? hand) (flush? hand)))
 
 (defn value [hand]
-  nil)
+  (let [checkers #{[high-card? 0]  [pair? 1]
+                                    [two-pairs? 2]  [three-of-a-kind? 3]
+                                    [straight? 4]   [flush? 5]
+                                    [full-house? 6] [four-of-a-kind? 7]
+                                    [straight-flush? 8]}
+        true-checks (filter (fn [checker] (let [fun (first checker)] (fun hand))) checkers)
+        weights (map second true-checks)]
+    (apply max weights)))
