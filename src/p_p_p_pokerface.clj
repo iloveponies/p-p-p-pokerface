a959363bbbd72a927afb55e5b9430d2f229ebf6a
ArktinenSieni <matti.raty@helsinki.fi>
2015-06-11 12:19:50 +0300

    pokerinaama

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,67 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+  (let [[r _] card]
+    (if (Character/isDigit r)
+      (Integer/valueOf (str r))
+      (get {\A 14, \K 13, \Q 12,
+            \J 11, \T 10} r))))
 
 (defn suit [card]
-  nil)
+  (let [[_ st] card]
+    (str st)))
+
+(defn rank-frequencies [hand]
+  (let [ranks (map rank hand)]
+    (vals (frequencies ranks))))
 
 (defn pair? [hand]
-  nil)
+  (== 2 (apply max (rank-frequencies hand))))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (== 3 (apply max (rank-frequencies hand))))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (== 4 (apply max (rank-frequencies hand))))
 
 (defn flush? [hand]
-  nil)
+  (let [suits (mapv suit hand)]
+    (let [amount (vals (frequencies suits))]
+      (== 5 (apply max amount)))))
 
 (defn full-house? [hand]
-  nil)
+  (let [freq (rank-frequencies hand)]
+    (and
+     (== 3 (apply max freq))
+     (== 2 (apply min freq)))))
 
 (defn two-pairs? [hand]
-  nil)
+  (let [freq (sort > (rank-frequencies hand))]
+    (let [bigger (drop-last freq)]
+      (and
+       (<= 2 (apply max bigger))
+       (<= 2 (apply min bigger))))))
 
 (defn straight? [hand]
-  nil)
+  (let [ranks (sort (map rank hand))]
+    (let [ranks2 (sort (replace {14 1} ranks))]
+      (or
+       (= ranks (range (first ranks) (+ 1 (last ranks))))
+       (= ranks2 (range (first ranks2) (+ 1 (last ranks2))))))))
 
 (defn straight-flush? [hand]
-  nil)
+  (and (straight? hand)
+       (flush? hand)))
+
+(defn high-card? [hand]
+  true)
 
 (defn value [hand]
-  nil)
+  (let [checkers #{[high-card? 0]  [pair? 1]
+                 [two-pairs? 2]  [three-of-a-kind? 3]
+                 [straight? 4]   [flush? 5]
+                 [full-house? 6] [four-of-a-kind? 7]
+                 [straight-flush? 8]}]
+  (let [possible (filter (fn [x] (= true ((first x) hand))) checkers)]
+    (let [values (map second possible)]
+      (apply max values)))))
