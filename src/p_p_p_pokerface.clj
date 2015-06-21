50dba99b900de7ce4aef8db3ff6c0d19f3299d01
Aino Haikala <aino.haikala@student.tut.fi>
2015-06-21 20:54:20 +0300

    Poker hands evaluated

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,76 @@
 (ns p-p-p-pokerface)
 
+(def ranks-in-numbers {\T 10, \J 11, \Q 12, \K 13, \A 14})
+
 (defn rank [card]
-  nil)
+  (let [[rank _] card ]
+    (if (Character/isDigit rank) (Integer/valueOf (str rank))(get ranks-in-numbers rank))))
 
 (defn suit [card]
-  nil)
+  (let [[_ suit] card]
+    (str suit)))
+
+(defn frequencies-of-ranks [hand]
+  (vals (frequencies (map rank hand))))
+
+
+(defn amount-of-most-frequent-rank [hand]
+  (apply max (frequencies-of-ranks hand)))
 
 (defn pair? [hand]
-  nil)
+  (if (>= (amount-of-most-frequent-rank hand) 2)
+    true false))
 
 (defn three-of-a-kind? [hand]
-  nil)
+ (if (>= (amount-of-most-frequent-rank hand) 3)
+    true false))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (if (>= (amount-of-most-frequent-rank hand) 4)
+    true false))
+
+(defn frequencies-of-suits [hand]
+  (count (vals(frequencies(map suit hand)))))
 
 (defn flush? [hand]
-  nil)
+  (if (= 1 (frequencies-of-suits hand))
+    true false))
 
 (defn full-house? [hand]
-  nil)
+  (= [3 2] (frequencies-of-ranks hand)))
 
 (defn two-pairs? [hand]
-  nil)
+  (or (= [2 2 1] (frequencies-of-ranks hand))
+      (= [4 1] (frequencies-of-ranks hand))))
 
 (defn straight? [hand]
-  nil)
+    (let [ace-as-14 (map rank hand)
+        ace-as-1 (replace {14 1} ace-as-14)
+        is-straight? (fn [ranked-hand]
+                       (and (= (count ranked-hand) 5)
+                            (= 1 (apply max (vals (frequencies ranked-hand))))
+                            (= (- (apply max ranked-hand) 4)
+                               (apply min ranked-hand))))
+        ]
+    (or (is-straight? ace-as-1)
+        (is-straight? ace-as-14))))
 
 (defn straight-flush? [hand]
-  nil)
+  (and (flush? hand) (straight? hand)))
 
 (defn value [hand]
-  nil)
+    (cond (straight-flush? hand) 8
+        (four-of-a-kind? hand) 7
+        (full-house? hand) 6
+        (flush? hand) 5
+        (straight? hand) 4
+        (three-of-a-kind? hand) 3
+        (two-pairs? hand) 2
+        (pair? hand) 1
+        :else 0))
+
+(defn hand [a-hand]
+  a-hand)
+
+(defn card [a-card]
+  a-card)
