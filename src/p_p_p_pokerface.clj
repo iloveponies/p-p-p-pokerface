79739c54e60d5fa2702aeb5e3fb757a08ac20d1e
Jason Davidson <jad658@gmail.com>
2015-07-05 08:50:00 +0000

    gambling my night away

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,65 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+  (let [[r _] card
+        ranks {\T 10 \J 11 \Q 12 \K 13 \A 14}
+        card-rank (ranks r)]
+    (if card-rank
+      card-rank
+      (Integer/valueOf (str r)))))
 
 (defn suit [card]
-  nil)
+  (let [[_ s] card]
+    (str s)))
+
+(defn count-common [hand card-fn]
+  (reverse (sort (vals (frequencies (map card-fn hand))))))
+
+(defn count-common-rank [hand]
+  (count-common hand rank))
+
+(defn count-common-suit [hand]
+  (count-common hand suit))
 
 (defn pair? [hand]
-  nil)
+  (>= (first (count-common-rank hand)) 2))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (>= (first (count-common-rank hand)) 3))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (>= (first (count-common-rank hand)) 4))
 
 (defn flush? [hand]
-  nil)
+  (>= (first (count-common-suit hand)) 5))
 
 (defn full-house? [hand]
-  nil)
+  (= (count-common-rank hand) (seq [3 2])))
 
 (defn two-pairs? [hand]
-  nil)
+  (let [[fst snd] (count-common-rank hand)]
+    (>= fst snd 2)))
 
 (defn straight? [hand]
-  nil)
+  (let [hi-ranks (sort (map rank hand))
+        lo-ranks (sort (replace {14 1} hi-ranks))
+        hi-start (first hi-ranks)
+        lo-start (first lo-ranks)
+        hi-target (range hi-start (+ 5 hi-start))
+        lo-target (range lo-start (+ 5 lo-start))]
+    (or (= hi-ranks hi-target)
+        (= lo-ranks lo-target))))
 
 (defn straight-flush? [hand]
-  nil)
+  (and (flush? hand) (straight? hand)))
 
 (defn value [hand]
-  nil)
+  (cond (straight-flush? hand) 8
+        (four-of-a-kind? hand) 7
+        (full-house? hand) 6
+        (flush? hand) 5
+        (straight? hand) 4
+        (three-of-a-kind? hand) 3
+        (two-pairs? hand) 2
+        (pair? hand) 1
+        :else 0))
