68b5d4d6ed7f4a69dc7dc929a863ebe8a3d3c510
jason <jase@jase.uno>
2015-07-06 00:17:10 -0600

    poker project completed.

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,54 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+  (let [[first-character _] card]
+    (if (Character/isDigit first-character)
+      (Integer/valueOf (str first-character))
+      (get {\T 10 \J 11 \Q 12 \K 13 \A 14} first-character))))
 
 (defn suit [card]
-  nil)
+  (str (second (seq card))))
 
 (defn pair? [hand]
-  nil)
+  (= 2 (apply max (vals (frequencies (map rank hand))))))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (= 3 (apply max (vals (frequencies (map rank hand))))))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (= 4 (apply max (vals (frequencies (map rank hand))))))
 
 (defn flush? [hand]
-  nil)
+  (= 5 (apply max (vals (frequencies (map suit hand))))))
 
 (defn full-house? [hand]
-  nil)
+  (= [2 3] (sort (vals (frequencies (map rank hand))))))
 
 (defn two-pairs? [hand]
-  nil)
+  (let [x (sort (vals (frequencies (map rank hand))))]
+    (or (= x [1 2 2]) (= x [1 4])))) 
 
 (defn straight? [hand]
-  nil)
+  (let [regular-rank (sort (map rank hand))
+        ace-low-rank (sort (replace {14, 1} (map rank hand)))
+        hand-range (fn [modified-hand] 
+                     (range (apply min modified-hand) (inc (apply max modified-hand))))]
+    (or (= regular-rank (hand-range regular-rank))
+        (= ace-low-rank (hand-range ace-low-rank)))))
 
 (defn straight-flush? [hand]
-  nil)
+  (and (straight? hand) (flush? hand)))
 
 (defn value [hand]
-  nil)
+  (cond
+    (straight-flush? hand) 8
+    (four-of-a-kind? hand) 7
+    (full-house? hand) 6
+    (flush? hand) 5
+    (straight? hand) 4
+    (three-of-a-kind? hand) 3
+    (two-pairs? hand) 2
+    (pair? hand) 1
+    :else 0))
+
+
