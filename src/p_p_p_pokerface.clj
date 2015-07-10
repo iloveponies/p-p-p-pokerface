a90f419657f5f296f6a116799d5a8e4991afb1c4
nipakoo <niko.kortstrom@helsinki.fi>
2015-07-10 07:48:03 +0300

    returning all but one exersise

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,64 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+  (let [[fst _] card]
+    (if (Character/isDigit fst)
+      (Integer/valueOf (str fst))
+      ({\T 10, \J 11, \Q 12, \K 13, \A 14} fst))))
 
 (defn suit [card]
-  nil)
+  (let [[_ snd] card]
+    (str snd)))
+
+(defn ranks [hand]
+  (map rank hand))
+
+(defn suits [hand]
+  (map suit hand))
+
+(defn same-ranks [hand]
+  (vals (frequencies (ranks hand))))
+
+(defn same-suits [hand]
+  (vals (frequencies (suits hand))))
 
 (defn pair? [hand]
-  nil)
+  (== (apply max (same-ranks hand)) 2))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (if (== (apply max (same-ranks hand)) 3)
+    (if (== (apply min (same-ranks hand)) 2) false true)
+    false))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (== (apply max (same-ranks hand)) 4))
 
 (defn flush? [hand]
-  nil)
+  (== (count (same-suits hand)) 1))
 
 (defn full-house? [hand]
-  nil)
+  (if (== (apply max (same-ranks hand)) 3)
+    (if (== (apply min (same-ranks hand)) 2) true false)
+    false))
 
 (defn two-pairs? [hand]
-  nil)
+  (= (sort (same-ranks hand)) (seq [1 2 2])))
 
 (defn straight? [hand]
-  nil)
+  (let [new-hand (replace {"AH" "1H", "AC" "1C", "AD" "1D", "AS" "1S"} hand)
+        lowest (apply min (ranks hand))
+        new-lowest (apply min (ranks new-hand))]
+      (or
+       (= (sort (ranks hand)) (range lowest (+ lowest 5)))
+       (= (sort (ranks new-hand)) (range new-lowest (+ new-lowest 5))))))
 
 (defn straight-flush? [hand]
-  nil)
+  (and (straight? hand) (flush? hand)))
+
+(defn high-card? [hand]
+  (if (or (straight? hand) (flush? hand))
+    false
+    (== (apply max (same-ranks hand)) 1)))
 
 (defn value [hand]
   nil)
