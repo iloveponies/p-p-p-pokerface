bd4f6b13c666528495e4cf76ce24166f58913f43
cheukyin699 <cheukyin699@yahoo.com>
2015-07-14 19:45:16 -0700

    poker-faced submission

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,73 @@
 (ns p-p-p-pokerface)
 
-(defn rank [card]
-  nil)
+(def ranks [1 2 3 4 5 6 7 8 9 10 11 12 13 14])
+(def suits ["J" "Q" "K" "A"])
 
-(defn suit [card]
-  nil)
+(defn rank [[n s]]
+  (if (Character/isDigit n)
+    (Integer/valueOf (str n))
+    (get {\J 11, \Q 12, \K 13, \A 14, \T 10} n)))
+
+(defn suit [[n s]]
+  (str s))
+
+(defn n-of-a-kind? [n hand]
+  (>=
+    (apply max
+           (vals (frequencies (map rank hand))))
+    n))
 
 (defn pair? [hand]
-  nil)
+  (n-of-a-kind? 2 hand))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (n-of-a-kind? 3 hand))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (n-of-a-kind? 4 hand))
 
 (defn flush? [hand]
-  nil)
+  (==
+    (apply max
+           (vals (frequencies (map suit hand))))
+    5))
 
 (defn full-house? [hand]
-  nil)
+  (let [freqs (vals (frequencies (map rank hand)))]
+    (and
+      (== 2 (count freqs))
+      (== 3 (apply max freqs)))))
 
 (defn two-pairs? [hand]
-  nil)
+  (==
+    (count (filter (partial == 2)
+                   (vals (frequencies (map rank hand)))))
+    2))
 
 (defn straight? [hand]
-  nil)
+  (let [rhand (map rank hand)
+        rephand (replace {14 1} rhand)
+        mhand (apply min rhand)
+        mrephand (apply min rephand)]
+    (or
+      (= (sort rhand)
+         (range mhand (+ mhand 5)))
+      (= (sort rephand)
+         (range mrephand (+ mrephand 5))))))
 
 (defn straight-flush? [hand]
-  nil)
+  (and
+    (straight? hand)
+    (flush? hand)))
 
 (defn value [hand]
-  nil)
+  (cond
+    (straight-flush? hand)  8
+    (four-of-a-kind? hand)  7
+    (full-house? hand)      6
+    (flush? hand)           5
+    (straight? hand)        4
+    (three-of-a-kind? hand) 3
+    (two-pairs? hand)       2
+    (pair? hand)            1
+    :else 0))
