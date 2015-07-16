70eb95c640105d1fcfbed708e70038af8a1ba8e3
Esko Suomi <suomi.esko@gmail.com>
2015-07-16 17:36:11 +0300

    Exercises 1-6

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,22 +1,36 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+  (let [[r _] card
+        bigCards {\T 10 \J 11 \Q 12 \K 13 \A 14}]
+    (if (Character/isDigit r)
+      (Integer/valueOf (str r))
+      (get bigCards r))))
+
 
 (defn suit [card]
-  nil)
+  (let [[_ s] card]
+    (str s)))
+
+(defn has-freq? [i t m]
+  (let [f (fn [e] (== e i))]
+    (not
+      (empty?
+        (filter
+          f
+          (vals (frequencies (map t m))))))))
 
 (defn pair? [hand]
-  nil)
+  (has-freq? 2 rank hand))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (has-freq? 3 rank hand))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (has-freq? 4 rank hand))
 
 (defn flush? [hand]
-  nil)
+  (= 5 (count (frequencies (map rank hand)))))
 
 (defn full-house? [hand]
   nil)
