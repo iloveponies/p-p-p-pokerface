6f51a68f680094d137221fcb00c0084f48e3f477
Antti Haapaniemi <antti.haapaniemi@helsinki.fi>
2015-07-18 09:32:29 +0300

    Done.

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,68 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+  (let [[a _] card]
+    (if (Character/isDigit a)
+      (Integer/valueOf (str a))
+      (get {\T 10, \J 11, \Q 12, \K 13, \A 14} a))))
 
 (defn suit [card]
-  nil)
+  (let [[a b] card]
+    (str b)))
+
+(defn get-hand-ranks-freq [hand]
+  (vals (frequencies (map rank hand))))
+
+(defn get-hand-suit-freq [hand]
+  (vals (frequencies (map suit hand))))
 
 (defn pair? [hand]
-  nil)
+  (let [hand-ranks-freq (get-hand-ranks-freq hand)]
+    (if (< 1 (apply max hand-ranks-freq)) true false)))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (if (< 2 (apply max (get-hand-ranks-freq hand))) true false))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (if (< 3 (apply max (get-hand-ranks-freq hand))) true false))
 
 (defn flush? [hand]
-  nil)
+  (if (= 5 (apply max (get-hand-suit-freq hand))) true false))
 
 (defn full-house? [hand]
-  nil)
+  (let [ans (sort (get-hand-ranks-freq hand))
+        exp (range 2 4)]
+    (if (= ans exp) true false)))
 
 (defn two-pairs? [hand]
-  nil)
+  (let [ans (sort (get-hand-ranks-freq hand))
+        exp [1 2 2]]
+    (if (= ans exp) true false)))
+
+(defn in-order [map]
+  (let [exp (range (first map) (+ (first map) 5))]
+    (if (= exp map) true false)))
 
 (defn straight? [hand]
-  nil)
+  (let [hand-ranks (map rank hand)
+        ans1 (sort hand-ranks)
+        ans2 (sort (replace {14 1} hand-ranks))]
+    (if (or (in-order ans1) (in-order ans2)) true false)))
 
 (defn straight-flush? [hand]
-  nil)
+  (if (and (straight? hand) (flush? hand)) true false))
+
+(defn high-card? [hand]
+  true)
 
 (defn value [hand]
-  nil)
+  (cond
+   (straight-flush? hand) 8
+   (four-of-a-kind? hand) 7
+   (full-house? hand) 6
+   (flush? hand) 5
+   (straight? hand) 4
+   (three-of-a-kind? hand) 3
+   (two-pairs? hand) 2
+   (pair? hand) 1
+   :else 0))
