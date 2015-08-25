d186f9737212719877b156bc5e81da9ad2a9a279
Lauri Siltanen <lauri.siltanen@affecto.com>
2015-08-25 07:33:56 +0300

    done

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,65 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+  (let[[r _] card
+       mapper {\A 14, \K 13, \Q 12, \J 11, \T 10}]
+    (if (Character/isDigit r) (Integer/valueOf (str r)) (get mapper r))))
 
 (defn suit [card]
-  nil)
+  (let[[rank suit] card]
+    (str suit)))
+
+(defn ranks [hand]
+  (map rank hand))
+
+(defn suits [hand]
+  (map suit hand))
+
+(defn vcontains? [my-vector x]
+  (contains? (into #{} my-vector) x))
+
+(defn to-freq-values [my-vector]
+  (vals (frequencies my-vector)))
+
+(defn high-card? [card]
+  (= 1 1))
 
 (defn pair? [hand]
-  nil)
+  (vcontains? (vals (frequencies (ranks hand))) 2))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (vcontains? (vals (frequencies (ranks hand))) 3))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (vcontains? (vals (frequencies (ranks hand))) 4))
 
 (defn flush? [hand]
-  nil)
+  (vcontains? (vals (frequencies (suits hand))) 5))
 
 (defn full-house? [hand]
-  nil)
+  (and (vcontains? (vals (frequencies (ranks hand))) 2) (vcontains? (vals (frequencies (ranks hand))) 3)))
 
 (defn two-pairs? [hand]
-  nil)
+  (= (get (frequencies (to-freq-values (ranks hand))) 2) 2))
 
 (defn straight? [hand]
-  nil)
+  (let [hi-ace-hand (sort (ranks hand))
+        lo-ace-hand (sort (replace {14 1} (ranks hand)))
+        fst-lo (first lo-ace-hand)
+        fst-hi (first hi-ace-hand)
+        expected-straight-lo (range fst-lo (+ fst-lo 5))
+        expected-straight-hi (range fst-hi (+ fst-hi 5))]
+    (or (= expected-straight-lo lo-ace-hand) (= expected-straight-hi hi-ace-hand))))
 
 (defn straight-flush? [hand]
-  nil)
+  (and (flush? hand) (straight? hand)))
 
 (defn value [hand]
-  nil)
+  (let [checkers #{[high-card? 0]  [pair? 1]
+                 [two-pairs? 2]  [three-of-a-kind? 3]
+                 [straight? 4]   [flush? 5]
+                 [full-house? 6] [four-of-a-kind? 7]
+                 [straight-flush? 8]}
+      check (fn [entry] (if ((first entry) hand) (second entry) nil))
+      non-null? (fn [x] (not (= x nil)))]
+  (apply max (filter non-null? (map check checkers)))))
