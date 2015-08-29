0b528a1c0e68b75a408573f9b876f2ff607d1369
Igor Garcia <igarciauk@gmail.com>
2015-08-29 18:28:39 +0100

    Solved Exercise 9

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -28,7 +28,12 @@
   (= (sort (vals (frequencies (map rank hand)))) [1 2 2]))
 
 (defn straight? [hand]
-  nil)
+    (let [sorted-hand-ranks-high-ace (sort (map rank hand))
+        sorted-hand-ranks-low-ace (sort (replace {14 1} (map rank hand)))
+        expected-straight-high-ace (range (Integer/valueOf (first sorted-hand-ranks-high-ace)) (+ 5 (Integer/valueOf (first sorted-hand-ranks-high-ace))))
+        expected-straight-low-ace (range (Integer/valueOf (first sorted-hand-ranks-low-ace)) (+ 5 (Integer/valueOf (first sorted-hand-ranks-low-ace))))
+        ]
+  (or (= sorted-hand-ranks-low-ace expected-straight-low-ace) (= sorted-hand-ranks-high-ace expected-straight-high-ace))))
 
 (defn straight-flush? [hand]
   nil)
