52b70b0afc135d6fda30e1c1b5b6e6369288b2db
tsu-nera <fox10225fox@gmail.com>
2015-08-30 19:58:11 +0900

    ex2

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,10 +1,29 @@
 (ns p-p-p-pokerface)
 
+(def rank-character {\T 10, \J 11, \Q 12, \K 13, \A 14})
+
 (defn rank [card]
-  nil)
+  (let [[fst snd] card]
+    (if (Character/isDigit fst)
+      (Integer/valueOf (str fst))
+      (rank-character fst))))
+
+(rank "2H") ;=> 2
+(rank "4S") ;=> 4
+(rank "TS") ;=> 10
+(rank "JS") ;=> 11
+(rank "QS") ;=> 12
+(rank "KS") ;=> 13
+(rank "AS") ;=> 14
 
 (defn suit [card]
-  nil)
+  (let [[fst snd] card]
+    (str snd)))
+
+(suit "2H") ;=> "H"
+(suit "2D") ;=> "D"
+(suit "2C") ;=> "C"
+(suit "3S") ;=> "S"
 
 (defn pair? [hand]
   nil)
