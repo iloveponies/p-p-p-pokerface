98dc6e5ae842da40989ed01b9eb4e29b1e588c07
Kimmo Puputti <kpuputti@gmail.com>
2015-09-17 12:56:41 +0300

    Implement poker exercises.

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,65 @@
 (ns p-p-p-pokerface)
 
-(defn rank [card]
-  nil)
+(def replacements {\T 10, \J 11, \Q 12, \K 13, \A 14})
 
-(defn suit [card]
-  nil)
+(defn rank [[fst _]]
+  (if (Character/isDigit fst)
+    (Integer/valueOf (str fst))
+    (get replacements fst)))
+
+(defn suit [[_ snd]]
+  (str snd))
+
+(defn rank-freqs [hand]
+  (frequencies (map rank hand)))
+
+(defn n-of-a-kind? [n hand]
+  (contains? (set (vals (rank-freqs hand))) n))
 
 (defn pair? [hand]
-  nil)
+  (n-of-a-kind? 2 hand))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (n-of-a-kind? 3 hand))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (n-of-a-kind? 4 hand))
 
 (defn flush? [hand]
-  nil)
+  (= (count (set (map suit hand))) 1))
 
 (defn full-house? [hand]
-  nil)
+  (= (sort (set (vals (rank-freqs hand)))) '(2 3)))
 
 (defn two-pairs? [hand]
-  nil)
+  (let [freq-vals (vals (rank-freqs hand))
+        pair-vals (filter #(= % 2) freq-vals)]
+    (= (count pair-vals) 2)))
+
+(defn continuous? [nums]
+  (let [smallest (apply min nums)
+        minrange (range smallest (+ smallest (count nums)))]
+    (= minrange (sort nums))))
 
 (defn straight? [hand]
-  nil)
+  (let [ranks (map rank hand)
+        ranks-replaced (replace {14 1} (map rank hand))]
+    (or (continuous? ranks) (continuous? ranks-replaced))))
 
 (defn straight-flush? [hand]
-  nil)
+  (and (straight? hand) (flush? hand)))
+
+(defn high-card? [hand]
+  true) ; All hands have a high card.
 
 (defn value [hand]
-  nil)
+  (let [checkers #{[high-card? 0]   [pair? 1]
+                   [two-pairs? 2]   [three-of-a-kind? 3]
+                   [straight? 4]    [flush? 5]
+                   [full-house? 6]  [four-of-a-kind? 7]
+                   [straight-flush? 8]}
+        checker-value (fn [[func n]]
+                        (if (func hand)
+                          n
+                          -1))]
+    (apply max (map checker-value checkers))))
