ee6cabc96831d28e8c3d1028488c9f6fdb06de93
Aurélien Bertron <aurelienbertron@gmail.com>
2015-09-27 19:05:20 +0300

    I call

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,73 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+  (let [ranks {\T 10, \J 11, \Q 12, \K 13, \A 14}
+        [s-rank _] card]
+    (if (Character/isDigit s-rank)
+      (Integer/valueOf (str s-rank))
+      (ranks s-rank))))
 
 (defn suit [card]
-  nil)
+  (let [[_ suit] card]
+    (str suit)))
 
 (defn pair? [hand]
-  nil)
+  (if (contains? (set (vals (frequencies (map rank hand)))) 2)
+    true
+    false))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (if (contains? (set (vals (frequencies (map rank hand)))) 3)
+    true
+    false))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (if (contains? (set (vals (frequencies (map rank hand)))) 4)
+    true
+    false))
 
 (defn flush? [hand]
-  nil)
+  (if (= 5 (first (vals (frequencies (map suit hand)))))
+    true
+    false))
 
 (defn full-house? [hand]
-  nil)
+  (and (pair? hand) (three-of-a-kind? hand)))
 
 (defn two-pairs? [hand]
-  nil)
+  (if (four-of-a-kind? hand)
+    true
+    (if (contains? (set (frequencies (vals (frequencies (map rank hand)))))
+                   [2 2])
+      true
+      false)))
 
 (defn straight? [hand]
-  nil)
+  (let [ranks-set     (set (map rank hand))
+        test-straight (fn [set]
+                        (let [sorted       (sort set)
+                              first-sorted (first sorted)]
+                          (= sorted (range first-sorted (+ first-sorted 5)))))]
+    (if (contains? ranks-set 14)
+      (or
+        (test-straight ranks-set)
+        (test-straight (replace {14 1} ranks-set)))
+      (test-straight ranks-set))))
 
 (defn straight-flush? [hand]
-  nil)
+  (and (straight? hand) (flush? hand)))
+
+(defn high-card? [hand]
+  true)
 
 (defn value [hand]
-  nil)
+  (let [checkers #{[high-card? 0]  [pair? 1]
+                 [two-pairs? 2]  [three-of-a-kind? 3]
+                 [straight? 4]   [flush? 5]
+                 [full-house? 6] [four-of-a-kind? 7]
+                 [straight-flush? 8]}
+        checkers-filter (fn [[func val]]
+                          (if (func hand)
+                            val
+                            0))]
+    (apply max (map checkers-filter checkers))))
