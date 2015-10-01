36d8a6445f0eec9b8709626542d24029abea1838
Marco Travaglini <marcotravaglini81@gmail.com>
2015-10-01 18:25:35 +0200

    poker face

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,65 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+  (let [[fst _] card]
+    (if (Character/isDigit fst)
+       (Integer/valueOf (str fst))
+       (let [rankCharacter {
+           "T" 10
+           "J" 11
+           "Q" 12
+           "K" 13
+           "A" 14 }]
+           (rankCharacter (str fst))))))
 
 (defn suit [card]
-  nil)
+  (let [[_ snd] card]
+    (str snd)))
+
+(defn frequencies-vals [hand]
+    (vals (frequencies (map rank hand))))
+
+(defn frequencies-max [hand]
+    (apply max (frequencies-vals hand)))
 
 (defn pair? [hand]
-  nil)
+  (< 1 (frequencies-max hand)))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (< 2 (frequencies-max hand)))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (< 3 (frequencies-max hand)))
 
 (defn flush? [hand]
-  nil)
+  (= 5 (apply max (vals (frequencies (map suit hand))))))
 
 (defn full-house? [hand]
-  nil)
+  (let [[fst snd] (sort (frequencies-vals hand))]
+    (= [2 3] [fst snd])))
 
 (defn two-pairs? [hand]
-  nil)
+  (let [sorted (sort (frequencies-vals hand))]
+    (or (= [1 4] sorted) (= [1 2 2] sorted))))
 
 (defn straight? [hand]
-  nil)
+  (let [ace-up (sort (map rank hand))
+        ace-down (sort (replace {14 1} ace-up))
+        compare-sorted-aces (fn [sorted-hand]
+            (let [hand-min (apply min sorted-hand)]
+                (= (range hand-min (+ hand-min 5)) sorted-hand)))]
+      (or (compare-sorted-aces ace-up) (compare-sorted-aces ace-down))))
 
 (defn straight-flush? [hand]
-  nil)
+  (and (flush? hand) (straight? hand)))
+
+(defn high-card? [hand]
+    true)
 
 (defn value [hand]
-  nil)
+    (let [checkers #{[high-card? 0]  [pair? 1]
+                   [two-pairs? 2]  [three-of-a-kind? 3]
+                   [straight? 4]   [flush? 5]
+                   [full-house? 6] [four-of-a-kind? 7]
+                   [straight-flush? 8]}]
+          (apply max (map second (filter (fn [item] ((first item) hand)) checkers)))))
