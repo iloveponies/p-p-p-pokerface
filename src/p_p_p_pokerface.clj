7404742f706880b3f62c51b59a9ce92e0344705e
JON-ERIK <jon-erik.klint@luukku.com>
2015-10-15 16:15:11 +0300

    i got dem pokerface

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,68 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+  (let [[a] card
+        card-value {\T 10 \J 11 \Q 12 \K 13 \A 14}]
+    (if (Character/isDigit a)
+      (Integer/valueOf (str a))
+      (card-value a))))
 
 (defn suit [card]
-  nil)
+  (let [[_ suit] card]
+    (str suit)))
+
+(defn rank-freqs [hand]
+  (vals (frequencies (map rank hand))))
+
+(defn freqs-in-order [hand]
+  (reverse (sort (rank-freqs hand))))
+
+(defn same-cards-exist? [hand amount]
+  (<= amount (apply max (rank-freqs hand))))
 
 (defn pair? [hand]
-  nil)
+  (same-cards-exist? hand 2))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (same-cards-exist? hand 3))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (same-cards-exist? hand 4))
 
 (defn flush? [hand]
-  nil)
+  (= 1(count (frequencies (map suit hand)))))
 
 (defn full-house? [hand]
-  nil)
+  (let [freqs (freqs-in-order hand)]
+    (and (= 3 (first freqs)) (= 2 (first (rest freqs))))))
 
 (defn two-pairs? [hand]
-  nil)
+  (let [freqs (freqs-in-order hand)]
+    (or (= 4 (first freqs))
+        (and (= 2 (first freqs))
+             (= 2 (first (rest freqs)))))))
 
 (defn straight? [hand]
-  nil)
+  (let [sorted (sort (map rank hand))
+        change (fn [v] (replace {14 1} v))
+        min (fn [v] (apply min v))]
+    (or (= sorted
+           (range (min sorted) (+ (min sorted) 5)))
+        (= (sort(change sorted))
+           (range (min (change sorted)) (+ (min (change sorted)) 5))))
+    ))
 
 (defn straight-flush? [hand]
-  nil)
+  (and (straight? hand)(flush? hand)))
 
 (defn value [hand]
-  nil)
+  (cond
+   (straight-flush? hand) 8
+   (four-of-a-kind? hand) 7
+   (full-house? hand) 6
+   (flush? hand) 5
+   (straight? hand) 4
+   (three-of-a-kind? hand) 3
+   (two-pairs? hand) 2
+   (pair? hand) 1
+   :else 0))
