068de635c532c3020b794ad1b77868703cce36cf
Mark Sponsler <mark.sponsler@gmail.com>
2015-10-22 22:48:14 +0100

    Partial implementation of exercises

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,22 +1,29 @@
 (ns p-p-p-pokerface)
 
+(def face-card-ranks
+  {\T 10 \J 11 \Q 12 \K 13 \A 14})
+
 (defn rank [card]
-  nil)
+  (let [[r _] card]
+    (if (Character/isDigit r)
+      (Integer/valueOf (str r))
+      (get face-card-ranks r))))
 
 (defn suit [card]
-  nil)
+  (let [[_ s] card]
+    (str s)))
 
 (defn pair? [hand]
-  nil)
+  (= 2 (some #{2} (vals (frequencies (map rank hand))))))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (= 3 (some #{3} (vals (frequencies (map rank hand))))))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (= 4 (some #{4} (vals (frequencies (map rank hand))))))
 
 (defn flush? [hand]
-  nil)
+  (= 5 (some #{5} (vals (frequencies (map suit hand))))))
 
 (defn full-house? [hand]
   nil)
