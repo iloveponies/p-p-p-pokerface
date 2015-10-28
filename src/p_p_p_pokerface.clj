b51859b3afe01292ec3c134379e98309201dd488
Alex Tsikhanovich <a_tsikhanovich@adamantium.net>
2015-10-28 18:48:21 +0300

    p_p_p_pokerface.1

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,19 +1,31 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+  (let [[s-rank _] card
+        rank-map { \T 10 \J 11 \Q 12 \K 13 \A 14 }
+        h-rank (get rank-map s-rank)]
+    (if h-rank
+      h-rank
+      (Integer/valueOf (str s-rank)))))
 
 (defn suit [card]
-  nil)
+  (let [[_ suit] card]
+    (str suit)))
+
+(defn count-ranks [hand cut-level]
+  (filter 
+    (fn [freqq] (let [[rank freq] freqq]
+                  (> freq cut-level)))
+    (frequencies (map rank hand))))
 
 (defn pair? [hand]
-  nil)
+  (not (empty? (count-ranks hand 1))))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (not (empty? (count-ranks hand 2))))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (not (empty? (count-ranks hand 3))))
 
 (defn flush? [hand]
   nil)
