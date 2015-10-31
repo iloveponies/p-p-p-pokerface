981d6d78776d21248f615cbc7743dc3d2806a680
Emil <emil.bergstrom@packsize.com>
2015-10-31 10:39:58 +0100

    finished exercises

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,77 @@
 (ns p-p-p-pokerface)
 
+(def any? (comp boolean some))
+
 (defn rank [card]
-  nil)
+  (let [[r s] card]
+    (case r
+      \T 10
+      \J 11
+      \Q 12
+      \K 13
+      \A 14
+      (Integer/valueOf (str r)))))
 
 (defn suit [card]
-  nil)
+  (let [[r s] card]
+    (str s)))
+
+(defn group-by-rank [hand]
+  (group-by rank hand))
+
+(defn group-by-suite [hand]
+  (group-by suit hand))
+
+(defn get-rank-sequence [hand]
+  (sort (map rank hand)))
+
+(defn a-kind?
+  ([hand nr] (a-kind? hand nr 1))
+  ([hand nr times]
+    (>= (count (filter #(= (count (val %)) nr) (group-by-rank hand))) times)))
 
 (defn pair? [hand]
-  nil)
+  (a-kind? hand 2))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (a-kind? hand 3))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (a-kind? hand 4))
 
 (defn flush? [hand]
-  nil)
+  (= 1 (count (group-by-suite hand))))
 
 (defn full-house? [hand]
-  nil)
+  (and (pair? hand)
+       (three-of-a-kind? hand)))
 
 (defn two-pairs? [hand]
-  nil)
+  (a-kind? hand 2 2))
+
+(defn straight-helper? [hand]
+  (let [sorted (get-rank-sequence hand)
+        lowest (first sorted)
+        straightrange (range lowest (+ lowest 5))]
+    (= sorted straightrange)))
 
 (defn straight? [hand]
-  nil)
+  (or (straight-helper? hand)
+      (straight-helper? (replace {"AH" "1H", "AD" "1D", "AC" "1C" "AS" "1S"} hand))))
 
 (defn straight-flush? [hand]
-  nil)
+  (and (straight? hand)
+       (flush? hand)))
 
 (defn value [hand]
-  nil)
+  (cond
+   (straight-flush? hand) 8
+   (four-of-a-kind? hand) 7
+   (full-house? hand) 6
+   (flush? hand) 5
+   (straight? hand) 4
+   (three-of-a-kind? hand) 3
+   (two-pairs? hand) 2
+   (pair? hand) 1
+   :else 0))
+
