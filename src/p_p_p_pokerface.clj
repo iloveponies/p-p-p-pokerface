c139c463e15ea70d6de8fbb1bea900b0a39d63d5
Miika Säynevirta <mayse@iki.fi>
2015-11-04 18:31:39 +0200

    ex 6

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,23 +1,35 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+  (let [[r _] [(first card) (str(second card))]]
+    (def replacements {\T 10, \J 11, \Q 12, \K 13, \A 14})
+      (if(Character/isDigit r)
+        (Integer/valueOf (str r)) ;1-9 converted to integer
+        (replacements r)))) ;character to number conversion
 
 (defn suit [card]
-  nil)
+  (let [[_ s] [(str(first card)) (str(second card))]]
+    (str s)))
 
 (defn pair? [hand]
-  nil)
+  (let [ranks (map rank hand)
+        pairs (apply max(vals(frequencies ranks)))]
+          (< 1 pairs)))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (let [ranks (map rank hand)
+        pairs (apply max(vals(frequencies ranks)))]
+          (< 2 pairs)))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (let [ranks (map rank hand)
+        pairs (apply max(vals(frequencies ranks)))]
+          (< 3 pairs)))
 
 (defn flush? [hand]
-  nil)
-
+  (let [suits (map suit hand)
+        pairs (apply max(vals(frequencies suits)))]
+          (= 5 pairs)))
 (defn full-house? [hand]
   nil)
 
