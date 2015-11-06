55d340291cd773aea04a3e72235a6b716c4af216
Miika Säynevirta <mayse@iki.fi>
2015-11-06 14:44:39 +0200

    done

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -31,16 +31,37 @@
         pairs (apply max(vals(frequencies suits)))]
           (= 5 pairs)))
 (defn full-house? [hand]
-  nil)
+  (let [groups (sort(vals(frequencies (map rank hand))))]
+    (and (= 2 (first groups)) 
+         (= 3 (second groups)))))
 
 (defn two-pairs? [hand]
-  nil)
-
+  (let [groups (sort(vals(frequencies (map rank hand))))]
+    (and (= 2 (second groups))
+         (= 2 (nth groups 2)))))
 (defn straight? [hand]
-  nil)
+  (let [ranks-a (sort (map rank hand)) ;aces as fourteens
+        ranks-b (sort (replace {14 1} ranks-a)) ;aces as ones
+        comp-a (range (first ranks-a) (+ 5 (first ranks-a))) ;these are what the straights
+        comp-b (range (first ranks-b) (+ 5 (first ranks-b))) ;should look like
+        ]
+    (cond
+      (= ranks-a comp-a) true
+      (= ranks-b comp-b) true
+      :else false)))
 
 (defn straight-flush? [hand]
-  nil)
+    (and (flush? hand) (straight? hand)))
 
-(defn value [hand]
-  nil)
+(defn value [hand] ;trololo, didn't learn a thing
+  (cond
+    (straight-flush? hand) 8
+    (four-of-a-kind? hand) 7
+    (full-house? hand) 6
+    (flush? hand) 5
+    (straight? hand) 4
+    (three-of-a-kind? hand) 3
+    (two-pairs? hand) 2
+    (pair? hand) 1
+    :else 0
+    ))
