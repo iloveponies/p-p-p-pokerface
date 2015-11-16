925057cfa6f6d22532a4f63238da7fc654af579d
Esko Suomi <suomi.esko@gmail.com>
2015-11-16 14:52:39 +0200

    another refactor, another cleaner solution

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -7,68 +7,46 @@
       (Integer/valueOf (str r))
       (get bigCards r))))
 
-
 (defn suit [card]
   (let [[_ s] card]
     (str s)))
 
-(defn has-freq? [i t m]
-  (let [f (fn [e] (== e i))]
-    (not
-      (empty?
+(defn has-freq? [freq hits hand]
+  (== hits
+      (count
         (filter
-          f
-          (vals (frequencies (map t m))))))))
-
-;; How the whole thing works:
-
-; 1) map cards in hand to ranks (=numeric values) and suits
-(defn card-obj [card-str]
-  {:rank (rank card-str), :suit (suit card-str)})
-
-; 2) map entire hand to objects
-(defn hand-obj [hand-strs]
-  (let [cards (map card-obj hand-strs)
-        freqs (map
-                (fn [[freq matches]] [freq (map first matches)])
-                (group-by
-                  (fn [[_ v]] v)
-                  (frequencies
-                    (map
-                      (fn [card] (get card :rank))
-                      cards))))]
-    {:cards cards, :freqs freqs}))
-
-(defn has-n-freq? [frequency hits hand-str]
-  (not
-    (empty?
-      (filter
-        (fn [[freq ranks]] (and (== freq frequency) (== (count ranks) hits)))
-        (get (hand-obj hand-str) :freqs)))))
+          (fn [n] (== n freq))
+          (vals (frequencies (map rank hand)))))))
 
 (defn pair? [hand]
-  (has-n-freq? 2 1 hand))
+  (has-freq? 2 1 hand))
 
 (defn three-of-a-kind? [hand]
-  (has-n-freq? 3 1 hand))
+  (has-freq? 3 1 hand))
 
 (defn four-of-a-kind? [hand]
-  (has-n-freq? 4 1 hand))
+  (has-freq? 4 1 hand))
 
 (defn flush? [hand]
-  (= 5 (count (frequencies (map rank hand)))))
+  (= 1 (count (frequencies (map suit hand)))))
 
 (defn full-house? [hand]
-  (and (has-n-freq? 3 1 hand) (has-n-freq? 2 1 hand)))
+  (and (has-freq? 3 1 hand) (has-freq? 2 1 hand)))
 
 (defn two-pairs? [hand]
-  (or (has-n-freq? 4 1 hand) (has-n-freq? 2 2 hand)))
+  (or (has-freq? 4 1 hand) (has-freq? 2 2 hand)))
 
 (defn straight? [hand]
-  nil)
+  (let [ranks (sort < (map rank hand))
+        smallest (apply min ranks)]
+    (or
+      (= ranks (range smallest (+ smallest 5)))
+      (= (sort < (replace {14 1} ranks)) (range 1 6)))))
 
 (defn straight-flush? [hand]
-  nil)
+  (and
+    (straight? hand)
+    (flush? hand)))
 
 (defn value [hand]
   nil)
