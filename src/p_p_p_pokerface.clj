061771346f16b6730ccb31af9cfa9f5ca7567c77
jviding <jasu.viding@helsinki.fi>
2015-11-17 06:09:56 +0300

    doneded

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,63 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+  (let [[fst _] card]
+    (if (Character/isDigit fst)
+      (Integer/valueOf (str fst))
+      (get {\T 10, \J 11, \Q 12, \K 13, \A 14} fst))))
 
 (defn suit [card]
-  nil)
+  (let [[_ snd] card]
+    (str snd)))
 
 (defn pair? [hand]
-  nil)
+  (contains? (set (vals (frequencies (map rank hand)))) 2))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (contains? (set (vals (frequencies (map rank hand)))) 3))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (contains? (set (vals (frequencies (map rank hand)))) 4))
 
 (defn flush? [hand]
-  nil)
+  (contains? (set (vals (frequencies (map suit hand)))) 5))
 
 (defn full-house? [hand]
-  nil)
+  (if (pair? hand) 
+    (if (three-of-a-kind? hand)
+      true
+      false) 
+    false))
 
 (defn two-pairs? [hand]
-  nil)
+  (cond
+    (contains? (set (vals (frequencies (map rank hand)))) 4) true
+    (= [1 2 2] (sort (vals (frequencies (map rank hand))))) true
+    :else false))
 
 (defn straight? [hand]
-  nil)
+  (let [[fst snd trd frt fif] (sort (map rank hand))]
+    (if (= (+ fst 1) snd) 
+      (if (= (+ snd 1) trd) 
+        (if (= (+ trd 1) frt) 
+          (if (= (+ frt 1) fif) 
+            true 
+            (if (= fif 14) 
+              (if (= fst 2) 
+                true false) 
+              false)) false) false) false)))
 
 (defn straight-flush? [hand]
-  nil)
+  (if (straight? hand) (if (flush? hand) true false) false))
 
 (defn value [hand]
-  nil)
+  (cond
+    (straight-flush? hand) 8
+    (four-of-a-kind? hand) 7
+    (full-house? hand) 6
+    (flush? hand) 5
+    (straight? hand) 4
+    (three-of-a-kind? hand) 3
+    (two-pairs? hand) 2
+    (pair? hand) 1
+    :else 0))
