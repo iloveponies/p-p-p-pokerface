6b078d9c28f4b6a46164b5f1d6da6b780e359aa2
Karolina Behalova <karolina.behalova@helsinki.fi>
2015-11-30 18:11:48 +0200

    pppokerface done

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,73 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+  (def replacements {\T 10, \J 11, \Q 12, \K 13, \A 14})
+  (let [[fst _] card]
+    (if (Character/isDigit fst) (Integer/valueOf (str fst)) (replacements fst))
+    ))
 
 (defn suit [card]
-  nil)
+  (let [[fst snd] card]
+    (str snd)
+    ))
 
 (defn pair? [hand]
-  nil)
+  (let [ranks (map rank hand)]
+    (>= (apply max (vals (frequencies ranks))) 2)
+    ; apply frequencies, then minimum from that
+    ; how to get vector of ranks?
+    )
+  )
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (let [ranks (map rank hand)]
+    (>= (apply max (vals (frequencies ranks))) 3)
+    ))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (let [ranks (map rank hand)]
+    (>= (apply max (vals (frequencies ranks))) 4)
+    ))
 
 (defn flush? [hand]
-  nil)
+  (let [suits (map suit hand)]
+    (= (apply max(vals (frequencies suits))) 5)
+    ))
 
 (defn full-house? [hand]
-  nil)
+  (let [ranks (map rank hand) model (range 2 4)]
+    (= (sort (vals (frequencies ranks))) model)
+    ))
 
 (defn two-pairs? [hand]
-  nil)
+  (let [ranks (map rank hand)]
+    (or (= (sort (vals (frequencies ranks))) (seq [1 2 2]))
+        (= (sort (vals (frequencies ranks))) (seq [1 4])))
+    ))
+
 
 (defn straight? [hand]
-  nil)
+  (let [ranks (map rank hand)
+        lowest (apply min ranks)
+        alt_ranks (replace {14 1} (map rank hand))
+        ]
+    (or (= (sort ranks) (range lowest (+ lowest 5)))
+        (= (sort alt_ranks) (range 1 6)))
+    ))
 
 (defn straight-flush? [hand]
-  nil)
+  (and (flush? hand) (straight? hand)))
 
 (defn value [hand]
-  nil)
+  (cond
+   (straight-flush? hand) 8
+   (four-of-a-kind? hand) 7
+   (full-house? hand) 6
+   (flush? hand) 5
+   (straight? hand) 4
+   (three-of-a-kind? hand) 3
+   (two-pairs? hand) 2
+   (pair? hand) 1
+   :else 0
+   )
+  )
