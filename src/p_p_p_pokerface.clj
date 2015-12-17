ffb55431ba4642ba41f4b26fa33b818600351cb8
aam <aam@anahoret.com>
2015-12-17 13:42:58 +0200

    Implement functions

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,61 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+  (let [high-ranks {\T 10, \J 11, \Q 12 \K 13 \A 14}]
+    (let [[rk _] card]
+      (if (Character/isDigit rk)
+        (Integer/valueOf (str rk))
+        (get high-ranks rk)))))
 
 (defn suit [card]
-  nil)
+  (let [[_ st] card]
+    (str st)))
+
+(defn freqs [hand freq-fn]
+  (vals (frequencies (map freq-fn hand))))
+
+(defn has-frequency? [freq freq-fn hand]
+  (not (empty?
+        (filter (fn [x] (== x freq))
+                (freqs hand freq-fn)))))
 
 (defn pair? [hand]
-  nil)
+  (has-frequency? 2 rank hand))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (has-frequency? 3 rank hand))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (has-frequency? 4 rank hand))
 
 (defn flush? [hand]
-  nil)
+  (has-frequency? 5 suit hand))
 
 (defn full-house? [hand]
-  nil)
+  (= [2 3] (sort (freqs hand rank))))
 
 (defn two-pairs? [hand]
-  nil)
+  (or (four-of-a-kind? hand) (= [1 2 2] (sort (freqs hand rank)))))
 
 (defn straight? [hand]
-  nil)
+  (let [strght (fn [s] (= (range (apply min s) (+ (apply max s) 1)) s))]
+    (or
+     (strght (sort (map rank hand)))
+     (strght (sort (replace {14 1} (map rank hand)))))))
 
 (defn straight-flush? [hand]
-  nil)
+  (and (flush? hand) (straight? hand)))
+
+(defn high-card? [hand]
+  true)
 
 (defn value [hand]
-  nil)
+  (let [checkers #{[high-card? 0]  [pair? 1]
+                 [two-pairs? 2]  [three-of-a-kind? 3]
+                 [straight? 4]   [flush? 5]
+                 [full-house? 6] [four-of-a-kind? 7]
+                 [straight-flush? 8]}]
+    (apply max
+           (map (fn [x] (get x 1))
+                (filter (fn [x] (apply (get x 0) [hand]))
+                        checkers)))))
