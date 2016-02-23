9b0d856c99564edf092081ade84d3346a6265080
Tuomas Siipola <siiptuo@kapsi.fi>
2016-02-23 17:07:15 +0200

    message here

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,51 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+  (let [[rank _] card
+        rank-map {\T 10 \J 11 \Q 12 \K 13 \A 14}]
+    (if (Character/isDigit rank) (Integer/valueOf (str rank)) (get rank-map rank))))
 
 (defn suit [card]
-  nil)
+  (let [[_ suit] card]
+    (str suit)))
 
 (defn pair? [hand]
-  nil)
+  (boolean (some #{2} (vals (frequencies (map rank hand))))))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (boolean (some #{3} (vals (frequencies (map rank hand))))))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (boolean (some #{4} (vals (frequencies (map rank hand))))))
 
 (defn flush? [hand]
-  nil)
+  (= (count (distinct (map suit hand))) 1))
 
 (defn full-house? [hand]
-  nil)
+  (and (pair? hand) (three-of-a-kind? hand)))
 
 (defn two-pairs? [hand]
-  nil)
+  (= (get (frequencies (vals (frequencies (map rank hand)))) 2) 2))
 
 (defn straight? [hand]
-  nil)
+  (let [ranks  (map rank hand)
+        sorted (sort (if (= (apply min ranks) 2) (replace {14 1} ranks) ranks))
+        fst    (apply min sorted)
+        refe   (range fst (+ fst 5))]
+    (= sorted refe)))
 
 (defn straight-flush? [hand]
-  nil)
+  (and (straight? hand) (flush? hand)))
+
+(defn high-card? [hand]
+  true)
 
 (defn value [hand]
-  nil)
+  (let [checkers #{[high-card? 0]  [pair? 1]
+                   [two-pairs? 2]  [three-of-a-kind? 3]
+                   [straight? 4]   [flush? 5]
+                   [full-house? 6] [four-of-a-kind? 7]
+                   [straight-flush? 8]},
+        active (filter (fn [checker] ((first checker) hand)) checkers)
+        scores (map second active)]
+    (apply max scores)))
