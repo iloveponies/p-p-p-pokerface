2561e1811aa823733ae8df8188ad182fb6966e68
Matti Remes <matti.remes@gmail.com>
2016-03-20 14:24:03 +0200

    done exercises

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,55 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+  (let[[fst snd] card]
+    snd))
 
 (defn suit [card]
-  nil)
+  (let [[fst snd] card]
+    fst))
 
 (defn pair? [hand]
-  nil)
+  (let [[fst snd] (first hand)
+        [fst1 snd1] (second hand)]
+  (= fst fst1)))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (= (first (vals (frequencies (map #(first %) hand)))) 3))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (= (first (vals (frequencies (map #(first %) hand)))) 4))
 
 (defn flush? [hand]
-  nil)
+  (= (first (vals (frequencies (map #(second %) hand)))) 5))
 
 (defn full-house? [hand]
-  nil)
+  (and
+  (= (first (vals (sort (frequencies (map #(first %) hand))))) 3)
+  (= (second (vals (sort (frequencies (map #(first %) hand))))) 2)))
 
 (defn two-pairs? [hand]
-  nil)
+  (or (= (+
+    (first (vals (sort (frequencies (map #(first %) hand)))))
+    (second (vals (sort (frequencies (map #(first %) hand)))))) 4)
+      (= (first (vals (sort (frequencies (map #(first %) hand))))) 4)))
 
 (defn straight? [hand]
-  nil)
+  (let [sorted (sort (map first hand))
+        ace (if (= (apply min (map int sorted)) 50) (sort (map int (replace {\A 49} sorted))) (sort (map int (replace {\T 58, \J 59, \Q 60, \K 61 \A 62} sorted))))
+        min-value (apply min (map int ace))]
+    (= ace (take 5 (iterate inc min-value)))))
 
 (defn straight-flush? [hand]
-  nil)
+  (and (straight? hand) (flush? hand)))
 
 (defn value [hand]
-  nil)
+  (cond
+    (straight-flush? hand) 8
+    (four-of-a-kind? hand) 7
+    (full-house? hand) 6
+    (flush? hand) 5
+    (straight? hand) 4
+    (three-of-a-kind? hand) 3
+    (two-pairs? hand) 2
+    (pair? hand) 1
+    :else 0))
