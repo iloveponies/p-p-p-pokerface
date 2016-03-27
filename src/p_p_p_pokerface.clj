6bcaf90c00e5706f6c9743904570e9c54bd8940d
Philippus <philippus@gmail.com>
2016-03-28 00:44:39 +0200

    - finished exercises

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,61 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+  (let [[fst _] card
+        replacements {\T 10, \J 11, \Q 12, \K 13, \A 14}]
+    (cond
+      (Character/isDigit fst) (Integer/valueOf (str fst))
+      :else (Integer/valueOf (str (replacements fst))))))
+
+(defn ranklow [card]
+  (let [[fst _] card
+        replacements {\T 10, \J 11, \Q 12, \K 13, \A 1}]
+    (cond
+      (Character/isDigit fst) (Integer/valueOf (str fst))
+      :else (Integer/valueOf (str (replacements fst))))))
 
 (defn suit [card]
-  nil)
+  (let [[_ snd] card]
+    (str snd)))
 
 (defn pair? [hand]
-  nil)
+  (> (apply max (vals (frequencies (map rank hand)))) 1))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (> (apply max (vals (frequencies (map rank hand)))) 2))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (> (apply max (vals (frequencies (map rank hand)))) 3))
 
 (defn flush? [hand]
-  nil)
+  (= (count (vals (frequencies (map suit hand)))) 1))
 
 (defn full-house? [hand]
-  nil)
+  (= [2 3] (sort (vals (frequencies (map rank hand))))))
 
 (defn two-pairs? [hand]
-  nil)
+  (or (full-house? hand) (four-of-a-kind? hand) (= [1 2 2] (sort (vals (frequencies (map rank hand)))))))
+
+(defn straightlow? [hand]
+  (= (range (apply min (map ranklow hand)) (+ 5 (apply min (map ranklow hand)))) (sort (map ranklow hand))))
 
 (defn straight? [hand]
-  nil)
+  (or (straightlow? hand) (= (range (apply min (map rank hand)) (+ 5 (apply min (map rank hand)))) (sort (map rank hand)))))
 
 (defn straight-flush? [hand]
-  nil)
+  (and (straight? hand) (flush? hand)))
+
+(defn high-card? [hand]
+  true)                                                     ; All hands have a high card.
 
 (defn value [hand]
-  nil)
+  (cond
+    (straight-flush? hand) 8
+    (four-of-a-kind? hand) 7
+    (full-house? hand) 6
+    (flush? hand) 5
+    (straight? hand) 4
+    (three-of-a-kind? hand) 3
+    (two-pairs? hand) 2
+    (pair? hand) 1
+    (high-card? hand) 0))
