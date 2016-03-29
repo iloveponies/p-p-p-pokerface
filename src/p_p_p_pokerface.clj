d7d2c59075e0f64df827d956ce8e231ee8ca1424
Petr Brabač <p.brabac@lectura.de>
2016-03-30 00:45:46 +0200

    :)

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,68 @@
 (ns p-p-p-pokerface)
 
+(def card-map {\T 10,
+               \J 11,
+               \Q 12
+               \K 13
+               \A 14})
+
 (defn rank [card]
-  nil)
+  (let [[fst _] card]
+    (if (Character/isDigit fst)
+      (Integer/valueOf (str fst))
+      (card-map fst))))
 
 (defn suit [card]
-  nil)
+  (let [[_ snd] card]
+    (str snd)))
+
+(defn ranks [hand]
+  (vals (frequencies (map rank hand))))
+
+(defn max-same-rank [hand]
+  (apply max (ranks hand)))
+
+(defn max-same-suit [hand]
+  (apply max (vals (frequencies (map suit hand)))))
+
+(defn high-card? [hand]
+  true)
 
 (defn pair? [hand]
-  nil)
+  (>= (max-same-rank hand) 2))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (>= (max-same-rank hand) 3))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (>= (max-same-rank hand) 4))
 
 (defn flush? [hand]
-  nil)
+  (= (max-same-suit hand) 5))
 
 (defn full-house? [hand]
-  nil)
+  (let [rnks (sort (ranks hand))]
+    (= rnks [2 3])))
 
 (defn two-pairs? [hand]
-  nil)
+  (let [four (four-of-a-kind? hand)
+        pairs (= (sort (ranks hand)) [1 2 2])]
+    (or four pairs)))
 
 (defn straight? [hand]
-  nil)
+  (let [straight-helper? (fn [ranks] (let [min-rank (apply min ranks)
+                                           max-rank (apply max ranks)]
+                                       (= ranks (range min-rank (+ 1 max-rank)))))
+        high-ace (sort (map rank hand))
+        low-ace (sort (replace {14 1} (map rank hand)))]
+    (or (straight-helper? high-ace) (straight-helper? low-ace))))
 
 (defn straight-flush? [hand]
-  nil)
+  (and (flush? hand) (straight? hand)))
 
 (defn value [hand]
-  nil)
+  (let [checkers #{[high-card? 0] [pair? 1] [two-pairs? 2]
+                   [three-of-a-kind? 3] [straight? 4] [flush? 5]
+                   [full-house? 6] [four-of-a-kind? 7] [straight-flush? 8]}
+        hand-value (fn [[fnc val]] (if (fnc hand) val 0))]
+    (apply max (map hand-value checkers))))
