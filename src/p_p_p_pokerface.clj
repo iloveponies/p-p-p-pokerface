c49f164047f0c4ee16e989b961727b3dc68f6abd
emiguelt <edwin.triana@bizagi.com>
2016-05-04 09:09:31 -0500

    cljmarathon - ex 1-8

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,25 +1,38 @@
 (ns p-p-p-pokerface)
 
-(defn rank [card]
-  nil)
+(defn rank [[rank _]]
+  (let [letters {\A 14,
+                 \T 10,
+                 \J 11,
+                 \Q 12,
+                 \K 13}]
+    (if (Character/isDigit rank)
+      (Integer/valueOf (str rank))
+      (get letters rank))))
 
-(defn suit [card]
-  nil)
+(defn suit [[_ suit]] 
+  (str suit))
 
 (defn pair? [hand]
-  nil)
+  (let [freq (frequencies (map rank hand))]
+    (= 1 (count (filter (fn [[k v]] (and (= 0 (mod k 2)) (= 2 v))) freq)))))
+
+(defn x-of-a-kind? [x hand]
+  (let [freq (frequencies (map rank hand))]
+    (= 1 (count (filter (fn [[_ v]] (= x v)) freq)))))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (x-of-a-kind? 3 hand))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (x-of-a-kind? 4 hand))
 
 (defn flush? [hand]
-  nil)
+  (= 1 (count (frequencies (map suit hand)))))
 
 (defn full-house? [hand]
-  nil)
+  (let [values (vals (frequencies (map rank hand)))]
+    (and (= 2 (count values)) (or (= 3 (first values)) (= 3 (second values))))))
 
 (defn two-pairs? [hand]
   nil)
