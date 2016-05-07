dbc26977158caa93108e0496dfd359ef68211250
wallinm1 <michaelmwallin@gmail.com>
2016-05-07 19:19:59 +0300

    Solved pokerface

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,52 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+  (let [[fst _] card]
+    (if (Character/isDigit fst)
+      (Integer/valueOf (str fst))
+      ({\T 10, \J 11, \Q 12, \K 13, \A 14} fst))))
 
 (defn suit [card]
-  nil)
+  (let [[_ snd] card]
+    (str snd)))
 
 (defn pair? [hand]
-  nil)
+  (> (apply max (vals (frequencies (map rank hand)))) 1))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (> (apply max (vals (frequencies (map rank hand)))) 2))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (> (apply max (vals (frequencies (map rank hand)))) 3))
 
 (defn flush? [hand]
-  nil)
+  (= (apply max (vals (frequencies (map suit hand)))) 5))
 
 (defn full-house? [hand]
-  nil)
+  (= (sort (vals (frequencies (map rank hand)))) [2, 3]))
 
 (defn two-pairs? [hand]
-  nil)
+  (let [counts (sort (vals (frequencies (map rank hand))))]
+    (or (= counts [1,2,2]) (= counts [1,4]))))
 
 (defn straight? [hand]
-  nil)
+  (let [ranks-ordered1 (sort (map rank hand))
+        ranks-ordered2 (sort (replace {14 1} (map rank hand)))
+        min1 (apply min ranks-ordered1)
+        min2 (apply min ranks-ordered2)]
+        (or (= ranks-ordered1 (range min1 (+ min1 5)))
+            (= ranks-ordered2 (range min2 (+ min2 5))))))
 
 (defn straight-flush? [hand]
-  nil)
+  (and (straight? hand) (flush? hand)))
+
+(defn high-card? [hand]
+  true) ; All hands have a high card.
 
 (defn value [hand]
-  nil)
+  (let [checkers #{[high-card? 0]  [pair? 1]
+                 [two-pairs? 2]  [three-of-a-kind? 3]
+                 [straight? 4]   [flush? 5]
+                 [full-house? 6] [four-of-a-kind? 7]
+                 [straight-flush? 8]}]
+    (apply max (map second (filter (fn [x] ((first x) hand)) checkers)))))
