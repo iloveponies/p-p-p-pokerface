e9a6012f6387411b1d42dd989766a2da96de50c9
tbucic <tamara.bucic@keross.com>
2016-05-17 18:01:19 +0200

    Exercises 1-7

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,26 +1,56 @@
 (ns p-p-p-pokerface)
 
-(defn rank [card]
-  nil)
 
+;Ex1 Write the function (suit card) which takes a singe card and returns the suit of the card as a one character string.
 (defn suit [card]
-  nil)
+  (let [[_ snd] card] (str snd)))
 
+;Ex2 Write the function (rank card) which takes a single card and returns the rank as a number between 2 and 14.
+(defn rank [card]
+  (let [[fst _] card
+        replacements {\T 10 \J 11 \Q 12 \K 13 \A 14}]
+    (if (Character/isDigit fst)
+      (Integer/valueOf (str fst))
+      (replacements fst))))
+
+
+(defn rank-freq [hand]
+  (set (vals (frequencies (map rank hand)))))
+
+(defn suit-freq [hand]
+  (set (vals (frequencies (map suit hand)))))
+
+;Ex3 Write the function (pair? hand) that returns true if there is a pair in hand and false if there is no pair in hand.
 (defn pair? [hand]
-  nil)
+  (contains?
+    (rank-freq hand)
+    2))
 
+;Ex4 Write the function (three-of-a-kind? hand) that returns true if the hand contains a three of a kind.
 (defn three-of-a-kind? [hand]
-  nil)
+  (contains?
+    (rank-freq hand)
+    3))
 
+
+;Ex5 Write the function (four-of-a-kind? hand) that returns true if the hand contains a four of a kind.
 (defn four-of-a-kind? [hand]
-  nil)
+  (contains?
+    (rank-freq hand)
+    4))
 
+;Ex6 Write the function (flush? hand) that returns true if the hand is a flush.
 (defn flush? [hand]
-  nil)
+  (= 1 (count (suit-freq hand))))
 
+;Ex7 Write the function (full-house? hand) that returns true if hand is a full house, and otherwise false.
 (defn full-house? [hand]
-  nil)
+  (and
+    (pair? hand)
+    (three-of-a-kind? hand)))
 
+;Ex8 Write the function (two-pairs? hand) that return true if hand has two pairs, and otherwise false.
+;Note that a four of a kind is also two pairs.
 (defn two-pairs? [hand]
   nil)
 
@@ -32,3 +62,5 @@
 
 (defn value [hand]
   nil)
+
+
