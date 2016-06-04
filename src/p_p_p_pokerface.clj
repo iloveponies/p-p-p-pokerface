d24ad12cda3ab19b689710b5d0fd2a4c629732a7
Michał Wesołowski <wesolowski@gmail.com>
2016-06-04 17:41:06 +0200

    done

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,64 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+  (let [[card-rank _] card]
+    (if (Character/isDigit card-rank)
+      (Integer/valueOf (str card-rank))
+      (get { \T 10 \J 11 \Q 12 \K 13 \A 14} card-rank))))
 
 (defn suit [card]
-  nil)
+  (let [[_ card-suit] card]
+    (str card-suit)))
+
+(defn ranks [hand]
+  (map rank hand))
+
+(defn rank-occurences [hand]
+  (frequencies (ranks hand)))
+
+(defn rank-groups [hand]
+  (vals (rank-occurences hand)))
 
 (defn pair? [hand]
-  nil)
+  (= 2 (apply max (rank-groups hand))))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (and
+   (= 1 (apply min (rank-groups hand)))
+   (= 3 (apply max (rank-groups hand)))))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (= 4 (apply max (rank-groups hand))))
 
 (defn flush? [hand]
-  nil)
+  (let [suits (map suit hand)]
+    (apply = suits)))
 
 (defn full-house? [hand]
-  nil)
+  (= '(2 3) (sort (rank-groups hand))))
 
 (defn two-pairs? [hand]
-  nil)
+  (= '(1 2 2) (sort (rank-groups hand))))
 
 (defn straight? [hand]
-  nil)
+  (let [sorted-ranks (sort (ranks hand))
+        min-rank (apply min sorted-ranks)]
+    (or (= '(2 3 4 5 14) sorted-ranks)
+        (= (range min-rank (+ 5 min-rank)) sorted-ranks))))
 
 (defn straight-flush? [hand]
-  nil)
+  (and
+   (straight? hand)
+   (flush? hand)))
 
 (defn value [hand]
-  nil)
+  (cond
+   (straight-flush? hand) 8
+   (four-of-a-kind? hand) 7
+   (full-house? hand) 6
+   (flush? hand) 5
+   (straight? hand) 4
+   (three-of-a-kind? hand) 3
+   (two-pairs? hand) 2
+   (pair? hand) 1
+   :else 0))
