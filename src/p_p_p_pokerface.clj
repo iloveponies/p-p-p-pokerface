45a59a514fd230ae22050c5986b1df6e59a933fb
Olli Rouvinen <olli.rouvinen@gmail.com>
2016-06-13 00:20:40 +0300

    fix formatting

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -49,11 +49,11 @@
 
 (defn value [hand]
   (let [checkers [[high-card? 0] [pair? 1]
-                   [two-pairs? 2] [three-of-a-kind? 3]
-                   [straight? 4] [flush? 5]
-                   [full-house? 6] [four-of-a-kind? 7]
-                   [straight-flush? 8]]]
-  (apply max (filter (fn [x] x) (map (fn [matcher]
-                                       (let [f (first matcher)]
-                                         (if (f hand) (second matcher)))) checkers)))))
+                  [two-pairs? 2] [three-of-a-kind? 3]
+                  [straight? 4] [flush? 5]
+                  [full-house? 6] [four-of-a-kind? 7]
+                  [straight-flush? 8]]]
+    (apply max (filter (fn [x] x) (map (fn [matcher]
+                                         (let [f (first matcher)]
+                                           (if (f hand) (second matcher)))) checkers)))))
 
