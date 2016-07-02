0e57f1030d2d7dd9ab2dc640dc7c7a752d2fb620
Juho Leinonen <juholei@gmail.com>
2016-07-02 15:17:33 +0300

    Exercises done up to and including 5

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,19 +1,33 @@
 (ns p-p-p-pokerface)
 
+(def replacements {\T 10
+                   \J 11
+                   \Q 12
+                   \K 13
+                   \A 14})
+
 (defn rank [card]
-  nil)
+  (let [[rank _] card]
+    (if (Character/isDigit rank)
+      (Integer/valueOf (str rank))
+      (replacements rank))))
 
 (defn suit [card]
-  nil)
+  (let [[_ suit] card]
+    (str suit)))
+
+(defn max-count-of-same-rank [hand]
+  (apply max
+         (vals (frequencies (map rank hand)))))
 
 (defn pair? [hand]
-  nil)
+  (= 2 (max-count-of-same-rank hand)))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (= 3 (max-count-of-same-rank hand)))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (= 4 (max-count-of-same-rank hand)))
 
 (defn flush? [hand]
   nil)
