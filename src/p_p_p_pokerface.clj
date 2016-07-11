ac11dfcef4177e51d0f80de8b913da05ad779a5a
Tony Cronin <cronintony@icloud.com>
2016-07-11 22:36:15 +0100

    all working

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,72 +1,81 @@
 (ns p-p-p-pokerface)
 
-(def rank-map {\1 1 \2 2 \3 3 \4 4 \5 5 \6 6 \7 7 \8 8 \9 9 \T 10 \J 11 \Q 12 \K 13 \A 14})
-
-; in buffer use C-c M-n to change namespace
-; C-c C-k to compile code to repl
-(defn rank [param]
-  (let [[r _] param] (rank-map r)))
+(defn rank [card]
+  (let [rank-subs {\T 10, \J 11 \Q 12 \K 13 \A 14}
+        [rank _] card]
+    (if (Character/isDigit rank)
+      (Integer/valueOf (str rank))
+      (get rank-subs rank))))
 
 (defn suit [card]
-  (let [[_ c] card ] (str c)))
+  (let [[_ suit] card]
+    (str suit)))
 
-(defn x-of-a-kind? [x hand]
-  (<= x (apply max (into [] (vals (frequencies (map rank hand)))))))
-
-(defn hand-val->vector [hand]
-  (into [] (sort (map rank hand))))  
-
-(defn hand-freq->vector [hand] 
-  (into [] (sort (vals (frequencies (map rank hand))))))
-
-(defn average [hand]
-  (let [values (hand-val->vector hand)
-        aver (apply + values)
-        length (count values)] 
-    (quot aver length)))
-  
 (defn pair? [hand]
-  (x-of-a-kind? 2 hand))
+  (let [ranks (map rank hand)]
+    (if (< (apply max (vals (frequencies ranks))) 2)
+      false
+      true)))
 
 (defn three-of-a-kind? [hand]
-  (x-of-a-kind? 3 hand))
+  (let [ranks (map rank hand)]
+    (if (< (apply max (vals (frequencies ranks))) 3)
+      false
+      true)))
 
 (defn four-of-a-kind? [hand]
-  (x-of-a-kind? 4 hand))
+  (let [ranks (map rank hand)]
+    (if (< (apply max (vals (frequencies ranks))) 4)
+      false
+      true)))
 
 (defn flush? [hand]
-  (= 1 (count (set (map suit hand)))))
+  (let [suits (map suit hand)]
+    (if (= (apply max (vals (frequencies suits))) 5)
+      true
+      false)))
 
 (defn full-house? [hand]
-  (= [2 3] (count (hand-freq->vector hand))))
+  (let [ranks (map rank hand)
+        freqs (vals (frequencies ranks))]
+    (if (= (sort freqs) '(2 3))
+      true
+      false)))
 
 (defn two-pairs? [hand]
-  (= [1 2 2] (hand-freq->vector hand)))
-
-(defn straight-helper [vector-hand]
-  (let [vec vector-hand 
-          len (count vec) 
-          start (first vec)
-          straight (take len (drop start (range)))]
-    (= vec straight)))
-
-(defn straight-flush? [hand]
-  (let [vec (hand-val->vector hand)]
-    (or (straight-helper (into [] (conj (filter #(> 14 %) vec) 1))) (straight-helper vec))))
+  (let [ranks (map rank hand)
+        freqs (vals (frequencies ranks))]
+    (if (or
+          (= (sort freqs) '(2 3))
+          (= (sort freqs) '(1 2 2))
+          (= (sort freqs) '(1 4)))
+      true
+      false)))
 
 (defn straight? [hand]
-  (or (straight-helper (hand-val->vector hand)) (straight-flush? hand)))
+  (let [ranks (map rank hand)
+        low-ace-ranks (replace {14 1} ranks)
+        straight (fn [x] (range x (+ x 5)))]
+    (if (or
+          (= (sort ranks) (straight (apply min ranks)))
+          (= (sort low-ace-ranks) (straight (apply min low-ace-ranks))))
+      true
+      false)))
 
-(defn high-card? [hand]
-  true)
+(defn straight-flush? [hand]
+  (if (and (straight? hand) (flush? hand))
+    true
+    false))
 
- (defn value [hand]
-   (let [checkers #{[high-card? 0]  [pair? 1]
-                    [two-pairs? 2]  [three-of-a-kind? 3]
-                    [straight? 4]   [flush? 5]
-                    [full-house? 6] [four-of-a-kind? 7]
-                    [straight-flush? 8]}
-         check-hand (fn [checker] ((first checker) hand))]
-     (let [hands-present (filter check-hand checkers)
-           scores (map second hands-present)]
-       (apply max scores))))
+(defn value [hand]
+  (cond
+    (straight-flush? hand) 8
+    (four-of-a-kind? hand) 7
+    (full-house? hand) 6
+    (flush? hand) 5
+    (straight? hand) 4
+    (three-of-a-kind? hand) 3
+    (two-pairs? hand) 2
+    (pair? hand) 1
+    :else 0))
+
