3582e8a48923b74f63f377eddb56c1ab35bc53aa
Tiera <tiera.hirvonen@gmail.com>
2016-07-12 16:50:22 +0300

    pppface tehtävät

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,55 @@
 (ns p-p-p-pokerface)
 
+
+(def replacements {\T 10, \J 11, \Q 12, \K 13, \A 14})
+
 (defn rank [card]
-  nil)
+  (let [[fst snd] card]
+    (if (Character/isDigit fst)
+      (Integer/valueOf (str fst))
+      (replacements fst))))
+
+
 
 (defn suit [card]
-  nil)
+  (let [[_ snd] card]
+  (str snd)))
 
 (defn pair? [hand]
-  nil)
+  (not (empty? (filter (fn [x] (= x 2)) (vals (frequencies (map rank hand)))))))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (not (empty? (filter (fn [x] (= x 3)) (vals (frequencies (map rank hand)))))))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (not (empty? (filter (fn [x] (= x 4)) (vals (frequencies (map rank hand)))))))
 
 (defn flush? [hand]
-  nil)
+  (not (empty? (filter (fn [x] (= x 5)) (vals (frequencies (map suit hand)))))))
+
 
 (defn full-house? [hand]
-  nil)
+  (and (pair? hand) (three-of-a-kind? hand)))
 
 (defn two-pairs? [hand]
-  nil)
+  (> (apply + (filter (fn [x] (or (= x 4) (= x 2))) (vals (frequencies (map rank hand))))) 3))
+
+(defn high-card? [hand]
+  true)
 
 (defn straight? [hand]
-  nil)
+  (let [x (sort (map rank hand))]
+    (if (and (= (first x) 2) (contains? (set x) 14))
+      (= (apply + x) 28)
+      (= x (or (range (first x) (+ (first x) 5)))))))
 
 (defn straight-flush? [hand]
-  nil)
+  (and (straight? hand) (flush? hand)))
 
 (defn value [hand]
-  nil)
+  (let [checkers #{[high-card? 0]  [pair? 1]
+                 [two-pairs? 2]  [three-of-a-kind? 3]
+                 [straight? 4]   [flush? 5]
+                 [full-house? 6] [four-of-a-kind? 7]
+                 [straight-flush? 8]}]
+  (apply max (map second (filter (fn [x] ((first x) hand)) checkers)))))
