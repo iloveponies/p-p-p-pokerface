9c224c3fe38dd46f9bd8a9da035ba00fc80dc79c
Prashant Sinha <prashantsinha@Prashants-MacBook-Pro.local>
2016-07-13 11:27:35 +0530

    All donecd p-p-p-pokerface/

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,76 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+  (let [[r _] card]
+    (if (Character/isDigit r)
+      (Integer/valueOf (str r))
+      (get ranks-ext r))))
 
 (defn suit [card]
-  nil)
+  (let [[_ s] card]
+    (str s)))
+
+(defn cards-of-a-kind? [hand no-of-kind frequency]
+  (let [rank-frequencies (vals (frequencies (map rank hand)))
+          filter-fn (fn [x] (= x no-of-kind))]
+    (if (= (count (filter filter-fn rank-frequencies)) frequency)
+      true
+      false)))
 
 (defn pair? [hand]
-  nil)
+  (cards-of-a-kind? hand 2 1))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (cards-of-a-kind? hand 3 1))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (cards-of-a-kind? hand 4 1))
 
 (defn flush? [hand]
-  nil)
+ (let [suit-frequencies (vals (frequencies (map suit hand)))
+          filter-fn (fn [x] (= x 5))
+        ]
+    (if (= (count (filter filter-fn suit-frequencies) ) 1)
+      true
+      false)))
 
 (defn full-house? [hand]
-  nil)
+  (if (and (pair? hand) (three-of-a-kind? hand))
+    true
+    false))
 
 (defn two-pairs? [hand]
-  nil)
+  (or (cards-of-a-kind? hand 2 2) (four-of-a-kind? hand)))
+
+
+(defn straight-hand? [hand operator-for-straight]
+  (let [suit-frequency (vals (frequencies (map suit hand)))
+        ranks-with-14 (sort (map rank hand))
+        ranks-with-1 (sort (replace {14 1} ranks-with-14))
+        min-ranks-with-14 (apply min ranks-with-14)
+        max-ranks-with-14 (apply max ranks-with-14)
+        min-ranks-with-1 (apply min ranks-with-1)
+        max-ranks-with-1 (apply max ranks-with-1)
+        rank-frequency (vals (frequencies ranks-with-14))]
+
+    (if (and (operator-for-straight (count suit-frequency) 1) (= (count ranks-with-1) 5))
+    (or (= ranks-with-14 (range min-ranks-with-14 (+ max-ranks-with-14 1)))
+        (= ranks-with-1 (range min-ranks-with-1 (+ max-ranks-with-1 1))))
+    false)))
 
 (defn straight? [hand]
-  nil)
+  (straight-hand? hand >))
 
 (defn straight-flush? [hand]
-  nil)
+  (and (straight-hand? hand =) (flush? hand)))
 
 (defn value [hand]
-  nil)
+    (let [checkers #{[high-card? 0]  [pair? 1]
+                 [two-pairs? 2]  [three-of-a-kind? 3]
+                 [straight? 4]   [flush? 5]
+                 [full-house? 6] [four-of-a-kind? 7]
+                 [straight-flush? 8]}
+        filter-hand-value (fn [x] (let [[fnc? hand-val] x]
+                                    (if (fnc? hand) x)))]
+
+    (apply max (map (fn [x] (get x 1))(filter filter-hand-value checkers)))))
