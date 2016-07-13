3bf5e780fab2ce6c787aa7ec7a9d4fad01a4763d
baobabKoodaa <baobabKoodaa@users.noreply.github.com>
2016-07-13 08:00:47 +0300

    ex3 tehty

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,13 +1,29 @@
 (ns p-p-p-pokerface)
 
+(defn int-value [raw]
+  (get {\T 10, \J 11, \Q 12, \K 13, \A 14} raw))
+
 (defn rank [card]
-  nil)
+  (let [[value suit] card]
+    (if (Character/isDigit value)
+      (Integer/valueOf (str value))
+      (int-value value))))
 
 (defn suit [card]
-  nil)
+  (let [[value suit] card]
+    (str suit)))
+
+(rank "8S")
+
+(defn has? [seq val]
+  (not (empty? (filter (fn [x] (= x val)) seq))))
 
 (defn pair? [hand]
-  nil)
+    (has? (vals (frequencies (map rank hand))) 2))
+
+(pair? ["3H" "9S" "4C" "5C" "7D"]);
+
+(vals (frequencies (map rank ["2H" "2S" "4C" "5C" "7D"])))
 
 (defn three-of-a-kind? [hand]
   nil)
