875a4670da5ac296b4bbfe980e4c59e1de807643
ahri <ahri.spam@gmail.com>
2016-07-19 15:54:48 +0200

    gaga olala (initial commit, no clue if 3-3-3-4-J hand is pair?)

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,72 @@
 (ns p-p-p-pokerface)
 
+(def card-values
+	{\1 1,
+	 \2 2,
+	 \3 3,
+	 \4 4,
+	 \5 5,
+	 \6 6,
+	 \7 7,
+	 \8 8,
+	 \9 9,
+	 \T 10,
+	 \J 11,
+	 \Q 12,
+	 \K 13,
+	 \A 14})
+
 (defn rank [card]
-  nil)
+	(card-values (first card)))
+
+(defn rank-frequencies [hand]
+	(vals (frequencies (map rank hand))))
 
 (defn suit [card]
-  nil)
+	(str (second card)))
 
 (defn pair? [hand]
-  nil)
+	(not (empty? (filter #(< 1 %) (rank-frequencies hand)))))
 
 (defn three-of-a-kind? [hand]
-  nil)
+	(not (empty? (filter #(< 2 %) (rank-frequencies hand)))))
 
 (defn four-of-a-kind? [hand]
-  nil)
+	(not (empty? (filter #(< 3 %) (rank-frequencies hand)))))
 
 (defn flush? [hand]
-  nil)
+	(= 5 (apply max (vals (frequencies (map suit hand))))))
 
 (defn full-house? [hand]
-  nil)
+	(= [2 3] (sort (rank-frequencies hand))))
 
 (defn two-pairs? [hand]
-  nil)
+	(< 3 (apply + (filter #(< 1 %) (rank-frequencies hand)))))
 
 (defn straight? [hand]
-  nil)
+	(let [h (sort (map rank hand))
+		  c (count (set h))
+		  replacements {14 1}
+		  h2 (sort (replace replacements h))
+		  diff #(- (last %) (first %))
+		  ]
+		(and (= 5 c)
+			 (or
+				 (= 4 (diff h))
+				 (= 4 (diff h2))))))
 
 (defn straight-flush? [hand]
-  nil)
+	(and
+		(flush? hand)
+		(straight? hand)))
+
+(defn high-card? [hand]
+	true) ; All hands have a high card.
 
 (defn value [hand]
-  nil)
+	(let [checkers #{[high-card? 0]  [pair? 1]
+					 [two-pairs? 2]  [three-of-a-kind? 3]
+					 [straight? 4]   [flush? 5]
+					 [full-house? 6] [four-of-a-kind? 7]
+					 [straight-flush? 8]}]
+		(apply max (map second (filter #((first %) hand) checkers)))))
