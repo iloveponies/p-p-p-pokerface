ebbdcf953da733cea13f5a14b26968e5d11e6296
tleht <toni.lehtonen7@gmail.com>
2016-07-21 12:37:28 +0300

    Neljännet tehtävät

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,56 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+  (let [[r _] card]
+    (cond (Character/isDigit r) (Integer/valueOf (str r))
+          (= \T r) 10
+          (= \J r) 11
+          (= \Q r) 12
+          (= \K r) 13
+          (= \A r) 14)))
 
 (defn suit [card]
-  nil)
+  (let [[_ s] card]
+    (str s)))
 
 (defn pair? [hand]
-  nil)
+  (= ((frequencies (vals (frequencies (map rank hand)))) 2) 1))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (= ((frequencies (vals (frequencies (map rank hand)))) 3) 1))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (= ((frequencies (vals (frequencies (map rank hand)))) 4) 1))
 
 (defn flush? [hand]
-  nil)
+  (= ((frequencies (vals (frequencies (map suit hand)))) 5) 1))
 
 (defn full-house? [hand]
-  nil)
+  (and (pair? hand) (three-of-a-kind? hand)))
 
 (defn two-pairs? [hand]
-  nil)
+  (= ((frequencies (vals (frequencies (map rank hand)))) 2) 2))
 
 (defn straight? [hand]
-  nil)
+  (let [ranked (map rank hand)
+        sortedAceXIV (sort ranked)
+        sortedAceI (sort (replace {14 1} ranked))
+        diff1 (- (inc (apply max sortedAceXIV)) (apply min sortedAceXIV)) ;; Pienimmän ja suurimman kortin lukuarvon erotus
+        diff2 (- (inc (apply max sortedAceI)) (apply min sortedAceI))]    ;;
+    (and (= ((frequencies (vals (frequencies (map rank hand)))) 1) 5)     ;; Suorassa kullakin viidestä kortista on eri lukuarvo
+         (or (== diff1 5) (== diff2 5)))))                                ;; ja suurimman ja pienimmän lukuarvon erotus on 5
 
 (defn straight-flush? [hand]
-  nil)
+  (and (straight? hand) (flush? hand)))
 
 (defn value [hand]
-  nil)
+  (cond (straight-flush? hand) 8
+        (four-of-a-kind? hand) 7
+        (full-house? hand) 6
+        (flush? hand) 5
+        (straight? hand) 4
+        (three-of-a-kind? hand) 3
+        (two-pairs? hand) 2
+        (pair? hand) 1
+        :else 0
+        ))
