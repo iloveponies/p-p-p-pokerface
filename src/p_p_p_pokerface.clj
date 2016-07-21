674d11a3dab0546a82031b409957a9a49bdec03d
cjvolkert <christianjvolkert@gmail.com>
2016-07-21 23:10:00 +0200

    poker lesson, thursday 21

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,115 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+  (let [[rank _] card]
+    (if (Character/isDigit rank)
+      (Integer/valueOf (str rank))
+      (get {\T 10 \J 11 \Q 12 \K 13 \A 14} rank)
+      )
+    )
+  )
 
 (defn suit [card]
-  nil)
+  (let [[_ suit] card]
+  (str suit))
+)
 
 (defn pair? [hand]
-  nil)
+  (let [ result
+	(filter (fn [x] (= x 2))
+	(vals (frequencies 
+	 (map rank hand)
+	 )
+	      )
+	)]
+  
+   (not (empty? result) )
+    )
+  )
 
 (defn three-of-a-kind? [hand]
-  nil)
+    (let [ result
+	(filter (fn [x] (= x 3))
+	(vals (frequencies 
+	 (map rank hand)
+	 )
+	      )
+	)]
+  
+   (not (empty? result) )
+    )
+
+  )
 
 (defn four-of-a-kind? [hand]
-  nil)
+      (let [ result
+	(filter (fn [x] (= x 4))
+	(vals (frequencies 
+	 (map rank hand)
+	 )
+	      )
+	)]
+  
+   (not (empty? result) )
+    )
+
+  )
 
 (defn flush? [hand]
-  nil)
+ (= 5  (apply max (vals (frequencies (map suit hand)))))
+  ;; (let [ranks (map rank hand)]
+  ;;   (cond 
+  ;;    (not (= (- (max ranks) (min ranks)) 0)) false
+  ;;    (> (max  (frequencies ranks)) 1) false
+  ;;    :else true	 
+  ;;  )
+  ;; )
+  )
 
 (defn full-house? [hand]
-  nil)
+  (let [freq (sort (vals (frequencies (map rank hand))))
+	]
+    (=  [2 3] (seq freq   ))
+    )
+  )
 
 (defn two-pairs? [hand]
-  nil)
+;;   (= [2 2 ]  (filter (fn [x] (= x 2))  (sort (vals (frequencies (map rank hand))))))
+
+  (let [freq (sort (vals (frequencies (map rank hand))))
+;	pairs (filter (fn [x] (= x 2) ) freq)
+	]
+    (=  [2 2]   (filter (fn [x] (= x 2) ) freq))
+    )  
+  )
 
 (defn straight? [hand]
-  nil)
+  (let [ ranks (sort (map rank hand))
+	ranks-alter (sort (replace {14 1}  (map rank hand)))
+;;	rank-seq (range (min ranks) (max ranks)
+	]
+    (cond
+     (= (range (apply min ranks) (inc (apply max ranks))) ranks) true
+     (= (range (apply min ranks-alter) (inc (apply max ranks-alter))) ranks-alter) true
+     :else false
+     )
+    )  
+  )
 
 (defn straight-flush? [hand]
-  nil)
+  (and (straight? hand) (flush? hand))
+  )
 
 (defn value [hand]
-  nil)
+  (cond
+   (straight-flush? hand) 8
+   (four-of-a-kind? hand) 7
+   (full-house? hand ) 6
+   (flush? hand) 5
+   (straight? hand) 4
+   (three-of-a-kind? hand) 3
+   (two-pairs? hand) 2
+   (pair? hand) 1
+   :else 0
+   )
+  )
