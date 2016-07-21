f19c56e6166415042147e9d1ab044992c81e7006
Tania Sierra <tania.sierra@helsinki.fi>
2016-07-21 17:42:41 +0300

    p-p-p-poker done

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,48 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+  (let [[rank _] card
+        values {\T "10" \J "11" \Q "12" \K "13" \A "14"}]
+    (if (Character/isDigit rank)
+      (Integer/valueOf (str rank))
+      (Integer/valueOf (get values rank)))))
 
 (defn suit [card]
-  nil)
+  (let [[_ s] card]
+  (str s)))
 
 (defn pair? [hand]
-  nil)
+  (contains? (set (mapv (fn [x] (= 2 x)) (vals (frequencies (mapv rank hand))))) true))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (contains? (set (mapv (fn [x] (= 3 x)) (vals (frequencies (mapv rank hand))))) true))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (contains? (set (mapv (fn [x] (= 4 x)) (vals (frequencies (mapv rank hand))))) true))
 
 (defn flush? [hand]
-  nil)
+  (apply = (mapv suit hand)))
 
 (defn full-house? [hand]
-  nil)
+  (and (pair? hand) (three-of-a-kind? hand)))
 
 (defn two-pairs? [hand]
-  nil)
+  (or (= (count (filterv (fn [x] (= 2 x)) (vals (frequencies (mapv rank hand))))) 2) (four-of-a-kind? hand)))
 
 (defn straight? [hand]
-  nil)
+  (let [x (sort (mapv rank hand))
+        y (apply min x)] (or (= x (range y (+ 5 y))) (= x (sort(conj (range 2 6) 14))))))
 
 (defn straight-flush? [hand]
-  nil)
+  (and (flush? hand) (straight? hand)))
+
+(defn high-card? [hand]
+  true)
 
 (defn value [hand]
-  nil)
+  (let [checkers #{[high-card? 0] [pair? 1]
+                   [two-pairs? 2] [three-of-a-kind? 3]
+                   [straight? 4] [flush? 5]
+                   [full-house? 6] [four-of-a-kind? 7]
+                   [straight-flush? 8]}]
+  (apply max(mapv (fn [x] (if ((first x) hand) (second x) 0)) checkers))))
