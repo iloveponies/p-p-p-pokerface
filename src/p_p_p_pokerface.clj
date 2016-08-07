2c469328d2b7c2b044e648c4f7c7595bbdb824ea
9illes <9illes@workstation.localdomain>
2016-08-07 22:30:05 +0200

    completed

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,77 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+  (let [values {\T 10
+                \J 11
+                \Q 12
+                \K 13
+                \A 14}
+        [r _] card]
+    (if (Character/isDigit r)
+      (Integer/valueOf (str r))
+      (values r))))
 
 (defn suit [card]
-  nil)
+  (let [[_ s] card]
+    (str s)))
 
 (defn pair? [hand]
-  nil)
+  (let [ranks (map rank hand)]
+    (not (nil? (some #{2} (vals (frequencies ranks)))))))
+
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (let [ranks (map rank hand)]
+    (not (nil? (some #{3} (vals (frequencies ranks)))))))
+
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (let [ranks (map rank hand)]
+    (not (nil? (some #{4} (vals (frequencies ranks)))))))
 
 (defn flush? [hand]
-  nil)
+  (let [suits (map suit hand)]
+    (= 1 (count (set suits)))))
 
 (defn full-house? [hand]
-  nil)
+  (and (pair? hand) (three-of-a-kind? hand)))
 
 (defn two-pairs? [hand]
-  nil)
+  (let [ranks (map rank hand)
+        value-freq (vals (frequencies ranks))
+        pair-count (count (filter (fn [f] (= 2 f)) value-freq))]
+    (or
+      (= 2 pair-count)
+      (four-of-a-kind? hand))))
 
 (defn straight? [hand]
-  nil)
+  (let [straight-from-min (fn [lower-card]
+                            (range lower-card (+ 5 lower-card)))
+        sorted-hand-ace (sort (map rank hand))
+        min-hand-ace (apply min sorted-hand-ace)
+        straight-ace (straight-from-min min-hand-ace)
+        sorted-hand-one (sort (replace {14 1} sorted-hand-ace))
+        min-hand-one (apply min sorted-hand-one)
+        straight-one (straight-from-min min-hand-one)]
+    (or (= sorted-hand-ace straight-ace) (= sorted-hand-one straight-one))))
 
 (defn straight-flush? [hand]
-  nil)
+  (and (flush? hand) (straight? hand)))
+
+(defn high-card? [hand]
+  true)
 
 (defn value [hand]
-  nil)
+  (let [checkers #{[high-card? 0]
+                   [pair? 1]
+                   [two-pairs? 2]
+                   [three-of-a-kind? 3]
+                   [straight? 4]
+                   [flush? 5]
+                   [full-house? 6]
+                   [four-of-a-kind? 7]
+                   [straight-flush? 8]}]
+    (apply max (map second
+         (filter (fn [checker]
+              ((first checker) hand)) checkers)))))
+
