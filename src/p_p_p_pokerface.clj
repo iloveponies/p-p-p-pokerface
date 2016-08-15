ea3a5750a0b12d59fad650e00c4ba2dcf55a5c1c
Mikko <mikko.o.niemi@gmail.com>
2016-08-15 20:54:47 +0300

    Tehtävä 1 - 11

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,65 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+  (let [valuemap {\T 10 \J 11 \Q 12 \K 13 \A 14}
+        [rank _] card]
+     (if (Character/isDigit rank)
+       (Integer/valueOf (str rank))
+       (get valuemap rank))))
 
 (defn suit [card]
-  nil)
+  (let [[_ suit] card]
+    (str suit)))
 
 (defn pair? [hand]
-  nil)
+  (let [parit (fn [x] (<= 2 x))]
+   (not (empty?
+         (filter parit
+                 (vals (frequencies (map rank hand))))))))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (let [parit (fn [x] (<= 3 x))]
+   (not (empty?
+         (filter parit
+                 (vals (frequencies (map rank hand))))))))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (let [parit (fn [x] (<= 4 x))]
+   (not (empty?
+         (filter parit
+                 (vals (frequencies (map rank hand))))))))
 
 (defn flush? [hand]
-  nil)
+  (== 5 (first (vals (frequencies
+                      (map suit hand))))))
 
 (defn full-house? [hand]
-  nil)
+  (= (seq [2 3])
+     (sort (vals (frequencies (map rank hand))))))
 
 (defn two-pairs? [hand]
-  nil)
+  (let [arvot (sort (vals (frequencies (map rank hand))))]
+    (or (= (seq [1 2 2]) arvot)
+        (= (seq [2 3]) arvot)
+        (= (seq [1 4]) arvot))))
 
 (defn straight? [hand]
-  nil)
+  (let [ordered-hand (sort (map rank hand))
+        lowest (first ordered-hand)
+        highest (last ordered-hand)]
+    (or (= ordered-hand (range lowest (+ 1 highest)))
+        (= ordered-hand (seq [2 3 4 5 14])))))
 
 (defn straight-flush? [hand]
-  nil)
+  (and (straight? hand) (flush? hand)))
 
 (defn value [hand]
-  nil)
+  (cond (straight-flush? hand) 8
+        (four-of-a-kind? hand) 7
+        (full-house? hand) 6
+        (flush? hand) 5
+        (straight? hand) 4
+        (three-of-a-kind? hand) 3
+        (two-pairs? hand) 2
+        (pair? hand) 1
+        :else 0))
