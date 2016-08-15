2e16c6760e1369870b50b9932d15e9598081d643
Hedgehogger <wvanderweg@gmail.com>
2016-08-15 07:27:09 +0200

    hope this works

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,88 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+  (let [[fst _] card]
+    (if (Character/isDigit fst)
+      (Integer/valueOf (str fst))
+      (get {\T 10, \J 11, \Q 12, \K 13, \A 14} fst)
+    )
+  )
+)
 
 (defn suit [card]
-  nil)
+  (let [[_ snd] card]
+    (str snd))
+)
 
 (defn pair? [hand]
-  nil)
+  (contains? (set (vals (frequencies (map rank hand)))) 2)
+)
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (contains? (set (vals (frequencies (map rank hand)))) 3)
+)
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (contains? (set (vals (frequencies (map rank hand)))) 4)
+)
 
 (defn flush? [hand]
-  nil)
+  (contains? (set (vals (frequencies (map suit hand)))) 5)
+)
 
 (defn full-house? [hand]
-  nil)
+  (and (pair? hand)
+       (three-of-a-kind? hand))
+)
 
 (defn two-pairs? [hand]
-  nil)
+  (let [is-pair? (fn [x] (= x 2))]
+    (< 1 (count (filter is-pair? (vals (frequencies (map rank hand)))))))
+)
 
 (defn straight? [hand]
-  nil)
+  (let [sortedhand (fn [x] (sort (map rank x)))
+        sortedhandace (fn [x] (sort (replace {14 1} (map rank x))))]
+    (or 
+      (= (sortedhand hand) (range (first (sortedhand hand)) (+ (first (sortedhand hand)) 5)))
+      (= (sortedhandace hand) (range (first (sortedhandace hand)) (+ (first (sortedhandace hand)) 5)))
+    )
+  )
+)
 
 (defn straight-flush? [hand]
-  nil)
+  (and (straight? hand) (flush? hand))
+  )
+
+(defn high-card? [hand]
+  true)
+
+(defn handval [v hand]
+  (if ((get v 0) hand)
+    (get v 1)
+    0
+  )
+)
 
 (defn value [hand]
-  nil)
+  (let [checkers #{[high-card? 0]  [pair? 1]
+                 [two-pairs? 2]  [three-of-a-kind? 3]
+                 [straight? 4]   [flush? 5]
+                 [full-house? 6] [four-of-a-kind? 7]
+                 [straight-flush? 8]}]
+    (max
+      (handval (get (vec checkers) 0) hand)
+      (handval (get (vec checkers) 1) hand)
+      (handval (get (vec checkers) 2) hand)
+      (handval (get (vec checkers) 3) hand)
+      (handval (get (vec checkers) 4) hand)
+      (handval (get (vec checkers) 5) hand)
+      (handval (get (vec checkers) 6) hand)
+      (handval (get (vec checkers) 7) hand)
+      (handval (get (vec checkers) 8) hand)
+    )
+  )
+)
+
+; (doseq [i checkers] ((first i) ["2H" "3S" "4C" "5D" "AD"]))
+; (if ((first (first checkers)) straight-hand) (second (first checkers)) 0)
