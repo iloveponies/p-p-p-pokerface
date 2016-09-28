97815cc53a77761068a50807fc4dcf38097c4ec6
Hannu Hartikainen <hannu.hartikainen@gmail.com>
2016-09-28 07:38:26 +0300

    solve exercises

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,66 @@
 (ns p-p-p-pokerface)
 
-(defn rank [card]
-  nil)
+(defn rank [[r s]]
+  (cond
+    (= r \T) 10
+    (= r \J) 11
+    (= r \Q) 12
+    (= r \K) 13
+    (= r \A) 14
+    :else (Integer/valueOf (str r))))
 
-(defn suit [card]
-  nil)
+(defn suit [[r s]]
+  (str s))
+
+(defn max-same-rank [hand]
+  (apply max (vals (frequencies (map rank hand)))))
 
 (defn pair? [hand]
-  nil)
+  (= 2 (max-same-rank hand)))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (= 3 (max-same-rank hand)))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (= 4 (max-same-rank hand)))
 
 (defn flush? [hand]
-  nil)
+  (= 5 (apply max (vals (frequencies (map suit hand))))))
 
 (defn full-house? [hand]
-  nil)
+  (let [frqs (vals (frequencies (map rank hand)))]
+    (and (.contains frqs 2)
+         (.contains frqs 3))))
 
 (defn two-pairs? [hand]
-  nil)
+  (or (four-of-a-kind? hand)
+      (let [fs (vals (frequencies (map rank hand)))
+            sfs (sort (filter pos? fs))]
+        (= sfs [1 2 2])))) ; magic constant: two pairs consists of 1, 2 and 2 cards of same rank
 
 (defn straight? [hand]
-  nil)
+  (let [sorted (sort (map rank hand))
+        fst (first sorted)
+        straight-from-first (range fst (+ fst 5))]
+    (or (= sorted straight-from-first)
+        (= sorted [2 3 4 5 14])))) ; allow for ace
 
 (defn straight-flush? [hand]
-  nil)
+  (and (straight? hand)
+       (flush? hand)))
 
 (defn value [hand]
-  nil)
+  (let [checkers #{[pair? 1]
+                   [two-pairs? 2]
+                   [three-of-a-kind? 3] 
+                   [straight? 4]   
+                   [flush? 5] 
+                   [full-house? 6] 
+                   [four-of-a-kind? 7]  
+                   [straight-flush? 8]}
+        getval (fn [[c v]] 
+                 (if (c hand) 
+                   v 
+                   0))
+        hand-values (map getval checkers)]
+    (apply max hand-values)))
