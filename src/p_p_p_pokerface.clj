2060de76555946460c403472484eb5308dce135a
Joonas Taivainen <joonas.taivainen@gmail.com>
2016-10-16 15:25:31 +0300

    The last exercise was the tricky one.

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,76 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+  (let [replacements {\T 10, \J 11, \Q 12, \K 13, \A 14}
+        [fst _ ] card]
+    (if (Character/isDigit fst)
+      (Integer/valueOf (str fst))
+      (Integer/valueOf (get replacements fst)))))
 
 (defn suit [card]
-  nil)
+  (let [[_ snd] card]
+    (str snd)))
 
 (defn pair? [hand]
-  nil)
+  (let [freqs   (frequencies (map rank hand))
+        maximum (apply max (vals freqs))]
+    (== maximum 2)))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (let [freqs   (frequencies (map rank hand))
+        maximum (apply max (vals freqs))]
+   (and (== maximum 3) (not (pair? hand)))))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (let [freqs   (frequencies (map rank hand))
+        maximum (apply max (vals freqs))]
+    (== maximum 4)))
 
 (defn flush? [hand]
-  nil)
+  (let [suits      (map suit hand)
+        ranks      (sort (map rank hand))
+        min-rank   (apply min ranks)
+        rank-freqs (vals (frequencies ranks))
+        suit-freqs (vals (frequencies suits))]
+    (and (= (apply max rank-freqs) 1)
+         (= (apply max suit-freqs) 5)
+         (not= ranks (range min-rank (+ min-rank 5))))))
 
 (defn full-house? [hand]
-  nil)
+  (let [ranks (sort (map rank hand))
+        freqs (vals (frequencies ranks))]
+    (= freqs (seq [3 2]))))
 
 (defn two-pairs? [hand]
-  nil)
+  (let [ranks (map rank hand)
+        freqs (vals (frequencies ranks))]
+    (= freqs (seq [2 2 1]))))
 
 (defn straight? [hand]
-  nil)
+  (let [ranks   (sort (map rank hand))
+        minimum (apply min ranks)
+        freqs   (vals (frequencies ranks))]
+    (or (= ranks (range minimum (+ minimum 5)))
+        (= ranks (seq [2 3 4 5 14]))
+        (= ranks (seq [10 11 12 13 14])))))
 
 (defn straight-flush? [hand]
-  nil)
+  (let [rank-freqs (vals (frequencies (map rank hand)))
+        suit-freqs (vals (frequencies (map suit hand)))]
+    (and (straight? hand)
+         (= (apply max rank-freqs) 1)
+         (= (apply max suit-freqs) 5))))
+
+(defn high-card? [hand]
+  true)
 
 (defn value [hand]
-  nil)
+  (let [checkers #{[high-card? 0]  [pair? 1]
+                   [two-pairs? 2]  [three-of-a-kind? 3]
+                   [straight? 4]   [flush? 5]
+                   [full-house? 6] [four-of-a-kind? 7]
+                   [straight-flush? 8]}
+        apply-checker  (fn [checker] ((first checker) hand))
+        matching-hands (filter apply-checker checkers)
+        hand-values    (map second matching-hands)]
+    (apply max hand-values)))
