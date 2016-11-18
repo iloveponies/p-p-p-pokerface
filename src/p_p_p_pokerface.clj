15e33857d20b04f9cf88cac2494d18d97f41543c
thakuli <tero.hakuli@nokia.com>
2016-11-18 15:41:26 +0200

    first exercises

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,28 +1,48 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+  (let [[rank suit] card
+        replacements {\T 10 \J 11 \Q 12 \K 13 \A 14}]
+    (if (Character/isDigit rank)
+      (Integer/valueOf (str rank))
+      (get replacements rank))))
+      
 
 (defn suit [card]
-  nil)
+  (let [[rank suit] card]
+    (str suit)))
+
+(defn kind? [num freqs]
+  (if (contains? (set (vals freqs)) num)
+    true
+    false ))
 
 (defn pair? [hand]
-  nil)
+  (let [ ranks (map rank hand) 
+         freqs (frequencies ranks) ]
+    (kind? 2 freqs)))
 
 (defn three-of-a-kind? [hand]
-  nil)
+   (let [ ranks (map rank hand) 
+         freqs (frequencies ranks) ]
+    (kind? 3 freqs))) 
 
 (defn four-of-a-kind? [hand]
-  nil)
+   (let [ ranks (map rank hand) 
+         freqs (frequencies ranks) ]
+    (kind? 4 freqs))) 
 
 (defn flush? [hand]
-  nil)
+   (let [ suits (map suit hand) 
+         freqs (frequencies suits) ]
+    (kind? 5 freqs))) 
 
 (defn full-house? [hand]
   nil)
 
 (defn two-pairs? [hand]
-  nil)
+  ( let [ freqs (vals (frequencies(map rank hand))) ]
+    (= 2 (count (filter (fn [x] (= 2 x)) freqs)))))
 
 (defn straight? [hand]
   nil)
