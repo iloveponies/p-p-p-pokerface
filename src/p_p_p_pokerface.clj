bcb568dffbc81722657c0acf9068445fec2919e8
Patrick Lafleur <patricklafleur@MacBook-Pro.kirasystems.com>
2016-11-25 15:51:09 -0500

    Finished pokerface

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,75 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+  (let [[fst _] card]
+    (if (Character/isDigit fst)
+      (Integer/valueOf (str fst))
+      (get {\T 10, \J 11, \Q 12, \K 13, \A 14} fst)
+      )))
+
 
 (defn suit [card]
-  nil)
+  (let [[_ snd] card]
+    (str snd)))
 
 (defn pair? [hand]
-  nil)
+  (if (> (apply max (vals (frequencies (map rank hand)))) 1)
+    true
+    false
+  ))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (if (> (apply max (vals (frequencies (map rank hand)))) 2)
+    true
+    false
+  ))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (if (> (apply max (vals (frequencies (map rank hand)))) 3)
+    true
+    false
+  ))
 
 (defn flush? [hand]
-  nil)
+  (if (= (apply max (vals (frequencies (map suit hand)))) 5)
+    true
+    false
+  ))
 
 (defn full-house? [hand]
-  nil)
+  (if (and (= (apply max (vals (frequencies (map rank hand)))) 3)(= (apply min (vals (frequencies (map rank hand)))) 2))
+    true
+    false
+  ))
 
 (defn two-pairs? [hand]
-  nil)
+  (if (or (= (count (filter #(= 2 %) (vals (frequencies (map rank hand))))) 2) (= (count(filter #(= 4 %) (vals (frequencies (map rank hand))))) 1))
+    true
+    false))
 
 (defn straight? [hand]
-  nil)
+  (let [ranks (map rank hand)
+        alt-ranks (replace {14 1} ranks)
+        minranks (apply min ranks)
+        minal (apply min alt-ranks)]
+    (or (= (range minranks (+ minranks 5))(sort ranks))
+        (= (range minal (+ minal 5))(sort alt-ranks))
+        )))
 
 (defn straight-flush? [hand]
-  nil)
+  (and (straight? hand) (flush? hand)
+       ))
+(defn high-card? [hand]
+  true)
 
 (defn value [hand]
-  nil)
+  (cond (straight-flush? hand) 8
+        (four-of-a-kind? hand) 7
+        (full-house? hand) 6
+        (flush? hand) 5
+        (straight? hand) 4
+        (three-of-a-kind? hand) 3
+        (two-pairs? hand) 2
+        (pair? hand) 1
+        (high-card? hand) 0
+    ))
