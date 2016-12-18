ee4b2d2f883b598fe58b4799e81b93bcbf6472b9
Henri Suominen <erakoura@gmail.com>
2016-12-18 23:50:22 +0200

    all tests pass

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -53,5 +53,12 @@
 
 (defn value [hand]
   (let [checkers #{[high-card? 0] [pair? 1]
-                   [two-pairs? 2] [three-of-a-kind? 3]}]
-    nil))
+                   [two-pairs? 2] [three-of-a-kind? 3]
+                   [straight? 4] [flush? 5]
+                   [full-house? 6] [four-of-a-kind? 7]
+                   [straight-flush? 8]}
+        check (fn [checker] 
+                (if((first checker) hand)
+                  (second checker)
+                  0))]
+    (apply max (map check checkers))))
