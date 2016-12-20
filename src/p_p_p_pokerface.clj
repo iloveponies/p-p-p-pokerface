f74e5770df19f11b315ac0742a55b96f4bbfcf6e
Otto Nascarella <ottoln@gmail.com>
2016-12-20 17:40:11 -0200

    fixing stuff

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,31 +1,45 @@
 (ns p-p-p-pokerface)
 
+(def replacements {\T 10 \J 11 \Q 12 \K 13 \A 14})
+
 (defn rank [card]
-  nil)
+  (let [ [r _] card]
+    (if (not= nil (replacements r))
+      (replacements r)
+      (Integer/valueOf (str r)))))
 
 (defn suit [card]
-  nil)
+  (let [ [_ s] card]
+    (str s)))
 
 (defn pair? [hand]
-  nil)
+  (let [cards (map rank hand)]
+    (.contains (vals (frequencies cards)) 2)))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (let [cards (map rank hand)]
+    (.contains (vals (frequencies cards)) 3)))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (let [cards (map rank hand)]
+    (.contains (vals (frequencies cards)) 4)))
 
 (defn flush? [hand]
-  nil)
+  (let [cards (map suit hand)]
+    (.contains (vals (frequencies cards)) 5)))
 
 (defn full-house? [hand]
-  nil)
+  (and (pair? hand) (three-of-a-kind? hand)))
 
 (defn two-pairs? [hand]
-  nil)
+  (let [hand-ranks (map rank hand)]
+    (= 2 (get (frequencies (vals (frequencies hand-ranks))) 2))))
+
+;; if there is \A, add rank 1
 
 (defn straight? [hand]
-  nil)
+  (let [hand-ranks (map rank hand)]
+    ))
 
 (defn straight-flush? [hand]
   nil)
