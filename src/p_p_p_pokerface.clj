aa0327126134728a2097858b97a90e725fadef57
Ilmo Raunio <ilmo.raunio@solita.fi>
2016-12-27 11:06:10 +0200

    Rank, suit, pair? implementation added

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,13 +1,36 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+  (let [ranks {\1 1
+               \2 2
+               \3 3
+               \4 4
+               \5 5
+               \6 6
+               \7 7
+               \8 8
+               \9 9
+               \T 10
+               \J 11
+               \Q 12
+               \K 13
+               \A 14}
+        [rank _] card]
+    (get ranks rank)))
 
 (defn suit [card]
-  nil)
+  (let [[_ suit] card]
+    (str suit)))
 
 (defn pair? [hand]
-  nil)
+  (let [not-empty? (complement empty?)]
+    (->> hand
+         (map rank)
+         frequencies
+         (filter
+           (fn [[rank freqs]]
+             (>= freqs 2)))
+         not-empty?)))
 
 (defn three-of-a-kind? [hand]
   nil)
