ea11e8281328f9cbbd1922afeee2be0ffbc581ef
Heliozoa <dm89132@gmail.com>
2017-01-10 19:57:31 +0200

    poker

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,75 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+  (let [[r _] card]
+    (if (Character/isDigit r)
+      (Integer/valueOf (str r))
+      (cond
+        (= r \T) 10
+        (= r \J) 11
+        (= r \Q) 12
+        (= r \K) 13
+        (= r \A) 14))))
 
 (defn suit [card]
-  nil)
+  (let [[_ s] card]
+    (str s)))
+
+(defn rankvals [hand]
+  (vals (frequencies (map rank hand))))
+
+(defn suitvals [hand]
+  (vals (frequencies (map suit hand))))
+
+(defn conts [lst vl]
+  (if (= nil (some #(= vl %) lst))
+    false
+    true))
+
+(defn contr [hand r]
+  (conts (rankvals hand) r))
 
 (defn pair? [hand]
-  nil)
+  (contr hand 2))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (contr hand 3))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (contr hand 4))
 
 (defn flush? [hand]
-  nil)
+  (= 1 (count (suitvals hand))))
 
 (defn full-house? [hand]
-  nil)
+  (and (pair? hand) (three-of-a-kind? hand)))
 
 (defn two-pairs? [hand]
-  nil)
+  (let [rh (rankvals hand)
+        rfreqs (frequencies rh)]
+    (or (four-of-a-kind? hand) (if (contains? rfreqs 2)
+                                 (= 2 (get rfreqs 2))
+                                 false))))
 
 (defn straight? [hand]
-  nil)
+  (let [rh (map rank hand)
+        rrh (replace {14 1} (map rank hand))
+        rm (apply min rh)
+        rrm (apply min rrh)
+        rrng (range rm (+ rm 5))
+        rrrng (range rrm (+ rrm 5))]
+    (or (= rrng (sort rh)) (= rrrng (sort rrh)))))
 
 (defn straight-flush? [hand]
-  nil)
+  (and (straight? hand) (flush? hand)))
 
 (defn value [hand]
-  nil)
+  (cond (straight-flush? hand) 8
+        (four-of-a-kind? hand) 7
+        (full-house? hand) 6
+        (flush? hand) 5
+        (straight? hand) 4
+        (three-of-a-kind? hand) 3
+        (two-pairs? hand) 2
+        (pair? hand) 1
+        :else 0))
