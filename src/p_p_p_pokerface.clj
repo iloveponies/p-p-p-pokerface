b60cb2aed5b824bb5e7d5aa64bdfd899d10d2321
Juha Lindfors <juha.lindfors@gmail.com>
2017-03-06 03:31:22 +0800

    Exercises

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,81 @@
 (ns p-p-p-pokerface)
 
-(defn rank [card]
-  nil)
+(defn rank [[card-rank]]
+  (let [replacements {\T 10 \J 11 \Q 12 \K 13 \A 14}]
+    (if (Character/isDigit card-rank)
+        (Integer/valueOf (str card-rank))
+        (replacements card-rank))))
 
-(defn suit [card]
-  nil)
+(defn suit [[_ card-suit]]
+  (str card-suit))
+
+
+(defn has-cards? [hand cards]
+  (let [card-configs
+          {:pair           [rank 1 1 1 2]
+           :three-of-kind  [rank 1 1 3]
+           :four-of-kind   [rank 1 4]
+           :flush          [suit 5]
+           :full-house     [rank 2 3]
+           :two-pairs      [rank 1 2 2]}
+        config
+          (card-configs cards)
+        card-type
+          (first config)
+        all-types 
+          (map card-type hand)
+        sorted-kind-freq 
+          (sort (vals (frequencies all-types)))]
+    
+  (= sorted-kind-freq (rest config))))
+
+
+(defn has-consecutive-ranks? [hand replacement-map]
+  (let [sorted-ranks
+          (sort (map rank (replace replacement-map hand)))
+        min-rank
+          (apply min sorted-ranks)
+        normalize-ranks
+          (fn [rank-value] (- rank-value min-rank))]
+    
+  (= (map normalize-ranks sorted-ranks) [0 1 2 3 4])))
+
+
 
 (defn pair? [hand]
-  nil)
+  (has-cards? hand :pair))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (has-cards? hand :three-of-kind))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (has-cards? hand :four-of-kind))
 
 (defn flush? [hand]
-  nil)
+  (has-cards? hand :flush))
 
 (defn full-house? [hand]
-  nil)
+  (has-cards? hand :full-house))
 
 (defn two-pairs? [hand]
-  nil)
+  (has-cards? hand :two-pairs))
 
 (defn straight? [hand]
-  nil)
+  (let [replacement-map {"AC" "1C", "AS" "1S", "AD" "1D", "AH" "1H"}]
+  (or (has-consecutive-ranks? hand {}) 
+      (has-consecutive-ranks? hand replacement-map))))
 
 (defn straight-flush? [hand]
-  nil)
+  (and (straight? hand) (flush? hand)))
 
 (defn value [hand]
-  nil)
+  (cond 
+    (straight-flush? hand)    8
+    (four-of-a-kind? hand)    7
+    (full-house? hand)        6
+    (flush? hand)             5
+    (straight? hand)          4
+    (three-of-a-kind? hand)   3 
+    (two-pairs? hand)         2
+    (pair? hand)              1
+    :else                     0))
