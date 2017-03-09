8d1572d230d5bd46d5c86b7f884b3dc99485c01f
luke.dacyl <lukasz.dacyl@farmaprom.pl>
2017-03-09 09:21:18 +0100

    poker game exercise

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,62 @@
 (ns p-p-p-pokerface)
 
-(defn rank [card]
-  nil)
+(defn rank [[rank _]]
+  (let [high-rank-map {\T 10 \J 11 \Q 12 \K 13 \A 14}]
+    (if (contains? high-rank-map rank)
+      (high-rank-map rank)
+      (Integer/valueOf (str rank)))))
 
-(defn suit [card]
-  nil)
+(defn suit [[_ suit]]
+  (str suit))
+
+(defn hand-frequencies
+  [hand mapper]
+  (vals (frequencies (map mapper hand))))
+
+(defn x-of-a-kind?
+  ([x hand mapper] (boolean (some #(= % x) (hand-frequencies hand mapper))))
+  ([x hand] (x-of-a-kind? x hand rank)))
 
 (defn pair? [hand]
-  nil)
+  (x-of-a-kind? 2 hand))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (x-of-a-kind? 3 hand))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (x-of-a-kind? 4 hand))
 
 (defn flush? [hand]
-  nil)
+  (x-of-a-kind? 5 hand suit))
 
 (defn full-house? [hand]
-  nil)
+  (and (pair? hand) (three-of-a-kind? hand)))
 
 (defn two-pairs? [hand]
-  nil)
+  (let [rank-frequencies (hand-frequencies hand rank)]
+    (boolean (or (four-of-a-kind? hand) (= [2 2] (filter #(= % 2) rank-frequencies))))))
 
 (defn straight? [hand]
-  nil)
+  (let [ranks (sort (map rank hand))
+        incrementing? #(let [min-rank (apply min %)] (= % (range min-rank (+ min-rank 5))))]
+    (cond
+      (incrementing? ranks) true
+      (= 14 (apply max ranks)) (incrementing? (sort (replace {2 15, 3 16, 4 17, 5 18} ranks)))
+      :else false )))
 
 (defn straight-flush? [hand]
-  nil)
+  (and (straight? hand) (flush? hand)))
+
+(defn high-card? [hand] true)
 
 (defn value [hand]
-  nil)
+  (let [checkers #{[high-card? 0]
+                   [pair? 1]
+                   [two-pairs? 2]
+                   [three-of-a-kind? 3]
+                   [straight? 4]
+                   [flush? 5]
+                   [full-house? 6]
+                   [four-of-a-kind? 7]
+                   [straight-flush? 8]}]
+    (apply max (map (fn [[checker value]] (if (checker hand) value -1)) checkers))))
