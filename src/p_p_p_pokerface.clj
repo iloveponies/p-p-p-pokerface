57ff9fab4b28b448363c00c4e3e589d7d6b2fb78
matslinds <mats.lindstrom@helsinki.fi>
2017-03-17 18:02:48 +0200

    Done
    The value function was fun!

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,80 @@
 (ns p-p-p-pokerface)
 
+(def replacements {\T 10, \J 11, \Q 12, \K 13, \A 14})
+
 (defn rank [card]
-  nil)
+  (if (Character/isDigit (let [[fst _] card] fst))
+    (Integer/valueOf (str (let [[fst _] card] fst)))
+    (get replacements (let [[fst _] card] fst))))
 
 (defn suit [card]
-  nil)
+  (str (let [[_ snd] card] snd)))
+
 
 (defn pair? [hand]
-  nil)
+ (let [[fst snd thrd frth ffth] hand
+       value (apply max (vals (frequencies [(rank fst) (rank snd) (rank thrd) (rank frth) (rank ffth)])))]
+  (if (== 2 value)
+   true
+   false)))
+
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (let [[fst snd thrd frth ffth] hand
+        value (apply max (vals (frequencies [(rank fst) (rank snd) (rank thrd) (rank frth) (rank ffth)])))]
+   (if (== 3 value)
+     true
+     false)))
 
 (defn four-of-a-kind? [hand]
-  nil)
+ (let [[fst snd thrd frth ffth] hand
+       value (apply max (vals (frequencies [(rank fst) (rank snd) (rank thrd) (rank frth) (rank ffth)])))]
+  (if (== 4 value)
+    true
+    false)))
 
 (defn flush? [hand]
-  nil)
+  (let [[fst snd thrd frth ffth] hand
+        value (apply max (vals (frequencies [(suit fst) (suit snd) (suit thrd) (suit frth) (suit ffth)])))]
+   (if (== 5 value)
+     true
+     false)))
 
 (defn full-house? [hand]
-  nil)
+  (let [[fst snd thrd frth ffth] hand
+        seq (sort (vals (frequencies [(rank fst) (rank snd) (rank thrd) (rank frth) (rank ffth)])))]
+   (if (= [2 3] seq)
+     true
+     false)))
 
 (defn two-pairs? [hand]
-  nil)
+  (let [[fst snd thrd frth ffth] hand
+        seq (sort (vals (frequencies [(rank fst) (rank snd) (rank thrd) (rank frth) (rank ffth)])))]
+   (if (= [1 2 2] seq)
+     true
+     false)))
 
 (defn straight? [hand]
-  nil)
+  (let [[fst snd thrd frth ffth] hand
+        seq (sort (keys (frequencies [(rank fst) (rank snd) (rank thrd) (rank frth) (rank ffth)])))]
+   (cond
+     (= seq (range (apply min seq) (+ (apply min seq) 5))) true
+     (= seq [2 3 4 5 14]) true
+     :else false)))
 
 (defn straight-flush? [hand]
-  nil)
+  (if (and (flush? hand) (straight? hand))
+    true
+    false))
+
+(defn high-card? [hand]
+  true)
 
 (defn value [hand]
-  nil)
+  (let [checker #{[high-card? 0] [pair? 1] [two-pairs? 2]
+                  [three-of-a-kind? 3] [flush? 5] [straight? 4]
+                  [full-house? 6] [four-of-a-kind? 7] [straight-flush? 8]}
+        func (fn [v] (v hand))
+        filt (fn [v] (func (first v)))]
+   (apply max (map second (filter filt checker)))))
+
