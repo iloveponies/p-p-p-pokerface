6d36a93cbfb8a3bfff2bd112f7b6d4ba360c67d7
Markku Korkeala <markku.korkeala@iki.fi>
2017-03-26 19:21:37 +0300

    Add: Implementations for exercises

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,101 @@
 (ns p-p-p-pokerface)
 
+(def ranks {\A 14, \T 10, \Q 12, \J 11, \K 13})
 (defn rank [card]
-  nil)
+  (let [[r _] card]
+	(if (Character/isDigit r)
+		(Integer/valueOf (str r))
+		(get ranks r))))
+
 
 (defn suit [card]
-  nil)
+  (let [[_ s] card]
+	(str s)))
 
 (defn pair? [hand]
-  nil)
-
+  (let [ hand-seq (seq hand)
+	 x (map rank hand-seq)
+	 freq (frequencies x)
+	 values (vals freq)
+	 filters (filter (fn [x] (= x 2)) values)]
+	 (if (empty? filters)
+		false
+		true)))
+	
 (defn three-of-a-kind? [hand]
-  nil)
+  (let [ hand-seq (seq hand)
+	 x (map rank hand-seq)
+	 freq (frequencies x)
+	 values (vals freq)
+	 filters (filter (fn [x] (= x 3)) values)]
+	 (if (empty? filters)
+		false
+		true)))
+	
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (let [ hand-seq (seq hand)
+	 x (map rank hand-seq)
+	 freq (frequencies x)
+	 values (vals freq)
+	 filters (filter (fn [x] (= x 4)) values)]
+	 (if (empty? filters)
+		false
+		true)))
 
 (defn flush? [hand]
-  nil)
+  (let [ hand-seq (seq hand)
+	 x (map suit hand-seq)
+	 freq (frequencies x)
+	 values (vals freq)
+	 filters (filter (fn [x] (= x 5)) values)]
+	 (if (empty? filters)
+		false
+		true)))
+
 
 (defn full-house? [hand]
-  nil)
-
+	(and (three-of-a-kind? hand) (pair? hand)))
 (defn two-pairs? [hand]
-  nil)
+  (let [ hand-seq (seq hand)
+	 x (map rank hand-seq)
+	 freq (frequencies x)
+	 values (vals freq)
+	 filters (filter (fn [x] (= x 2)) values)]
+	(if (= 4 (apply max values))
+		true
+		(if (= 2 (count filters))
+			true
+			false))))
+
 
 (defn straight? [hand]
-  nil)
+  (let [ hand-seq (seq hand)
+	 x (map rank hand-seq)
+	 normal-range (range (apply min x) (+ (apply min x) 5))
+	 low-range (range 1 6)
+	 y (replace {14 1} x)
+	 x-sort (sort x)
+	 y-sort (sort y)]
+	(if (= x-sort normal-range)
+		true
+		(if (= y-sort low-range)
+			true
+			false))))
+
 
 (defn straight-flush? [hand]
-  nil)
+	(and (straight? hand) (flush? hand)))
 
+(defn high-card? [hand]
+	true)
 (defn value [hand]
-  nil)
+	(let [checkers #{[high-card? 0]  [pair? 1]
+                 [two-pairs? 2]  [three-of-a-kind? 3]
+                 [straight? 4]   [flush? 5]
+                 [full-house? 6] [four-of-a-kind? 7]
+                 [straight-flush? 8]}]
+	(apply max (map second (filter (fn [x] ((first x) hand)) checkers) ))))
+
+
+
