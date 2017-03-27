77d2e90afeb4ce816511d0e905033a2663afe20a
Sebastian Björkqvist <sebastian@sebastianbjorkqvist.com>
2017-03-27 20:30:29 +0300

    Very ugly version of straight? done

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -11,9 +11,11 @@
   (let [[_ suit] card]
     (str suit)))
 
+(defn sorted-ranks [hand]
+  (sort (map rank hand)))
+
 (defn frequency-values [hand]
-  (let [ranks (map rank hand)]
-    (vals (frequencies ranks))))
+    (vals (frequencies (sorted-ranks hand))))
 
 (defn pair? [hand]
   (contains? (set (frequency-values hand)) 2))
@@ -32,11 +34,33 @@
 (defn full-house? [hand]
   (and (pair? hand) (three-of-a-kind? hand)))
 
+(defn pure-two-pairs? [hand]
+  (let [sorted-freq-vals (sort (frequency-values hand))]
+    (= [1 2 2] sorted-freq-vals)))
+
 (defn two-pairs? [hand]
-  nil)
+  (let [pure-two-pair false]
+    (cond
+      (four-of-a-kind? hand) true
+      (pure-two-pairs? hand) true
+      :else false)))
 
 (defn straight? [hand]
-  nil)
+  (let [sorted (sorted-ranks hand)
+        ace-1 (replace {14 1} sorted)
+        ace-1-sorted (sort ace-1)]
+    (cond
+      (= ace-1-sorted (seq (range 1 6))) true
+      (= sorted (seq (range 2 7))) true
+      (= sorted (seq (range 3 8))) true
+      (= sorted (seq (range 4 9))) true
+      (= sorted (seq (range 5 10))) true
+      (= sorted (seq (range 6 11))) true
+      (= sorted (seq (range 7 12))) true
+      (= sorted (seq (range 8 13))) true
+      (= sorted (seq (range 9 14))) true
+      (= sorted (seq (range 10 15))) true
+      :else false)))
 
 (defn straight-flush? [hand]
   nil)
