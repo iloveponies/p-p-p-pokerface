f287f7b171fb644e120c3f5ef3ba3074d0280bae
Peter Hayes <peterhayes1@hotmail.com>
2017-04-13 23:37:17 +0100

    first attempt

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,58 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+  (let [ [rank _] card]
+  ({\2 2 \3 3 \4 4 \5 5 \6 6 \7 7 \8 8 \9 9 \T 10 \J 11 \Q 12 \K 13 \A 14} rank)))
 
 (defn suit [card]
-  nil)
+  (let [ [_ suit] card]
+  (str suit)))
 
 (defn pair? [hand]
-  nil)
+  (not (apply distinct? (map rank hand))))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (contains? (set (vals (frequencies (map rank hand)))) 3))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (contains? (set (vals (frequencies (map rank hand)))) 4))
 
 (defn flush? [hand]
-  nil)
+  (= 1 (count (set (map suit hand)))))
 
 (defn full-house? [hand]
-  nil)
+  (= #{3 2} (set (vals (frequencies (map rank hand))))))
 
 (defn two-pairs? [hand]
-  nil)
+  (let [card-ranks               (map rank hand)
+        card-rank-freqs          (frequencies card-ranks)
+        card-rank-freq-vals      (vals card-rank-freqs)
+        card-rank-freq-val-freqs (frequencies card-rank-freq-vals)]
+    (or (= 1 (get card-rank-freq-val-freqs 4 ))
+        (= 2 (get card-rank-freq-val-freqs 2 )))))
 
 (defn straight? [hand]
-  nil)
+  (let [all-ranks (map rank hand)
+        max-card (apply max all-ranks)
+        min-card (apply min all-ranks)
+        all-ranks-lo (map #(if (= 14 (rank %1)) 1 (rank %1)) hand)
+        max-card-lo (apply max all-ranks-lo)
+        min-card-lo (apply min all-ranks-lo)]
+    (and
+      (or (= 4 (- max-card min-card))
+          (= 4 (- max-card-lo min-card-lo)))
+      (= 5 (count (distinct all-ranks))))))
 
 (defn straight-flush? [hand]
-  nil)
+  (and (straight? hand) (flush? hand)))
 
 (defn value [hand]
-  nil)
+  (cond (straight-flush? hand)  8
+        (four-of-a-kind? hand)  7
+        (full-house? hand)      6
+        (flush? hand)           5
+        (straight? hand)        4
+        (three-of-a-kind? hand) 3
+        (two-pairs? hand)       2
+        (pair? hand)            1
+        :else 0))
