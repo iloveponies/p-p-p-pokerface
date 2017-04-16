a82d2a7cf68fc2fc72b76464d40d7476495baed9
devgranlund <tuomas.granlund@gmail.com>
2017-04-16 18:30:59 +0300

    34 checks failed.

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,7 +1,15 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+  (let [[value _] card
+        symbols {\T 10, \J 11, \Q 12, \K 13, \A 14}]
+    (cond
+      (and (Character/isDigit value) (< 1 (Integer/valueOf (str value)) 10)) (Integer/valueOf (str value))
+      (and (not (Character/isDigit value)) (contains? symbols value)) (get symbols value)
+      :else nil
+      )
+    )
+  )
 
 (defn suit [card]
   (let [[_ sui] card]
