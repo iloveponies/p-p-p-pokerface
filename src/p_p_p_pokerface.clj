ff6319187c78b4d5b6c4131bd8e5aec51ff6b2e8
Hannu Penttinen <koti@Hannus-MacBook-Pro.local>
2017-04-23 17:46:40 +0300

    Completed exercises

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,68 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+  (let [[^Character rank] card
+        replacements {\T 10 \J 11 \Q 12 \K 13 \A 14}]
+    (if (Character/isDigit rank)
+      (Integer/valueOf (str rank))
+      (get replacements rank))))
 
 (defn suit [card]
-  nil)
+  (let [[_ suit] card]
+    (str suit)))
+
+(defn rank-frequencies [hand]
+  (vals
+    (frequencies (map rank hand))))
+
+(defn n-of-a-kind [hand n]
+  (if (some (fn [freq] (= n freq)) (rank-frequencies hand))
+    true
+    false))
 
 (defn pair? [hand]
-  nil)
+  (n-of-a-kind hand 2))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (n-of-a-kind hand 3))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (n-of-a-kind hand 4))
 
 (defn flush? [hand]
-  nil)
+  (apply = (map suit hand)))
 
 (defn full-house? [hand]
-  nil)
+  (= (set '(2 3)) (set (rank-frequencies hand))))
 
 (defn two-pairs? [hand]
-  nil)
+  (= 2 (count (filter (fn [freq] (= 2 freq)) (rank-frequencies hand)))))
 
 (defn straight? [hand]
-  nil)
+  (let [normalized (fn [ranks]
+                     (let [smallest-rank (apply min ranks)
+                           sorted-ranks (sort ranks)]
+                       (map (fn [rank] (- rank smallest-rank)) sorted-ranks)))
+        ranks (map rank hand)
+        alt-ranks (replace {14 1} ranks)
+        normalized-ranks (normalized ranks)
+        normalized-alt-ranks (normalized alt-ranks)
+        normalized-straight (range 0 5)]
+    (or (= normalized-straight normalized-ranks)
+        (= normalized-straight normalized-alt-ranks))))
 
 (defn straight-flush? [hand]
-  nil)
+  (and (flush? hand)
+       (straight? hand)))
 
 (defn value [hand]
-  nil)
+  (cond
+    (straight-flush? hand) 8
+    (four-of-a-kind? hand) 7
+    (full-house? hand) 6
+    (flush? hand) 5
+    (straight? hand) 4
+    (three-of-a-kind? hand) 3
+    (two-pairs? hand) 2
+    (pair? hand) 1
+    :else 0))
