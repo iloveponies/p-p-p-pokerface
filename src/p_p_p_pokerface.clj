79cf73500580ceae68ffcc42d4963ff008e4c499
Olli Makinen <Olli.Makinen@solita.fi>
2017-04-25 09:40:40 +0300

    Pokerface exercises

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,57 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+  (def matches {\T 10, \J 11, \Q 12, \K 13, \A 14})
+  (let [[r _] card]
+    (if (Character/isDigit r)
+      (Integer/valueOf (str r))
+      (matches r))))
 
 (defn suit [card]
-  nil)
+  (let [[_ s] card]
+    (str s)))
 
 (defn pair? [hand]
-  nil)
+  (= (apply max (vals (frequencies (map rank hand)))) 2))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (= (apply max (vals (frequencies (map rank hand)))) 3))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (= (apply max (vals (frequencies (map rank hand)))) 4))
 
 (defn flush? [hand]
-  nil)
+  (= (count (vals (frequencies (map suit hand)))) 1))
 
 (defn full-house? [hand]
-  nil)
+  (and (= (apply max (vals (frequencies (map rank hand)))) 3)
+       (= (count (vals (frequencies (map rank hand)))) 2)))
 
 (defn two-pairs? [hand]
-  nil)
+  (and (= (apply max (vals (frequencies (map rank hand)))) 2)
+       (= (count (vals (frequencies (map rank hand)))) 3)))
 
 (defn straight? [hand]
-  nil)
+  (def low-ace [2 3 4 5 14])
+  (let [sorted-ranks (sort (map rank hand))
+        min-rank (apply min sorted-ranks)
+        test-ranks (range min-rank (+ min-rank 5))]
+    (or (= sorted-ranks low-ace) (= sorted-ranks test-ranks))))
 
 (defn straight-flush? [hand]
-  nil)
+  (and (flush? hand) (straight? hand)))
+
+(defn high-card? [hand]
+  true)
 
 (defn value [hand]
-  nil)
+  (cond
+    (straight-flush? hand) 8
+    (four-of-a-kind? hand) 7
+    (full-house? hand) 6
+    (flush? hand) 5
+    (straight? hand) 4
+    (three-of-a-kind? hand) 3
+    (two-pairs? hand) 2
+    (pair? hand) 1
+    :else 0))
