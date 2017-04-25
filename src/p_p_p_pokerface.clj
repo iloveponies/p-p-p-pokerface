5513c1249ef6dc854738e9f06cc591d0aeda7447
Henri Kontio <henri.kontio@gmail.com>
2017-04-25 23:06:23 +0300

    Add implementation for rank and suit

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,10 +1,15 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+  (let [replacements {\T 10 \J 11 \Q 12 \K 13 \A 14}
+        [rank _] card]
+    (if (Character/isDigit rank)
+      (Integer/valueOf (str rank))
+      (replacements rank))))
 
 (defn suit [card]
-  nil)
+  (let [[_ suit] card]
+    (str suit)))
 
 (defn pair? [hand]
   nil)
