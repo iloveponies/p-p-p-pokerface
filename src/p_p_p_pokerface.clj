182689a52ca9679ac8b4ad72c0af8889199c66e1
hannu <hannu.karna@helsinki.fi>
2017-05-06 19:44:35 +0300

    4. palautus

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,47 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+  (let [[fst _] card]
+    (if (Character/isDigit fst)
+      (Integer/valueOf (str fst))
+      (get {\A 14, \K 13, \Q 12, \J 11, \T 10} fst))))
 
 (defn suit [card]
-  nil)
+  (let [[_ snd] card]
+    (str snd)))
 
 (defn pair? [hand]
-  nil)
+  (== 4 (count (set (mapv rank hand)))))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (and (== 3 (apply max (vals (frequencies (mapv rank hand))))) (== 3 (count (set (mapv rank hand))))))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (and (== 4 (apply max (vals (frequencies (mapv rank hand))))) (== 2 (count (set (mapv rank hand))))))
 
 (defn flush? [hand]
-  nil)
+  (apply = (mapv suit hand)))
 
 (defn full-house? [hand]
-  nil)
+  (and (== 3 (apply max (vals (frequencies (mapv rank hand))))) (== 2 (count (set (mapv rank hand))))))
 
 (defn two-pairs? [hand]
-  nil)
+  (and (== 2 (apply max (vals (frequencies (mapv rank hand))))) (== 3 (count (set (mapv rank hand))))))
 
 (defn straight? [hand]
-  nil)
+  (and (== 5 (count (set (mapv rank hand)))) (or (== 4 (- (apply max (map rank hand)) (apply min (map rank hand))))
+      (== 4 (- (apply max (replace {14 1} (map rank hand))) (apply min (replace {14 1} (map rank hand))))))))
 
 (defn straight-flush? [hand]
-  nil)
+  (and (straight? hand) (flush? hand)))
 
 (defn value [hand]
-  nil)
+  (cond (straight-flush? hand) 8
+        (four-of-a-kind? hand) 7
+        (full-house? hand) 6
+        (flush? hand) 5
+        (straight? hand) 4
+        ( three-of-a-kind? hand) 3
+        (two-pairs? hand) 2
+        (pair? hand) 1
+        :else 0))
