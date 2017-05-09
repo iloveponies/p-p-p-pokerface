5c6f59c5dc4eb04e8c23f62aa689b2568e205562
Topi T <smobey@gmail.com>
2017-05-09 18:05:28 +0300

    can't read my (...)

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,95 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+  (let [[fst _] card]
+  (if (Character/isDigit fst)
+    (Integer/valueOf (str fst))
+    ({\T 10, \J 11, \Q 12, \K 13, \A 14} fst)
+  ))
+)
 
 (defn suit [card]
-  nil)
+  (let [[_ snd] card]
+    (str snd))
+)
+
+(defn same-ranks [hand]
+  (apply max (vals (frequencies (map rank hand))))
+)
+
+(defn same-suits [hand]
+  (apply max (vals (frequencies (map suit hand))))
+)
 
 (defn pair? [hand]
-  nil)
+  (if (>= (same-ranks hand) 2)
+    true
+    false
+  )
+)
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (if (>= (same-ranks hand) 3)
+    true
+    false
+  )
+)
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (if (>= (same-ranks hand) 4)
+    true
+    false
+  )
+)
 
 (defn flush? [hand]
-  nil)
+  (if (== (same-suits hand) 5)
+    true
+    false
+  )
+)
 
 (defn full-house? [hand]
-  nil)
+  (= (range 2 4) (sort (vals (frequencies (map rank hand)))))
+)
 
 (defn two-pairs? [hand]
-  nil)
+  (let [freqs (sort (vals (frequencies (map rank hand))))]
+    (cond
+      (= freqs (seq [1 2 2])) true
+      (= freqs (seq [1 4])) true
+      :else false))
+)
 
 (defn straight? [hand]
-  nil)
+  (let [sorted (sort (map rank hand))
+        ranged (range (first sorted) (+ (first sorted) 5))]
+    (cond
+      (= sorted (seq [2 3 4 5 14])) true
+      (= sorted ranged) true
+      :else false))
+)
 
 (defn straight-flush? [hand]
-  nil)
+  (and (straight? hand) (flush? hand))
+)
 
 (defn value [hand]
-  nil)
+  (cond
+    (straight-flush? hand) 8
+    (four-of-a-kind? hand) 7
+    (full-house? hand) 6
+    (flush? hand) 5
+    (straight? hand) 4
+    (three-of-a-kind? hand) 3
+    (two-pairs? hand) 2
+    (pair? hand) 1
+    :else 0
+  )
+)
+
+
+
+
+
+
