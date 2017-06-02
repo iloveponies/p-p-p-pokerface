244feb0f5854db72a61c5c504e9bd9c20e21835b
unknown <fabiankoskinen@gmail.com>
2017-06-02 18:19:43 +0300

    Best poker player

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,70 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+  (let [[r _] card]
+    (if (Character/isDigit r) (Integer/valueOf (str r))
+      (let [ranks {\T 10, \J 11, \Q 12, \K 13, \A 14}]
+        (ranks r)))))
+
 
 (defn suit [card]
-  nil)
+  (let [[_ s] card]
+    (str s)))
+
 
 (defn pair? [hand]
-  nil)
+  (= 2 (apply max (vals (frequencies (map rank hand))))))
+
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (and (= 3 (apply max (vals (frequencies (map rank hand)))))
+       (not (= 2 (apply min (vals (frequencies (map rank hand))))))))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (= 4 (apply max (vals (frequencies (map rank hand))))))
 
 (defn flush? [hand]
-  nil)
+  (= 5 (apply max (vals (frequencies (map suit hand))))))
 
 (defn full-house? [hand]
-  nil)
+  (and (= 3 (apply max (vals (frequencies (map rank hand)))))
+       (= 2 (apply min (vals (frequencies (map rank hand)))))))
 
 (defn two-pairs? [hand]
-  nil)
+  (cond (four-of-a-kind? hand) true
+        (three-of-a-kind? hand) false
+        (= 2 (apply max (vals (frequencies (vals (frequencies (map rank hand))))))) true
+        :else false))
+
 
 (defn straight? [hand]
-  nil)
+  (let [ranks (sort (map rank hand))
+        low (apply min ranks)
+        high (apply max ranks)
+        oneup (sort (replace {14 1} ranks))
+        newlow (apply min oneup)
+        newhigh (apply max oneup)]
+    (cond
+      (= ranks (range low (+ 1 high))) true
+      (= oneup (range newlow (+ 1 newhigh))) true
+      :else false)))
+
 
 (defn straight-flush? [hand]
-  nil)
+  (and (straight? hand) (flush? hand)))
 
 (defn value [hand]
-  nil)
+  (cond (straight-flush? hand) 8
+        (four-of-a-kind? hand) 7
+        (full-house? hand) 6
+        (flush? hand) 5
+        (straight? hand) 4
+        (three-of-a-kind? hand) 3
+        (two-pairs? hand) 2
+        (pair? hand) 1
+        :else 0))
+
+
+
+
+
