1c3a06138f8680d4869ddc75625c0aa1d731b88d
unknown <joonas.somero@gmail.com>
2017-06-27 15:20:27 +0300

    message here

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,28 +1,46 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+  (let [[rank _] card]
+  (if (Character/isDigit rank)
+    (Integer/valueOf (str rank))
+    (let [court->integer {\T 10, \J 11, \Q 12, \K 13, \A 14}]
+      (court->integer rank)))))
 
 (defn suit [card]
-  nil)
+  (let [[_ suit] card]
+  (str suit)))
 
 (defn pair? [hand]
-  nil)
+  (if (= (apply max (vals (frequencies (map rank hand)))) 2)
+    true
+    false))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (if (= (apply max (vals (frequencies (map rank hand)))) 3)
+    true
+    false))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (if (= (apply max (vals (frequencies (map rank hand)))) 4)
+    true
+    false))
 
 (defn flush? [hand]
-  nil)
+(if (= (apply max (vals (frequencies (map suit hand)))) 5)
+  true
+  false))
 
 (defn full-house? [hand]
-  nil)
+(if (= [2 3] (sort (vals (frequencies (map rank hand)))))
+    true
+    false))
 
 (defn two-pairs? [hand]
-  nil)
+  (cond
+    (= [1 2 2] (sort (vals (frequencies (map rank hand)))))  true
+    (four-of-a-kind? hand)                                   true
+    :else                                                    false))
 
 (defn straight? [hand]
   nil)
