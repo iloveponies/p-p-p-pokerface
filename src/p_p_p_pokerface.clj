6ad29613301219953445e1bcc58b15f18264a571
Saska Dönges <tuomionpupu@gmail.com>
2017-07-07 22:28:36 +0300

    pppp start

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,22 +1,31 @@
 (ns p-p-p-pokerface)
 
-(defn rank [card]
-  nil)
+(def replacements {\1 1, \2 2, \3 3, \4 4, \5 5, \6 6, \7 7
+                   \8 8, \9 9, \T 10, \J 11, \Q 12, \K 13, \A 14})
 
-(defn suit [card]
-  nil)
+(defn rank [[r _]]
+  (get replacements r))
+
+(defn suit [[_ s]]
+  (str s))
 
 (defn pair? [hand]
-  nil)
+  (if (some (fn [x] (> x 1)) (vals (frequencies (map rank hand))))
+    true
+    false))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (if (some (fn [x] (> x 2)) (vals (frequencies (map rank hand))))
+    true
+    false))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (if (some (fn [x] (> x 3)) (vals (frequencies (map rank hand))))
+    true
+    false))
 
 (defn flush? [hand]
-  nil)
+  (apply = (map suit hand)))
 
 (defn full-house? [hand]
   nil)
