262b3884f70c3bee185e6c55667698b4d8fd0a8c
sukanya <sukanya.aneja@gmail.com>
2017-07-11 21:59:09 -0400

    four aces in hand

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,89 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+  (let [shorts {\T 10
+                \J 11
+                \Q 12
+                \K 13
+                \A 14}
+        [r _] card]
+    (if (Character/isDigit r)
+        (Integer/valueOf (str r))
+        (get shorts r))))
+
 
 (defn suit [card]
-  nil)
+  (let [[_ s] card]
+    (str s)))
+
 
 (defn pair? [hand]
-  nil)
+  (let [ranks (map rank hand)
+        freqs (frequencies ranks)
+        counts (vals freqs)]
+    (not (empty? (filter (fn [x] (> x 1)) counts)))))
+
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (let [ranks (map rank hand)
+        freqs (frequencies ranks)
+        counts (vals freqs)]
+    (not (empty? (filter (fn [x] (> x 2)) counts)))))
+
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (let [ranks (map rank hand)
+        freqs (frequencies ranks)
+        counts (vals freqs)]
+    (not (empty? (filter (fn [x] (> x 3)) counts)))))
+
 
 (defn flush? [hand]
-  nil)
+  (let [suits (map suit hand)]
+    (apply = suits)))
+
 
 (defn full-house? [hand]
-  nil)
+  (let [freqs (frequencies (map rank hand))
+        ordfreqs (sort (vals freqs))]
+    (= ordfreqs [2 3])))
+
 
 (defn two-pairs? [hand]
-  nil)
+  (let [freqs (frequencies (map rank hand))
+        ordfreqs (sort (vals freqs))]
+    (or (= ordfreqs [1 4])
+        (= ordfreqs [1 2 2]))))
+
 
 (defn straight? [hand]
-  nil)
+  (let [ranks (sort (map rank hand))
+        ace-low-ranks (sort (map (fn [x] (if (= x 14) 1 x)) ranks))
+        minrank (fn [x] (apply min x))
+        normalize (fn [x]
+                    (let [m (minrank x)]
+                      (map (fn [y] (- y m)) x)))]
+
+    (or (= [0 1 2 3 4] (normalize ranks))
+        (= [0 1 2 3 4] (normalize ace-low-ranks)))))
+
 
 (defn straight-flush? [hand]
-  nil)
+  (and (straight? hand)
+       (flush? hand)))
+
 
 (defn value [hand]
-  nil)
+  (cond
+    (straight-flush? hand) 8
+    (four-of-a-kind? hand) 7
+    (full-house? hand) 6
+    (flush? hand) 5
+    (straight? hand) 4
+    (three-of-a-kind? hand) 3
+    (two-pairs? hand) 2
+    (pair? hand) 1
+    :else 0
+    ))
+
+
