f6fc6a81a2026a3cf0c743543210b603345ab724
Kalle Myllymaa <kalle.myllymaa@gmail.com>
2017-07-13 19:21:36 +0300

    some progress

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,28 +1,51 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+  (
+     let [[fst _] card]
+     (if (Character/isDigit fst)
+       (Integer/valueOf (str fst))
+       ({\T 10, \J 11, \Q 12, \K 13, \A 14} fst)
+     )
+  )
+)
 
 (defn suit [card]
-  nil)
+  (
+    let [[_ snd] card]
+    (str snd)
+  )
+)
+
+(defn helper [hand degree kind]
+  (
+    > (apply max (vals (frequencies (map kind hand)))) degree
+  )
+)
 
 (defn pair? [hand]
-  nil)
+  (helper hand 1 rank)
+)
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (helper hand 2 rank)
+)
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (helper hand 3 rank)
+)
 
 (defn flush? [hand]
-  nil)
+    (helper hand 4 suit)
+)
 
 (defn full-house? [hand]
-  nil)
+  ( = (sort(vals (frequencies (map rank hand)))) (seq [2,3]))
+)
 
 (defn two-pairs? [hand]
-  nil)
+  ( or ( = ( rest ( sort( vals (frequencies (map rank hand))))) (seq [2,2])) (four-of-a-kind? hand) (full-house? hand) )
+)
 
 (defn straight? [hand]
   nil)
