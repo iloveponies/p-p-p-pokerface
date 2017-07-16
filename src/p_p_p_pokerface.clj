b11477d9f40be2723e22893de031ae1563c86719
impliedfeline <kinnunen.jussi@hotmail.com>
2017-07-16 17:07:44 +0300

    Finish exercises

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,57 @@
 (ns p-p-p-pokerface)
 
-(defn rank [card]
-  nil)
+(defn rank [[fst _]]
+  (let [ranks {\T 10, \J 11, \Q 12, \K 13, \A 14}]
+    (if (Character/isDigit fst)
+      (Integer/valueOf (str fst)) (ranks fst))))
 
-(defn suit [card]
-  nil)
+(defn suit [[_ snd]]
+  (str snd))
+
+(defn rank-evaluation [hand]
+  (set (vals (frequencies (map rank hand)))))
 
 (defn pair? [hand]
-  nil)
+  (contains? (rank-evaluation hand) 2))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (contains? (rank-evaluation hand) 3))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (contains? (rank-evaluation hand) 4))
 
 (defn flush? [hand]
-  nil)
+  (contains? (set (vals (frequencies (map suit hand)))) 5))
 
 (defn full-house? [hand]
-  nil)
+  (and (pair? hand) (three-of-a-kind? hand)))
 
 (defn two-pairs? [hand]
-  nil)
+  (or (four-of-a-kind? hand)
+      (and (pair? hand)
+           (and (== (count (frequencies (map rank hand))) 3)
+                (contains? (rank-evaluation hand) 1)))))
 
 (defn straight? [hand]
-  nil)
+  (let [hand-of-ranks (map rank hand)
+        high-ace-hand (sort hand-of-ranks)
+        low-ace-hand (sort (replace {14 1} hand-of-ranks))
+        hand-in-range?
+        (fn [x] (= x (range (apply min x) (+ (apply max x) 1))))]
+    (or (hand-in-range? high-ace-hand) (hand-in-range? low-ace-hand))))
 
 (defn straight-flush? [hand]
-  nil)
+  (and (straight? hand) (flush? hand)))
+
+(defn high-card? [hand] true)
 
 (defn value [hand]
-  nil)
+  (let [checkers #{[high-card? 0]  [pair? 1]
+                   [two-pairs? 2]  [three-of-a-kind? 3]
+                   [straight? 4]   [flush? 5]
+                   [full-house? 6] [four-of-a-kind? 7]
+                   [straight-flush? 8]}  
+        matching-hands (filter (fn [x] ((first x) hand)) checkers)
+        hand-values (map second matching-hands)]
+  (apply max hand-values)))
+
