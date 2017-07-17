5c0a6d6a5b7078fc498aa275c26134ff59978185
Saqib Saleem <saqib@trywe.co.uk>
2017-07-17 07:11:59 +0100

    Initial attempt - all tests passing.

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,67 @@
 (ns p-p-p-pokerface)
 
+(defn rankl [card]
+  (let [[r _] card m {\T 10 \J 11 \Q 12 \K 13 \A 1}]
+    (cond
+      (Character/isDigit r) (Integer/valueOf (str r))
+      :else (m r))))
+
 (defn rank [card]
-  nil)
+  (let [[r _] card m {\T 10 \J 11 \Q 12 \K 13 \A 14}]
+    (cond
+      (Character/isDigit r) (Integer/valueOf (str r))
+      :else (m r))))
 
 (defn suit [card]
-  nil)
+  (let [[_ s] card] (str s)))
+
+(defn freqs [c h] (frequencies (map c h)))
 
 (defn pair? [hand]
-  nil)
+  (contains?
+    (set (vals (freqs rank hand)))
+    2))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (contains?
+    (set (vals (freqs rank hand)))
+    3))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (contains?
+    (set (vals (freqs rank hand)))
+    4))
 
 (defn flush? [hand]
-  nil)
+  (= (first (vals (freqs suit hand))) 5))
 
 (defn full-house? [hand]
-  nil)
+  (= (sort (vals (freqs rank hand))) '(2 3)))
 
 (defn two-pairs? [hand]
-  nil)
+  (let [v (vals (freqs rank hand))
+        vs (set v)
+        twos (fn [x] (= x 2))]
+    (or (contains? vs 4)
+        (and
+          (contains? vs 2)
+          (= (count (filter twos v)) 2)))))
 
 (defn straight? [hand]
-  nil)
+  (let [rankedHi (sort (map rank hand))
+        rankedLow (sort (map rankl hand))
+        rangeHi (range (first rankedHi) (+ (count rankedHi) (first rankedHi)))
+        rangeLow (range (first rankedLow) (+ (count rankedLow) (first rankedLow)))]
+    (or (= rankedHi rangeHi) (= rankedLow rangeLow))))
 
-(defn straight-flush? [hand]
-  nil)
+(defn straight-flush? [hand] (and (flush? hand) (straight? hand)))
 
-(defn value [hand]
-  nil)
+(defn high-card? [hand]
+  true)
+
+(defn value [hand] (let [checkers [[high-card? 0] [pair? 1]
+                                   [two-pairs? 2] [three-of-a-kind? 3]
+                                   [straight? 4] [flush? 5]
+                                   [full-house? 6] [four-of-a-kind? 7]
+                                   [straight-flush? 8]]] (apply max (map (fn [r] (second r)) (filter (fn [r] (true? (first r))) (map (fn [c] [((first c) hand) (second c)]) checkers))))))
+
