e96e0b5a6035aca3117d5a341b0288c46dc62fbd
saaninko <saaninko@cs.helsinki.fi>
2017-07-18 03:43:20 +0300

    poker set

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,59 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+  (let [ch-ranks {\T 10, \J 11, \Q 12, \K 13, \A 14}
+        [fst _] card]
+        (if (Character/isDigit fst)
+          (Integer/valueOf (str fst))
+          (get ch-ranks fst))))
 
 (defn suit [card]
-  nil)
+  (let [[_ snd] card]
+    (str snd)))
+
+(defn rank-freq [hand]
+  (vals (frequencies (map rank hand))))
 
 (defn pair? [hand]
-  nil)
+  (>= (apply max (rank-freq hand)) 2))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (>= (apply max (rank-freq hand)) 3))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (>=  (apply max (rank-freq hand)) 4))
 
 (defn flush? [hand]
-  nil)
+  (== 5 (apply max (vals (frequencies (map suit hand))))))
 
 (defn full-house? [hand]
-  nil)
+  (= (sort (rank-freq hand)) [2 3]))
 
 (defn two-pairs? [hand]
-  nil)
+ (or (four-of-a-kind? hand) (= (sort (rank-freq hand)) [1 2 2])))
 
 (defn straight? [hand]
-  nil)
+  (let [sorted (sort (map rank hand))
+        sorted-ace (replace {14 1} sorted)
+        min-s (apply min sorted)
+        max-s (apply max sorted)
+        min-sa (apply min sorted-ace)
+        max-sa (apply max sorted-ace)]
+    (and (not (pair? hand))
+         (or (= 5 (count (range min-s (+ 1 max-s))))
+             (= 5 (count (range min-sa (+ 1 max-sa))))))))
 
 (defn straight-flush? [hand]
-  nil)
+  (and (straight? hand) (flush? hand)))
 
 (defn value [hand]
-  nil)
+  (cond
+   (straight-flush? hand) 8
+   (four-of-a-kind? hand) 7
+   (full-house? hand) 6
+   (flush? hand) 5
+   (straight? hand) 4
+   (three-of-a-kind? hand) 3
+   (two-pairs? hand) 2
+   (pair? hand) 1
+   :else 0))
