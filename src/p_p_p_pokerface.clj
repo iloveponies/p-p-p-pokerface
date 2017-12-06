6e359c113d5fabdca56fade54e9baa045eb65daa
Luis P. Mendes <luislupe@gmail.com>
2017-12-06 19:25:15 +0000

    Removed checkers definition.

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -50,12 +50,6 @@
 (defn high-card? [hand]
   true)
 
-(def checkers #{[high-card? 0]  [pair? 1]
-                [two-pairs? 2]  [three-of-a-kind? 3]
-                [straight? 4]   [flush? 5]
-                [full-house? 6] [four-of-a-kind? 7]
-                [straight-flush? 8]})
-
 (defn value [hand]
   (let [checkers #{[high-card? 0]  [pair? 1]
                    [two-pairs? 2]  [three-of-a-kind? 3]
