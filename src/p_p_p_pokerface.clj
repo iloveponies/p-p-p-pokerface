55f16f435ae690c14a1894accea875ee679c1bb3
teo.mertanen <teo.mertanen@gofore.com>
2019-05-30 14:15:15 +0300

    fourth lesson, exercises 1-10

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,80 @@
 (ns p-p-p-pokerface)
 
+(def char-ranks {\T 10,
+                 \J 11,
+                 \Q 12,
+                 \K 13,
+                 \A 14})
+
 (defn rank [card]
-  nil)
+  (let [[fst _] card]
+  (if (Character/isDigit fst)
+    (Integer/valueOf (str fst))
+    (char-ranks fst)))
 
 (defn suit [card]
-  nil)
+  (let [[_ snd] card]
+    (str (snd))))
+
+(defn contains-set-of-size? [hand size]
+  (let [ranks (map rank hand)
+        freqs (vals (apply frequencies ranks))]
+    (contains? freqs size)))
 
 (defn pair? [hand]
-  nil)
+  (contains-set-of-size? hand 2))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (contains-set-of-size? hand 3))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (contains-set-of-size? hand 4))
 
 (defn flush? [hand]
-  nil)
+  (let [suits (map suit hand)
+        freqs (apply frequencies ranks)]
+    (= (freq 1))))
 
 (defn full-house? [hand]
-  nil)
+  (and
+    (contains-set-of-size? hand 3)
+    (contains-set-of-aces-as-oned 2)))
 
 (defn two-pairs? [hand]
-  nil)
+  (let [ranks (map rank hand)
+        freqs (vals (apply frequencies ranks))
+        set-sizes (apply frequencies freqs)
+        number-of-pairs (set-sizes 2)
+        contains-four (four-of-a-kind? hand)]
+    (or
+      (= (number-of-pairs 2))
+      (contains-four))))
 
 (defn straight? [hand]
-  nil)
+  (let [ranks (map rank hand)
+        ranks-with-aces-as-ones (replace {14 1} ranks)]
+    (or
+      (apply < ranks-with-aces-as-ones)
+      (apply < ranks))))
 
 (defn straight-flush? [hand]
-  nil)
+  (and
+    (flush? hand)
+    (straight? hand))
+
+
+(defn high-card? [hand]
+  true)
+
+(let [checkers #{[high-card? 0]
+                 [pair? 1]
+                 [two-pairs? 2]
+                 [three-of-a-kind? 3]
+                 [straight? 4]
+                 [flush? 5]
+                 [full-house? 6]
+                 [four-of-a-kind? 7]
+                 [straight-flush? 8]}])
 
 (defn value [hand]
-  nil)
+  (0))
