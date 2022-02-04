7b384e25681cc9326987201dfa8df89919e99649
Daniel <daniel@Daniels-MacBook-Pro.local>
2022-02-04 14:16:52 +0200

    almost all test are passed. two are missing

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,71 @@
 (ns p-p-p-pokerface)
 
 (defn rank [card]
-  nil)
+  (let [rank (first card)
+        rank-map {\T 10, \J 11, \Q 12, \K 13, \A 14}]
+    (if (Character/isDigit rank)
+      (Integer/valueOf (str rank))
+      (rank-map rank))
+    )
+  )
+
+(defn ranks-freq [hand]
+  (frequencies (map rank hand)))
+
+(defn repeats-ranks-freq? [hand freq repeats]
+  (let [rf-seq (vals (ranks-freq hand))]
+    (== (count (filter (fn [n] (== n freq)) rf-seq)) repeats)
+    )
+  )
 
 (defn suit [card]
-  nil)
+  (let [[_ suit] card]
+    (str suit))
+  )
+
+(defn high-card? [_]
+  true)
 
 (defn pair? [hand]
-  nil)
+  (repeats-ranks-freq? hand 2 1))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (repeats-ranks-freq? hand 3 1))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (repeats-ranks-freq? hand 4 1))
 
 (defn flush? [hand]
-  nil)
+  (let [suits (map suit hand)]
+    (= (count (set suits)) 1)
+    ))
 
 (defn full-house? [hand]
-  nil)
+  (and (repeats-ranks-freq? hand 3 1) (repeats-ranks-freq? hand 2 1))
+  )
 
 (defn two-pairs? [hand]
-  nil)
+  (or (repeats-ranks-freq? hand 2 2) (four-of-a-kind? hand)))
 
 (defn straight? [hand]
-  nil)
+  (let [ranks (map rank hand)
+        min-rank (apply min ranks)]
+    (= (range min-rank (+ min-rank 5)) (sort ranks))
+    ))
 
 (defn straight-flush? [hand]
-  nil)
+  (and (straight? hand) (flush? hand)))
 
 (defn value [hand]
-  nil)
+  (cond
+    (straight-flush? hand) 8
+    (four-of-a-kind? hand) 7
+    (full-house? hand) 6
+    (flush? hand) 5
+    (straight? hand) 4
+    (three-of-a-kind? hand) 3
+    (two-pairs? hand) 2
+    (pair? hand) 1
+    (high-card? hand) 0
+    :else             -1)
+  )
